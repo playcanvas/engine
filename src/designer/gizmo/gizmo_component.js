22e388c92425f577071ea0ddd929da878b047689
willeastcott <will@playcanvas.com>
2011-10-24 18:18:26 +0100

    Moved all Designer code out of the engine into the Designer repo where it belongs.
    --HG--
    rename : src/designer/editor_interface.dev.js => src/framework/framework_link.dev.js
    rename : src/designer/editor_interface.prod.js => src/framework/framework_link.prod.js
    rename : src/designer/livelink/livelink_close_entity_message.js => src/framework/livelink/livelink/livelink_close_entity_message.js
    rename : src/designer/livelink/livelink_livelink.js => src/framework/livelink/livelink/livelink_livelink.js
    rename : src/designer/livelink/livelink_message.js => src/framework/livelink/livelink/livelink_message.js
    rename : src/designer/livelink/livelink_open_entity_message.js => src/framework/livelink/livelink/livelink_open_entity_message.js
    rename : src/designer/livelink/livelink_update_component_message.js => src/framework/livelink/livelink/livelink_update_component_message.js
    rename : src/designer/livelink/livelink_update_entity_message.js => src/framework/livelink/livelink/livelink_update_entity_message.js

--- src/designer/gizmo/gizmo_component.js
+++
@@ -1,6 +0,0 @@
-function GizmoComponentData() {
-    this.gizmoType = "translate"
-    this.scale = 1;
-    this.activeAxis = -1;
-    this.pickEntity = null;
-};
--- src/designer/graphics/graphics_graphics.js
+++
@@ -1,6 +0,0 @@
-/**
- * @namespace The Designer graphics namespace contains functionality for rendering high level graphics primitives.
- * @name pc.designer.graphics
- * @private
- */
-pc.designer.graphics = {};
