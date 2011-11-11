add3a1c4a7efb8405053417b545dc29065303900
willeastcott <will@playcanvas.com>
2011-11-11 23:33:12 +0000

    Improved docs for pc.gfx.RenderTarget.

--- src/graphics/graphics_rendertarget.js
+++ src/graphics/graphics_rendertarget.js
@@ -1,10 +1,13 @@
 pc.extend(pc.gfx, function () {
     /**
      * @name pc.gfx.RenderTarget
-     * @class A frame buffer is a rectangular rendering surface. It can be rendered to via a pc.gfx.RenderTarget.
-     * @param {number} width The width of the frame buffer in pixels.
-     * @param {number} height The height of the frame buffer in pixels.
-     * @param {boolean} depth True if the frame buffer is to include a depth buffer and false otherwise.
+     * @class A render target is a viewport onto a frame buffer.
+     * @param {pc.gfx.FrameBuffer} width The width of the frame buffer in pixels.
+     * @param {Object} viewport The viewport to set with the following members:
+     * @param {Number} viewport.x The top left corner x coordinate in pixel space.
+     * @param {Number} viewport.y The top left corner y coordinate in pixel space.
+     * @param {Number} viewport.width The viewport width in pixels.
+     * @param {Number} viewport.height The viewport height in pixels.
      */
     var RenderTarget = function (framebuffer, viewport) {
         this._framebuffer = framebuffer || pc.gfx.FrameBuffer.getBackBuffer();
@@ -20,8 +23,13 @@ pc.extend(pc.gfx, function () {
     /**
      * @function
      * @name pc.gfx.RenderTarget#setViewport
-     * @description 
-     * @param {Object} viewport
+     * @description Sets the viewport on the render target's frame buffer. The viewport is a
+     * rectangular region specified by a top left corner coordinate and a width and height.
+     * @param {Object} viewport The viewport to set with the following members:
+     * @param {Number} viewport.x The top left corner x coordinate in pixel space.
+     * @param {Number} viewport.y The top left corner y coordinate in pixel space.
+     * @param {Number} viewport.width The viewport width in pixels.
+     * @param {Number} viewport.height The viewport height in pixels.
      * @author Will Eastcott
      */
     RenderTarget.prototype.setViewport = function (viewport) {
@@ -31,8 +39,8 @@ pc.extend(pc.gfx, function () {
     /**
      * @function
      * @name pc.gfx.RenderTarget#getViewport
-     * @description 
-     * @returns {Object}
+     * @description Returns the viewport used by the specified render target.
+     * @returns {Object} The viewport for the specified render target.
      * @author Will Eastcott
      */
     RenderTarget.prototype.getViewport = function () {
@@ -42,8 +50,11 @@ pc.extend(pc.gfx, function () {
     /**
      * @function
      * @name pc.gfx.RenderTarget#setFrameBuffer
-     * @description 
-     * @param {pc.gfx.RenderTarget} framebuffer
+     * @description Assigns a frame buffer to the render target. The frame buffer will
+     * receive the results of all rendering output sent to this render target according
+     * to the viewport.
+     * @param {pc.gfx.RenderTarget} framebuffer The frame buffer to set on the specified render
+     * target.
      * @author Will Eastcott
      */
     RenderTarget.prototype.setFrameBuffer = function (framebuffer) {
@@ -53,8 +64,8 @@ pc.extend(pc.gfx, function () {
     /**
      * @function
      * @name pc.gfx.RenderTarget#getFrameBuffer
-     * @description 
-     * @returns {pc.gfx.RenderTarget}
+     * @description Returns the frame buffer assigned to the specified render target.
+     * @returns {pc.gfx.FrameBuffer} The frame buffer assigned to the render target.
      * @author Will Eastcott
      */
     RenderTarget.prototype.getFrameBuffer = function () {
@@ -64,7 +75,9 @@ pc.extend(pc.gfx, function () {
     /**
      * @function
      * @name pc.gfx.RenderTarget#bind
-     * @description 
+     * @description Activates the specified render target to receive all subsequent rendering
+     * output. All rendering will be clipped to the viewport of the render target and rendered
+     * to the render target's frame buffer.
      * @author Will Eastcott
      */
     RenderTarget.prototype.bind = function () {
