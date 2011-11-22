9676148c8ea14363d5b9f3da3546aea0e598752d
willeastcott <will@playcanvas.com>
2011-11-22 00:40:04 +0000

    [FIX] Viewport of back buffer render targets now automatically resize.

--- src/graphics/graphics_rendertarget.js
+++ src/graphics/graphics_rendertarget.js
@@ -2,22 +2,20 @@ pc.extend(pc.gfx, function () {
     /**
      * @name pc.gfx.RenderTarget
      * @class A render target is a viewport onto a frame buffer.
-     * @param {pc.gfx.FrameBuffer} width The width of the frame buffer in pixels.
-     * @param {Object} viewport The viewport to set with the following members:
+     * @constructor Creates a new render target. A render target is a viewport onto a frame
+     * buffer. If no frame buffer is passed to the constructor, the back buffer is assumed.
+     * If no viewport is specified, it is assumed that the entire frame buffer is rendered to.
+     * @param {pc.gfx.FrameBuffer} frameBuffer (Optional) The frame buffer bound to this render
+     * target.
+     * @param {Object} viewport (Optional) The viewport to set with the following members:
      * @param {Number} viewport.x The top left corner x coordinate in pixel space.
      * @param {Number} viewport.y The top left corner y coordinate in pixel space.
      * @param {Number} viewport.width The viewport width in pixels.
      * @param {Number} viewport.height The viewport height in pixels.
      */
-    var RenderTarget = function (framebuffer, viewport) {
-        this._framebuffer = framebuffer || pc.gfx.FrameBuffer.getBackBuffer();
-        this._viewport = viewport ||
-            {
-                x: 0,
-                y: 0,
-                width: this._framebuffer.getWidth(),
-                height: this._framebuffer.getHeight()
-            };
+    var RenderTarget = function (frameBuffer, viewport) {
+        this._frameBuffer = frameBuffer || pc.gfx.FrameBuffer.getBackBuffer();
+        this._viewport = viewport;
     };
 
     /**
@@ -44,7 +42,16 @@ pc.extend(pc.gfx, function () {
      * @author Will Eastcott
      */
     RenderTarget.prototype.getViewport = function () {
-        return this._viewport;
+        if (this._viewport) {
+            return this._viewport;
+        } else {
+            return {
+                x: 0,
+                y: 0,
+                width: this._frameBuffer.getWidth(),
+                height: this._frameBuffer.getHeight()
+            };
+        }
     };
 
     /**
@@ -53,12 +60,12 @@ pc.extend(pc.gfx, function () {
      * @description Assigns a frame buffer to the render target. The frame buffer will
      * receive the results of all rendering output sent to this render target according
      * to the viewport.
-     * @param {pc.gfx.RenderTarget} framebuffer The frame buffer to set on the specified render
+     * @param {pc.gfx.FrameBuffer} frameBuffer The frame buffer to set on the specified render
      * target.
      * @author Will Eastcott
      */
-    RenderTarget.prototype.setFrameBuffer = function (framebuffer) {
-        this._framebuffer = framebuffer;
+    RenderTarget.prototype.setFrameBuffer = function (frameBuffer) {
+        this._frameBuffer = frameBuffer;
     };
 
     /**
@@ -69,7 +76,7 @@ pc.extend(pc.gfx, function () {
      * @author Will Eastcott
      */
     RenderTarget.prototype.getFrameBuffer = function () {
-        return this._framebuffer;
+        return this._frameBuffer;
     };
 
     /**
@@ -82,9 +89,13 @@ pc.extend(pc.gfx, function () {
      */
     RenderTarget.prototype.bind = function () {
         var gl = pc.gfx.Device.getCurrent().gl;
-        gl.viewport(this._viewport.x, this._viewport.y, this._viewport.width, this._viewport.height);
-        gl.scissor(this._viewport.x, this._viewport.y, this._viewport.width, this._viewport.height);
-        this._framebuffer.bind();
+        // Make the render target's frame buffer the current recipient of any rasterization.
+        this._frameBuffer.bind();
+
+        // Restrict rendering to the viewport of the rendering target.
+        var vp = this.getViewport();
+        gl.viewport(vp.x, vp.y, vp.width, vp.height);
+        gl.scissor(vp.x, vp.y, vp.width, vp.height);
     };
 
     return {
