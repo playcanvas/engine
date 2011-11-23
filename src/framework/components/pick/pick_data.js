68c36c8c2f7c2404746dd7a58782cd29d3a4c63b
willeastcott <will@playcanvas.com>
2011-11-23 00:17:57 +0000

    Picking API in the framework is now removed. Instead, use pc.scene.Picker.

--- src/framework/components/pick/pick_data.js
+++ src/framework/components/pick/pick_data.js
@@ -1,20 +1,8 @@
 pc.extend(pc.fw, function() {
     function PickComponentData() {
-        this.layer = "default";
+        this.layer = 'default';
         this.shapes = [];
-        this.geometries = [];
-        this.pickMaterial = null;
-        /*
-        new pc.scene.Material();
-        this.pickMaterial.setState({
-            cull: false,
-            depthTest: true,
-            depthWrite: true
-        });
-        var device = pc.gfx.Device.getCurrent();
-        var programs = device.getProgramLibrary();
-        this.pickMaterial.setProgram(programs.getProgram("pick", { skinning: false }));
-        */
+        this.material = null;
     };
     PickComponentData = PickComponentData.extendsFrom(pc.fw.ComponentData);
     
