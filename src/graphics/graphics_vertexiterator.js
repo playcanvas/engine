e341dc0dc4c9ca52fe8464a01cb8ebbc2c50fbf8
willeastcott <will@playcanvas.com>
2012-01-16 13:39:52 +0000

    Rework of previous BYTES_PER_ELEMENT fix.

--- src/graphics/graphics_vertexiterator.js
+++ src/graphics/graphics_vertexiterator.js
@@ -83,11 +83,9 @@ pc.gfx.VertexIterator.prototype.next = function () {
     var vertexFormat = this.vertexBuffer.getFormat();
     while (i < numSetters) {
         var setter = setters[i++];
-        if (setter.array.BYTES_PER_ELEMENT) {
-            setter.index += vertexFormat.size / setter.array.BYTES_PER_ELEMENT;
-        } else { // Opera!!
-            setter.index += vertexFormat.size / setter.array.constructor.BYTES_PER_ELEMENT;
-        }
+        // BYTES_PER_ELEMENT is on the instance and constructor for Chrome, Safari and Firefox
+        // but just the constructor for Opera
+        setter.index += vertexFormat.size / setter.array.constructor.BYTES_PER_ELEMENT;
     }
 }
 
