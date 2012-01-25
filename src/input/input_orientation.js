4a7bea2a77ab3b46b77e9b09fd03b50ba3d80c90
dave_e <dave@playcanvas.com>
2012-01-25 21:27:44 +0000

    Added first pass at device orientation input handler

---
+++ src/input/input_orientation.js
@@ -0,0 +1,35 @@
+pc.extend(pc.input, function () {
+	    
+    var Orientation = function () {
+        pc.extend(this, pc.events);
+    };
+    
+    Orientation.prototype.attach = function (window) {
+        this.window = window;
+        
+        this.onOrientationEvent = this.onOrientationEvent.bind(this);
+        
+        this.window.addEventListener('deviceorientation', this.onOrientationEvent, false);
+        this.window.addEventListener('MozOrientation', this.onOrientationEvent, false);
+        
+        this.window = null;
+    };
+    
+    Orientation.prototype.detach = function () {
+        this.window.removeEventListener('deviceorientation', this.onOrientationEvent);
+        this.window.removeEventListener('MozOrientation', this.onOrientationEvent);
+    };
+    
+    Orientation.prototype.onOrientationEvent = function (e) {
+        // For FireFox 3.6+  
+        if (!e.gamma && !e.beta) {  
+          e.gamma = -(e.x * (180 / Math.PI));  
+          e.beta = -(e.y * (180 / Math.PI));  
+        }
+        this.fire('deviceorientation', e);
+    };
+
+    return {
+        Orientation: Orientation
+    };
+}());
