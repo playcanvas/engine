60e87ed892ca3b449b95e57b583ce3c533c282c6
willeastcott <will@playcanvas.com>
2012-02-16 16:31:35 +0000

    Render spotlight cone in tools.

--- src/framework/components/light/spotlight_component.js
+++ src/framework/components/light/spotlight_component.js
@@ -1,15 +1,38 @@
 pc.extend(pc.fw, function () {
     var _createGfxResources = function () {
-        var lightMat = new pc.scene.Material();
-        lightMat.setProgramName("phong");
-        lightMat.setParameter("material_ambient",   [0,0,0]);
-        lightMat.setParameter("material_diffuse",   [0,0,0]);
-        lightMat.setParameter("material_specular",  [0,0,0]);
-        lightMat.setParameter("material_shininess", 1);
-        lightMat.setParameter("material_emissive",  [1,1,0]);
-        lightMat.setParameter("material_opacity",   1);
+        // Create the graphical resources required to render a light
+        var device = pc.gfx.Device.getCurrent();
+        var library = device.getProgramLibrary();
+        var program = library.getProgram("basic", { vertexColors: false, diffuseMap: false });
+        var format = new pc.gfx.VertexFormat();
+        format.begin();
+        format.addElement(new pc.gfx.VertexElement("vertex_position", 3, pc.gfx.VertexElementType.FLOAT32));
+        format.end();
+        var vertexBuffer = new pc.gfx.VertexBuffer(format, 42, pc.gfx.VertexBufferUsage.DYNAMIC);
+        var indexBuffer = new pc.gfx.IndexBuffer(pc.gfx.IndexFormat.UINT8, 88);
+        var inds = new Uint8Array(indexBuffer.lock());
+        // Spot cone side lines
+        inds[0] = 0;
+        inds[1] = 1;
+        inds[2] = 0;
+        inds[3] = 11;
+        inds[4] = 0;
+        inds[5] = 21;
+        inds[6] = 0;
+        inds[7] = 31;
+        // Spot cone circle - 40 segments
+        for (var i = 0; i < 40; i++) {
+            inds[8 + i * 2 + 0] = i + 1;
+            inds[8 + i * 2 + 1] = i + 2;
+        }
+        indexBuffer.unlock();
 
-        return pc.scene.procedural.createSphere({material: lightMat});
+        // Set the resources on the component
+        return {
+            program: program,
+            vertexBuffer: vertexBuffer,
+            indexBuffer: indexBuffer
+        };
     };
 
     /**
@@ -29,7 +52,7 @@ pc.extend(pc.fw, function () {
         this.bind("set_attenuationEnd", this.onSetAttenuationEnd.bind(this));
         // Handle changes to the 'castShadows' value
         this.bind("set_castShadows", this.onSetCastShadows.bind(this));
-        // Handle changes to the 'enable' value
+        // Handle changes to the 'color' value
         this.bind("set_color", this.onSetColor.bind(this));
         // Handle changes to the 'enable' value
         this.bind("set_enable", this.onSetEnable.bind(this));
@@ -75,9 +98,47 @@ pc.extend(pc.fw, function () {
             if (components.hasOwnProperty(id)) {
                 var entity = components[id].entity;
                 var componentData = components[id].component;
+                var program = this.renderable.program;
+                var indexBuffer = this.renderable.indexBuffer;
+                var vertexBuffer = this.renderable.vertexBuffer;
 
-                var transform = entity.getWorldTransform();
-                this.renderable.dispatch(transform);
+                var light = componentData.light;
+                var oca = Math.PI * light.getOuterConeAngle() / 180;
+                var ae = light.getAttenuationEnd();
+                var y = -ae * Math.cos(oca);
+                var r = ae * Math.sin(oca);
+
+                var positions = new Float32Array(vertexBuffer.lock());
+                positions[0] = 0;
+                positions[1] = 0;
+                positions[2] = 0;
+                var numVerts = vertexBuffer.getNumVertices();
+                for (var i = 0; i < numVerts-1; i++) {
+                    var theta = 2 * Math.PI * (i / (numVerts-2));
+                    var x = r * Math.cos(theta);
+                    var z = r * Math.sin(theta);
+                    positions[(i+1)*3+0] = x;
+                    positions[(i+1)*3+1] = y;
+                    positions[(i+1)*3+2] = z;
+                }
+                vertexBuffer.unlock();
+
+                // Render a representation of the light
+                var device = pc.gfx.Device.getCurrent();
+                device.setProgram(program);
+                device.setIndexBuffer(indexBuffer);
+                device.setVertexBuffer(vertexBuffer, 0);
+
+                transform = entity.getWorldTransform();
+                device.scope.resolve("matrix_model").setValue(transform);
+                var c = light.getColor();
+                device.scope.resolve("constant_color").setValue([c[0], c[1], c[2], 1]);
+                device.draw({
+                    type: pc.gfx.PrimType.LINES,
+                    base: 0,
+                    count: indexBuffer.getNumIndices(),
+                    indexed: true
+                });
             }
         }
     };
