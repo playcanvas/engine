f8cfa9867303f909184fdb84ddd885a1616b7572
dave_e <dave@playcanvas.com>
2012-04-26 15:39:02 +0100

    [FIX] Probably fixed #38 by checking for 'loadstart', 'canplaythrough' and 'loadeddata' events to see if Audio tag has loaded Also add pc.audio.isSupported(url) to check if an audio url is a support file type

--- tests/audio/test_audio.js
+++ tests/audio/test_audio.js
@@ -1,10 +1,33 @@
-module('sdk.tests.audio.audio');
+module('pc.audio');
 
-test("Sample Test", function () {
-    var expected = 1;
-    var result = 1;
-    
-    ok(true);
-    equal(result, expected);
-    strictEqual(result, expected);
+test('pc.audio.isSupported MP3', function () {
+    var url = '/test/file.mp3';
+    var a = new Audio();
+
+    equal(pc.audio.isSupported(url), true);
+    equal(pc.audio.isSupported(url, a), true);
+});
+
+test('pc.audio.isSupported ogg', function () {
+    var url = '/test/file.ogg';
+    var a = new Audio();
+
+    equal(pc.audio.isSupported(url), true);
+    equal(pc.audio.isSupported(url, a), true);
+});
+
+test('pc.audio.isSupported wav', function () {
+    var url = '/test/file.wav';
+    var a = new Audio();
+
+    equal(pc.audio.isSupported(url), true);
+    equal(pc.audio.isSupported(url, a), true);
+});
+
+test('pc.audio.isSupported other', function () {
+    var url = '/test/file.other';
+    var a = new Audio();
+
+    equal(pc.audio.isSupported(url), false);
+    equal(pc.audio.isSupported(url, a), false);
 });
