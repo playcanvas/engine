879dc6623ef602ad48de2d8a37b14a5d2d03b6bf
dave_e <dave@playcanvas.com>
2012-04-29 18:29:05 +0100

    Add setVolume(), resume() andsuspend() to AudioManager resume and suspend pause and resume all channels playing in the AudioManager setVolume() applies a global volume setting which acts as a multiplier to all sounds in the AudioManager

--- src/audio/audio_audio.js
+++ src/audio/audio_audio.js
@@ -5,79 +5,88 @@ pc.audio = function () {
             this.context = new AudioContext();
         }
         this.listener = new pc.audio.Listener(this);
+        
+        this.volume = 1;
+
+        pc.extend(this, pc.events);
     };
     
-    AudioManager.prototype.createSound = function (url, success, error) {
-        var sound = null;
-        if (pc.audio.Sound) {
-            sound = new pc.audio.Sound(this, url, success, error);
-        } else {
-            error();
-        }
-        return sound;
-    };
-    
-    AudioManager.prototype.playSound = function (sound, options) {
-        options = options || {};
-        var channel = null;
-        if (pc.audio.Channel) {
-            channel = new pc.audio.Channel(this, sound, options);
-            if (options.volume) {
-                channel.setVolume(options.volume);
+    AudioManager.prototype = {
+        createSound: function (url, success, error) {
+            var sound = null;
+            if (pc.audio.Sound) {
+                sound = new pc.audio.Sound(this, url, success, error);
+            } else {
+                error();
             }
-            if (options.loop) {
-                channel.setLoop(options.loop);
+            return sound;
+        },
+        
+        playSound: function (sound, options) {
+            options = options || {};
+            var channel = null;
+            if (pc.audio.Channel) {
+                channel = new pc.audio.Channel(this, sound, options);
+                if (options.volume) {
+                    channel.setVolume(options.volume);
+                }
+                if (options.loop) {
+                    channel.setLoop(options.loop);
+                }
+                channel.play();
             }
-            channel.play();
+            return channel;
+        },
+
+        playSound3d: function (sound, position, options) {
+            options = options || {};
+            var channel = null;
+            if (pc.audio.Channel3d) {
+                channel = new pc.audio.Channel3d(this, sound, options);
+                channel.setPosition(position);
+                if (options.volume) {
+                    channel.setVolume(options.volume);
+                }
+                if (options.loop) {
+                    channel.setLoop(options.loop);
+                }
+                if (options.maxDistance) {
+                    channel.setMaxDistance(options.maxDistance);
+                }
+                if (options.minDistance) {
+                    channel.setMinDistance(options.minDistance);
+                }
+                if (options.rollOffFactor) {
+                    channel.setRollOffFactor(options.rollOffFactor);
+                }
+                channel.play();
+            }
+            
+            return channel;
+        },
+        
+        getListener: function () {
+            return this.listener;
+        },
+        
+        getVolume: function () {
+            return this.volume; 
+        },
+
+        setVolume: function (volume) {
+            this.volume = volume;
+            this.fire('volumechange', volume);
+        },
+
+        suspend: function  () {
+            this.fire('suspend');
+        },
+
+        resume: function () {
+            this.fire('resume');
         }
-        return channel;
     };
 
-    AudioManager.prototype.playSound3d = function (sound, position, options) {
-        options = options || {};
-        var channel = null;
-        if (pc.audio.Channel3d) {
-            channel = new pc.audio.Channel3d(this, sound, options);
-            channel.setPosition(position);
-            if (options.volume) {
-                channel.setVolume(options.volume);
-            }
-            if (options.loop) {
-                channel.setLoop(options.loop);
-            }
-            if (options.maxDistance) {
-                channel.setMaxDistance(options.maxDistance);
-            }
-            if (options.minDistance) {
-                channel.setMinDistance(options.minDistance);
-            }
-            if (options.rollOffFactor) {
-                channel.setRollOffFactor(options.rollOffFactor);
-            }
-            channel.play();
-        }
-        
-        return channel;
-    };
-    
-    AudioManager.prototype.getListener = function () {
-        return this.listener;
-    };
-    
-    AudioManager.prototype.setVolume = function (volume) {
-        this.volume = volume;
-        // TODO set all channel volumes here
-    };
-    
-/*
-    Audio.prototype.createReverb = function () {
-        
-    };
-    
-    Audio.prototype.addReverb = function () {
-        
-    };
-*/ 
     return {
         AudioManager: AudioManager,
         hasAudio: function () {
