608717f0188cc925ad66bb2a7b84ba57d16a766d
dave_e <dave@playcanvas.com>
2012-11-04 15:12:13 +0000

    Update bloom component
    --HG--
    branch : feature/component-refactor

--- src/framework/components/bloom/bloom_component.js
+++ src/framework/components/bloom/bloom_component.js
@@ -7,67 +7,80 @@ pc.extend(pc.fw, function () {
      * @param {pc.fw.ApplicationContext} context
      * @extends pc.fw.ComponentSystem
      */
-    var BloomComponentSystem = function BloomComponentSystem (context) {
-        context.systems.add("bloom", this);
-    }
-    BloomComponentSystem = pc.inherits(BloomComponentSystem, pc.fw.ComponentSystem);
-    
-    BloomComponentSystem.prototype.createComponent = function (entity, data) {
-        var componentData = new pc.fw.BloomComponentData();
+    var BloomComponent = function BloomComponent (context) {
+        var schema = [{
+         name: "bloomThreshold",
+         displayName: "Bloom Threshold",
+            description: "The luminance threshold above which blooming is applied",
+            type: "number",
+            options: {
+                max: 1,
+                min: 0,
+                step: 0.05
+            },
+            defaultValue: 0.25
+        }, {
+         name: "blurAmount",
+         displayName: "Blur Amount",
+            description: "The luminance threshold above which blooming is applied",
+            type: "number",
+            options: {
+                max: 10,
+                min: 1,
+                step: 0.5
+            },
+            defaultValue: 4
+        }, { 
+         name: "bloomIntensity",
+         displayName: "Bloom Intensity",
+         description: "TBD",
+            type: "number",
+            options: {
+                max: 3,
+                min: 0,
+                step: 0.05
+            },
+            defaultValue: 1.25
+        }, {
+         name: "baseIntensity",
+         displayName: "Base Intensity",
+         description: "TBD",
+            type: "number",
+            options: {
+                max: 3,
+                min: 0,
+                step: 0.05
+            },
+            defaultValue: 1
+        }, {
+         name: "bloomSaturation",
+         displayName: "Bloom Saturation",
+         description: "TBD",
+            type: "number",
+            options: {
+                max: 3,
+                min: 0,
+                step: 0.05
+            },
+            defaultValue: 1
+        }, {
+         name: "baseSaturation",
+         displayName: "Base Saturation",
+         description: "TBD",
+            type: "number",
+            options: {
+                max: 3,
+                min: 0,
+                step: 0.05
+            },
+            defaultValue: 1
+        }];
 
-        this.initializeComponent(entity, componentData, data, ['bloomThreshold', 'blurAmount', 'bloomIntensity', 'baseIntensity', 'bloomSaturation', 'baseSaturation']);
-
-        return componentData;
+        this.assignSchema(schema);
     };
-    
-    BloomComponentSystem.prototype.deleteComponent = function (entity) {
-        var componentData = this.getComponentData(entity);
-        this.removeComponent(entity);
-    };
-    
-    BloomComponentSystem.prototype.update = function (dt) {
-        var components = this.getComponents();
-
-        for (var id in components) {
-            if (components.hasOwnProperty(id)) {
-                var entity = components[id].entity;
-                var componentData = components[id].component;
-
-                this.context.scene.enqueue("post", (function(data, e, c) {
-                        return function () {
-                            var offscreen = c.systems.camera.get(e, "offscreen");
-                            var camera = c.systems.camera.get(e, "camera");
-                            var currentEntity = c.systems.camera.getCurrent();
-                            var currentCamera = c.systems.camera.get(currentEntity, "camera");
-
-                            if (offscreen && (currentCamera === camera)) {
-                                var input = camera.getRenderTarget();
-                                var output = new pc.gfx.RenderTarget(pc.gfx.FrameBuffer.getBackBuffer());
-
-                                // HACK: Remove when BitBucket #70 is fixed
-                                data.bloomThreshold = typeof data.bloomThreshold == "string" ? parseFloat(data.bloomThreshold) : data.bloomThreshold;
-                                data.blurAmount = typeof data.blurAmount == "string" ? parseFloat(data.blurAmount) : data.blurAmount;
-                                data.bloomIntensity = typeof data.bloomIntensity == "string" ? parseFloat(data.bloomIntensity) : data.bloomIntensity;
-                                data.baseIntensity = typeof data.baseIntensity == "string" ? parseFloat(data.baseIntensity) : data.baseIntensity;
-                                data.bloomSaturation = typeof data.bloomSaturation == "string" ? parseFloat(data.bloomSaturation) : data.bloomSaturation;
-                                data.baseSaturation = typeof data.baseSaturation == "string" ? parseFloat(data.baseSaturation) : data.baseSaturation;
-
-                                pc.gfx.post.bloom.render(input, output, {
-                                    bloomThreshold: data.bloomThreshold,
-                                    blurAmount: data.blurAmount,
-                                    bloomIntensity: data.bloomIntensity,
-                                    baseIntensity: data.baseIntensity,
-                                    bloomSaturation: data.bloomSaturation,
-                                    baseSaturation: data.baseSaturation
-                                });
-                            }
-                        }
-                    })(componentData, entity, this.context));
-            }
-        }
-    };
+    BloomComponent = pc.inherits(BloomComponent, pc.fw.Component);
 
     return {
-        BloomComponentSystem: BloomComponentSystem
-    }
+        BloomComponent: BloomComponent
+    };
 }());
