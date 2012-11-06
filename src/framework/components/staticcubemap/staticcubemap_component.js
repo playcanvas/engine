21b9e220486949a01fe434a8e14d1ba90518eed5
dave_e <dave@playcanvas.com>
2012-11-06 17:55:42 +0000

    StaticCubeMap component updated
    --HG--
    branch : feature/component-refactor

--- src/framework/components/staticcubemap/staticcubemap_component.js
+++ src/framework/components/staticcubemap/staticcubemap_component.js
@@ -1,6 +1,88 @@
 pc.extend(pc.fw, function () {
 
-    var _cubeMapNames = [
+    
+    
+    /**
+     * @name pc.fw.StaticCubeMapComponentSystem
+     * @constructor Create a new StaticCubeMapComponentSystem
+     * @class Creates a static cubemap from assigned images
+     * @param {pc.fw.ApplicationContext} context
+     * @extends pc.fw.ComponentSystem
+     */
+    var StaticCubeMapComponent = function StaticCubeMapComponent () {
+        this.bind("set", this.onSet.bind(this));
+    };
+    StaticCubeMapComponent = pc.inherits(StaticCubeMapComponent, pc.fw.Component);
+    
+    pc.extend(StaticCubeMapComponent.prototype, {
+        onSet: function (name, oldValue, newValue) {
+            function _loadTextureAsset(name, guid) {
+                if(!guid)
+                    return;
+
+                var index = CUBE_MAP_NAMES.indexOf(name);
+                var assets = this.assets;
+                var options = {
+                    batch: this.entity.getRequestBatch()
+                };
+                
+                // clear existing cubemap
+                //this.set(entity, "cubemap", null);
+                this.cubemap = null;
+
+                if(guid) {
+                    this.system.context.loader.request(new pc.resources.AssetRequest(guid), function (resources) {
+                        assets[index] = resources[guid];
+                        //this.set(entity, "assets", assets);
+                        this.assets = assets;
+
+                        if(assets[0] && assets[1] && assets[2] 
+                        && assets[3] && assets[4] && assets[5]) {
+                            var urls = assets.map(function (asset) { 
+                                return asset.getFileUrl(); 
+                            });
+                            var cubemap = _createCubemap(this.entity, this.system.context, urls);
+                            //this.set(entity, "cubemap", cubemap);
+                            this.cubemap = cubemap;
+                        }
+                    }.bind(this), function (errors) {
+                        
+                    }, function (progress) {
+                        
+                    }, options);
+                } else {
+                    delete assets[index];                
+                }
+            };
+
+            var functions = {
+                "posx": function (name, oldValue, newValue) { 
+                        _loadTextureAsset.call(this, name, newValue) 
+                    },
+                "negx": function (name, oldValue, newValue) { 
+                        _loadTextureAsset.call(this, name, newValue) 
+                    },
+                "posy": function (name, oldValue, newValue) { 
+                        _loadTextureAsset.call(this, name, newValue) 
+                    },
+                "negy": function (name, oldValue, newValue) { 
+                        _loadTextureAsset.call(this, name, newValue) 
+                    },
+                "posz": function (name, oldValue, newValue) { 
+                        _loadTextureAsset.call(this, name, newValue) 
+                    },
+                "negz": function (name, oldValue, newValue) { 
+                        _loadTextureAsset.call(this, name, newValue) 
+                    }
+            }
+
+            if (functions[name]) {
+                functions[name].call(this, name, oldValue, newValue);
+            }
+        }
+    });
+
+    var CUBE_MAP_NAMES = [
         'posx',
         'negx',
         'posy',
@@ -14,118 +96,26 @@ pc.extend(pc.fw, function () {
         var texture = new pc.gfx.TextureCube();
         texture.setFilterMode(pc.gfx.TextureFilter.LINEAR_MIPMAP_LINEAR, pc.gfx.TextureFilter.LINEAR);
         texture.setAddressMode(pc.gfx.TextureAddress.CLAMP_TO_EDGE, pc.gfx.TextureAddress.CLAMP_TO_EDGE);
-		
-		var requests = urls.map(function (url) {
-			return new pc.resources.ImageRequest(url);
-		});
-		var options = {
-			batch: entity.getRequestBatch()
-		};
-		context.loader.request(requests, function (resources) {
-			var images = urls.map(function (url) {
-				return resources[url];
-			});
-			texture.setSource(images);
-		}.bind(this), function (errors) {
-		}, function (progress) {
-		}, options);
-		
+        
+        var requests = urls.map(function (url) {
+            return new pc.resources.ImageRequest(url);
+        });
+        var options = {
+            batch: entity.getRequestBatch()
+        };
+        context.loader.request(requests, function (resources) {
+            var images = urls.map(function (url) {
+                return resources[url];
+            });
+            texture.setSource(images);
+        }.bind(this), function (errors) {
+        }, function (progress) {
+        }, options);
+        
         return texture;
     };
 
-    function _onSet(entity, name, oldValue, newValue) {
-        function _loadTextureAsset(name, guid) {
-            if(!guid)
-                return;
-
-            var index = _cubeMapNames.indexOf(name);
-            var assets = this.get(entity, "assets");
-            var options = {
-            	batch: entity.getRequestBatch()
-            };
-            
-            // clear existing cubemap
-            this.set(entity, "cubemap", null);
-
-            if(guid) {
-                this.context.loader.request(new pc.resources.AssetRequest(guid), function (resources) {
-                    assets[index] = resources[guid];
-                    this.set(entity, "assets", assets);
-                    
-                    if(assets[0] && assets[1] && assets[2] 
-                    && assets[3] && assets[4] && assets[5]) {
-                        var urls = assets.map(function (asset) { 
-                            return asset.getFileUrl(); 
-                        });
-                        var cubemap = _createCubemap(entity, this.context, urls);
-                        this.set(entity, "cubemap", cubemap);
-                    }
-                }.bind(this), function (errors) {
-                	
-                }, function (progress) {
-                	
-                }, options);
-            } else {
-                delete assets[index];                
-            }
-        };
-
-        var functions = {
-            "posx": function (entity, name, oldValue, newValue) { 
-                    _loadTextureAsset.call(this, name, newValue) 
-                },
-            "negx": function (entity, name, oldValue, newValue) { 
-                    _loadTextureAsset.call(this, name, newValue) 
-                },
-            "posy": function (entity, name, oldValue, newValue) { 
-                    _loadTextureAsset.call(this, name, newValue) 
-                },
-            "negy": function (entity, name, oldValue, newValue) { 
-                    _loadTextureAsset.call(this, name, newValue) 
-                },
-            "posz": function (entity, name, oldValue, newValue) { 
-                    _loadTextureAsset.call(this, name, newValue) 
-                },
-            "negz": function (entity, name, oldValue, newValue) { 
-                    _loadTextureAsset.call(this, name, newValue) 
-                }
-        }
-
-        if (functions[name]) {
-            functions[name].call(this, entity, name, oldValue, newValue);
-        }
-    }
-    
-    /**
-     * @name pc.fw.StaticCubeMapComponentSystem
-     * @constructor Create a new StaticCubeMapComponentSystem
-     * @class Creates a static cubemap from assigned images
-     * @param {pc.fw.ApplicationContext} context
-     * @extends pc.fw.ComponentSystem
-     */
-    var StaticCubeMapComponentSystem = function StaticCubeMapComponentSystem (context) {
-        context.systems.add("staticcubemap", this);
-        
-        this.bind("set", _onSet.bind(this));
-    }
-    StaticCubeMapComponentSystem = pc.inherits(StaticCubeMapComponentSystem, pc.fw.ComponentSystem);
-    
-    StaticCubeMapComponentSystem.prototype.createComponent = function (entity, data) {
-        var componentData = new pc.fw.StaticCubeMapComponentData();
-
-        this.initializeComponent(entity, componentData, data, _cubeMapNames);
-
-        return componentData;
-    };
-    
-    StaticCubeMapComponentSystem.prototype.deleteComponent = function (entity) {
-        var component = this._getComponentData(entity);
-        delete component.cubemap;
-        this.removeComponent(entity);
-    };
-    
     return {
-        StaticCubeMapComponentSystem: StaticCubeMapComponentSystem
+        StaticCubeMapComponent: StaticCubeMapComponent
     }
 }());
-
