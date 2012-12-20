25c8f6e00ec057e723ea2a29ce370051a000ae14
willeastcott <will@playcanvas.com>
2012-12-20 02:08:27 +0000

    Particle emitter code updated and now renders.

--- src/graphics/programlib/programlib_particle.js
+++ src/graphics/programlib/programlib_particle.js
@@ -1,98 +1,98 @@
-pc.gfx.programlib.particle = {};
+pc.gfx.programlib.particle = {
+    generateKey: function (options) {
+        var key = "particle";
+        return key;
+    },
 
-pc.gfx.programlib.particle.generateKey = function (options) {
-    var key = "particle";
-    return key;
-}
+    generateVertexShader: function (options) {
+        var code = "";
 
-pc.gfx.programlib.particle.generateVertexShader = function (options) {
-    var code = "";
+        // VERTEX SHADER INPUTS: ATTRIBUTES
+        code += "attribute vec4 particle_uvLifeTimeFrameStart;\n"; // uv, lifeTime, frameStart
+        code += "attribute vec4 particle_positionStartTime;\n";    // position.xyz, startTime
+        code += "attribute vec4 particle_velocityStartSize;\n";    // velocity.xyz, startSize
+        code += "attribute vec4 particle_accelerationEndSize;\n";  // acceleration.xyz, endSize
+        code += "attribute vec4 particle_spinStartSpinSpeed;\n";   // spinStart.x, spinSpeed.y
+        code += "attribute vec4 particle_colorMult;\n";            // multiplies color and ramp textures
 
-    // VERTEX SHADER INPUTS: ATTRIBUTES
-    code += "attribute vec4 particle_uvLifeTimeFrameStart;\n"; // uv, lifeTime, frameStart
-    code += "attribute vec4 particle_positionStartTime;\n";    // position.xyz, startTime
-    code += "attribute vec4 particle_velocityStartSize;\n";    // velocity.xyz, startSize
-    code += "attribute vec4 particle_accelerationEndSize;\n";  // acceleration.xyz, endSize
-    code += "attribute vec4 particle_spinStartSpinSpeed;\n";   // spinStart.x, spinSpeed.y
-    code += "attribute vec4 particle_colorMult;\n";            // multiplies color and ramp textures
+        // VERTEX SHADER INPUTS: UNIFORMS
+        code += "uniform mat4 matrix_viewProjection;\n";
+        code += "uniform mat4 matrix_model;\n";
+        code += "uniform mat4 matrix_viewInverse;\n";
+        code += "uniform vec3 particle_worldVelocity;\n";
+        code += "uniform vec3 particle_worldAcceleration;\n";
+        code += "uniform float particle_timeRange;\n";
+        code += "uniform float particle_time;\n";
+        code += "uniform float particle_timeOffset;\n";
+        code += "uniform float particle_frameDuration;\n";
+        code += "uniform float particle_numFrames;\n\n";
 
-    // VERTEX SHADER INPUTS: UNIFORMS
-    code += "uniform mat4 matrix_viewProjection;\n";
-    code += "uniform mat4 matrix_model;\n";
-    code += "uniform mat4 matrix_viewInverse;\n";
-    code += "uniform vec3 worldVelocity;\n";
-    code += "uniform vec3 worldAcceleration;\n";
-    code += "uniform float timeRange;\n";
-    code += "uniform float time;\n";
-    code += "uniform float timeOffset;\n";
-    code += "uniform float frameDuration;\n";
-    code += "uniform float numFrames;\n\n";
+        // VERTEX SHADER OUTPUTS
+        code += "varying vec2 vUv0;\n";
+        code += "varying float vAge;\n";
+        code += "varying vec4 vColor;\n\n";
 
-    // VERTEX SHADER OUTPUTS
-    code += "varying vec2 vUv0;\n";
-    code += "varying float vAge;\n";
-    code += "varying vec4 vColor;\n\n";
+        // VERTEX SHADER BODY
+        code += "void main(void)\n";
+        code += "{\n";
+        code += "    vec2 uv = particle_uvLifeTimeFrameStart.xy;\n";
+        code += "    float lifeTime = particle_uvLifeTimeFrameStart.z;\n";
+        code += "    float frameStart = particle_uvLifeTimeFrameStart.w;\n";
+        code += "    vec3 position = particle_positionStartTime.xyz;\n";
+        code += "    float startTime = particle_positionStartTime.w;\n";
+        code += "    vec3 velocity = (matrix_model * vec4(particle_velocityStartSize.xyz, 0.0)).xyz + particle_worldVelocity;\n";
+        code += "    float startSize = particle_velocityStartSize.w;\n";
+        code += "    vec3 acceleration = (matrix_model * vec4(particle_accelerationEndSize.xyz, 0.0)).xyz + particle_worldAcceleration;\n";
+        code += "    float endSize = particle_accelerationEndSize.w;\n";
+        code += "    float spinStart = particle_spinStartSpinSpeed.x;\n";
+        code += "    float spinSpeed = particle_spinStartSpinSpeed.y;\n";
+        code += "    float localTime = mod((particle_time - particle_timeOffset - startTime), particle_timeRange);\n";
+        code += "    float percentLife = localTime / lifeTime;\n";
+        code += "    float frame = mod(floor(localTime / particle_frameDuration + frameStart), particle_numFrames);\n";
+        code += "    float uOffset = frame / particle_numFrames;\n";
+        code += "    float u = uOffset + (uv.x + 0.5) * (1.0 / particle_numFrames);\n";
+        code += "    vUv0 = vec2(u, uv.y + 0.5);\n";
+        code += "    vColor = particle_colorMult;\n";
+        code += "    vec3 basisX = matrix_viewInverse[0].xyz;\n";
+        code += "    vec3 basisZ = matrix_viewInverse[1].xyz;\n";
+        code += "    float size = mix(startSize, endSize, percentLife);\n";
+        code += "    size = (percentLife < 0.0 || percentLife > 1.0) ? 0.0 : size;\n";
+        code += "    float s = sin(spinStart + spinSpeed * localTime);\n";
+        code += "    float c = cos(spinStart + spinSpeed * localTime);\n";
+        code += "    vec2 rotatedPoint = vec2(uv.x * c + uv.y * s, \n";
+        code += "                             -uv.x * s + uv.y * c);\n";
+        code += "    vec3 localPosition = vec3(basisX * rotatedPoint.x +\n";
+        code += "                              basisZ * rotatedPoint.y) * size +\n";
+        code += "                              velocity * localTime +\n";
+        code += "                              acceleration * localTime * localTime + \n";
+        code += "                              position;\n";
+        code += "    vAge = percentLife;\n";
+        code += "    gl_Position = matrix_viewProjection * vec4(localPosition + matrix_model[3].xyz, 1.0);\n";
+        code += "}";
+        
+        return code;
+    },
 
-    // VERTEX SHADER BODY
-    code += "void main(void)\n";
-    code += "{\n";
-    code += "    vec2 uv = particle_uvLifeTimeFrameStart.xy;\n";
-    code += "    float lifeTime = particle_uvLifeTimeFrameStart.z;\n";
-    code += "    float frameStart = particle_uvLifeTimeFrameStart.w;\n";
-    code += "    vec3 position = particle_positionStartTime.xyz;\n";
-    code += "    float startTime = particle_positionStartTime.w;\n";
-    code += "    vec3 velocity = (matrix_model * vec4(particle_velocityStartSize.xyz, 0.0)).xyz + worldVelocity;\n";
-    code += "    float startSize = particle_velocityStartSize.w;\n";
-    code += "    vec3 acceleration = (matrix_model * vec4(particle_accelerationEndSize.xyz, 0.0)).xyz + worldAcceleration;\n";
-    code += "    float endSize = particle_accelerationEndSize.w;\n";
-    code += "    float spinStart = particle_spinStartSpinSpeed.x;\n";
-    code += "    float spinSpeed = particle_spinStartSpinSpeed.y;\n";
-    code += "    float localTime = mod((time - timeOffset - startTime), timeRange);\n";
-    code += "    float percentLife = localTime / lifeTime;\n";
-    code += "    float frame = mod(floor(localTime / frameDuration + frameStart), numFrames);\n";
-    code += "    float uOffset = frame / numFrames;\n";
-    code += "    float u = uOffset + (uv.x + 0.5) * (1.0 / numFrames);\n";
-    code += "    vUv0 = vec2(u, uv.y + 0.5);\n";
-    code += "    vColor = particle_colorMult;\n";
-    code += "    vec3 basisX = matrix_viewInverse[0].xyz;\n";
-    code += "    vec3 basisZ = matrix_viewInverse[1].xyz;\n";
-    code += "    float size = mix(startSize, endSize, percentLife);\n";
-    code += "    size = (percentLife < 0.0 || percentLife > 1.0) ? 0.0 : size;\n";
-    code += "    float s = sin(spinStart + spinSpeed * localTime);\n";
-    code += "    float c = cos(spinStart + spinSpeed * localTime);\n";
-    code += "    vec2 rotatedPoint = vec2(uv.x * c + uv.y * s, \n";
-    code += "                             -uv.x * s + uv.y * c);\n";
-    code += "    vec3 localPosition = vec3(basisX * rotatedPoint.x +\n";
-    code += "                              basisZ * rotatedPoint.y) * size +\n";
-    code += "                              velocity * localTime +\n";
-    code += "                              acceleration * localTime * localTime + \n";
-    code += "                              position;\n";
-    code += "    vAge = percentLife;\n";
-    code += "    gl_Position = matrix_viewProjection * vec4(localPosition + matrix_model[3].xyz, 1.0);\n";
-    code += "}";
-    
-    return code;
-}
+    generateFragmentShader: function (options) {
+        var code = "";
 
-pc.gfx.programlib.particle.generateFragmentShader = function (options) {
-    var code = "";
+        code += "precision mediump float;\n";
 
-    code += "precision mediump float;\n";
+        // FRAGMENT SHADER INPUTS: VARYINGS
+        code += "varying vec2 vUv0;\n";
+        code += "varying float vAge;\n";
+        code += "varying vec4 vColor;\n";
 
-    // FRAGMENT SHADER INPUTS: VARYINGS
-    code += "varying vec2 vUv0;\n";
-    code += "varying float vAge;\n";
-    code += "varying vec4 vColor;\n";
+        // FRAGMENT SHADER INPUTS: UNIFORMS
+        code += "uniform sampler2D texture_colorMap;\n";
+        code += "uniform sampler2D texture_rampMap;\n\n";
 
-    // FRAGMENT SHADER INPUTS: UNIFORMS
-    code += "uniform sampler2D texture_diffuseMap;\n";
-    code += "uniform sampler2D texture_rampMap;\n\n";
+        code += "void main(void)\n";
+        code += "{\n";
+        code += "    vec4 colorMult = texture2D(texture_rampMap, vec2(vAge, 0.5)) * vColor;\n";
+        code += "    gl_FragColor = texture2D(texture_colorMap, vUv0) * colorMult;\n";
+        code += "}";
 
-    code += "void main(void)\n";
-    code += "{\n";
-    code += "    vec4 colorMult = texture2D(texture_rampMap, vec2(vAge, 0.5)) * vColor;\n";
-    code += "    gl_FragColor = texture2D(texture_diffuseMap, vUv0) * colorMult;\n";
-    code += "}";
-
-    return code;
-}
+        return code;
+    }
+};
--- src/scene/scene_particleemitter.js
+++ src/scene/scene_particleemitter.js
@@ -1,10 +1,9 @@
 pc.extend(pc.scene, function () {
 
-    var now = new Date();
     var start = new Date();
 
     var getTime = function () {
-        return (now.getTime() - base.getTime()) / 1000.0;
+        return (new Date().getTime() - start.getTime()) / 1000.0;
     }
 
     var particleVerts = [
@@ -83,6 +82,16 @@ pc.extend(pc.scene, function () {
             // The orientation of a particle. This is only used if billboard is false.
             orientation: [0, 0, 0, 1]
         };
+
+        this.config.numParticles = 20;
+        this.config.lifeTime = 2;
+        this.config.timeRange = 2;
+        this.config.startSize = 50;
+        this.config.endSize = 90;
+        this.config.velocity = [0, 60, 0];
+        this.config.velocityRange = [15, 15, 15];
+        this.config.worldAcceleration = [0, -20, 0];
+        this.config.spinSpeedRange = 4;
         
         var device = pc.gfx.Device.getCurrent();
         var programLib = device.getProgramLibrary();
@@ -99,35 +108,42 @@ pc.extend(pc.scene, function () {
         particleFormat.addElement(new pc.gfx.VertexElement("particle_colorMult", 4, pc.gfx.VertexElementType.FLOAT32));
         particleFormat.end();
 
-        var vertexBuffer = new pc.gfx.VertexBuffer(particleFormat, 4 * numParticles);
+        var vertexBuffer = new pc.gfx.VertexBuffer(particleFormat, 4 * this.config.numParticles);
 
         var iterator = new pc.gfx.VertexIterator(vertexBuffer);
-        for (var p = 0; p < numParticles; p++) {
+        for (var p = 0; p < this.config.numParticles; p++) {
             var lifeTime = this.config.lifeTime;
-            var frameStart = this.config.frameStart + plusMinus(this.config.frameStartRange);
-            var pPosition = pc.math.vec3.create();
-            o3djs.math.addVector(parameters.position, plusMinusVector(parameters.positionRange));
+            var frameStart = this.config.frameStart; // + plusMinus(this.config.frameStartRange);
+            var position = this.config.position;
+//            o3djs.math.addVector(parameters.position, plusMinusVector(parameters.positionRange));
+            var startSize = this.config.startSize;
+            var endSize = this.config.endSize;
+            var spinStart = this.config.spinStart;
+            var spinSpeed = this.config.spinSpeed;
+            var velocity = this.config.velocity;
+            var acceleration = this.config.acceleration;
+            var startTime = (p * lifeTime / this.config.numParticles);
 
             for (var corner = 0; corner < 4; corner++) {
                 var e = iterator.element;
-                e["particle_uvLifeTimeFrameStart"].set(particleVerts[corner][0], particleVerts[corner][0], lifeTime, frameStart);
+                e["particle_uvLifeTimeFrameStart"].set(particleVerts[corner][0], particleVerts[corner][1], lifeTime, frameStart);
                 e["particle_positionStartTime"].set(position[0], position[1], position[2], startTime);
-                e["particle_velocityStartSize"].set(velocityStart[0], velocityStart[1], velocityStart[2], startSize);
-                e["particle_accelerationEndSize"].set(accelerationEnd[0], accelerationEnd[1], accelerationEnd[2], endSize);
+                e["particle_velocityStartSize"].set(velocity[0], velocity[1], velocity[2], startSize);
+                e["particle_accelerationEndSize"].set(acceleration[0], acceleration[1], acceleration[2], endSize);
                 e["particle_spinStartSpinSpeed"].set(spinStart, spinSpeed, 0.0, 0.0);
-                e["particle_colorMult"].set(0.9, 0.9, 0.0);
+                e["particle_colorMult"].set(1, 1, 1, 1);
                 iterator.next();
             }
         }
         iterator.end();
 
         // Create a index buffer
-        var indexBuffer = new pc.gfx.IndexBuffer(pc.gfx.IndexFormat.UINT16, 6 * numParticles);
+        var indexBuffer = new pc.gfx.IndexBuffer(pc.gfx.IndexFormat.UINT16, 6 * this.config.numParticles);
 
         // Fill the index buffer
         var dst = 0;
         var indices = new Uint16Array(indexBuffer.lock());
-        for (var i = 0; i < numParticles; i++) {
+        for (var i = 0; i < this.config.numParticles; i++) {
             var baseIndex = i * 4;
             indices[dst++] = baseIndex;
             indices[dst++] = baseIndex + 1;
@@ -137,14 +153,34 @@ pc.extend(pc.scene, function () {
             indices[dst++] = baseIndex + 3;
         }
         indexBuffer.unlock()
-        
+
+        var mesh = new pc.scene.Mesh();
+        mesh.vertexBuffer = vertexBuffer;
+        mesh.indexBuffer[0] = indexBuffer;
+        mesh.primitive[0].type = pc.gfx.PrimType.TRIANGLES;
+        mesh.primitive[0].base = 0;
+        mesh.primitive[0].count = indexBuffer.getNumIndices();
+        mesh.primitive[0].indexed = true;
+
         var _createTexture = function (width, height, pixelData) {
-            var texture = new pc.gfx.Texture2D(width, height, pc.gfx.TextureFormat.RGBA);
-            texture.allocate();
-            texture.lock().set(pixelData);
+            var texture = new pc.gfx.Texture({
+                width: width,
+                height: height,
+                format: pc.gfx.PIXELFORMAT_R8_G8_B8_A8,
+                cubemap: false,
+                autoMipmap: true
+            });
+            var pixels = texture.lock();
+
+            pixels.set(pixelData);
+
             texture.unlock();
-            texture.setAddressMode(pc.gfx.TextureAddress.CLAMP_TO_EDGE, pc.gfx.TextureAddress.CLAMP_TO_EDGE);
-            texture.setFilterMode(pc.gfx.TextureFilter.LINEAR, pc.gfx.TextureFilter.LINEAR);
+
+            texture.addressU = pc.gfx.ADDRESS_CLAMP_TO_EDGE;
+            texture.addressV = pc.gfx.ADDRESS_CLAMP_TO_EDGE;
+            texture.minFilter = pc.gfx.FILTER_LINEAR;
+            texture.magFilter = pc.gfx.FILTER_LINEAR;
+
             return texture;
         };
 
@@ -156,53 +192,31 @@ pc.extend(pc.scene, function () {
                 pixels.push(pixelComponent, pixelComponent, pixelComponent, pixelComponent);
             }
         }
-        var colorMap = _createTexture(8, 8, pixels);
-        var rampMap = _createTexture(2, 1, [255,255,255,255,255,255,255,0]);
 
-        this.resources = {
-            program: program,
-            indexBuffer: indexBuffer,
-            vertexBuffer: vertexBuffer,
-            colorMap: colorMap,
-            rampMap: rampMap
-        };
-    }
+        this.colorMap = _createTexture(8, 8, pixels);
+        this.rampMap = _createTexture(2, 1, [255,255,255,255,255,255,255,0]);
 
-    ParticleEmitter = pc.inherits(ParticleEmitter, pc.scene.GraphNode);
-
-    ParticleEmitter.prototype.dispatch = function () {
-        var res = this.resources;
-
-        var device = pc.gfx.Device.getCurrent();
-        // Set vertex shader uniforms
-        device.scope.resolve("particle_velocity").setValue(this.config.worldVelocity);
-        device.scope.resolve("particle_acceleration").setValue(this.config.worldAcceleration);
-        device.scope.resolve("particle_numFrames").setValue(this.config.numFrames);
-        device.scope.resolve("particle_frameDuration").setValue(this.config.frameDuration);
-        device.scope.resolve("particle_time").setValue(getTime());
-        device.scope.resolve("particle_timeRange").setValue(this.config.timeRange);
-        device.scope.resolve("particle_timeOffset").setValue(0.0);
-
-        // Set fragment shader uniforms
-        device.scope.resolve("texture_colorMap").setValue(res.colorMap);
-        device.scope.resolve("texture_rampMap").setValue(res.rampMap);
-
-        // Set the shader program
-        device.setProgram(res.program);
-
-        // Set the index buffer
-        device.setIndexBuffer(res.indexBuffer);
-
-        // Set the vertex buffer
-        device.setVertexBuffer(res.vertexBuffer, 0);
-
-        // Now draw the triangle
-        device.draw({
-            type: pc.gfx.PrimType.TRIANGLES,
-            base: 0,
-            count: res.indexBuffer.getNumIndices(),
-            indexed: true
+        var material = new pc.scene.Material();
+        material.setProgram(program);
+        material.setParameter('particle_worldVelocity', this.config.worldVelocity);
+        material.setParameter('particle_worldAcceleration', this.config.worldAcceleration);
+        material.setParameter('particle_numFrames', this.config.numFrames);
+        material.setParameter('particle_frameDuration', this.config.frameDuration);
+        material.setParameter('particle_timeRange', this.config.timeRange);
+        material.setParameter('particle_timeOffset', 0);
+        material.setParameter('texture_colorMap', this.colorMap);
+        material.setParameter('texture_rampMap', this.rampMap);
+        material.setState({
+            cull: false
         });
+
+        this.meshInstance = new pc.scene.MeshInstance(null, mesh, material);
+    };
+
+    ParticleEmitter.prototype = {
+        update: function () {
+            this.meshInstance.material.setParameter('particle_time', getTime());
+        }
     };
 
     return {
