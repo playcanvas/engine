d639500dababd5dadf4e1fd1f3ef3d7b4f995931
dave_e <dave@playcanvas.com>
2013-01-16 12:17:36 +0000

    Set type for Component#data

--- src/framework/components/component_component.js
+++ src/framework/components/component_component.js
@@ -27,7 +27,7 @@ pc.extend(pc.fw, function () {
         * @description Access the {@link pc.fw.ComponentData} directly. Usually you should
         * access the data properties via the individual properties as modifying this data directly 
         * will not fire 'set' events.
-        * @type {Object}
+        * @type {pc.fw.ComponentData} 
         */
         get data() {
             var record = this.system.store[this.entity.getGuid()];
