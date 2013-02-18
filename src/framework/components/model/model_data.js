b83b9ae441d384a413119bfd1ffdbf4fdd6f2eed
dave_e <dave@playcanvas.com>
2013-02-18 08:32:57 +0000

    more docs fixes

--- src/framework/components/model/model_data.js
+++ src/framework/components/model/model_data.js
@@ -5,10 +5,6 @@ pc.extend(pc.fw, function() {
     * @name pc.fw.ModelComponentData
     * @class Data for a {@link pc.fw.ModelComponent}
     * @constructor Create a new data object
-    * @property {String} asset The GUID of the asset for the model
-    * @property {Boolean} castShadows If true, this model will cast shadows for lights that have shadow casting enabled.
-    * @property {Boolean} receiveShadows If true, shadows will be cast on this model
-    * @property {pc.scene.Model} model The mode; node that is added to the scene graph.
     * @extends pc.fw.ComponentData
     */
     function ModelComponentData() {
