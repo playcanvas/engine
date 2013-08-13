ea5afd22d1b5fb50b119e9ca93c51a4dbc62fddc
dave_e <dave@playcanvas.com>
2013-08-13 15:02:16 +0100

    Added JsonResourceHandler for loading files of type 'json'

---
+++ src/resources/resources_json.js
@@ -0,0 +1,36 @@
+pc.extend(pc.resources, function () {
+    var JsonResourceHandler = function () {
+    };
+    JsonResourceHandler = pc.inherits(JsonResourceHandler, pc.resources.ResourceHandler);
+    
+    JsonResourceHandler.prototype.load = function (request, options) {
+        var self = this;
+        
+        var promise = new RSVP.Promise(function (resolve, reject) {
+            pc.net.http.get(request.canonical, function(response) {
+                resolve(response);
+            }, {
+                error: function () {
+                    reject();
+                }
+            });
+        });
+
+        return promise;
+    };
+
+    JsonResourceHandler.prototype.open = function (data, request, options) {
+        return data;
+    };
+    
+    var JsonRequest = function JsonRequest(identifier) {
+    };
+    JsonRequest = pc.inherits(JsonRequest, pc.resources.ResourceRequest);
+    JsonRequest.prototype.type = "json";
+    JsonRequest.prototype.Type = Object;
+    
+    return {
+        JsonResourceHandler: JsonResourceHandler,
+        JsonRequest: JsonRequest
+    };
+}());
