2434985865462186e2d049d4b10d00223171dff8
dave_e <dave@playcanvas.com>
2013-09-18 14:59:04 +0100

    [FIX] Disable cache-busting on resources so that scripts can be debugged. Also prevents double loading of scripts.

--- src/script/resources/resources_script.js
+++ src/script/resources/resources_script.js
@@ -30,6 +30,8 @@ pc.extend(pc.resources, function () {
     ScriptResourceHandler.prototype.load = function (request, options) {
         options = options || {};
         options.timeout = options.timeout || 60000; // default 10 second timeout
+        
+        options.cache = true; // For cache-busting off, so that script debugging works
 
         var promise = new RSVP.Promise(function (resolve, reject) {
             var processedUrl = request.canonical;
