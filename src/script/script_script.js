49aaef1918eddc8046e8c7779c1e1d484acc576e
Vaios Kalpias Ilias <vaios@playcanvas.com>
2013-11-20 13:47:42 +0200

    Changed pc.script.attribute argument order
    --HG--
    branch : feature/scripts-as-components

--- src/script/script_script.js
+++ src/script/script_script.js
@@ -64,8 +64,8 @@ pc.script = (function () {
         * inside the script instance like so 'this.attributeName' or outside a script instance like so 'entity.script.attributeName'.
         * Script attributes can be edited from the Attribute Editor of the designer like normal Components.
         * @param {string} name The name of the attribute
-        * @param {Object} defaultValue The default value of the attribute
         * @param {string} type The type of the attribute. Can be one of the following: 'number', 'string', 'boolean', 'asset', 'rgb', 'rgba', 'vector', 'enumeration'
+        * @param {Object} defaultValue The default value of the attribute
         * @param {Object} options Optional parameters for the attribute. Valid values are:
         * <ul>
         *   <li>{Number} min: The minimum value of the attribute</li>
@@ -75,10 +75,10 @@ pc.script = (function () {
         * </ul> 
         * @example
         * pc.script.create( function (context) {
-        *  pc.script.attribute('speed', 5);
-        *  pc.script.attribute('message', "My message");
-        *  pc.script.attribute('enemyPosition', [1,0,0], "vector");
-        *  pc.script.attribute('spellType', 0, "enumeration", {
+        *  pc.script.attribute('speed', 'number', 5);
+        *  pc.script.attribute('message', 'string', "My message");
+        *  pc.script.attribute('enemyPosition', 'vector', [1,0,0]);
+        *  pc.script.attribute('spellType', 'enumeration', 0, {
         *     enumerations: [{
         *        name: "Fire",
         *        value: 0
@@ -96,7 +96,7 @@ pc.script = (function () {
         *  return Scriptable;
         * }
         */
-        attribute: function (name, defaultValue, type, options) {
+        attribute: function (name, type, defaultValue, options) {
             // only works when parsing the script...
         },
         
