fcad733490f347de681cb038338891b917f15e00
willeastcott <will@playcanvas.com>
2014-01-05 01:01:54 +0000

    Update intersection functions to latest Math API.
    --HG--
    branch : feature/math-objects

--- src/shape/shape_intersection.js
+++ src/shape/shape_intersection.js
@@ -6,14 +6,15 @@ pc.shape.intersection = function () {
     return {
         
         /**
-         * @name pc.shape.intersection.aabbAabb
+         * @private
          * @function
+         * @name pc.shape.intersection.aabbAabb
          * @description Test whether two Axis-aligned bounding boxes intersect.
-         * @param a {pc.shape.Aabb} First bounding box
-         * @param b {pc.shape.Aabb} Second bounding box
+         * @param {pc.shape.Aabb} a First bounding box
+         * @param {pc.shape.Aabb} b Second bounding box
          * @returns {Boolean} True if there is an intersection
          */
-        aabbAabb: function (a,b) {
+        aabbAabb: function (a, b) {
             var aMax = a.getMax();
             var aMin = a.getMin();
             var bMax = b.getMax();
@@ -25,44 +26,48 @@ pc.shape.intersection = function () {
         },
         
         /**
-         * Intersection test between a ray and an AABB
-         * @param {Vec3} rayOrigin The origin of the ray
-         * @param {Vec3} rayDir The length vector of the ray
-         * @param {Aabb} aabb The AABB to test against
-         * @return {Boolean} True if intersection occurs
+         * @function
+         * @name pc.shape.intersection.rayAabb
+         * @description Intersection test between a ray and an AABB
+         * @param {pc.Vec3} rayOrigin The origin of the ray
+         * @param {pc.Vec3} rayDir The length vector of the ray
+         * @param {pc.shape.Aabb} aabb The AABB to test against
+         * @returns {Boolean} True if intersection occurs
          */
         rayAabb: function (rayOrigin, rayDir, aabb) {
-            var diff = pc.math.vec3.create(),
-            absDiff,
-            absDir,
-            cross = pc.math.vec3.create(),
-            prod  = pc.math.vec3.create(),
-            i;
-                
-            pc.math.vec3.subtract(rayOrigin, aabb.center, diff);
-            absDiff = pc.math.vec3.create(Math.abs(diff[0]), Math.abs(diff[1]), Math.abs(diff[2]));
+            var diff = new pc.Vec3(),
+                absDiff,
+                absDir,
+                cross = new pc.Vec3(),
+                prod  = new pc.Vec3(),
+                i;
+
+            diff.sub2(rayOrigin, aabb.center);
+            absDiff = new pc.Vec3(Math.abs(diff.x), Math.abs(diff.y), Math.abs(diff.z));
             
-            pc.math.vec3.multiply(diff, rayDir, prod);
+            prod.mul2(diff, rayDir);
             
-            for (i = 0; i < 3; ++i) {
-                if (absDiff[i] > aabb.halfExtents[i] && prod[i] >= 0) {
-                    return false;
-                }
-            }
-            
-            absDir = pc.math.vec3.create(Math.abs(rayDir[0]), Math.abs(rayDir[1]), Math.abs(rayDir[2]));
-            pc.math.vec3.cross(rayDir, diff, cross);
-            cross[0] = Math.abs(cross[0]);
-            cross[1] = Math.abs(cross[1]);
-            cross[2] = Math.abs(cross[2]);
-            
-            if (cross[0] > aabb.halfExtents[1]*absDir[2] + aabb.halfExtents[2]*absDir[1]) {
+            if (absDiff.x > aabb.halfExtents.x && prod.x >= 0) {
                 return false;
             }
-            if (cross[1] > aabb.halfExtents[0]*absDir[2] + aabb.halfExtents[2]*absDir[0]) {
+            if (absDiff.y > aabb.halfExtents.y && prod.y >= 0) {
                 return false;
             }
-            if (cross[2] > aabb.halfExtents[0]*absDir[1] + aabb.halfExtents[1]*absDir[0]) {
+            if (absDiff.z > aabb.halfExtents.z && prod.z >= 0) {
+                return false;
+            }
+            
+            absDir = new pc.Vec3(Math.abs(rayDir.x), Math.abs(rayDir.y), Math.abs(rayDir.z));
+            cross.cross(rayDir, diff);
+            cross.set(Math.abs(cross.x), Math.abs(cross.y), Math.abs(cross.z));
+            
+            if (cross.x > aabb.halfExtents.y*absDir.z + aabb.halfExtents.z*absDir.y) {
+                return false;
+            }
+            if (cross.y > aabb.halfExtents.x*absDir.z + aabb.halfExtents.z*absDir.x) {
+                return false;
+            }
+            if (cross.z > aabb.halfExtents.x*absDir.y + aabb.halfExtents.y*absDir.x) {
                 return false;
             }
         
@@ -70,94 +75,93 @@ pc.shape.intersection = function () {
         },
         
         /**
-         * Intersection test between a ray and a Sphere
-         * @param {Vec3} rayOrigin The origin point of the ray
-         * @param {Vec3} rayDir The length vector of the ray
-         * @param {Sphere} sphere The Sphere to test against
+         * @function
+         * @name pc.shape.intersection.raySphere
+         * @description Intersection test between a ray and a Sphere
+         * @param {pc.Vec3} rayOrigin The origin point of the ray
+         * @param {pc.Vec3} rayDir The length vector of the ray
+         * @param {pc.shape.Sphere} sphere The Sphere to test against
          * @param {Object} result The results object. result.success is true if an intersection occured, results.t is the fraction along the ray the intersection occured at (0-1)
-         * @return {Boolean} True if intersection occurs
+         * @returns {Boolean} True if intersection occurs
          */
         raySphere: function (rayOrigin, rayDir, sphere, result) {
-            var diff = pc.math.vec3.create();
+            var diff = new pc.Vec3();
             var a = 0;
             var b = 0;
             var c = 0;
             var discr = 0;
             result = result || {};
-            
-            pc.math.vec3.subtract(rayOrigin, sphere.center, diff);
-            if (pc.math.vec3.dot(diff, diff) < sphere.radius * sphere.radius ) {
+
+            diff.sub2(rayOrigin, sphere.center);
+            if (diff.dot(diff) < sphere.radius * sphere.radius ) {
                 // starts inside sphere
                 result.success = true;
                 result.t = 0;
                 return true;
             }
-        
-            a = pc.math.vec3.dot(rayDir, rayDir);
-            b = 2 * pc.math.vec3.dot(rayDir, diff);
-            c = pc.math.vec3.dot(sphere.center, sphere.center);
-            c += pc.math.vec3.dot(rayOrigin, rayOrigin);
-            c -= 2 * pc.math.vec3.dot(sphere.center, rayOrigin);
+
+            a = rayDir.dot(rayDir);
+            b = 2 * rayDir.dot(diff);
+            c = sphere.center.dot(sphere.center);
+            c += rayOrigin.dot(rayOrigin);
+            c -= 2 * sphere.center.dot(rayOrigin);
             c -= sphere.radius * sphere.radius;
-        
+
             discr = (b * b) - (4 * a * c);
             if (discr < 0) {
                 result.success = false;
                 result.t = 0;
                 return false;
             }
-            
+
             result.success = true;
             result.t = (-b - Math.sqrt(discr)) / (2 * a);
-            
+
             return true;
         },
-        
+
         /**
          * Intersection test between a ray and a triangle
-         * @param {Vec3} rayOrigin The origin point of the ray
-         * @param {Vec3} rayDir The direction vector of the ray
+         * @param {pc.Vec3} rayOrigin The origin point of the ray
+         * @param {pc.Vec3} rayDir The direction vector of the ray
          * @param {Object} t The triangle to test against
          * @param {Vec3} intersection The intersection point
-         * @return {Number} 0 if disjoint (no intersection), 1 if intersection found and 2 if ray is in plane of triangle
+         * @returns {Number} 0 if disjoint (no intersection), 1 if intersection found and 2 if ray is in plane of triangle
          */
         rayTriangle: function (rayOrigin, rayDir, t, intersection) {
-            var w0 = pc.math.vec3.create();
-            pc.math.vec3.sub(rayOrigin, t.v0, w0);
-            var a = -pc.math.vec3.dot(t.n, w0);
-            var b = pc.math.vec3.dot(t.n, rayDir);
+            var w0 = new pc.Vec3();
+            w0.sub(rayOrigin, t.v0);
+            var a = -t.n.dot(w0);
+            var b = t.n.dot(rayDir);
             if (Math.fabs(b) < 0.00000001) {     // ray is parallel to triangle plane
-                if (a === 0)                // ray lies in triangle plane
+                if (a === 0)               // ray lies in triangle plane
                     return 2;
                 else return 0;             // ray disjoint from plane
             }
-        
+
             // get intersect point of ray with triangle plane
             var r = a / b;
-            if (r < 0.0)                   // ray goes away from triangle
+            if (r < 0)                     // ray goes away from triangle
                 return 0;                  // => no intersect
             // for a segment, also test if (r > 1.0) => no intersect
-        
-            var fromOrigin = pc.math.vec3.create();
-            pc.math.vec3.scale(rayDir, r, fromOrigin);
-            pc.math.vec3.add(rayOrigin, fromOrigin, intersection);
-        
+
+            var fromOrigin = rayDir.clone().scale(r);
+            intersection.add2(rayOrigin, fromOrigin);
+
             // is I inside T?
-            var w = pc.math.vec3.create();
-            pc.math.vec3.sub(intersection, t.v0, w);
-            var wu = pc.math.vec3.dot(w, t.u);
-            var wv = pc.math.vec3.dot(w, t.v);
-        
+            var w = new pc.Vec3().sub2(intersection, t.v0);
+            var wu = w.dot(t.u);
+            var wv = w.dot(t.v);
+
             // get and test parametric coords
             var s = (t.uv * wv - t.vv * wu) / t.d;
-            if (s < 0.0 || s > 1.0)        // I is outside T
+            if (s < 0 || s > 1)        // I is outside T
                 return 0;
             var t = (t.uv * wu - t.uu * wv) / t.d;
-            if (t < 0.0 || (s + t) > 1.0)  // I is outside T
+            if (t < 0 || (s + t) > 1)  // I is outside T
                 return 0;
-        
-            return 1;                      // I is in T
+
+            return 1;                  // I is in T
         }
     };
 }();
-
