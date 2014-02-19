caa10af52b951da27a8efd3b2270b64bf31fb03b
Vaios Kalpias Ilias <vaios@playcanvas.com>
2014-02-19 15:20:15 +0200

    Added enabled property in cubemap component but it's not currently doing anything
    --HG--
    branch : feature/enable-disable

--- src/framework/components/cubemap/cubemap_data.js
+++ src/framework/components/cubemap/cubemap_data.js
@@ -5,6 +5,7 @@ pc.extend(pc.fw, function () {
         this.farClip = 100000;
         this.width = 256;
         this.height = 256;
+        this.enabled = true;
     };
     CubeMapComponentData = pc.inherits(CubeMapComponentData, pc.fw.ComponentData);
     
