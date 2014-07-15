890d334059c57cfaad3d4dbc9a5d8712301133ce
Vaios Kalpias-Ilias <vaios@playcanvas.com>
2014-07-15 19:39:39 +0300

    Added pc.shape.Sphere#intersectRay and related tests

--- tests/shape/test_sphere.js
+++ tests/shape/test_sphere.js
@@ -2,7 +2,7 @@ module('pc.shape.Sphere');
 
 test('Sphere: correct default values', function() {
     var sphere = new pc.shape.Sphere();
-    
+
     QUnit.deepEqual(sphere.center.x, 0);
     QUnit.deepEqual(sphere.center.y, 0);
     QUnit.deepEqual(sphere.center.z, 0);
@@ -21,13 +21,86 @@ test('Sphere: Constructor sets correct values', function() {
 test('containsPoint: Point is in sphere returns true', function() {
     var sphere = new pc.shape.Sphere(new pc.Vec3(1, 1, 1), 2);
     var point  = new pc.Vec3(0.5, 0.5, 0.5);
-    
+
     QUnit.deepEqual(sphere.containsPoint(point), true);
 });
 
 test('containsPoint: Point not in sphere returns false', function() {
     var sphere = new pc.shape.Sphere(new pc.Vec3(1, 1, 1), 2);
     var point = new pc.Vec3(0, 0, 5);
-    
+
     QUnit.deepEqual(sphere.containsPoint(point), false);
+});
+
+test('intersectRay: Intersection is correct', function () {
+    var sphere = new pc.shape.Sphere(new pc.Vec3(0,0,0), 5);
+    var start = new pc.Vec3(0,0, 10);
+    var direction = new pc.Vec3(0,0,-1);
+    var intersection = sphere.intersectRay(start, direction);
+
+    QUnit.equal(intersection.x, 0);
+    QUnit.equal(intersection.y, 0);
+    QUnit.equal(intersection.z, 5);
+});
+
+test('intersectRay: Intersection from center of sphere is correct', function () {
+    var sphere = new pc.shape.Sphere(new pc.Vec3(0,0,0), 5);
+    var start = new pc.Vec3(0,0,0);
+    var direction = new pc.Vec3(1,0,0);
+    var intersection = sphere.intersectRay(start, direction);
+
+    QUnit.equal(intersection.x, 5);
+    QUnit.equal(intersection.y, 0);
+    QUnit.equal(intersection.z, 0);
+});
+
+test('intersectRay: Tangential intersection works', function () {
+    var sphere = new pc.shape.Sphere(new pc.Vec3(0,0,0), 5);
+    var start = new pc.Vec3(0,5,0);
+    var direction = new pc.Vec3(0,0,-1);
+    var intersection = sphere.intersectRay(start, direction);
+
+    QUnit.equal(intersection.x, 0);
+    QUnit.equal(intersection.y, 5);
+    QUnit.equal(intersection.z, 0);
+});
+
+test('intersectRay: Ray starting from sphere returns ray origin', function () {
+    var sphere = new pc.shape.Sphere(new pc.Vec3(0,0,0), 5);
+    var start = new pc.Vec3(0,0,5);
+    var direction = new pc.Vec3(0,0,-1);
+    var intersection = sphere.intersectRay(start, direction);
+
+    QUnit.equal(intersection.x, 0);
+    QUnit.equal(intersection.y, 0);
+    QUnit.equal(intersection.z, 5);
+});
+
+test('intersectRay: Ray starting from sphere returns ray origin', function () {
+    var sphere = new pc.shape.Sphere(new pc.Vec3(0,0,0), 5);
+    var start = new pc.Vec3(0,0,5);
+    var direction = new pc.Vec3(0,0,-1);
+    var intersection = sphere.intersectRay(start, direction);
+
+    QUnit.equal(intersection.x, 0);
+    QUnit.equal(intersection.y, 0);
+    QUnit.equal(intersection.z, 5);
+});
+
+test('intersectRay: Ray pointing away from sphere does not return intersection', function () {
+    var sphere = new pc.shape.Sphere(new pc.Vec3(0,0,0), 5);
+    var start = new pc.Vec3(0,6,0);
+    var direction = new pc.Vec3(0,0,-1);
+    var intersection = sphere.intersectRay(start, direction);
+
+    QUnit.equal(intersection, null);
+});
+
+test('intersectRay: Ray pointing away from sphere does not return intersection #2', function () {
+    var sphere = new pc.shape.Sphere(new pc.Vec3(0,0,0), 5);
+    var start = new pc.Vec3(0,0,6);
+    var direction = new pc.Vec3(0,0,1);
+    var intersection = sphere.intersectRay(start, direction);
+
+    QUnit.equal(intersection, null);
 });
