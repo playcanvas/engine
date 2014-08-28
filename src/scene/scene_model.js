92ec4e3a9e3625ad63435aa46faab3a98ea7c076
Will Eastcott <will@playcanvas.com>
2014-08-28 23:31:15 +0100

    Updated docs for pc.scene.Model.

--- src/scene/scene_model.js
+++ src/scene/scene_model.js
@@ -1,8 +1,14 @@
 pc.extend(pc.scene, function () {
-
     /**
      * @name pc.scene.Model
-     * @class A model.
+     * @class A model is a graphical object that can be added to or removed from a scene.
+     * It contains a hierarchy and any number of mesh instances.
+     * @constructor Creates a new model.
+     * @example
+     * // Create a new model
+     * var model = new pc.scene.Model();
+     * @property {pc.scene.GraphNode} graph The root node of the model's graph node hierarchy.
+     * @property {Array} meshInstances An array of meshInstances contained in this model.
      */
     var Model = function Model() {
         this.graph = null;
@@ -13,185 +19,197 @@ pc.extend(pc.scene, function () {
         this.lights = [];
     };
 
-    Model.prototype.getGraph = function () {
-        return this.graph;
-    };
+    Model.prototype = {
+        getGraph: function () {
+            return this.graph;
+        },
 
-    Model.prototype.setGraph = function (graph) {
-        this.graph = graph;
-    };
+        setGraph: function (graph) {
+            this.graph = graph;
+        },
 
-    Model.prototype.getCameras = function () {
-        return this.cameras;
-    };
+        getCameras: function () {
+            return this.cameras;
+        },
 
-    Model.prototype.setCameras = function (cameras) {
-        this.cameras = cameras;
-    };
+        setCameras: function (cameras) {
+            this.cameras = cameras;
+        },
 
-    Model.prototype.getLights = function () {
-        return this.lights;
-    };
+        getLights: function () {
+            return this.lights;
+        },
 
-    Model.prototype.setLights = function (lights) {
-        this.lights = lights;
-    };
+        setLights: function (lights) {
+            this.lights = lights;
+        },
 
-    Model.prototype.getTextures = function () {
-        return this.textures;
-    };
-
-    Model.prototype.getMaterials = function () {
-        var i;
-        var materials = [];
-        for (i = 0; i < this.meshInstances.length; i++) {
-            var meshInstance = this.meshInstances[i];
-            if (materials.indexOf(meshInstance.material) === -1) {
-                materials.push(meshInstance.material);
+        getMaterials: function () {
+            var i;
+            var materials = [];
+            for (i = 0; i < this.meshInstances.length; i++) {
+                var meshInstance = this.meshInstances[i];
+                if (materials.indexOf(meshInstance.material) === -1) {
+                    materials.push(meshInstance.material);
+                }
             }
-        }
-        return materials;
-    };
+            return materials;
+        },
 
-    /**
-     * @function
-     * @name pc.scene.Model#clone
-     * @description Clones a model. The returned model has a newly created hierarchy
-     * but actually just references the geometries of the model being cloned. This is
-     * a very memory efficient way of generating lots of copies of a model at runtime.
-     * The caveat is that modifications to the cloned model's geometries, submeshes,
-     * materials or textures will actually also modify the source model (and vice versa).
-     * @returns {pc.scene.Model} A cloned Model.
-     * @author Will Eastcott
-     */
-	Model.prototype.clone = function () {
-        var i;
+        /**
+         * @function
+         * @name pc.scene.Model#clone
+         * @description Clones a model. The returned model has a newly created hierarchy
+         * and mesh instances, but meshes are shared between the clone and the specified
+         * model.
+         * @returns {pc.scene.Model} A clone of the specified model.
+         * @example
+         * var clonedModel = model.clone();
+         * @author Will Eastcott
+         */
+    	clone: function () {
+            var i;
 
-        // Duplicate the node hierarchy
-        var srcNodes = [];
-        var cloneNodes = [];
+            // Duplicate the node hierarchy
+            var srcNodes = [];
+            var cloneNodes = [];
 
-        var _duplicate = function (node) {
-            var newNode = node.clone();
+            var _duplicate = function (node) {
+                var newNode = node.clone();
 
-            srcNodes.push(node);
-            cloneNodes.push(newNode);
+                srcNodes.push(node);
+                cloneNodes.push(newNode);
 
-            if (node instanceof pc.scene.CameraNode)
-                clone.cameras.push(newNode);
-            else if (node instanceof pc.scene.LightNode)
-                clone.lights.push(newNode);
+                if (node instanceof pc.scene.CameraNode)
+                    clone.cameras.push(newNode);
+                else if (node instanceof pc.scene.LightNode)
+                    clone.lights.push(newNode);
 
-            var children = node.getChildren();
-            for (var i = 0; i < children.length; i++) {
-                newNode.addChild(_duplicate(children[i]));
-            }
+                var children = node.getChildren();
+                for (var i = 0; i < children.length; i++) {
+                    newNode.addChild(_duplicate(children[i]));
+                }
 
-            return newNode;
-        };
+                return newNode;
+            };
 
-        var cloneGraph = _duplicate(this.graph);
-        var cloneMeshInstances = [];
-        var cloneSkinInstances = [];
+            var cloneGraph = _duplicate(this.graph);
+            var cloneMeshInstances = [];
+            var cloneSkinInstances = [];
 
-        // Clone the skin instances
-        for (i = 0; i < this.skinInstances.length; i++) {
-            var skin = this.skinInstances[i].skin;
-            var cloneSkinInstance = new pc.scene.SkinInstance(skin);
+            // Clone the skin instances
+            for (i = 0; i < this.skinInstances.length; i++) {
+                var skin = this.skinInstances[i].skin;
+                var cloneSkinInstance = new pc.scene.SkinInstance(skin);
 
-            // Resolve bone IDs to actual graph nodes
-            var bones = [];
-            for (j = 0; j < skin.boneNames.length; j++) {
-                var boneName = skin.boneNames[j];
-                var bone = cloneGraph.findByName(boneName);
-                bones.push(bone);
-            }
-            cloneSkinInstance.bones = bones;
+                // Resolve bone IDs to actual graph nodes
+                var bones = [];
+                for (j = 0; j < skin.boneNames.length; j++) {
+                    var boneName = skin.boneNames[j];
+                    var bone = cloneGraph.findByName(boneName);
+                    bones.push(bone);
+                }
+                cloneSkinInstance.bones = bones;
 
-            cloneSkinInstances.push(cloneSkinInstance);
-        }
-
-        // Clone the mesh instances
-        for (i = 0; i < this.meshInstances.length; i++) {
-            var meshInstance = this.meshInstances[i];
-            var nodeIndex = srcNodes.indexOf(meshInstance.node);
-            var cloneMeshInstance = new pc.scene.MeshInstance(cloneNodes[nodeIndex], meshInstance.mesh, meshInstance.material);
-
-            if (meshInstance.skinInstance) {
-                var skinInstanceIndex = this.skinInstances.indexOf(meshInstance.skinInstance);
-                cloneMeshInstance.skinInstance = cloneSkinInstances[skinInstanceIndex];
+                cloneSkinInstances.push(cloneSkinInstance);
             }
 
-            cloneMeshInstances.push(cloneMeshInstance);
-        }
+            // Clone the mesh instances
+            for (i = 0; i < this.meshInstances.length; i++) {
+                var meshInstance = this.meshInstances[i];
+                var nodeIndex = srcNodes.indexOf(meshInstance.node);
+                var cloneMeshInstance = new pc.scene.MeshInstance(cloneNodes[nodeIndex], meshInstance.mesh, meshInstance.material);
 
-        var clone = new pc.scene.Model();
-        clone.graph = cloneGraph;
-        clone.meshInstances = cloneMeshInstances;
-        clone.skinInstances = cloneSkinInstances;
+                if (meshInstance.skinInstance) {
+                    var skinInstanceIndex = this.skinInstances.indexOf(meshInstance.skinInstance);
+                    cloneMeshInstance.skinInstance = cloneSkinInstances[skinInstanceIndex];
+                }
 
-        clone.getGraph().syncHierarchy();
-
-        var meshInstances = clone.meshInstances;
-        for (i = 0; i < meshInstances.length; i++) {
-            meshInstances[i].syncAabb();
-        }
-
-        return clone;
-    };
-
-    Model.prototype.generateWireframe = function () {
-        var i, j, k;
-        var i1, i2;
-        var mesh, base, count, indexBuffer, wireBuffer;
-        var srcIndices, dstIndices;
-
-        // Build an array of unique meshes in this model
-        var meshes = [];
-        for (i = 0; i < this.meshInstances.length; i++) {
-            mesh = this.meshInstances[i].mesh;
-            if (meshes.indexOf(mesh) === -1) {
-                meshes.push(mesh);
+                cloneMeshInstances.push(cloneMeshInstance);
             }
-        }
 
-        var offsets = [[0, 1], [1, 2], [2, 0]];
-        for (i = 0; i < meshes.length; i++) {
-            mesh = meshes[i];
-            base = mesh.primitive[pc.scene.RENDERSTYLE_SOLID].base;
-            count = mesh.primitive[pc.scene.RENDERSTYLE_SOLID].count;
-            indexBuffer = mesh.indexBuffer[pc.scene.RENDERSTYLE_SOLID];
+            var clone = new pc.scene.Model();
+            clone.graph = cloneGraph;
+            clone.meshInstances = cloneMeshInstances;
+            clone.skinInstances = cloneSkinInstances;
 
-            srcIndices = new Uint16Array(indexBuffer.lock());
+            clone.getGraph().syncHierarchy();
 
-            var uniqueLineIndices = {};
-            var lines = [];
-            for (j = base; j < base + count; j+=3) {
-                for (k = 0; k < 3; k++) {
-                    i1 = srcIndices[j + offsets[k][0]];
-                    i2 = srcIndices[j + offsets[k][1]];
-                    var line = (i1 > i2) ? ((i2 << 16) | i1) : ((i1 << 16) | i2);
-                    if (uniqueLineIndices[line] === undefined) {
-                        uniqueLineIndices[line] = 0;
-                        lines.push(i1, i2);
-                    }
+            var meshInstances = clone.meshInstances;
+            for (i = 0; i < meshInstances.length; i++) {
+                meshInstances[i].syncAabb();
+            }
+
+            return clone;
+        },
+
+        /**
+         * @function
+         * @name pc.scene.Model#generateWireframe
+         * @description Generates the necessary internal data for a model to be
+         * renderable as wireframe. Once this function has been called, any mesh
+         * instance in the model can have its renderStyle property set to
+         * pc.scene.RENDERSTYLE_WIREFRAME
+         * @example
+         * model.generateWireframe();
+         * for (var i = 0; i < model.meshInstances.length; i++) {
+         *     model.meshInstances[i].renderStyle = pc.scene.RENDERSTYLE_WIREFRAME;
+         * }
+         * @author Will Eastcott
+         */
+        generateWireframe: function () {
+            var i, j, k;
+            var i1, i2;
+            var mesh, base, count, indexBuffer, wireBuffer;
+            var srcIndices, dstIndices;
+
+            // Build an array of unique meshes in this model
+            var meshes = [];
+            for (i = 0; i < this.meshInstances.length; i++) {
+                mesh = this.meshInstances[i].mesh;
+                if (meshes.indexOf(mesh) === -1) {
+                    meshes.push(mesh);
                 }
             }
 
-            indexBuffer.unlock();
+            var offsets = [[0, 1], [1, 2], [2, 0]];
+            for (i = 0; i < meshes.length; i++) {
+                mesh = meshes[i];
+                base = mesh.primitive[pc.scene.RENDERSTYLE_SOLID].base;
+                count = mesh.primitive[pc.scene.RENDERSTYLE_SOLID].count;
+                indexBuffer = mesh.indexBuffer[pc.scene.RENDERSTYLE_SOLID];
 
-            wireBuffer = new pc.gfx.IndexBuffer(indexBuffer.device, pc.gfx.INDEXFORMAT_UINT16, lines.length);
-            dstIndices = new Uint16Array(wireBuffer.lock());
-            dstIndices.set(lines);
-            wireBuffer.unlock();
+                srcIndices = new Uint16Array(indexBuffer.lock());
 
-            mesh.primitive[pc.scene.RENDERSTYLE_WIREFRAME] = {
-                type: pc.gfx.PRIMITIVE_LINES,
-                base: 0,
-                count: lines.length,
-                indexed: true
-            };
-            mesh.indexBuffer[pc.scene.RENDERSTYLE_WIREFRAME] = wireBuffer;
+                var uniqueLineIndices = {};
+                var lines = [];
+                for (j = base; j < base + count; j+=3) {
+                    for (k = 0; k < 3; k++) {
+                        i1 = srcIndices[j + offsets[k][0]];
+                        i2 = srcIndices[j + offsets[k][1]];
+                        var line = (i1 > i2) ? ((i2 << 16) | i1) : ((i1 << 16) | i2);
+                        if (uniqueLineIndices[line] === undefined) {
+                            uniqueLineIndices[line] = 0;
+                            lines.push(i1, i2);
+                        }
+                    }
+                }
+
+                indexBuffer.unlock();
+
+                wireBuffer = new pc.gfx.IndexBuffer(indexBuffer.device, pc.gfx.INDEXFORMAT_UINT16, lines.length);
+                dstIndices = new Uint16Array(wireBuffer.lock());
+                dstIndices.set(lines);
+                wireBuffer.unlock();
+
+                mesh.primitive[pc.scene.RENDERSTYLE_WIREFRAME] = {
+                    type: pc.gfx.PRIMITIVE_LINES,
+                    base: 0,
+                    count: lines.length,
+                    indexed: true
+                };
+                mesh.indexBuffer[pc.scene.RENDERSTYLE_WIREFRAME] = wireBuffer;
+            }
         }
     };
     
