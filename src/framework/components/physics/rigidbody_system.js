6f02c6cd4d7f4bab4d46f67fae881fbca7224bce
Vaios Kalpias-Ilias <vaios@playcanvas.com>
2014-11-03 11:46:26 +0200

    Fix for new ammo.js

--- src/framework/components/physics/rigidbody_system.js
+++ src/framework/components/physics/rigidbody_system.js
@@ -416,8 +416,7 @@ pc.extend(pc.fw, function () {
 
             this.dynamicsWorld.rayTest(ammoRayStart, ammoRayEnd, rayCallback);
             if (rayCallback.hasHit()) {
-                var collisionObjPtr = rayCallback.get_m_collisionObject();
-                var collisionObj = Ammo.wrapPointer(collisionObjPtr, Ammo.btCollisionObject);
+                var collisionObj = rayCallback.get_m_collisionObject();
                 var body = Ammo.castObject(collisionObj, Ammo.btRigidBody);
                 var point = rayCallback.get_m_hitPointWorld();
                 var normal = rayCallback.get_m_hitNormalWorld();
