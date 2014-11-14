05c612dde0bcd48da3da9a763ccc857904b63c61
Vaios Kalpias-Ilias <vaios@playcanvas.com>
2014-11-14 10:17:26 +0000

    Shadow resolution is now available for all light types

--- src/framework/components/light/light_system.js
+++ src/framework/components/light/light_system.js
@@ -84,10 +84,7 @@ pc.extend(pc.fw, function () {
                     value: 2048
                 }]
             },
-            defaultValue: 1024,
-            filter: {
-                type: ['directional', 'spot']
-            }
+            defaultValue: 1024
         }, {
             name: "range",
             displayName: "Range",
