a221a680808d73b46cd8d3094401bc8db18de0fd
Dave Evans <dave@playcanvas.com>
2014-11-18 19:01:53 +0000

    Added CURVE_CATMULL and CURVE_CARDINAL to pc.Curve()

--- src/math/math_curve.js
+++ src/math/math_curve.js
@@ -2,10 +2,14 @@ pc.extend(pc, (function () {
 
     var CURVE_LINEAR = 0;
     var CURVE_SMOOTHSTEP = 1;
+    var CURVE_CATMULL = 2;
+    var CURVE_CARDINAL = 3;
 
     var Curve = function (data) {
         this.keys = [];
         this.type = CURVE_SMOOTHSTEP;
+
+        this.tension = 0.5; // used for CURVE_CARDINAL
 
         if (data) {
             for (var i = 0; i < data.length - 1; i += 2) {
@@ -17,6 +21,15 @@ pc.extend(pc, (function () {
     };
 
     Curve.prototype = {
+        /**
+        * @private
+        * @function
+        * @name pc.Curve#add
+        * @description Add new key
+        * @param {Number} time Time to add new key
+        * @param {Number} value Value of new key
+        * @returns {Array} [time, value] pair
+        */
         add: function (time, value) {
             var keys = this.keys;
             var len = keys.length;
@@ -33,16 +46,38 @@ pc.extend(pc, (function () {
             return key;
         },
 
+        /**
+        * @private
+        * @function
+        * @name pc.Curve#get
+        * @description Return a specific key
+        * @param {Number} index The index of the key to return
+        * @returns {Array} [time, value] pair
+        */
         get: function (index) {
             return this.keys[index];
         },
 
+        /**
+        * @private
+        * @function
+        * @name pc.Curve#sort
+        * @description Sort keys by time
+        */
         sort: function () {
             this.keys.sort(function (a, b) {
                 return a[0] - b[0];
             });
         },
 
+        /**
+        * @private
+        * @function
+        * @name pc.Curve#value
+        * @description return value interpolated at time
+        * @param {Number} time The time at which to calculate the value
+        * @return {Number} The interpolated value
+        */
         value: function (time) {
             var keys = this.keys;
 
@@ -53,7 +88,6 @@ pc.extend(pc, (function () {
             var rightValue = 0;
 
             for (var i = 0, len = keys.length; i < len; i++) {
-
                 // early exit check
                 if (keys[i][0] === time) {
                     return keys[i][1];
@@ -70,15 +104,66 @@ pc.extend(pc, (function () {
                 leftValue = keys[i][1];
             }
 
-            var div = rightTime - leftTime;
+            // Clamp values before first and after last key
+            if (time < keys[0][0]) {
+                return keys[0][1];
+            } else if (time > keys[keys.length-1][0]) {
+                return keys[keys.length-1][1];
+            }
 
+            var div = rightTime - leftTime;
             var interpolation = (div === 0 ? 0 : (time - leftTime) / div);
 
             if (this.type === CURVE_SMOOTHSTEP) {
                 interpolation *= interpolation * (3 - 2 * interpolation);
+            } else if (this.type === CURVE_CATMULL || this.type === CURVE_CARDINAL) {
+                var p1 = leftValue;
+                var p2 = rightValue;
+                var p0 = p1;
+                var p3 = p2;
+
+                if (i > 0) {
+                    i = i - 1;
+                }
+
+                if (i > 0) {
+                    p0 = keys[i-1][1];
+                }
+
+                if (i < keys.length-2) {
+                    p3 = keys[i+2][1];
+                }
+
+                if (this.type === CURVE_CATMULL) {
+                    return this._interpolateCatmullRom(p0, p1, p2, p3, interpolation);
+                } else {
+                    return this._interpolateCardinal(p0, p1, p2, p3, interpolation, this.tension);
+                }
             }
 
             return pc.math.lerp(leftValue, rightValue, interpolation);
+        },
+
+        _interpolateHermite: function (p0, p1, t0, t1, s) {
+            var s2 = s*s;
+            var s3 = s*s*s;
+            var h0 = 2*s3 - 3*s2 + 1;
+            var h1 = -2*s3 + 3*s2;
+            var h2 = s3 - 2*s2 + s;
+            var h3 = s3 - s2;
+
+            return p0 * h0 + p1 * h1 + t0 * h2 + t1 * h3;
+        },
+
+        _interpolateCardinal: function (p0, p1, p2, p3, s, t) {
+            var t0 = t*(p2 - p0);
+            var t1 = t*(p3 - p1);
+
+            return this._interpolateHermite(p1, p2, t0, t1, s);
+        },
+
+        _interpolateCatmullRom: function (p0, p1, p2, p3, s) {
+            return this._interpolateCardinal(p0, p1, p2, p3, s, 0.5);
         },
 
         closest: function (time) {
@@ -132,6 +217,8 @@ pc.extend(pc, (function () {
     return {
         Curve: Curve,
         CURVE_LINEAR: CURVE_LINEAR,
-        CURVE_SMOOTHSTEP: CURVE_SMOOTHSTEP
+        CURVE_SMOOTHSTEP: CURVE_SMOOTHSTEP,
+        CURVE_CATMULL: CURVE_CATMULL,
+        CURVE_CARDINAL: CURVE_CARDINAL
     };
 }()));
