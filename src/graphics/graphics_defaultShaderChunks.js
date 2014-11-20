80be31410034a7812fad359fd8350790fa5f838d
Mr F <somemail256@gmail.com>
2014-11-20 18:58:59 +0300

    forgot this file

--- src/graphics/graphics_defaultShaderChunks.js
+++ src/graphics/graphics_defaultShaderChunks.js
@@ -1,9 +1,7 @@
 pc.extend(pc.gfx.shaderChunks, (function () {
     return {
         defaultGamma: pc.gfx.shaderChunks.gamma1_0PS,
-        defaultTonemapping: pc.gfx.shaderChunks.tonemappingNonePS,
-        defaultSpecular: pc.gfx.shaderChunks.lightSpecularPhongPS,
-        defaultFresnel: pc.gfx.shaderChunks.fresnelSimplePS
+        defaultTonemapping: pc.gfx.shaderChunks.tonemappingNonePS
     };
 }()));
 
