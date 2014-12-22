59f45a32835c6286620ce86459ec1252575218f5
Will Eastcott <will@playcanvas.com>
2014-12-22 15:27:38 +0000

    Change fill mode and resolution enums back to original values because they are stored in the database.

--- src/framework/framework_application.js
+++ src/framework/framework_application.js
@@ -816,31 +816,32 @@ pc.extend(pc, function () {
          * @name pc.FILLMODE_NONE
          * @description When resizing the window the size of the canvas will not change.
          */
-        FILLMODE_NONE: 0,
+        FILLMODE_NONE: 'NONE',
         /**
          * @enum pc.FILLMODE
          * @name pc.FILLMODE_FILL_WINDOW
          * @description When resizing the window the size of the canvas will change to fill the window exactly.
          */
-        FILLMODE_FILL_WINDOW: 1,
+        FILLMODE_FILL_WINDOW: 'FILL_WINDOW',
         /**
          * @enum pc.FILLMODE
          * @name pc.FILLMODE_KEEP_ASPECT
          * @description When resizing the window the size of the canvas will change to fill the window as best it can, while maintaining the same aspect ratio.
          */
-        FILLMODE_KEEP_ASPECT: 2,
+        FILLMODE_KEEP_ASPECT: 'KEEP_ASPECT',
         /**
         * @enum pc.RESOLUTION
         * @name pc.RESOLUTION_AUTO
         * @description When the canvas is resized the resolution of the canvas will change to match the size of the canvas.
         */
-        RESOLUTION_AUTO: 0,
+        RESOLUTION_AUTO: 'AUTO',
         /**
         * @enum pc.RESOLUTION
         * @name pc.RESOLUTION_FIXED
         * @description When the canvas is resized the resolution of the canvas will remain at the same value and the output will just be scaled to fit the canvas.
         */
-        RESOLUTION_FIXED: 1,
+        RESOLUTION_FIXED: 'FIXED',
+
         Application: Application
     };
 } ());
