ff23236c136d499c6e740e0dfc323ce54e88a787
Mr F <somemail256@gmail.com>
2015-04-29 19:29:53 +0300

    clamp anisotropy values to avoid webgl errors

--- src/graphics/graphics_texture.js
+++ src/graphics/graphics_texture.js
@@ -130,7 +130,8 @@ pc.extend(pc, function () {
     Object.defineProperty(Texture.prototype, 'anisotropy', {
         get: function () { return this._anisotropy; },
         set: function (anisotropy) {
-            this._anisotropy = anisotropy;
+            console.log("!");
+            this._anisotropy = pc.math.clamp(anisotropy, 1, this.device.maxAnisotropy);
         }
     });
 
