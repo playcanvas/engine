b1ac54be60904dd2ed452a6a78c5110fb7e891c6
Dave Evans <dave@playcanvas.com>
2015-05-26 19:01:13 +0100

    Convert url to lowercase before checking for absolute URL in script component

--- src/framework/components/script/script_component.js
+++ src/framework/components/script/script_component.js
@@ -195,7 +195,7 @@ pc.extend(pc, function () {
             urls.forEach(function (url) {
                 var _url = null;
                 // support absolute URLs (for now)
-                if (pc.string.startsWith(url, "http")) {
+                if (pc.string.startsWith(url.toLowerCase(), "http")) {
                     _url = url;
                 } else {
                     _url = pc.path.join(prefix, url);
