d2e643b61ba10f9403ace83ccebdec92fca27e03
Mr F <somemail256@gmail.com>
2015-06-11 16:26:58 +0300

    var name changes

--- src/graphics/graphics_prefiltercubemap.js
+++ src/graphics/graphics_prefiltercubemap.js
@@ -286,32 +286,32 @@ pc.extend(pc, (function () {
     }
     function texelCoordSolidAngle(u, v, size) {
        //scale up to [-1, 1] range (inclusive), offset by 0.5 to point to texel center.
-       var U = (2.0 * (u + 0.5) / size ) - 1.0;
-       var V = (2.0 * (v + 0.5) / size ) - 1.0;
+       var _u = (2.0 * (u + 0.5) / size ) - 1.0;
+       var _v = (2.0 * (v + 0.5) / size ) - 1.0;
 
        // fixSeams
-        U *= 1.0 - 1.0 / size;
-        V *= 1.0 - 1.0 / size;
+        _u *= 1.0 - 1.0 / size;
+        _v *= 1.0 - 1.0 / size;
 
-       var InvResolution = 1.0 / size;
+       var invResolution = 1.0 / size;
 
         // U and V are the -1..1 texture coordinate on the current face.
         // Get projected area for this texel
-        var x0 = U - InvResolution;
-        var y0 = V - InvResolution;
-        var x1 = U + InvResolution;
-        var y1 = V + InvResolution;
-        var SolidAngle = areaElement(x0, y0) - areaElement(x0, y1) - areaElement(x1, y0) + areaElement(x1, y1);
+        var x0 = _u - invResolution;
+        var y0 = _v - invResolution;
+        var x1 = _u + invResolution;
+        var y1 = _v + invResolution;
+        var solidAngle = areaElement(x0, y0) - areaElement(x0, y1) - areaElement(x1, y0) + areaElement(x1, y1);
 
         // fixSeams cut
         if ((u==0 && v==0) || (u==size-1 && v==0) || (u==0 && v==size-1) || (u==size-1 && v==size-1)) {
-            SolidAngle /= 3;
+            solidAngle /= 3;
         }
         else if (u==0 || v==0 || u==size-1 || v==size-1) {
-            SolidAngle *= 0.5;
+            solidAngle *= 0.5;
         }
 
-        return SolidAngle;
+        return solidAngle;
     }
 
     function shFromCubemap(source) {
