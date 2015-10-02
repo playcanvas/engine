c49a5f1e758de8f3af076b418891ed527d436e7b
Mr F <somemail256@gmail.com>
2015-10-02 21:21:13 +0300

    update description

--- src/core/core_time.js
+++ src/core/core_time.js
@@ -52,7 +52,7 @@ pc.extend(pc, (function () {
          * @private
          * @function
          * @name pc.now
-         * @description Get current time in milliseconds
+         * @description Get current time in milliseconds. Use it to measure time difference. Reference point may differ on different platforms.
          * @return {Number} The time in milliseconds
          */
         now: (!window.performance || !performance.now || !performance.timing)? Date.now : function () {
