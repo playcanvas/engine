f48af40ae3f58385e7f647e60870b15cbe3cddc8
Mr F <somemail256@gmail.com>
2016-06-24 17:17:11 +0300

    add files

--- src/scene/light.js
+++ src/scene/light.js
@@ -295,7 +295,7 @@ pc.extend(pc, function () {
         setShadowType: function (mode) {
             var device = pc.Application.getApplication().graphicsDevice;
 
-            if (mode===pc.SHADOW_VSM32 && !device.extTextureFloatHighPrecision) {
+            if (mode===pc.SHADOW_VSM32 && !device.extTextureFloatRenderable) {
                 mode = pc.SHADOW_VSM16;
             }
             if (mode===pc.SHADOW_VSM16 && !device.extTextureHalfFloatRenderable) {
