33202d2720ac2b6349635a46af57aedf5df1195b
Mr F <somemail256@gmail.com>
2016-07-06 20:30:50 +0300

    fix

--- src/scene/forward-renderer.js
+++ src/scene/forward-renderer.js
@@ -977,6 +977,7 @@ pc.extend(pc, function () {
             if (!camera.frustumCulling) {
                 for (i = 0; i < drawCallsCount; i++) {
                     // need to copy array anyway because sorting will happen and it'll break original draw call order assumption
+                    drawCall = drawCalls[i];
                     if (!drawCall.visible) continue;
                     culled.push(drawCalls[i]);
                 }
