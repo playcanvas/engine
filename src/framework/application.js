64528f178139a4a1b6eb944605489b58e238ee72
Dave Evans <dave@playcanvas.com>
2016-11-07 17:49:22 +0000

    Remove reference to hasWebVr

--- src/framework/application.js
+++ src/framework/application.js
@@ -550,7 +550,7 @@ pc.extend(pc, function () {
             // if VR is enabled in the project and there is no native VR support
             // load the polyfill
             if (props.vr && props.vr_polyfill_url) {
-                if (!pc.VrManager.hasWebVr()) {
+                if (!pc.VrManager.isSupported) {
                     props.libraries.push(props.vr_polyfill_url);
                 }
             }
