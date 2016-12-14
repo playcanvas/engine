8061ca1b2aac0d423a5717bd7e8992b4492c2ca7
Steven Yau <steven@playcanvas.com>
2016-12-14 11:46:25 +0000

    Changed pc.Plane.point and pc.Plane.normal to properties Removed unused variable. Renamed pc.Plane.intersectsPoint to pc.Plane.intersectsLine

--- src/shape/plane.js
+++ src/shape/plane.js
@@ -1,5 +1,4 @@
 pc.extend(pc, function () {
-    var rayDepth = 100000;
     var tmpVecA = new pc.Vec3();
 
     /**
@@ -11,42 +10,29 @@ pc.extend(pc, function () {
      * @param {pc.Vec3} normal Normal of the plane.
      */
     var Plane = function Plane (point, normal) {
-        this.normal = normal ? normal.clone() : new pc.Vec3(0, 0, 1);
-        this.point = point ? point.clone() : new pc.Vec3(0, 0, 0);
-        this.d = -this.normal.dot(this.point);
+        this._normal = normal ? normal.clone() : new pc.Vec3(0, 0, 1);
+        this._point = point ? point.clone() : new pc.Vec3(0, 0, 0);
+        this._d = -this._normal.dot(this._point);
     };
 
     Plane.prototype = {
-         /**
-         * @function
-         * @name pc.Plane#set
-         * @description Sets the plane point and normal. Use this method when moving the plane.
-         * @param {pc.Vec3} point Point position on the plane.
-         * @param {pc.Vec3} normal Normal of the plane.
-         */
-        set: function (point, normal) {
-            this.normal.copy(normal);
-            this.point.copy(point);
-            this.d = -this.normal.dot(this.point);
-        },
-
         _intersect: function (p0, p1) {
-            var d0 = this.normal.dot(p0) + this.d;
-            var d1 = this.normal.dot(p1) + this.d;
+            var d0 = this._normal.dot(p0) + this._d;
+            var d1 = this._normal.dot(p1) + this._d;
 
             return d0 / (d0 - d1);
         },
 
         /**
          * @function
-         * @name pc.Plane#intersectsPoint
+         * @name pc.Plane#intersectsLine
          * @description Test if the plane intersects between two points.
          * @param {pc.Vec3} p0 First point position.
          * @param {pc.Vec3} p1 Second point position.
          * @param {pc.Vec3} [point] If there is an intersection, the intersection point will be copied into here.
          * @returns {Boolean} True if there is an intersection.
          */
-        intersectsPoint: function (p0, p1, point) {
+        intersectsLine: function (p0, p1, point) {
             var t = this._intersect(p0, p1);
             var intersects = t >= 0;
             if (intersects && point)
@@ -64,8 +50,8 @@ pc.extend(pc, function () {
          * @returns {Boolean} True if there is an intersection
          */
         intersectsRay: function (ray, point) {
-            var pointToOrigin = tmpVecA.sub2(this.point, ray.origin);
-            var t = this.normal.dot(pointToOrigin) / this.normal.dot(ray.direction);
+            var pointToOrigin = tmpVecA.sub2(this._point, ray.origin);
+            var t = this._normal.dot(pointToOrigin) / this._normal.dot(ray.direction);
             var intersects = t >= 0;
 
             if (intersects && point)
@@ -74,6 +60,36 @@ pc.extend(pc, function () {
             return intersects;
         },
     };
+
+    /**
+     * @name pc.Plane#normal
+     * @type pc.Vec3
+     * @description The normal of the plane.
+     */
+    Object.defineProperty(Plane.prototype, 'normal', {
+        get: function () {
+            return this._normal;
+        },
+        set: function (value) {
+            this._normal.copy(value);
+            this._d = -this._normal.dot(this._point);
+        }
+    });
+
+    /**
+     * @name pc.Plane#point
+     * @type pc.Vec3
+     * @description A point on the plane.
+     */
+    Object.defineProperty(Plane.prototype, 'point', {
+        get: function () {
+            return this._point;
+        },
+        set: function (value) {
+            this._point.copy(value);
+            this._d = -this._normal.dot(this._point);
+        }
+    });
 
     return {
         Plane: Plane
