b3b6cc24e576b6951240e3825f8926bc16beff79
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2017-06-03 10:20:52 +0900

    Fix typo in comment.

--- src/framework/components/camera/component.js
+++ src/framework/components/camera/component.js
@@ -5,7 +5,7 @@ pc.extend(pc, function () {
      * @extends pc.Component
      * @class The Camera Component enables an Entity to render the scene. A scene requires at least one
      * enabled camera component to be rendered. Note that multiple camera components can be enabled
-     * simulataneously (for split-screen or offscreen rendering, for example).
+     * simultaneously (for split-screen or offscreen rendering, for example).
      * @description Create a new Camera Component.
      * @param {pc.CameraComponentSystem} system The ComponentSystem that created this Component.
      * @param {pc.Entity} entity The Entity that this Component is attached to.
@@ -25,7 +25,7 @@ pc.extend(pc, function () {
      * entity.camera.nearClip = 2;
      * @property {Number} projection The type of projection used to render the camera. Can be:
      * <ul>
-     *     <li>{@link pc.PROJECTION_PERSPECTIVE}: A persepctive projection. The camera frustum resembles a truncated pyramid.</li>
+     *     <li>{@link pc.PROJECTION_PERSPECTIVE}: A perspective projection. The camera frustum resembles a truncated pyramid.</li>
      *     <li>{@link pc.PROJECTION_ORTHOGRAPHIC}: An orthographic projection. The camera frustum is a cuboid.</li>
      * </ul>
      * Defaults to pc.PROJECTION_PERSPECTIVE.
@@ -177,7 +177,7 @@ pc.extend(pc, function () {
          * @param {Number} screenx x coordinate on PlayCanvas' canvas element.
          * @param {Number} screeny y coordinate on PlayCanvas' canvas element.
          * @param {Number} cameraz The distance from the camera in world space to create the new point.
-         * @param {pc.Vec3} [worldCoord] 3D vector to recieve world coordinate result.
+         * @param {pc.Vec3} [worldCoord] 3D vector to receive world coordinate result.
          * @example
          * // Get the start and end points of a 3D ray fired from a screen click position
          * var start = entity.camera.screenToWorld(clickX, clickY, entity.camera.nearClip);
@@ -199,7 +199,7 @@ pc.extend(pc, function () {
          * @name pc.CameraComponent#worldToScreen
          * @description Convert a point from 3D world space to 2D screen space.
          * @param {pc.Vec3} worldCoord The world space coordinate.
-         * @param {pc.Vec3} [screenCoord] 3D vector to recieve screen coordinate result.
+         * @param {pc.Vec3} [screenCoord] 3D vector to receive screen coordinate result.
          * @returns {pc.Vec3} The screen space coordinate.
          */
         worldToScreen: function (worldCoord, screenCoord) {
