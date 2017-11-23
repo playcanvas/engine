1244d0eb32bdecdfd0dfe6f5457aa043db91c97f
guycalledfrank <somemail256@gmail.com>
2017-11-23 20:55:34 +0000

    fix

--- src/scene/material.js
+++ src/scene/material.js
@@ -88,6 +88,8 @@ pc.extend(pc, function () {
         this.alphaWrite = true;
 
         this.meshInstances = []; // The mesh instances referencing this material
+
+        this._shaderVersion = 0;
     };
 
     Object.defineProperty(Material.prototype, 'shader', {
