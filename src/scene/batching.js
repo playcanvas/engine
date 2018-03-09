7a1bd63d7a7147ee14f4f2221f4c18a01ea3e3d0
guycalledfrank <somemail256@gmail.com>
2018-03-09 17:53:12 +0000

    document addGroup

--- src/scene/batching.js
+++ src/scene/batching.js
@@ -28,7 +28,7 @@ pc.extend(pc, function () {
      * {@link pc.BatchManager#prepare} will split objects into local groups based on this size.
      * @property {Number} id Unique id. Can be assigned to model and element components.
      * @property {String} name Name of the group.
-     * @property {Number} [layers] Layer ID array. Default is [pc.LAYERID_WORLD].
+     * @property {Number} [layers] Layer ID array. Default is [pc.LAYERID_WORLD]. The whole batch group will belong to these layers. Layers of source models will be ignored.
      */
     var BatchGroup = function (id, name, dynamic, maxAabbSize, layers) {
         this.dynamic = dynamic;
@@ -147,6 +147,7 @@ pc.extend(pc, function () {
      * @param {Number} maxAabbSize Maximum size of any dimension of a bounding box around batched objects.
      * {@link pc.BatchManager#prepare} will split objects into local groups based on this size.
      * @param {Number} [id] Optional custom unique id for the group (will be generated automatically otherwise).
+     * @property {Number} [layers] Optional layer ID array. Default is [pc.LAYERID_WORLD]. The whole batch group will belong to these layers. Layers of source models will be ignored.
      * @returns {pc.BatchGroup} Group object.
      */
     BatchManager.prototype.addGroup = function(name, dynamic, maxAabbSize, id, layers) {
