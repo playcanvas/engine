0b5c2b76d2c99e9fea4015e9180ef7024c13d562
Mr F <somemail256@gmail.com>
2018-03-13 18:45:31 +0000

    Fix createBox generating garbage with different segments count in different dimensions (#1133)
    * fix createBox generating garbage with different segments count in different dimensions

--- src/scene/procedural.js
+++ src/scene/procedural.js
@@ -842,7 +842,7 @@ pc.createPlane = function (device, opts) {
     // (0,0)x---------x(1,0)
     //         width
     var vcounter = 0;
-    
+
     for (i = 0; i <= ws; i++) {
         for (j = 0; j <= ls; j++) {
             x = -he.x + 2.0 * he.x * i / ws;
@@ -945,6 +945,7 @@ pc.createBox = function (device, opts) {
     var uvs = [];
     var uvs1 = [];
     var indices = [];
+    var vcounter = 0;
 
     var generateFace = function (side, uSegments, vSegments) {
         var u, v;
@@ -979,9 +980,11 @@ pc.createBox = function (device, opts) {
                 uvs1.push(u, v);
 
                 if ((i < uSegments) && (j < vSegments)) {
-                    indices.push(offset + j + i * (uSegments + 1),       offset + j + (i + 1) * (uSegments + 1),     offset + j + i * (uSegments + 1) + 1);
-                    indices.push(offset + j + (i + 1) * (uSegments + 1), offset + j + (i + 1) * (uSegments + 1) + 1, offset + j + i * (uSegments + 1) + 1);
+                    indices.push(vcounter+vSegments+1, vcounter+1, vcounter);
+                    indices.push(vcounter+vSegments+1, vcounter+vSegments+2, vcounter+1);
                 }
+
+                vcounter++;
             }
         }
     };
