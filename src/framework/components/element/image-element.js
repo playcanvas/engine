e48a33db4b843d52c50d2f03d1405f9b82c166d6
Vaios Kalpias Ilias <vaios@playcanvas.com>
2018-03-28 12:00:31 -0700

    Hide image element mesh instance if trying to show sprite frame that doesn't exist

--- src/framework/components/element/image-element.js
+++ src/framework/components/element/image-element.js
@@ -244,13 +244,7 @@ pc.extend(pc, function () {
             // force update meshInstance aabb
             if (this._meshInstance) {
                 this._meshInstance._aabbVer = -1;
-                this._meshInstance.visible = !!mesh;
-                if (mesh) {
-                    this._meshInstance.mesh = mesh;
-                }
             }
-
-            if (! mesh) return;
 
             if (this.sprite && (this.sprite.renderMode === pc.SPRITE_RENDERMODE_SLICED || this.sprite.renderMode === pc.SPRITE_RENDERMODE_TILED)) {
 
@@ -802,18 +796,34 @@ pc.extend(pc, function () {
         },
         set: function (value) {
             this._spriteFrame = value;
-            var mesh = this._defaultMesh;
 
+            var nineSlice = false
+            var mesh = null;
+
+            // take mesh from sprite
             if (this._sprite && this._sprite.atlas) {
-                if (this._sprite.renderMode === pc.SPRITE_RENDERMODE_SLICED ||
-                    this._sprite.renderMode === pc.SPRITE_RENDERMODE_TILED) {
-
-                    mesh = this._sprite.meshes[this.spriteFrame];
-                }
+                mesh = this._sprite.meshes[this.spriteFrame];
+                nineSlice = this._sprite.renderMode === pc.SPRITE_RENDERMODE_SLICED || this._sprite.renderMode === pc.SPRITE_RENDERMODE_SLICED;
             }
 
-            this._mesh = mesh;
-            this._updateMesh(this._mesh);
+            // make mesh instance visible or not depending on whether the mesh exists
+            if (this._meshInstance) {
+                this._meshInstance.visible = !!mesh;
+                // reset aabb
+                this._meshInstance._aabbVer = -1;
+            }
+
+            // if we use 9 slicing then use that mesh otherwise keep using the default mesh
+            this._mesh = nineSlice ? mesh : this._defaultMesh;
+
+            // update mesh on mesh instance
+            if (this._meshInstance) {
+                this._meshInstance.mesh = this._mesh;
+            }
+
+            if (this._mesh) {
+                this._updateMesh(this._mesh);
+            }
         }
     });
 
