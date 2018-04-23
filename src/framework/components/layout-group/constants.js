f28b3a933d400df986bbb4f63adba3228c863856
Yay Hi <37333094+yayhi@users.noreply.github.com>
2018-04-23 13:47:45 +0100

    Move constants to dedicated file

---
+++ src/framework/components/layout-group/constants.js
@@ -0,0 +1,39 @@
+pc.extend(pc, {
+    /**
+     * @enum pc.ORIENTATION
+     * @name pc.ORIENTATION_HORIZONTAL
+     * @description Horizontal orientation.
+     */
+    ORIENTATION_HORIZONTAL: 0,
+    /**
+     * @enum pc.ORIENTATION
+     * @name pc.ORIENTATION_VERTICAL
+     * @description Vertical orientation.
+     */
+    ORIENTATION_VERTICAL: 1,
+
+    /**
+     * @enum pc.FITTING
+     * @name pc.FITTING_NONE
+     * @description Disable all fitting logic.
+     */
+    FITTING_NONE: 0,
+    /**
+     * @enum pc.FITTING
+     * @name pc.FITTING_STRETCH
+     * @description Stretch child elements to fit the parent container
+     */
+    FITTING_STRETCH: 1,
+    /**
+     * @enum pc.FITTING
+     * @name pc.FITTING_SHRINK
+     * @description Shrink child elements to fit the parent container
+     */
+    FITTING_SHRINK: 2,
+    /**
+     * @enum pc.FITTING
+     * @name pc.FITTING_BOTH
+     * @description Apply both STRETCH and SHRINK fitting logic where applicable.
+     */
+    FITTING_BOTH: 3
+});
--- src/scene/scene.js
+++ src/scene/scene.js
@@ -271,44 +271,6 @@
         VIEW_RIGHT: 2,
 
         /**
-         * @enum pc.ORIENTATION
-         * @name pc.ORIENTATION_HORIZONTAL
-         * @description Horizontal orientation.
-         */
-        ORIENTATION_HORIZONTAL: 0,
-        /**
-         * @enum pc.ORIENTATION
-         * @name pc.ORIENTATION_VERTICAL
-         * @description Vertical orientation.
-         */
-        ORIENTATION_VERTICAL: 1,
-
-        /**
-         * @enum pc.FITTING
-         * @name pc.FITTING_NONE
-         * @description Disable all fitting logic.
-         */
-        FITTING_NONE: 0,
-        /**
-         * @enum pc.FITTING
-         * @name pc.FITTING_STRETCH
-         * @description Stretch child elements to fit the parent container
-         */
-        FITTING_STRETCH: 1,
-        /**
-         * @enum pc.FITTING
-         * @name pc.FITTING_SHRINK
-         * @description Shrink child elements to fit the parent container
-         */
-        FITTING_SHRINK: 2,
-        /**
-         * @enum pc.FITTING
-         * @name pc.FITTING_BOTH
-         * @description Apply both STRETCH and SHRINK fitting logic where applicable.
-         */
-        FITTING_BOTH: 3,
-
-        /**
          * @enum pc.SORTMODE
          * @name pc.SORTMODE_NONE
          * @description No sorting is applied. Mesh instances are rendered in the same order they were added to a layer.
