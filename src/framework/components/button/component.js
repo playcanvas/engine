53ea5b21461d925e6fa7c204c2eadf5c8a27d8ba
Yay Hi <37333094+yayhi@users.noreply.github.com>
2018-05-11 11:27:41 +0100

    Update tint types in JSDoc

--- src/framework/components/button/component.js
+++ src/framework/components/button/component.js
@@ -36,9 +36,9 @@ pc.extend(pc, function () {
      * @property {pc.Entity} imageEntity A reference to the entity to be used as the button background. The entity must have an ImageElement component.'
      * @property {pc.Vec4} hitPadding Padding to be used in hit-test calculations. Can be used to expand the bounding box so that the button is easier to tap.
      * @property {pc.BUTTON_TRANSITION_MODE} transitionMode Controls how the button responds when the user hovers over it/presses it.
-     * @property {pc.Vec4} hoverTint Color to be used on the button image when the user hovers over it.
-     * @property {pc.Vec4} pressedTint Color to be used on the button image when the user presses it.
-     * @property {pc.Vec4} inactiveTint Color to be used on the button image when the button is not interactive.
+     * @property {pc.Color} hoverTint Color to be used on the button image when the user hovers over it.
+     * @property {pc.Color} pressedTint Color to be used on the button image when the user presses it.
+     * @property {pc.Color} inactiveTint Color to be used on the button image when the button is not interactive.
      * @property {Number} fadeDuration Duration to be used when fading between tints, in milliseconds.
      * @property {pc.Asset} hoverSpriteAsset Sprite to be used as the button image when the user hovers over it.
      * @property {Number} hoverSpriteFrame Frame to be used from the hover sprite.
