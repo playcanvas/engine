1d36c362cb3334770c70f024113f97a6e2542837
Yay Hi <37333094+yayhi@users.noreply.github.com>
2018-05-24 13:28:31 -0700

    Move dragging related logic into a reusable helper

--- src/framework/components/scrollbar/component.js
+++ src/framework/components/scrollbar/component.js
@@ -1,12 +1,4 @@
 pc.extend(pc, function () {
-    var _inputScreenPosition = new pc.Vec2();
-    var _inputWorldPosition = new pc.Vec3();
-    var _rayOrigin = new pc.Vec3();
-    var _rayDirection = new pc.Vec3();
-    var _planeOrigin = new pc.Vec3();
-    var _planeNormal = new pc.Vec3();
-    var _entityRotation = new pc.Quat();
-
     /**
      * @component
      * @name pc.ScrollbarComponent
@@ -24,15 +16,9 @@ pc.extend(pc, function () {
         this._app = system.app;
 
         this._handleReference = new pc.EntityReference(this, 'handleEntity', {
-            'button#gain': this._onHandleButtonGain,
-            'button#press': this._onHandleButtonPress
+            'element#gain': this._onHandleElementGain,
+            'element#lose': this._onHandleElementLose
         });
-
-        this._dragScale = new pc.Vec3();
-        this._dragStartMousePosition = new pc.Vec3();
-        this._dragStartHandlePosition = new pc.Vec3();
-        this._deltaMousePosition = new pc.Vec3();
-        this._deltaHandlePosition = new pc.Vec3();
 
         this._toggleLifecycleListeners('on');
     };
@@ -40,130 +26,27 @@ pc.extend(pc, function () {
 
     pc.extend(ScrollbarComponent.prototype, {
         _toggleLifecycleListeners: function(onOrOff) {
-            if (!this._handleRelease) {
-                this._handleRelease = this._onRelease.bind(this);
-            }
-
-            var addOrRemoveEventListener = onOrOff === 'on' ? 'addEventListener' : 'removeEventListener';
-
-            // Note that we handle release events directly on the window object, rather than
-            // on app.mouse or app.touch. This is in order to correctly handle cases where the
-            // user releases the mouse/touch outside of the window.
-            this._app.mouse[onOrOff]('mousemove', this._onMove, this);
-            window[addOrRemoveEventListener]('mouseup', this._handleRelease, false);
-
-            if ('ontouchstart' in window) {
-                this._app.touch[onOrOff]('touchmove', this._onMove, this);
-                window[addOrRemoveEventListener]('touchend', this._handleRelease, false);
-                window[addOrRemoveEventListener]('touchcancel', this._handleRelease, false);
-            }
-
             this[onOrOff]('set_value', this._onSetValue, this);
             this[onOrOff]('set_handleSize', this._onSetHandleSize, this);
 
             // TODO Handle scrollwheel events
         },
 
-        _onHandleButtonGain: function() {
+        _onHandleElementGain: function() {
+            this._destroyDragHelper();
+            this._handleDragHelper = new pc.ElementDragHelper(this._handleReference.entity.element, this._getAxis());
+            this._handleDragHelper.on('drag', this._onHandleDrag, this);
+
             this._updateHandlePositionAndSize();
         },
 
-        _onHandleButtonPress: function(event) {
-            if (this._handleReference.entity) {
-                this._isDragging = true;
-
-                this._calculateDragScale();
-                this._dragCamera = event.camera;
-                this._dragStartMousePosition.copy(this._screenToLocal(event));
-                this._dragStartHandlePosition.copy(this._handleReference.entity.getLocalPosition());
-            }
+        _onHandleElementLose: function() {
+            this._destroyDragHelper();
         },
 
-        _screenToLocal: function(event) {
-            this._determineInputPosition(event);
-            this._chooseRayOriginAndDirection();
-
-            _planeOrigin.copy(this.entity.getPosition());
-            _planeNormal.copy(this.entity.forward).scale(-1);
-
-            var denominator = _planeNormal.dot(_rayDirection);
-
-            // If the ray and plane are not parallel
-            if (Math.abs(denominator) > 0) {
-                var rayOriginToPlaneOrigin = _planeOrigin.sub(_rayOrigin);
-                var collisionDistance = rayOriginToPlaneOrigin.dot(_planeNormal) / denominator;
-                var position = _rayOrigin.add(_rayDirection.scale(collisionDistance));
-
-                _entityRotation.copy(this.entity.getRotation()).invert().transformVector(position, position);
-
-                position.mul(this._dragScale);
-
-                return position;
-            }
-
-            return null;
-        },
-
-        _determineInputPosition: function(event) {
-            if (typeof event.x !== 'undefined' && typeof event.y !== 'undefined') {
-                _inputScreenPosition.x = event.x;
-                _inputScreenPosition.y = event.y;
-            } else if (event.changedTouches) {
-                _inputScreenPosition.x = event.changedTouches[0].x;
-                _inputScreenPosition.y = event.changedTouches[0].y;
-            } else {
-                console.warn('Could not determine position from input event');
-            }
-        },
-
-        _chooseRayOriginAndDirection: function() {
-            if (this.entity.element.screen && this.entity.element.screen.screen.screenSpace) {
-                _rayOrigin.set(_inputScreenPosition.x, -_inputScreenPosition.y, 0);
-                _rayDirection.set(0, 0, -1);
-            } else {
-                _inputWorldPosition.copy(this._dragCamera.screenToWorld(_inputScreenPosition.x, _inputScreenPosition.y, 1));
-                _rayOrigin.copy(this._dragCamera.entity.getPosition());
-                _rayDirection.copy(_inputWorldPosition).sub(_rayOrigin).normalize();
-            }
-        },
-
-        _calculateDragScale: function() {
-            var current = this.entity;
-            var screen = this.entity.element.screen && this.entity.element.screen.screen;
-            var isWithin2DScreen = screen && screen.screenSpace;
-            var screenScale = isWithin2DScreen ? screen.scale : 1;
-            var dragScale = this._dragScale;
-
-            dragScale.set(screenScale, screenScale, screenScale);
-
-            while (current) {
-                dragScale.mul(current.getLocalScale());
-                current = current.parent;
-
-                if (isWithin2DScreen && current.screen) {
-                    break;
-                }
-            }
-
-            dragScale.data[0] = 1 / dragScale.data[0];
-            dragScale.data[1] = 1 / dragScale.data[1];
-            dragScale.data[2] = 1 / dragScale.data[2];
-        },
-
-        _onRelease: function() {
-            this._isDragging = false;
-        },
-
-        _onMove: function(event) {
-            if (this._handleReference.entity && this._isDragging && this.enabled && this.entity.enabled) {
-                var currentMousePosition = this._screenToLocal(event);
-
-                if (this._dragStartMousePosition && currentMousePosition) {
-                    this._deltaMousePosition.copy(currentMousePosition).sub(this._dragStartMousePosition);
-                    this._deltaHandlePosition.copy(this._dragStartHandlePosition).add(this._deltaMousePosition);
-
-                    this.value = this._handlePositionToScrollValue(this._deltaHandlePosition[this._getAxis()]);
-                }
+        _onHandleDrag: function(position) {
+            if (this._handleReference.entity && this.enabled && this.entity.enabled) {
+                this.value = this._handlePositionToScrollValue(position[this._getAxis()]);
             }
         },
 
@@ -171,7 +54,7 @@ pc.extend(pc, function () {
             if (Math.abs(newValue - oldValue) > 1e-3) {
                 this.data.value = pc.math.clamp(newValue, 0, 1);
                 this._updateHandlePositionAndSize();
-                this.fire('set:value', newValue);
+                this.fire('set:value', this.data.value);
             }
         },
 
@@ -198,7 +81,7 @@ pc.extend(pc, function () {
         },
 
         _handlePositionToScrollValue: function(handlePosition) {
-            return pc.math.clamp(handlePosition * this._getSign() / this._getUsableTrackLength(), 0, 1);
+            return handlePosition * this._getSign() / this._getUsableTrackLength();
         },
 
         _scrollValueToHandlePosition: function(value) {
@@ -233,7 +116,14 @@ pc.extend(pc, function () {
             return this.orientation === pc.ORIENTATION_HORIZONTAL ? 'width' : 'height';
         },
 
+        _destroyDragHelper: function () {
+            if (this._handleDragHelper) {
+                this._handleDragHelper.destroy();
+            }
+        },
+
         onRemove: function () {
+            this._destroyDragHelper();
             this._toggleLifecycleListeners('off');
         }
     });
