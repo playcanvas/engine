d6d1959694f58205391a4073e133d4c54ca789d1
Yay Hi <37333094+yayhi@users.noreply.github.com>
2018-05-30 17:01:38 +0100

    Fix typo

--- src/framework/utils/entity-reference.js
+++ src/framework/utils/entity-reference.js
@@ -11,7 +11,7 @@ pc.extend(pc, function () {
      * <h2>Usage Scenario</h2>
      *
      * Imagine that you're creating a Checkbox component, which has a reference to an entity representing
-     * the checkmark/tickmark that is rendered in the Checkbox. The reference is modeled as an entity guid
+     * the checkmark/tickmark that is rendered in the Checkbox. The reference is modelled as an entity guid
      * property on the Checkbox component, called simply 'checkmark'. We have to implement a basic piece of
      * functionality whereby when the 'checkmark' entity reference is set, the Checkbox component must toggle
      * the tint of an ImageElementComponent present on the checkmark entity to indicate whether the Checkbox
