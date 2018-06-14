15089e23223e6f61b691a5f30522be3d386aff96
Yay Hi <37333094+yayhi@users.noreply.github.com>
2018-06-14 01:02:31 +0100

    Some additional tests for ElementDragHelper (#1236)

--- tests/framework/components/element/test_elementdraghelper.js
+++ tests/framework/components/element/test_elementdraghelper.js
@@ -43,16 +43,22 @@
             this.dragEndHandler = sinon.stub();
             this.dragMoveHandler = sinon.stub();
 
-            this.dragHelper = new pc.ElementDragHelper(this.element);
-            this.dragHelper.on("drag:start", this.dragStartHandler);
-            this.dragHelper.on("drag:end", this.dragEndHandler);
-            this.dragHelper.on("drag:move", this.dragMoveHandler);
+            this.createDragHelper(null);
 
             var cameraEntity = new pc.Entity("camera", this.app);
             cameraEntity.setPosition(new pc.Vec3(0, 0, 100));
             this.camera = cameraEntity.addComponent("camera", {});
 
-            this.app.root.addChild(this.entity);
+            this.parent = new pc.Entity("parent", this.app);
+            this.parent.addChild(this.entity);
+            this.app.root.addChild(this.parent);
+        },
+
+        createDragHelper: function(axis) {
+            this.dragHelper = new pc.ElementDragHelper(this.element, axis);
+            this.dragHelper.on("drag:start", this.dragStartHandler);
+            this.dragHelper.on("drag:end", this.dragEndHandler);
+            this.dragHelper.on("drag:move", this.dragMoveHandler);
         },
 
         teardown: function () {
@@ -194,7 +200,80 @@
         strictEqual(this.dragHelper.isDragging, false);
     });
 
-    // TODO Tests for screen space -> element space transform calculations
-    // TODO Tests for 2D screen vs 3D screen calculations
-    // TODO Tests for axis constraining
+    var defaultXDelta = 4.90;
+    var defaultYDelta = -2.07;
+
+    function runTransformTest(expectedXDelta, expectedYDelta) {
+        this.element.fire("mousedown", {
+            x: 50,
+            y: 50,
+            camera: this.camera
+        });
+
+        this.app.mouse.fire("mousemove", {
+            x: 60,
+            y: 60
+        });
+
+        strictEqual(this.dragMoveHandler.callCount, 1);
+        QUnit.close(this.dragMoveHandler.getCall(0).args[0].x, expectedXDelta, 0.01);
+        QUnit.close(this.dragMoveHandler.getCall(0).args[0].y, expectedYDelta, 0.01);
+    }
+
+    test("includes ancestral rotation in coordinate conversion", function () {
+        var fourtyFiveDegreesAboutZ = new pc.Quat();
+        fourtyFiveDegreesAboutZ.setFromAxisAngle(new pc.Vec3(0, 0, 1), 45);
+
+        this.parent.setLocalRotation(fourtyFiveDegreesAboutZ);
+        this.entity.setLocalRotation(fourtyFiveDegreesAboutZ);
+
+        // Note that x and y are swapped here because we've rotated 90 degrees about the Z axis in total
+        runTransformTest.call(this, defaultYDelta, -defaultXDelta);
+    });
+
+    test("includes ancestral scale in coordinate conversion", function () {
+        var twoXandFourY = new pc.Vec3(2, 4, 1);
+
+        this.parent.setLocalScale(twoXandFourY);
+        this.entity.setLocalScale(twoXandFourY);
+
+        runTransformTest.call(this, defaultXDelta / 2, defaultYDelta / 4);
+    });
+
+    test("includes camera rotation in coordinate conversion", function () {
+        var ninetyDegreesAboutZ = new pc.Quat();
+        ninetyDegreesAboutZ.setFromAxisAngle(new pc.Vec3(0, 0, 1), 90);
+        this.camera.entity.setLocalRotation(ninetyDegreesAboutZ);
+
+        // Note that x and y are swapped here because we've rotated the camera
+        runTransformTest.call(this, -defaultYDelta, defaultXDelta);
+    });
+
+    test("includes screen scale in coordinate conversion and disables perspective when using screen space", function () {
+        this.app.root.removeChild(this.parent);
+
+        var screen = new pc.Entity("screen", this.app);
+        screen.addComponent("screen", { screenSpace: true });
+        screen.addChild(this.parent);
+        screen.screen.scale = 0.5;
+
+        this.app.root.addChild(screen);
+        this.entity.element.screen = screen;
+
+        runTransformTest.call(this, 20, -20);
+    });
+
+    test("allows dragging to be constrained to the X axis", function () {
+        this.dragHelper.destroy();
+        this.createDragHelper("x");
+
+        runTransformTest.call(this, defaultXDelta, 0);
+    });
+
+    test("allows dragging to be constrained to the Y axis", function () {
+        this.dragHelper.destroy();
+        this.createDragHelper("y");
+
+        runTransformTest.call(this, 0, defaultYDelta);
+    });
 })();
