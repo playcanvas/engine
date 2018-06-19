ee40d98f4d49563014842766879be611dcbada16
sethgillett <30958915+sethgillett@users.noreply.github.com>
2018-06-18 22:51:54 -0400

    Clarified units of time for sound instance

--- src/sound/instance.js
+++ src/sound/instance.js
@@ -24,8 +24,8 @@ Object.assign(pc, function () {
         * @param {Number} [options.volume=1] The playback volume, between 0 and 1.
         * @param {Number} [options.pitch=1] The relative pitch, default of 1, plays at normal pitch.
         * @param {Boolean} [options.loop=false] Whether the sound should loop when it reaches the end or not.
-        * @param {Number} [options.startTime=0] The time from which the playback will start. Default is 0 to start at the beginning.
-        * @param {Number} [options.duration=null] The total time after the startTime when playback will stop or restart if loop is true.
+        * @param {Number} [options.startTime=0] The time from which the playback will start in seconds. Default is 0 to start at the beginning.
+        * @param {Number} [options.duration=null] The total time after the startTime in milliseconds when playback will stop or restart if loop is true.
         * @param {Function} [options.onPlay=null] Function called when the instance starts playing.
         * @param {Function} [options.onPause=null] Function called when the instance is paused.
         * @param {Function} [options.onResume=null] Function called when the instance is resumed.
