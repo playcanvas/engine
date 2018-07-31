af8003fd79f686c8e96ace9dacef26a4f779529c
David Evans <dave@playcanvas.com>
2018-07-31 14:29:30 -0700

    ESLint fixes (#1283)
    * Correct worldTransform in oriented bounding box
    * Doc fix in pick.js
    * Doc fixes for script.js
    * Remove unused cullTime variables
    * Ignore specific remaining lint issues, to be fixed at a future date...

--- src/core/events.js
+++ src/core/events.js
@@ -173,6 +173,10 @@ pc.events = {
             callbacks = this._callbacks[name].slice();
         }
 
+        // TODO: What does callbacks do here?
+        // In particular this condition check looks wrong: (i < (callbacks || this._callbackActive[name]).length)
+        // Because callbacks is not an integer
+        // eslint-disable-next-line no-unmodified-loop-condition
         for (var i = 0; (callbacks || this._callbackActive[name]) && (i < (callbacks || this._callbackActive[name]).length); i++) {
             var evt = (callbacks || this._callbackActive[name])[i];
             evt.callback.call(evt.scope, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
