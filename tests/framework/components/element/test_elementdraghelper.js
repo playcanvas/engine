b212738786c96fb1bef2de6d5f00e3a59b0699fa
Yay Hi <37333094+yayhi@users.noreply.github.com>
2018-08-21 19:31:03 +0100

    Use device pixel ratio in element drag helper (#1309)
    * Take devicePixelRatio into account in ElementDragHelper

--- tests/framework/components/element/test_elementdraghelper.js
+++ tests/framework/components/element/test_elementdraghelper.js
@@ -227,8 +227,8 @@ describe("pc.ElementDragHelper", function() {
         });
 
         expect(dragMoveHandler.callCount).to.equal(1);
-        expect(dragMoveHandler.getCall(0).args[0].x).to.be.closeTo(expectedXDelta, 0.01);
-        expect(dragMoveHandler.getCall(0).args[0].y).to.be.closeTo(expectedYDelta, 0.01);
+        expect(dragMoveHandler.getCall(0).args[0].x).to.be.closeTo(expectedXDelta, 0.02);
+        expect(dragMoveHandler.getCall(0).args[0].y).to.be.closeTo(expectedYDelta, 0.02);
     }
 
     it("includes ancestral rotation in coordinate conversion", function () {
@@ -286,5 +286,10 @@ describe("pc.ElementDragHelper", function() {
         createDragHelper("y");
 
         runTransformTest.call(this, 0, defaultYDelta);
+    });
+
+    it("takes device pixel ratio into account", function () {
+        app.graphicsDevice.maxPixelRatio = 2;
+        runTransformTest.call(this, defaultXDelta * 2, defaultYDelta * 2);
     });
 });
