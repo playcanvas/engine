4c13374afe5a95bfbb9dbc406f8019bb2b77f4e9
David Evans <dave@playcanvas.com>
2018-09-11 16:02:18 +1000

    Use legacy dds loader for textures

--- src/resources/texture.js
+++ src/resources/texture.js
@@ -120,6 +120,7 @@ Object.assign(pc, function () {
             var ext = pc.path.getExtension(url).toLowerCase();
             var format = null;
 
+
             // Every browser seems to pass data as an Image type. For some reason, the XDK
             // passes an HTMLImageElement. TODO: figure out why!
             // DDS textures are ArrayBuffers
@@ -137,49 +138,57 @@ Object.assign(pc, function () {
                 });
                 texture.setSource(img);
             } else if (data instanceof ArrayBuffer) { // Container format
-                var textureData;
+                var LEGACY = true;
 
-                switch (ext) {
-                    case '.dds':
-                        textureData = new pc.DdsParser(data);
-                        break;
-                    case '.ktx':
-                        textureData = new pc.KtxParser(data);
-                        break;
-                    case '.pvr':
-                        console.warn('PVR container not supported.');
-                        break;
-                }
+                if (LEGACY) {
+                    texture = _legacyDdsLoader(url, data, this._device);
+                } else {
+                    var textureData;
 
-                if (!textureData) {
-                    // #ifdef DEBUG
-                    console.warn("This DDS or KTX pixel format is currently unsupported. Empty texture will be created instead.");
-                    // #endif
-                    return new pc.Texture(this._device, {
-                        width: 4,
-                        height: 4,
-                        format: pc.PIXELFORMAT_R8_G8_B8
+                    switch (ext) {
+                        case '.dds':
+                            textureData = new pc.DdsParser(data);
+                            break;
+                        case '.ktx':
+                            textureData = new pc.KtxParser(data);
+                            break;
+                        case '.pvr':
+                            console.warn('PVR container not supported.');
+                            break;
+                    }
+
+                    if (!textureData) {
+                        // #ifdef DEBUG
+                        console.warn("This DDS or KTX pixel format is currently unsupported. Empty texture will be created instead.");
+                        // #endif
+                        return new pc.Texture(this._device, {
+                            width: 4,
+                            height: 4,
+                            format: pc.PIXELFORMAT_R8_G8_B8
+                        });
+                    }
+
+                    texture = new pc.Texture(this._device, {
+                        // #ifdef PROFILER
+                        profilerHint: pc.TEXHINT_ASSET,
+                        // #endif
+                        addressU: textureData.cubemap ? pc.ADDRESS_CLAMP_TO_EDGE : pc.ADDRESS_REPEAT,
+                        addressV: textureData.cubemap ? pc.ADDRESS_CLAMP_TO_EDGE : pc.ADDRESS_REPEAT,
+                        width: textureData.width,
+                        height: textureData.height,
+                        format: textureData.format,
+                        cubemap: textureData.cubemap,
+                        levels: textureData.levels
                     });
+
+                    texture.upload();
                 }
 
-                texture = new pc.Texture(this._device, {
-                    // #ifdef PROFILER
-                    profilerHint: pc.TEXHINT_ASSET,
-                    // #endif
-                    addressU: textureData.cubemap ? pc.ADDRESS_CLAMP_TO_EDGE : pc.ADDRESS_REPEAT,
-                    addressV: textureData.cubemap ? pc.ADDRESS_CLAMP_TO_EDGE : pc.ADDRESS_REPEAT,
-                    width: textureData.width,
-                    height: textureData.height,
-                    format: textureData.format,
-                    cubemap: textureData.cubemap,
-                    levels: textureData.levels
-                });
-
-                texture.upload();
             }
 
             return texture;
         },
+
 
         patch: function (asset, assets) {
             var texture = asset.resource;
@@ -214,6 +223,153 @@ Object.assign(pc, function () {
         }
     });
 
+    var _legacyDdsLoader = function (url, data, graphicsDevice) {
+
+        var ext = pc.path.getExtension(url).toLowerCase();
+
+        if (ext === ".crn") {
+            // Copy loaded file into Emscripten-managed memory
+            var srcSize = data.byteLength;
+            var bytes = new Uint8Array(data);
+            var src = Module._malloc(srcSize);
+            arrayBufferCopy(bytes, Module.HEAPU8, src, srcSize);
+
+            // Decompress CRN to DDS (minus the header)
+            var dst = Module._crn_decompress_get_data(src, srcSize);
+            var dstSize = Module._crn_decompress_get_size(src, srcSize);
+
+            data = Module.HEAPU8.buffer.slice(dst, dst + dstSize);
+        }
+
+        // DDS loading
+        var header = new Uint32Array(data, 0, 128 / 4);
+
+        var width = header[4];
+        var height = header[3];
+        var mips = Math.max(header[7], 1);
+        var isFourCc = header[20] === 4;
+        var fcc = header[21];
+        var bpp = header[22];
+        var isCubemap = header[28] === 65024; // TODO: check by bitflag
+
+        var FCC_DXT1 = 827611204; // DXT1
+        var FCC_DXT5 = 894720068; // DXT5
+        var FCC_FP32 = 116; // RGBA32f
+
+        // non standard
+        var FCC_ETC1 = 826496069;
+        var FCC_PVRTC_2BPP_RGB_1 = 825438800;
+        var FCC_PVRTC_2BPP_RGBA_1 = 825504336;
+        var FCC_PVRTC_4BPP_RGB_1 = 825439312;
+        var FCC_PVRTC_4BPP_RGBA_1 = 825504848;
+
+        var compressed = false;
+        var floating = false;
+        var etc1 = false;
+        var pvrtc2 = false;
+        var pvrtc4 = false;
+        if (isFourCc) {
+            if (fcc === FCC_DXT1) {
+                format = pc.PIXELFORMAT_DXT1;
+                compressed = true;
+            } else if (fcc === FCC_DXT5) {
+                format = pc.PIXELFORMAT_DXT5;
+                compressed = true;
+            } else if (fcc === FCC_FP32) {
+                format = pc.PIXELFORMAT_RGBA32F;
+                floating = true;
+            } else if (fcc === FCC_ETC1) {
+                format = pc.PIXELFORMAT_ETC1;
+                compressed = true;
+                etc1 = true;
+            } else if (fcc === FCC_PVRTC_2BPP_RGB_1 || fcc === FCC_PVRTC_2BPP_RGBA_1) {
+                format = fcc === FCC_PVRTC_2BPP_RGB_1 ? pc.PIXELFORMAT_PVRTC_2BPP_RGB_1 : pc.PIXELFORMAT_PVRTC_2BPP_RGBA_1;
+                compressed = true;
+                pvrtc2 = true;
+            } else if (fcc === FCC_PVRTC_4BPP_RGB_1 || fcc === FCC_PVRTC_4BPP_RGBA_1) {
+                format = fcc === FCC_PVRTC_4BPP_RGB_1 ? pc.PIXELFORMAT_PVRTC_4BPP_RGB_1 : pc.PIXELFORMAT_PVRTC_4BPP_RGBA_1;
+                compressed = true;
+                pvrtc4 = true;
+            }
+        } else {
+            if (bpp === 32) {
+                format = pc.PIXELFORMAT_R8_G8_B8_A8;
+            }
+        }
+
+        if (!format) {
+            // #ifdef DEBUG
+            console.error("This DDS pixel format is currently unsupported. Empty texture will be created instead.");
+            // #endif
+            texture = new pc.Texture(graphicsDevice, {
+                width: 4,
+                height: 4,
+                format: pc.PIXELFORMAT_R8_G8_B8
+            });
+            return texture;
+        }
+
+        var texOptions = {
+            // #ifdef PROFILER
+            profilerHint: pc.TEXHINT_ASSET,
+            // #endif
+            width: width,
+            height: height,
+            format: format,
+            cubemap: isCubemap
+        };
+        texture = new pc.Texture(graphicsDevice, texOptions);
+        if (isCubemap) {
+            texture.addressU = pc.ADDRESS_CLAMP_TO_EDGE;
+            texture.addressV = pc.ADDRESS_CLAMP_TO_EDGE;
+        }
+
+        var offset = 128;
+        var faces = isCubemap ? 6 : 1;
+        var mipSize;
+        var DXT_BLOCK_WIDTH = 4;
+        var DXT_BLOCK_HEIGHT = 4;
+        var blockSize = fcc === FCC_DXT1 ? 8 : 16;
+        var numBlocksAcross, numBlocksDown, numBlocks;
+        for (var face = 0; face < faces; face++) {
+            var mipWidth = width;
+            var mipHeight = height;
+            for (var i = 0; i < mips; i++) {
+                if (compressed) {
+                    if (etc1) {
+                        mipSize = Math.floor((mipWidth + 3) / 4) * Math.floor((mipHeight + 3) / 4) * 8;
+                    } else if (pvrtc2) {
+                        mipSize = Math.max(mipWidth, 16) * Math.max(mipHeight, 8) / 4;
+                    } else if (pvrtc4) {
+                        mipSize = Math.max(mipWidth, 8) * Math.max(mipHeight, 8) / 2;
+                    } else {
+                        numBlocksAcross = Math.floor((mipWidth + DXT_BLOCK_WIDTH - 1) / DXT_BLOCK_WIDTH);
+                        numBlocksDown = Math.floor((mipHeight + DXT_BLOCK_HEIGHT - 1) / DXT_BLOCK_HEIGHT);
+                        numBlocks = numBlocksAcross * numBlocksDown;
+                        mipSize = numBlocks * blockSize;
+                    }
+                } else {
+                    mipSize = mipWidth * mipHeight * 4;
+                }
+
+                var mipBuff = floating ? new Float32Array(data, offset, mipSize) : new Uint8Array(data, offset, mipSize);
+                if (!isCubemap) {
+                    texture._levels[i] = mipBuff;
+                } else {
+                    if (!texture._levels[i]) texture._levels[i] = [];
+                    texture._levels[i][face] = mipBuff;
+                }
+                offset += floating ? mipSize * 4 : mipSize;
+                mipWidth = Math.max(mipWidth * 0.5, 1);
+                mipHeight = Math.max(mipHeight * 0.5, 1);
+            }
+        }
+
+        texture.name = url;
+        texture.upload();
+
+        return texture;
+    };
     return {
         TextureHandler: TextureHandler
     };
