c302f2044b527141ce583d1966b0e6c86e588793
e-strokov <43282396+e-strokov@users.noreply.github.com>
2018-10-31 18:19:43 +0000

    Update only shaders that depends on lighting on COMPUPDATED_LIGHTS (#1421)

--- src/scene/forward-renderer.js
+++ src/scene/forward-renderer.js
@@ -2096,6 +2096,32 @@ Object.assign(pc, function () {
             // #endif
         },
 
+        updateLitShaders: function (drawCalls) {
+            // #ifdef PROFILER
+            var time = pc.now();
+            // #endif
+
+            for (var i = 0; i < drawCalls.length; i++) {
+                var drawCall = drawCalls[i];
+                if (drawCall.material !== undefined) {
+                    var mat = drawCall.material;
+                    if (mat.updateShader !== pc.Material.prototype.updateShader) {
+                        if (mat.useLighting === false || (mat.emitter && !mat.emitter.lighting)) {
+                            // skip unlit standard and particles materials
+                            continue;
+                        }
+                        mat.clearVariants();
+                        mat.shader = null;
+                    }
+                }
+            }
+
+            // #ifdef PROFILER
+            this.scene._stats.updateShadersTime += pc.now() - time;
+            // #endif
+        },
+
+
         beginFrame: function (comp) {
             var device = this.device;
             var scene = this.scene;
@@ -2112,6 +2138,11 @@ Object.assign(pc, function () {
             if (scene.updateShaders) {
                 this.updateShaders(meshInstances);
                 scene.updateShaders = false;
+                scene.updateLitShaders = false;
+                scene._shaderVersion++;
+            } else if (scene.updateLitShaders) {
+                this.updateLitShaders(meshInstances);
+                scene.updateLitShaders = false;
                 scene._shaderVersion++;
             }
 
@@ -2467,7 +2498,7 @@ Object.assign(pc, function () {
             // Update static layer data, if something's changed
             var updated = comp._update();
             if (updated & pc.COMPUPDATED_LIGHTS) {
-                this.scene.updateShaders = true;
+                this.scene.updateLitShaders = true;
             }
 
             // #ifdef PROFILER
