0a7b418feb178952ab2b6f7b195db7ad4613c001
stefnotch <stefnotch@users.noreply.github.com>
2018-11-03 14:09:58 +0100

    Fix #850

--- src/anim/skeleton.js
+++ src/anim/skeleton.js
@@ -114,7 +114,12 @@ Object.assign(pc, function () {
 
                 // Determine the interpolated keyframe for this animated node
                 interpKey = this._interpolatedKeyDict[nodeName];
-
+                if(interpKey === undefined) {
+                    // #ifdef DEBUG
+                    console.warn('Unknown skeleton node name: ' + nodeName);
+                    // #endif
+                    continue;
+                }
                 // If there's only a single key, just copy the key to the interpolated key...
                 foundKey = false;
                 if (keys.length !== 1) {
