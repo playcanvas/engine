d27a44b6d732c0f1bab94ef5126050db16a1d914
e-strokov <43282396+e-strokov@users.noreply.github.com>
2018-12-06 11:07:08 -0800

    ComponentSystem doesn't fire events, it directly calls subscribers (#1449)

--- src/framework/components/rigid-body/system.js
+++ src/framework/components/rigid-body/system.js
@@ -183,10 +183,10 @@ Object.assign(pc, function () {
                 // Lazily create temp vars
                 ammoRayStart = new Ammo.btVector3();
                 ammoRayEnd = new Ammo.btVector3();
-                pc.ComponentSystem.on('update', this.onUpdate, this);
+                pc.ComponentSystem.bind('update', this.onUpdate, this);
             } else {
                 // Unbind the update function if we haven't loaded Ammo by now
-                pc.ComponentSystem.off('update', this.onUpdate, this);
+                pc.ComponentSystem.unbind('update', this.onUpdate, this);
             }
         },
 
