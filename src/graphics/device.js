05a9fcf30155d67e8b949574af8cc8aa537f956c
issacclee <issacclee@live.cn>
2019-01-02 23:41:05 +0800

    Fix CubeMap Seams bug on A12 and A12X chips (#1465)
    A12 and A12X chips (late 2018 iPhones and iPads) had a regression in their WebGL implementation which removed a feature of CubeMaps seams filtering on the faces edges.
    So disabling the `unmaskedRenderer` forces engine to use manual seams path which avoids an issue.

--- src/graphics/device.js
+++ src/graphics/device.js
@@ -532,6 +532,11 @@ Object.assign(pc, function () {
         if (this.unmaskedRenderer === 'Apple A8 GPU') {
             this.forceCpuParticles = true;
         }
+        
+        // Workaround for A12 and A12X chips that removed smooth seams from CubeMaps
+        if (this.unmaskedRenderer === 'Apple A12 GPU' || this.unmaskedRenderer === 'Apple A12X GPU') {
+            this.useTexCubeLod = false;
+        }
 
         // Profiler stats
         this._drawCallsPerFrame = 0;
