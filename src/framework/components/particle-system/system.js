2a44a720270a6f6cdd8421ccf97aaef1fdb24498
Evgeny Strokov <evgenystrokov@gmail.com>
2019-02-19 15:59:47 +0000

    Particles single update.

--- src/framework/components/particle-system/system.js
+++ src/framework/components/particle-system/system.js
@@ -224,17 +224,10 @@ Object.assign(pc, function () {
 
                         if (!data.paused) {
                             emitter.simTime += dt;
-                            numSteps = 0;
                             if (emitter.simTime > emitter.fixedTimeStep) {
-                                numSteps = Math.floor(emitter.simTime / emitter.fixedTimeStep);
-                                emitter.simTime -= numSteps * emitter.fixedTimeStep;
-                            }
-                            if (numSteps) {
-                                numSteps = Math.min(numSteps, emitter.maxSubSteps);
-                                for (i = 0; i < numSteps; i++) {
-                                    emitter.addTime(emitter.fixedTimeStep);
-                                }
-                                stats._updatesPerFrame += numSteps;
+                                emitter.addTime(emitter.simTime);
+                                emitter.simTime = 0;
+                                stats._updatesPerFrame++;
                                 stats._frameTime += emitter._addTimeTime;
                                 emitter._addTimeTime = 0;
                             }
