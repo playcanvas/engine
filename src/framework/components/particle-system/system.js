58a3747192bc091f52873c18cf07620a5c9d4d42
Evgeny Strokov <evgenystrokov@gmail.com>
2019-02-20 15:17:13 +0000

    Revert "Particles single update."
    This reverts commit 2a44a720270a6f6cdd8421ccf97aaef1fdb24498.

--- src/framework/components/particle-system/system.js
+++ src/framework/components/particle-system/system.js
@@ -224,10 +224,17 @@ Object.assign(pc, function () {
 
                         if (!data.paused) {
                             emitter.simTime += dt;
+                            numSteps = 0;
                             if (emitter.simTime > emitter.fixedTimeStep) {
-                                emitter.addTime(emitter.simTime);
-                                emitter.simTime = 0;
-                                stats._updatesPerFrame++;
+                                numSteps = Math.floor(emitter.simTime / emitter.fixedTimeStep);
+                                emitter.simTime -= numSteps * emitter.fixedTimeStep;
+                            }
+                            if (numSteps) {
+                                numSteps = Math.min(numSteps, emitter.maxSubSteps);
+                                for (i = 0; i < numSteps; i++) {
+                                    emitter.addTime(emitter.fixedTimeStep);
+                                }
+                                stats._updatesPerFrame += numSteps;
                                 stats._frameTime += emitter._addTimeTime;
                                 emitter._addTimeTime = 0;
                             }
