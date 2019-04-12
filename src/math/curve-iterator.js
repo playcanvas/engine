49c62ab985996f0edd54311de8e44bf84460fe50
Donovan Hutchence <codebon77@gmail.com>
2019-04-11 18:27:56 -0700

    fixed lint errors, added iterator value() implmentation

--- src/math/curve-iterator.js
+++ src/math/curve-iterator.js
@@ -2,7 +2,7 @@ Object.assign(pc, (function () {
     'use strict';
 
     /**
-     * 
+     *
      * @constructor
      * @name pc.CurveIterator
      * @classdesc CurveIterator performs fast evaluation of a curve by caching
@@ -11,7 +11,7 @@ Object.assign(pc, (function () {
      * @param {Curve} curve The curve to iterator over
      * @param {Number} time The time to start iteration, defaults to 0.
      */
-    var CurveIterator = function(curve, time) {
+    var CurveIterator = function (curve, time) {
         this.curve = curve;
         this.time_ = time || 0;
         this.left = -Infinity;
@@ -30,8 +30,9 @@ Object.assign(pc, (function () {
          * @function
          * @name pc.CurveIterator#reset
          * @description Reset the iterator to the specified time
+         * @param {Number} time The new time for the iterator
          */
-        reset: function(time) {
+        reset: function (time) {
             var keys = this.curve.keys;
             var len = keys.length;
 
@@ -48,11 +49,11 @@ Object.assign(pc, (function () {
                     this.recip = 0;
                     this.p0 = this.p1 = keys[0][1];
                     this.m0 = this.m1 = 0;
-                } else if (this.time_ >= keys[len-1][0]) {
-                    this.left = keys[len-1][0];
+                } else if (this.time_ >= keys[len - 1][0]) {
+                    this.left = keys[len - 1][0];
                     this.right = Infinity;
                     this.recip = 0;
-                    this.p0 = this.p1 = keys[len-1][1];
+                    this.p0 = this.p1 = keys[len - 1][1];
                     this.m0 = this.m1 = 0;
                 } else {
                     // linear search for the left most key (TODO: for cases with more than
@@ -73,13 +74,13 @@ Object.assign(pc, (function () {
             }
         },
 
-        _isCurve: function() {
+        _isCurve: function () {
             return [pc.CURVE_CATMULL,
-                    pc.CURVE_CARDINAL,
-                    pc.CURVE_CARDINAL_STABLE].indexOf(this.curve.type) != -1;
+                pc.CURVE_CARDINAL,
+                pc.CURVE_CARDINAL_STABLE].indexOf(this.curve.type) != -1;
         },
 
-        _calcTangents: function(keys, index) {
+        _calcTangents: function (keys, index) {
             var a;
             var b = keys[index];
             var c = keys[index + 1];
@@ -87,33 +88,33 @@ Object.assign(pc, (function () {
 
             if (index === 0) {
                 a = [keys[0][0] + (keys[0][0] - keys[1][0]),
-                     keys[0][1] + (keys[0][1] - keys[1][1])];
+                    keys[0][1] + (keys[0][1] - keys[1][1])];
             } else {
                 a = keys[index - 1];
             }
 
             if (index == keys.length - 2) {
-                d = [keys[index+1][0] + (keys[index+1][0] - keys[index][0]),
-                     keys[index+1][1] + (keys[index+1][1] - keys[index][1])];
+                d = [keys[index + 1][0] + (keys[index + 1][0] - keys[index][0]),
+                    keys[index + 1][1] + (keys[index + 1][1] - keys[index][1])];
             } else {
                 d = keys[index + 2];
             }
 
             // tension
             var tension = this.curve.tension;
-            if (this.curve.type === pc.CURVE_CATMULL) {
-                tension = 0.5;
-            }
 
-            if (this.curve.type === pc.CURVE_CARDINAL_STABLE)
-            {
+            if (this.curve.type === pc.CURVE_CARDINAL_STABLE) {
                 // scale (due to non-uniform knot spacing)
-                var s1 = 2 * (c[0] - b[0]) / (c[0] - a[0]);
-                var s2 = 2 * (c[0] - b[0]) / (d[0] - b[0]);
+                var s1_ = 2 * (c[0] - b[0]) / (c[0] - a[0]);
+                var s2_ = 2 * (c[0] - b[0]) / (d[0] - b[0]);
 
-                this.m0 = tension * s1 * (c[1] - a[1]);
-                this.m1 = tension * s2 * (d[1] - b[1]);
+                this.m0 = tension * s1_ * (c[1] - a[1]);
+                this.m1 = tension * s2_ * (d[1] - b[1]);
             } else {
+                if (this.curve.type === pc.CURVE_CATMULL) {
+                    tension = 0.5;
+                }
+
                 // original cardinal tangent calc
                 var s1 = (c[0] - b[0]) / (b[0] - a[0]);
                 var s2 = (c[0] - b[0]) / (d[0] - c[0]);
@@ -130,8 +131,9 @@ Object.assign(pc, (function () {
          * @function
          * @name pc.CurveIterator#time
          * @description Get the current iterator time
+         * @returns {Number} The current time.
          */
-        time: function() {
+        time: function () {
             return this.time_;
         },
 
@@ -139,13 +141,15 @@ Object.assign(pc, (function () {
          * @function
          * @name pc.CurveIterator#evaluate
          * @description Evaluate the curve at the current time
+         * @returns {Number} The curve value at the current time
          */
-        evaluate: function() {
+        evaluate: function () {
             var curve = this.curve;
 
+            var result;
             if (curve.type === pc.CURVE_STEP) {
                 // step
-                return this.p0;
+                result = this.p0;
             } else {
                 // calculate normalized t
                 var t = 0;
@@ -155,24 +159,26 @@ Object.assign(pc, (function () {
 
                 if (curve.type === pc.CURVE_LINEAR) {
                     // linear
-                    return pc.math.lerp(this.p0, this.p1, t);
+                    result = pc.math.lerp(this.p0, this.p1, t);
                 } else if (curve.type === pc.CURVE_SMOOTHSTEP) {
                     // smoothstep
-                    return pc.math.lerp(this.p0, this.p1, t * t * (3 - 2 * t));
+                    result = pc.math.lerp(this.p0, this.p1, t * t * (3 - 2 * t));
                 } else {
                     // curve
-                    return curve._interpolateHermite(this.p0, this.p1, this.m0, this.m1, t);
+                    result = curve._interpolateHermite(this.p0, this.p1, this.m0, this.m1, t);
                 }
             }
+
+            return result;
         },
 
         /**
-         * 
          * @function
          * @name pc.CurveIterator#advance
          * @description Advance the iterator by the passed in amount
+         * @param {Number} amount The amount of time to advance the iterator
          */
-        advance: function(amount) {
+        advance: function (amount) {
             this.time_ += amount;
 
             if (amount >= 0) {
@@ -182,26 +188,46 @@ Object.assign(pc, (function () {
             } else if (this.time_ < this.left) {
                 this.reset(this.time_);
             }
+        },
+
+        /**
+         * @function
+         * @name pc.CurveIterator.value
+         * @description Evaluate the curve at the given time
+         * @param {Number} time The time to evaluate
+         * @returns {Number} The curve value at the given time
+         */
+        value: function (time) {
+            if (this.time < this.left || this.time > this.right) {
+                this.reset(this.time);
+            } else {
+                this.time = time;
+            }
+            return this.evaluate();
         }
     });
 
-    var DummyIterator = function(curve, time) {
+    var DummyIterator = function (curve, time) {
         this.curve = curve;
         this.time_ = time || 0;
     };
 
     Object.assign(DummyIterator.prototype, {
-        reset: function(time) {
+        reset: function (time) {
             this.time_ = time;
         },
-        time: function() {
+        time: function () {
             return this.time_;
         },
-        evaluate: function() {
+        evaluate: function () {
             return this.curve.value(this.time_);
         },
-        advance: function(amount) {
+        advance: function (amount) {
             this.time_ += amount;
+        },
+        value: function (time) {
+            reset(time);
+            return this.evaluate();
         }
     });
 
