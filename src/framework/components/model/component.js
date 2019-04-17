7ac2ad1a0f40ab2d461af004b63f15491797a318
Evgeny Strokov <evgenystrokov@gmail.com>
2019-04-17 13:02:59 +0100

    Batching supports async model load.

--- src/framework/components/model/component.js
+++ src/framework/components/model/component.js
@@ -266,7 +266,10 @@ Object.assign(pc, function () {
             }
 
             if (this._batchGroupId >= 0) {
-                app.batcher.insert(pc.BatchGroup.MODEL, this.batchGroupId, this.entity);
+                if (this._model)
+                    app.batcher.insert(pc.BatchGroup.MODEL, this.batchGroupId, this.entity);
+                else
+                    app.batcher.register(pc.BatchGroup.MODEL, this.batchGroupId, this.entity);
             }
         },
 
@@ -281,7 +284,10 @@ Object.assign(pc, function () {
             }
 
             if (this._batchGroupId >= 0) {
-                app.batcher.remove(pc.BatchGroup.MODEL, this.batchGroupId, this.entity);
+                if (this._model)
+                    app.batcher.remove(pc.BatchGroup.MODEL, this.batchGroupId, this.entity);
+                else
+                    app.batcher.unregister(pc.BatchGroup.MODEL, this.batchGroupId, this.entity);
             }
 
             if (this._model) {
@@ -608,8 +614,13 @@ Object.assign(pc, function () {
                 return;
             }
 
+            var batcher = this.system.app.batcher;
             if (this._model) {
-                this.removeModelFromLayers(this._model);
+                if (this._batchGroupId >= 0) {
+                    batcher.remove(pc.BatchGroup.MODEL, _batchGroupId, this.entity);
+                } else {
+                    this.removeModelFromLayers(this._model);
+                }
                 this.entity.removeChild(this._model.getGraph());
                 delete this._model._entity;
 
@@ -635,7 +646,11 @@ Object.assign(pc, function () {
                 this.entity.addChild(this._model.graph);
 
                 if (this.enabled && this.entity.enabled) {
-                    this.addModelToLayers();
+                    if (this._batchGroupId >= 0) {
+                        batcher.insert(pc.BatchGroup.MODEL, this._batchGroupId, this.entity);
+                    } else {
+                        this.addModelToLayers();
+                    }
                 }
 
                 // Store the entity that owns this model
@@ -838,10 +853,16 @@ Object.assign(pc, function () {
 
             var batcher = this.system.app.batcher;
             if (this.entity.enabled && this._batchGroupId >= 0) {
-                batcher.remove(pc.BatchGroup.MODEL, this.batchGroupId, this.entity);
+                if (this._model)
+                    batcher.remove(pc.BatchGroup.MODEL, this.batchGroupId, this.entity);
+                else
+                    batcher.unregister(pc.BatchGroup.MODEL, this.batchGroupId, this.entity);
             }
             if (this.entity.enabled && value >= 0) {
-                batcher.insert(pc.BatchGroup.MODEL, value, this.entity);
+                if (this._model)
+                    batcher.insert(pc.BatchGroup.MODEL, value, this.entity);
+                else
+                    batcher.register(pc.BatchGroup.MODEL, value, this.entity);
             }
 
             if (value < 0 && this._batchGroupId >= 0 && this.enabled && this.entity.enabled) {
--- src/scene/batching.js
+++ src/scene/batching.js
@@ -51,6 +51,7 @@ Object.assign(pc, function () {
         this.name = name;
         this.layers = layers === undefined ? [pc.LAYERID_WORLD] : layers;
         this._ui = false;
+        this._deferred = 0; // async models loading counter
         this._obj = {
             model: [],
             element: [],
@@ -315,8 +316,15 @@ Object.assign(pc, function () {
         if (group) {
             if (group._obj[type].indexOf(node) < 0) {
                 group._obj[type].push(node);
-                this.markGroupDirty(groupId);
+            } else {
+                group._deferred--;
+                // #ifdef DEBUG
+                if (group._deferred < 0)
+                    console.error('Invalid batch' + groupId + ' async loading counter');
+                // #endif
             }
+            if (group._deferred == 0)
+                this.markGroupDirty(groupId);
         } else {
             // #ifdef DEBUG
             console.warn('Invalid batch ' + groupId + ' insertion');
@@ -330,11 +338,51 @@ Object.assign(pc, function () {
             var idx = group._obj[type].indexOf(node);
             if (idx >= 0) {
                 group._obj[type].splice(idx, 1);
-                this.markGroupDirty(groupId);
+                if (group._deferred == 0)
+                    this.markGroupDirty(groupId);
             }
         } else {
             // #ifdef DEBUG
             console.warn('Invalid batch ' + groupId + ' insertion');
+            // #endif
+        }
+    };
+
+    BatchManager.prototype.register = function (type, groupId, node) {
+        var group = this._batchGroups[groupId];
+        if (group) {
+            if (group._obj[type].indexOf(node) < 0) {
+                group._deferred++;
+                group._obj[type].push(node);
+                var idx = this._dirtyGroups.indexOf(groupId);
+                if (idx >= 0) {
+                    this._dirtyGroups.splice(idx, 1);
+                }
+            }
+        } else {
+            // #ifdef DEBUG
+            console.warn('Invalid batch ' + groupId + ' register');
+            // #endif
+        }
+    };
+
+    BatchManager.prototype.unregister = function (type, groupId, node) {
+        var group = this._batchGroups[groupId];
+        if (group) {
+            var idx = group._obj[type].indexOf(node);
+            if (idx >= 0) {
+                group._obj[type].splice(idx, 1);
+                group._deferred--;
+                // #ifdef DEBUG
+                if (group._deferred < 0)
+                    console.error('Invalid batch' + groupId + ' async loading counter');
+                // #endif
+            }
+            if (group._deferred == 0)
+                this.markGroupDirty(groupId);
+        } else {
+            // #ifdef DEBUG
+            console.warn('Invalid batch ' + groupId + ' unregister');
             // #endif
         }
     };
