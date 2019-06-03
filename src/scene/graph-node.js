4064cbde8d0a65dbb927f7fabb5f1b0d2024cf00
Will Eastcott <will@playcanvas.com>
2019-06-03 16:54:49 -0700

    Revent hierarchy sync optimization.

--- src/scene/graph-node.js
+++ src/scene/graph-node.js
@@ -35,11 +35,6 @@ Object.assign(pc, function () {
         this._dirtyLocal = false;
         this._aabbVer = 0;
 
-        // _frozen flag marks the node to ignore hierarchy sync etirely (including children nodes)
-        // engine code automatically freezes and unfreezes objects whenever required
-        // segrigating dynamic and stationary nodes into subhierarchies allows to reduce sync time significantly
-        this._frozen = false;
-
         this.worldTransform = new pc.Mat4();
         this._dirtyWorld = false;
 
@@ -916,27 +911,12 @@ Object.assign(pc, function () {
             }
         },
 
-        _unfreezeParentToRoot: function () {
-            var p = this._parent;
-            while (p) {
-                p._frozen = false;
-                p = p._parent;
-            }
-        },
-
         _dirtifyWorld: function () {
-            if (!this._dirtyWorld)
-                this._unfreezeParentToRoot();
-            this._dirtifyWorldInternal();
-        },
-
-        _dirtifyWorldInternal: function () {
             if (!this._dirtyWorld) {
-                this._frozen = false;
                 this._dirtyWorld = true;
                 for (var i = 0; i < this._children.length; i++) {
                     if (!this._children[i]._dirtyWorld)
-                        this._children[i]._dirtifyWorldInternal();
+                        this._children[i]._dirtifyWorld();
                 }
             }
             this._dirtyNormal = true;
@@ -1144,9 +1124,6 @@ Object.assign(pc, function () {
 
             // The child (plus subhierarchy) will need world transforms to be recalculated
             node._dirtifyWorld();
-            // node might be already marked as dirty, in that case the whole chain stays frozen, so let's enforce unfreeze
-            if (this._frozen)
-                node._unfreezeParentToRoot();
 
             // alert an entity that it has been inserted
             if (node.fire) node.fire('insert', this);
@@ -1341,10 +1318,6 @@ Object.assign(pc, function () {
         syncHierarchy: function () {
             if (!this._enabled)
                 return;
-
-            if (this._frozen)
-                return;
-            this._frozen = true;
 
             if (this._dirtyLocal || this._dirtyWorld) {
                 this._sync();
