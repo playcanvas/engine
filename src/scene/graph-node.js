7a590f6786acd06e9b283bc819a721a5efeec27b
Evgeny Strokov <evgenystrokov@gmail.com>
2019-06-03 11:46:18 +0100

    Core review feedback - comment added.

--- src/scene/graph-node.js
+++ src/scene/graph-node.js
@@ -35,6 +35,9 @@ Object.assign(pc, function () {
         this._dirtyLocal = false;
         this._aabbVer = 0;
 
+        // _frozen flag marks the node to ignore hierarchy sync etirely (including children nodes)
+        // engine code automatically freezes and unfreezes objects whenever required
+        // segrigating dynamic and stationary nodes into subhierarchies allows to reduce sync time significantly
         this._frozen = false;
 
         this.worldTransform = new pc.Mat4();
