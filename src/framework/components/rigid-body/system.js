fe3d3daa7f9e3e842f74f3c406e03e20903aa5d2
Will Eastcott <will@playcanvas.com>
2019-07-20 13:19:28 -0700

    Tweak API ref docs.

--- src/framework/components/rigid-body/system.js
+++ src/framework/components/rigid-body/system.js
@@ -135,12 +135,15 @@ Object.assign(pc, function () {
     /**
      * @constructor
      * @name pc.RigidBodyComponentSystem
-     * @classdesc The RigidBodyComponentSystem maintains the dynamics world for simulating rigid bodies, it also controls global values for the world such as gravity.
-     * Note: The RigidBodyComponentSystem is only valid if 3D Physics is enabled in your application. You can enable this in the application settings for your Depot.
+     * @classdesc The RigidBodyComponentSystem maintains the dynamics world for simulating rigid bodies,
+     * it also controls global values for the world such as gravity. Note: The RigidBodyComponentSystem
+     * is only valid if 3D Physics is enabled in your application. You can enable this in the application
+     * settings for your project.
      * @description Create a new RigidBodyComponentSystem
      * @param {pc.Application} app The Application
      * @extends pc.ComponentSystem
-     * @property {pc.Vec3} gravity The gravity vector used by the physics simulation. Defaults to [0, -9.81, 0].
+     * @property {pc.Vec3} gravity The world space vector representing global gravity in the physics simulation.
+     * Defaults to [0, -9.81, 0] which is an approximation of the gravitational force on Earth.
      */
     var RigidBodyComponentSystem = function RigidBodyComponentSystem(app) {
         pc.ComponentSystem.call(this, app);
