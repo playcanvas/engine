6bbcdb7e212afcf17986363ce469879e851d7604
Donovan Hutchence <codebon77@gmail.com>
2019-07-22 14:48:19 +0100

    kerning information movied to kerning table

--- src/framework/components/element/text-element.js
+++ src/framework/components/element/text-element.js
@@ -694,9 +694,12 @@ Object.assign(pc, function () {
                     if (data) {
                         var kerning = 0;
                         if (numCharsThisLine > 0) {
-                            var prevData = json.chars[this._symbols[i - 1]];
-                            if (prevData && prevData.kerning) {
-                                kerning = prevData.kerning[pc.string.getCodePoint(this._symbols[i])] || 0;
+                            var table = this._font._data.kerning;
+                            if (table) {
+                                var left = table[pc.string.getCodePoint(this._symbols[i - 1]) || 0];
+                                if (left) {
+                                    kerning = left[pc.string.getCodePoint(this._symbols[i]) || 0] || 0;
+                                }
                             }
                         }
                         dataScale = data.scale || 1;
