5122fb1585614f7bd5f0426ebd79e571e50ffb3d
zachpeterpaul <zachpeterpaul@gmail.com>
2019-07-22 16:15:42 -0700

    rm tmp code

--- src/templates/template-utils.js
+++ src/templates/template-utils.js
@@ -55,37 +55,15 @@ Object.assign(pc, function () {
             for (var guid in entities) {
                 var h = entities[guid];
 
-                result[guid] = h.collapsed_template ?
+                result[guid] = h.collapsed_entity ?
                     pc.TemplateUtils.expandEntity(app, h) : h;
             }
 
             return result;
         },
 
-        // todo: return a new object, not 'data', which will not have 'collapsed' flags
-        // todo: replace this with an actual traversal-based tree-copy
         expandEntity: function (app, data) {
-            var template = app.assets.get(data.template_id);
-
-            // todo: code below is only for testing, replace this with an actual traversal-based tree-copy
-
-            var h = JSON.parse(JSON.stringify(template.resource.getExpandedData()));
-
-            var instId = data.resource_id;
-
-            var parent = data.parent;
-
-            var templId = Object.keys(h.entities)[0];
-
-            var result = Object.assign({}, data, h.entities[templId]);
-
-            result.resource_id = instId;
-
-            result.parent = parent;
-
-            result.collapsed_template = false;
-
-            return result;
+            // todo implement this
         }
     };
 
