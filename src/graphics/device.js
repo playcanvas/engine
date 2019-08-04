e00369cbfe5f02a62010d9ecf740210ea0bf104b
aidinabedi <aidinabedi@gmail.com>
2019-08-04 20:25:26 +0200

    improve grabpass and fix rendertarget issue

--- src/graphics/device.js
+++ src/graphics/device.js
@@ -585,7 +585,7 @@ Object.assign(pc, function () {
 
         this._textureFloatHighPrecision = undefined;
 
-        this.initializeGrabPassTexture();
+        this.initializeGrabPass(options);
     };
 
     Object.assign(GraphicsDevice.prototype, {
@@ -879,27 +879,42 @@ Object.assign(pc, function () {
             this.transformFeedbackBuffer = null;
         },
 
-        initializeGrabPassTexture: function () {
+        initializeGrabPass: function (options) {
             if (this.grabPassTexture) return;
 
+            var format = options.alpha ? pc.PIXELFORMAT_R8_G8_B8_A8 : pc.PIXELFORMAT_R8_G8_B8;
+
             var grabPassTexture = new pc.Texture(this, {
-                format: pc.PIXELFORMAT_R8_G8_B8_A8,
-                autoMipmap: false
+                format: format,
+                minFilter: pc.FILTER_LINEAR,
+                magFilter: pc.FILTER_LINEAR,
+                addressU: pc.ADDRESS_CLAMP_TO_EDGE,
+                addressV: pc.ADDRESS_CLAMP_TO_EDGE,
+                mipmaps: false
             });
 
-            grabPassTexture.minFilter = pc.FILTER_LINEAR;
-            grabPassTexture.magFilter = pc.FILTER_LINEAR;
-            grabPassTexture.addressU = pc.ADDRESS_CLAMP_TO_EDGE;
-            grabPassTexture.addressV = pc.ADDRESS_CLAMP_TO_EDGE;
-
+            grabPassTexture._pot = false; // never treat texture as "power of 2", framebuffer size is arbitrary.
             grabPassTexture.name = 'texture_grabPass';
-            grabPassTexture.setSource(this.canvas);
 
             var grabPassTextureId = this.scope.resolve(grabPassTexture.name);
             grabPassTextureId.setValue(grabPassTexture);
 
             this.grabPassTextureId = grabPassTextureId;
             this.grabPassTexture = grabPassTexture;
+        },
+
+        updateGrabPass: function () {
+            var gl = this.gl;
+
+            var format = this.grabPassTexture._glFormat;
+            var source = (this.renderTarget && this.renderTarget.colorBuffer) || this.canvas;
+            gl.copyTexImage2D(gl.TEXTURE_2D, 0, format, 0, 0, source.width, source.height, 0);
+        },
+
+        unnitializeGrabPass: function () {
+            this.grabPassTexture.destroy();
+            this.grabPassTexture = null;
+            this.grabPassTextureId = null;
         },
 
         updateClientRect: function () {
@@ -1840,7 +1855,7 @@ Object.assign(pc, function () {
             if (!texture._glTexture)
                 this.initializeTexture(texture);
 
-            if (texture._parameterFlags > 0 || texture._needsUpload || texture._needsMipmapsUpload) {
+            if (texture._parameterFlags > 0 || texture._needsUpload || texture._needsMipmapsUpload || texture === this.grabPassTexture) {
                 // Ensure the specified texture unit is active
                 this.activeTexture(textureUnit);
                 // Ensure the texture is bound on correct target of the specified texture unit
@@ -1851,13 +1866,13 @@ Object.assign(pc, function () {
                     texture._parameterFlags = 0;
                 }
 
-                if (texture._needsUpload || texture._needsMipmapsUpload) {
-                    this.uploadTexture(texture);
+                if (texture === this.grabPassTexture) {
+                    this.updateGrabPass();
 
-                    if (texture !== this.grabPassTexture) {
-                        texture._needsUpload = false;
-                        texture._needsMipmapsUpload = false;
-                    }
+                } else if (texture._needsUpload || texture._needsMipmapsUpload) {
+                    this.uploadTexture(texture);
+                    texture._needsUpload = false;
+                    texture._needsMipmapsUpload = false;
                 }
             } else {
                 // Ensure the texture is currently bound to the correct target on the specified texture unit.
@@ -3158,7 +3173,7 @@ Object.assign(pc, function () {
         destroy: function () {
             var gl = this.gl;
 
-            this.grabPassTexture.destroy();
+            this.unnitializeGrabPass();
 
             if (this.webgl2 && this.feedback) {
                 gl.deleteTransformFeedback(this.feedback);
