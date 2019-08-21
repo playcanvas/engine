240db6d7ebc6c81ae8e99e3b49e488d83ff88c0d
Aidin Abedi <Aidin.Abedi@animech.com>
2019-08-21 08:45:29 +0200

    improve documentation for pc.ResourceHandler

--- src/resources/handler.js
+++ src/resources/handler.js
@@ -3,7 +3,7 @@ Object.assign(pc, function () {
 
     /**
      * @interface pc.ResourceHandler
-     * @description Interface for ResourceHandlers
+     * @description Interface for ResourceHandlers used by {@link pc.ResourceLoader}.
      */
     var ResourceHandler = function () {};
 
@@ -12,22 +12,39 @@ Object.assign(pc, function () {
         /**
          * @function
          * @name pc.ResourceHandler#load
+         * @description Load a resource from a remote URL. When loaded (or failed),
+         * use the callback to return an the raw resource data (or error).
          * @param {String} url The URL of the resource to load.
          * @param {Function} callback The callback used when the resource is loaded or an error occurs.
+         * @param {pc.Asset} [asset] Optional asset that is passed by ResourceLoader.
          */
-        load: function (url, callback) {
+        load: function (url, callback, asset) {
             throw new Error('not implemented');
         },
 
         /**
          * @function
          * @name pc.ResourceHandler#open
+         * @description Convert raw resource data into a resource instance. e.g. take 3D model format JSON and return a pc.Model.
          * @param {String} url The URL of the resource to open.
-         * @param {Any} data The data passed into the callback from {@link pc.ResourceHandler#load}.
+         * @param {*} data The raw resource data passed by callback from {@link pc.ResourceHandler#load}.
          * @param {pc.Asset} [asset] Optional asset that is passed by ResourceLoader.
+         * @returns {*} The parsed resource data.
          */
         open: function (url, data, asset) {
             throw new Error('not implemented');
+        },
+
+        /**
+         * @function
+         * @name pc.ResourceHandler#patch
+         * @description Optional function to perform any operations on a resource, that requires a dependency on its asset data
+         * or any other asset data.
+         * @param {pc.Asset} asset The asset to patch.
+         * @param {pc.AssetRegistry} assets The asset registry.
+         */
+        patch: function (asset, assets) {
+            // optional function
         }
     });
 
