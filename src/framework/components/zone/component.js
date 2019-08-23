4e4cf5ead91dc20f006f654e947278189b146ac3
aidinabedi <aidinabedi@gmail.com>
2019-08-23 20:01:08 +0200

    replace tag @class (with @constructor and @classdesc) on pc.ZoneComponent to be consistent with other classes

--- src/framework/components/zone/component.js
+++ src/framework/components/zone/component.js
@@ -2,9 +2,10 @@ Object.assign(pc, function () {
     /**
      * @private
      * @component
+     * @constrcutor
      * @name pc.ZoneComponent
      * @extends pc.Component
-     * @class The ZoneComponent allows you to define an area in world space of certain size.
+     * @classdesc The ZoneComponent allows you to define an area in world space of certain size.
      * This can be used in various ways, such as affecting audio reverb when audiolistener is within zone.
      * Or create culling system with portals between zones to hide whole indoor sections for performance reasons.
      * And many other possible options. Zones are building blocks and meant to be used in many different ways.
