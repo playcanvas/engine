b1b8364c9124918477a6dfdb74596c9a04ea9df4
aidinabedi <aidinabedi@gmail.com>
2019-08-23 19:58:26 +0200

    replace tag @class (with @constructor and @classdesc) on pc.ScriptComponent to be consistent with other classes

--- src/framework/components/script/component.js
+++ src/framework/components/script/component.js
@@ -1,8 +1,9 @@
 Object.assign(pc, function () {
     /**
      * @component
+     * @constructor
      * @name pc.ScriptComponent
-     * @class The ScriptComponent allows you to extend the functionality of an Entity by attaching your own Script Types defined in JavaScript files
+     * @classdesc The ScriptComponent allows you to extend the functionality of an Entity by attaching your own Script Types defined in JavaScript files
      * to be executed with access to the Entity. For more details on scripting see <a href="//developer.playcanvas.com/user-manual/scripting/">Scripting</a>.
      * @param {pc.ScriptComponentSystem} system The ComponentSystem that created this Component
      * @param {pc.Entity} entity The Entity that this Component is attached to.
