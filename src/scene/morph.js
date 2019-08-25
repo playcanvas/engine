be360d354d438721c708f39fed2670629f198d33
aidinabedi <aidinabedi@gmail.com>
2019-08-25 21:53:40 +0200

    fix names for some options parameters in pc.MorphTarget constructor

--- src/scene/morph.js
+++ src/scene/morph.js
@@ -9,14 +9,14 @@ Object.assign(pc, function () {
      * @classdesc A Morph Target (also known as Blend Shape) contains deformation data to apply to existing mesh.
      * Multiple morph targets can be blended together on a mesh. This is useful for effects that are hard to achieve with conventional animation and skinning.
      * @param {Object} options Object for passing optional arguments.
-     * @param {Number[]} deltaPositions An array of 3-dimensional vertex position offsets.
-     * @param {Number[]} [deltaNormals] An array of 3-dimensional vertex normal offsets.
-     * @param {Number[]} [deltaTangents] An array of 4-dimensional vertex normal tangents.
-     * @param {Number[]} [options.indices] A morph target doesn't have to contain a full copy of the original mesh with added deformations.
+     * @param {Number[]} options.deltaPositions An array of 3-dimensional vertex position offsets.
+     * @param {Number[]} [options.deltaNormals] An array of 3-dimensional vertex normal offsets.
+     * @param {Number[]} [options.deltaTangents] An array of 4-dimensional vertex normal tangents.
+     * @param {Number[]} [options.options.indices] A morph target doesn't have to contain a full copy of the original mesh with added deformations.
      * Instead, only deformed vertices can be stored. This array contains indices to the original mesh's vertices and must be of the same size
      * as other arrays.
-     * @param {String} [name] Name
-     * @param {pc.BoundingBox} [aabb] Bounding box. Will be automatically generated, if undefined.
+     * @param {String} [options.name] Name
+     * @param {pc.BoundingBox} [options.aabb] Bounding box. Will be automatically generated, if undefined.
      */
     var MorphTarget = function (options) {
         if (options.indices) {
