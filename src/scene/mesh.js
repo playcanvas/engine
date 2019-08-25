78f488e6ca5ca95fe083f5dabe133e952e607bce
aidinabedi <aidinabedi@gmail.com>
2019-08-25 22:48:25 +0200

    document pc.Mesh#primitive

--- src/scene/mesh.js
+++ src/scene/mesh.js
@@ -16,6 +16,19 @@ Object.assign(pc, function () {
      * set to pc.RENDERSTYLE_WIREFRAME.
      * @property {Object[]} primitive Array of primitive objects defining how vertex (and index) data in the
      * mesh should be interpreted by the graphics device. For details on the primitive object, see
+     * @property {Number} primitive[].type The type of primitive to render. Can be:
+     * <ul>
+     *     <li>{@link pc.PRIMITIVE_POINTS}</li>
+     *     <li>{@link pc.PRIMITIVE_LINES}</li>
+     *     <li>{@link pc.PRIMITIVE_LINELOOP}</li>
+     *     <li>{@link pc.PRIMITIVE_LINESTRIP}</li>
+     *     <li>{@link pc.PRIMITIVE_TRIANGLES}</li>
+     *     <li>{@link pc.PRIMITIVE_TRISTRIP}</li>
+     *     <li>{@link pc.PRIMITIVE_TRIFAN}</li>
+     * </ul>
+     * @property {Number} primitive[].base The offset of the first index or vertex to dispatch in the draw call.
+     * @property {Number} primitive[].count The number of indices or vertices to dispatch in the draw call.
+     * @property {Boolean} [primitive[].indexed] True to interpret the primitive as indexed, thereby using the currently set index buffer and false otherwise.
      * {@link pc.GraphicsDevice#draw}. The primitive is ordered based on render style like the indexBuffer property.
      * @property {pc.BoundingBox} aabb The axis-aligned bounding box for the object space vertices of this mesh.
      */
