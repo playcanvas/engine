50f4ee3dbb96ee2cc7b4be38c0d4a39c8257b8bb
aidinabedi <aidinabedi@gmail.com>
2019-08-28 22:22:22 +0200

    improve documentation

--- src/resources/loader.js
+++ src/resources/loader.js
@@ -19,8 +19,8 @@ Object.assign(pc, function () {
         /**
          * @function
          * @name pc.ResourceLoader#addHandler
-         * @description Add a {@link pc.ResourceHandler} for a resource type. Handler should support: load(url, callback) and open(url, data).
-         * Handlers can optionally support patch(asset, assets) to handle dependencies on other assets
+         * @description Add a {@link pc.ResourceHandler} for a resource type. Handler should support atleast load() and open().
+         * Handlers can optionally support patch(asset, assets) to handle dependencies on other assets.
          * @param {String} type The name of the resource type that the handler will be registerd with. Can be:
          * <ul>
          *     <li>{@link pc.ASSET_ANIMATION}</li>
@@ -37,7 +37,7 @@ Object.assign(pc, function () {
          *     <li>{@link pc.ASSET_HTML}</li>
          *     <li>{@link pc.ASSET_SCRIPT}</li>
          * </ul>
-         * @param {pc.ResourceHandler} handler An instance of a resource handler supporting load() and open().
+         * @param {pc.ResourceHandler} handler An instance of a resource handler supporting atleast load() and open().
          * @example
          * var loader = new ResourceLoader();
          * loader.addHandler("json", new pc.JsonHandler());
