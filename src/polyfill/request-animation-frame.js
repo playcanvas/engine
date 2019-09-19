8f97ec17299ea7b8524abc675d7270ad1ddaa752
Will Eastcott <will@playcanvas.com>
2019-09-19 12:09:14 +0100

    Restore window in polyfill

--- src/polyfill/request-animation-frame.js
+++ src/polyfill/request-animation-frame.js
@@ -20,7 +20,7 @@
         window.requestAnimationFrame = function(callback, element) {
             var currTime = new Date().getTime();
             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = setTimeout(function() { callback(currTime + timeToCall); }, 
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
               timeToCall);
             lastTime = currTime + timeToCall;
             return id;
