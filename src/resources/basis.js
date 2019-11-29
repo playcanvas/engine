7438b86453c2175fad39def4baef61f5c94fc5f0
Donovan Hutchence <codebon77@gmail.com>
2019-11-29 16:01:50 +0000

    fallback to arraybuffer download when wasm module has incorrect mime type

--- src/resources/basis.js
+++ src/resources/basis.js
@@ -272,6 +272,19 @@ Object.assign(pc, function () {
                 .then(function (result) {
                     compiledModule = result;
                     downloadCompleted();
+                })
+                .catch(function () {
+                    // failed to stream download, attempt arraybuffer download
+                    pc.http.get(
+                        wasmUrl,
+                        { cache: true, responseType: "arraybuffer", retry: false },
+                        function (err, result) {
+                            WebAssembly.compile(result)
+                                .then(function (result) {
+                                    compiledModule = result;
+                                    downloadCompleted();
+                                });
+                        });
                 });
 
             // download glue script
