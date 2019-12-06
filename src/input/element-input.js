b6b1b165b1fdf816d6184bbc3c908062297a2b77
Will Eastcott <will@playcanvas.com>
2019-12-06 06:32:42 -0800

    Touch -> pc.Touch

--- src/input/element-input.js
+++ src/input/element-input.js
@@ -163,8 +163,8 @@ Object.assign(pc, function () {
      * @param {Number} x The x coordinate of the touch that triggered the event
      * @param {Number} y The y coordinate of the touch that triggered the event
      * @param {pc.ElementInput} input The pc.ElementInput instance
-     * @property {Touch[]} touches The Touch objects representing all current points of contact with the surface, regardless of target or changed status.
-     * @property {Touch[]} changedTouches The Touch objects representing individual points of contact whose states changed between the previous touch event and this one.
+     * @property {pc.Touch[]} touches The Touch objects representing all current points of contact with the surface, regardless of target or changed status.
+     * @property {pc.Touch[]} changedTouches The Touch objects representing individual points of contact whose states changed between the previous touch event and this one.
      */
     var ElementTouchEvent = function (event, element, camera, x, y, input) {
         ElementInputEvent.call(this, event, element, camera);
