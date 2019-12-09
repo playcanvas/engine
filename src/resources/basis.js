e9243036c36415997df87c997a0b2d46d99b0f14
Donovan Hutchence <slimbuck7@gmail.com>
2019-12-09 14:48:20 +0000

    Basis update (#1774)
    * kick off basis initialize from config if it isn't already

--- src/resources/basis.js
+++ src/resources/basis.js
@@ -179,10 +179,6 @@ Object.assign(pc, function () {
         };
     };
 
-    var worker = null;
-    var callbacks = { };
-    var format = null;
-
     // check for wasm module support
     var wasmSupported = (function () {
         try {
@@ -194,6 +190,31 @@ Object.assign(pc, function () {
         } catch (e) { }
         return false;
     })();
+
+    // select the most desirable gpu texture compression format given the device's capabilities
+    var selectTextureCompressionFormat = function (device) {
+        if (device.extCompressedTextureASTC) {
+            return 'astc';
+        } else if (device.extCompressedTextureS3TC) {
+            return 'dxt';
+        } else if (device.extCompressedTextureETC) {
+            return 'etc2';
+        } else if (device.extCompressedTextureETC1) {
+            return 'etc1';
+        } else if (device.extCompressedTexturePVRTC) {
+            return 'pvr';
+        } else if (device.extCompressedTextureATC) {
+            return 'atc';
+        }
+        return 'none';
+    };
+
+    // global state
+    var downloadInitiated = false;
+    var worker = null;
+    var callbacks = { };
+    var format = null;
+    var transcodeQueue = [];
 
     var handleWorkerResponse = function (message) {
         var url = message.data.url;
@@ -232,8 +253,25 @@ Object.assign(pc, function () {
         }
     };
 
-    // initialize the basis module given the module glue (or fallback) script and the optional
-    // accompanying wasm module.
+    // post a transcode job to the web worker
+    var transcode = function (url, data, callback) {
+        if (!callbacks.hasOwnProperty(url)) {
+            if (!format) {
+                format = selectTextureCompressionFormat(pc.app.graphicsDevice);
+            }
+            // store url and kick off worker job
+            callbacks[url] = [callback];
+            worker.postMessage({ type: 'transcode', url: url, format: format, data: data }, [data]);
+        } else {
+            // the basis worker is already busy processing this url, store callback
+            // (this shouldn't really happen since the asset system only requests
+            // a resource once)
+            callbacks[url].push(callback);
+        }
+    };
+
+    // initialize the basis worker given the basis module script (glue or fallback)
+    // and the optional accompanying wasm binary.
     var basisInitialize = function (basisCode, basisModule, callback) {
         var code = [
             "/* basis.js */",
@@ -253,10 +291,19 @@ Object.assign(pc, function () {
         if (callback) {
             callback();
         }
+        // module is initialized, initiate queued jobs
+        for (var i = 0; i < transcodeQueue.length; ++i) {
+            var entry = transcodeQueue[i];
+            transcode(entry[0], entry[1], entry[2]);
+        }
     };
 
-    // helper function which downloads and then initializes the basis module
+    // download the module files and initialize the basis worker
     var basisDownload = function (glueUrl, wasmUrl, fallbackUrl, callback) {
+        if (downloadInitiated) {
+            console.warn('basis module is being downloaded more than once');
+        }
+        downloadInitiated = true;
         if (wasmSupported) {
             var glueCode = null;
             var compiledModule = null;
@@ -312,48 +359,39 @@ Object.assign(pc, function () {
         }
     };
 
-    // select the most desirable gpu texture compression format given the device's capabilities
-    var selectTextureCompressionFormat = function (device) {
-        if (device.extCompressedTextureASTC) {
-            return 'astc';
-        } else if (device.extCompressedTextureS3TC) {
-            return 'dxt';
-        } else if (device.extCompressedTextureETC) {
-            return 'etc2';
-        } else if (device.extCompressedTextureETC1) {
-            return 'etc1';
-        } else if (device.extCompressedTexturePVRTC) {
-            return 'pvr';
-        } else if (device.extCompressedTextureATC) {
-            return 'atc';
+    // search for wasm module in the global config and initialize basis
+    var basisDownloadFromConfig = function (callback) {
+        var modules = (window.config ? window.config.wasmModules : window.PRELOAD_MODULES) || [];
+        var wasmModule = modules.find(function (m) {
+            return m.moduleName === 'BASIS';
+        });
+        if (wasmModule) {
+            var urlBase = window.ASSET_PREFIX ? window.ASSET_PREFIX : "";
+            basisDownload(urlBase + wasmModule.glueUrl,
+                          urlBase + wasmModule.wasmUrl,
+                          urlBase + wasmModule.fallbackUrl,
+                          callback);
         }
-        return 'none';
     };
 
     // render thread worker manager
     var basisTranscode = function (url, data, callback) {
         if (!worker) {
-            console.error('call pc.basisInitialize before loading basis textures');
-            return;
-        }
-        if (!callbacks.hasOwnProperty(url)) {
-            if (!format) {
-                format = selectTextureCompressionFormat(pc.app.graphicsDevice);
+            // store transcode job if no worker exists
+            transcodeQueue.push([url, data, callback]);
+            // if the basis module download has not yet been initiated, do so now
+            if (!downloadInitiated) {
+                basisDownloadFromConfig();
             }
-            // store url and kick off worker job
-            callbacks[url] = [callback];
-            worker.postMessage({ type: 'transcode', url: url, format: format, data: data }, [data]);
         } else {
-            // the basis worker is already busy processing this url, store callback
-            // (this shouldn't really happen since the asset system only requests
-            // a resource once)
-            callbacks[url].push(callback);
+            transcode(url, data, callback);
         }
     };
 
     return {
         basisInitialize: basisInitialize,
         basisDownload: basisDownload,
+        basisDownloadFromConfig: basisDownloadFromConfig,
         basisTranscode: basisTranscode
     };
 }());
