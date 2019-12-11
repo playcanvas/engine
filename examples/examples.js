cad0012cb07e715cb3924f863e1ba05f4f19736a
Will Eastcott <will@playcanvas.com>
2019-12-11 16:53:38 +0000

    UI -> User Interface

--- examples/examples.js
+++ examples/examples.js
@@ -1,37 +1,50 @@
-var examples = {
-    animation: [
-        "blend"
-    ],
-    graphics: [
-        "model-asset",
-        "model-box",
-        "model-textured-box",
-        "portal",
-        "shader-burn",
-        "shader-toon",
-        "shader-wobble",
-        "texture-basis"
-    ],
-    input: [
-        "gamepad",
-        "keyboard",
-        "mouse"
-    ],
-    physics: [
-        "falling-shapes"
-    ],
-    sound: [
-        "positional"
-    ],
-    ui: [
-        "button-basic",
-        "button-sprite",
-        "text-basic",
-        "text-drop-shadow",
-        "text-localization",
-        "text-markup",
-        "text-outline",
-        "text-typewriter",
-        "text-wrap"
-    ]
-};
+var categories = [
+    {
+        name: "animation",
+        examples: [
+            "blend"
+        ]
+    }, {
+        name: "graphics",
+        examples: [
+            "model-asset",
+            "model-box",
+            "model-textured-box",
+            "portal",
+            "shader-burn",
+            "shader-toon",
+            "shader-wobble",
+            "texture-basis"
+        ]
+    }, {
+        name: "input",
+        examples: [
+            "gamepad",
+            "keyboard",
+            "mouse"
+        ]
+    }, {
+        name: "physics",
+        examples: [
+            "falling-shapes"
+        ]
+    }, {
+        name: "sound",
+        examples: [
+            "positional"
+        ]
+    }, {
+        name: "user-interface",
+        examples: [
+            "button-basic",
+            "button-sprite",
+            "text-basic",
+            "text-drop-shadow",
+            "text-localization",
+            "text-markup",
+            "text-outline",
+            "text-typewriter",
+            "text-wrap"
+        ]
+    }
+];
