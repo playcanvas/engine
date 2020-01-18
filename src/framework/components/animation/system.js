1265e5ada691c0afebd6b34e75409810f90bc351
Will Eastcott <will@playcanvas.com>
2020-01-18 16:36:30 +0000

    Space after if statement to fix linting

--- src/framework/components/animation/system.js
+++ src/framework/components/animation/system.js
@@ -109,7 +109,7 @@ Object.assign(pc, function () {
                                 skeleton.addTime(delta);
                                 if (componentData.speed > 0 && (skeleton._time === skeleton._animation.duration) && !componentData.loop) {
                                     componentData.playing = false;
-                                } else if(componentData.speed < 0 && skeleton._time === 0 && !componentData.loop) {
+                                } else if (componentData.speed < 0 && skeleton._time === 0 && !componentData.loop) {
                                     componentData.playing = false;
                                 }
                             }
