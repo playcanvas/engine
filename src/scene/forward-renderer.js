8a9d3b00723a98259d11efc8e31af8351eb2e0ba
Martin Valigursky <mvaligursky@snap.com>
2020-01-27 12:15:33 +0000

    [FIX] Fix culling of objects into directional shadowmap (1799)
    camera node properties were changed, but worldMatrix does not take these into account, getWorldMatrix needs to be called to evaluate dirty matrix

--- src/scene/forward-renderer.js
+++ src/scene/forward-renderer.js
@@ -2352,7 +2352,7 @@ Object.assign(pc, function () {
 
             // 3. Transform the 8 corners of the camera frustum into the shadow camera's view space
             shadowCamView.copy( shadowCamNode.getWorldTransform() ).invert();
-            c2sc.copy( shadowCamView ).mul( camera._node.worldTransform );
+            c2sc.copy( shadowCamView ).mul( camera._node.getWorldTransform() );
             for (i = 0; i < 8; i++) {
                 c2sc.transformPoint(frustumPoints[i], frustumPoints[i]);
             }
