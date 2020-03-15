fe9a4207735d541f87a6b54d14d6585f4a2c330d
aidinabedi <aidinabedi@gmail.com>
2020-03-15 15:29:32 +0100

    add pc.GraphicsDevice#getCopyShader

--- src/graphics/simple-post-effect.js
+++ src/graphics/simple-post-effect.js
@@ -117,20 +117,13 @@ Object.assign(pc, (function () {
      * @param {pc.GraphicsDevice} device - The graphics device used to draw the texture.
      * @param {pc.Texture} texture - The source texture to be drawn. Accessible as `uniform sampler2D source` in shader.
      * @param {pc.RenderTarget} [target] - The destination render target. Defaults to the frame buffer.
-     * @param {pc.Shader} [shader] - The shader used for rendering the texture. Defaults to `fullscreenQuadVS` and `outputTex2DPS` in `pc.shaderChunks`.
+     * @param {pc.Shader} [shader] - The shader used for rendering the texture. Defaults to `pc.GraphicsDevice#getCopyShader()`.
      * @param {pc.Vec4} [rect] - The viewport rectangle to use for the texture, in pixels. Defaults to fullscreen (`0, 0, target.width, target.height`).
      * @param {pc.Vec4} [scissorRect] - The scissor rectangle to use for the texture, in pixels. Defaults to fullscreen (`0, 0, target.width, target.height`).
      * @param {boolean} [useBlend] - True to enable blending. Defaults to false, disabling blending.
      */
     function drawTexture(device, texture, target, shader, rect, scissorRect, useBlend) {
-        if (!shader) {
-            if (!device._copyShader) {
-                var chunks = pc.shaderChunks;
-                device._copyShader = chunks.createShaderFromCode(device, chunks.fullscreenQuadVS, chunks.outputTex2DPS, "outputTex2D");
-            }
-            shader = device._copyShader;
-        }
-
+        shader = shader || device.getCopyShader();
         device.constantTexSource.setValue(texture);
         drawQuadWithShader(device, target, shader, rect, scissorRect, useBlend);
     }
