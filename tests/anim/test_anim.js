7f256a32c261ac4f8dc1bc2bd608ff5bee57063e
Elliott Thompson <elliott@moo.com>
2020-05-01 16:23:32 +0100

    fix anim test

--- tests/anim/test_anim.js
+++ tests/anim/test_anim.js
@@ -1,10 +1,10 @@
-describe("pc.AnimController", function () {
+describe("pc.AnimEvaluator", function () {
 
-    it("AnimController: looping", function () {
+    it("AnimEvaluator: looping", function () {
         // create curve
         var keys = new pc.AnimData(1, [0, 1, 2]);
         var translations = new pc.AnimData(3, [0, 0, 0, 1, 0, 0, 1, 0, 1]);
-        var curve = new pc.AnimCurve(["child1.translation"], 0, 0, pc.INTERPOLATION_LINEAR);
+        var curve = new pc.AnimCurve(["child1/graph/translation"], 0, 0, pc.INTERPOLATION_LINEAR);
 
         // construct the animation track
         var track = new pc.AnimTrack("test track", 2, [keys], [translations], [curve]);
@@ -19,31 +19,31 @@ describe("pc.AnimController", function () {
         parent.addChild(child1);
         child1.addChild(child2);
 
-        // construct the animation controller
-        var animController = new pc.AnimController(new pc.DefaultAnimBinder(parent));
-        animController.addClip(clip);
+        // construct the animation evaluator
+        var animEvaluator = new pc.AnimEvaluator(new pc.DefaultAnimBinder(parent));
+        animEvaluator.addClip(clip);
 
         // check initial state
-        animController.update(0);
+        animEvaluator.update(0);
         equal(clip.time, 0);
         equal(child1.localPosition.x, 0);
         equal(child1.localPosition.y, 0);
         equal(child1.localPosition.z, 0);
 
-        animController.update(0.5);
+        animEvaluator.update(0.5);
         equal(clip.time, 0.5);
         equal(child1.localPosition.x, 0.5);
         equal(child1.localPosition.y, 0);
         equal(child1.localPosition.z, 0);
 
-        animController.update(1.0);
+        animEvaluator.update(1.0);
         equal(clip.time, 1.5);
         equal(child1.localPosition.x, 1.0);
         equal(child1.localPosition.y, 0);
         equal(child1.localPosition.z, 0.5);
 
         // checked looped state (current time 0.5)
-        animController.update(1.0);
+        animEvaluator.update(1.0);
         equal(clip.time, 0.5);
         equal(child1.localPosition.x, 0.5);
         equal(child1.localPosition.y, 0);
