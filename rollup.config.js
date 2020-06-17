c33ddd45f93d7a26375833e04593bb5406a671c9
Will Eastcott <will@playcanvas.com>
2020-06-17 09:15:12 +0100

    Remove license rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import replace from '@rollup/plugin-replace';
-import license from 'rollup-plugin-license';
 import pkg from './package.json';
 
 const execSync = require('child_process').execSync;
@@ -14,6 +13,7 @@ const notice = [
 export default [{
     input: 'src/index.js',
     output: {
+        banner: notice,
         file: 'build/playcanvas.js',
         format: 'umd',
         name: 'pc'
@@ -22,27 +22,14 @@ export default [{
         replace({
             __REVISION__: revision,
             __CURRENT_SDK_VERSION__: pkg.version
-        }),
-        license({
-            banner: {
-                content: notice,
-                commentStyle: 'none'
-            }
         })
     ]
 }, {
     input: 'extras/index.js',
     output: {
+        banner: notice,
         file: 'build/playcanvas-extras.js',
         format: 'umd',
         name: 'pc'
-    },
-    plugins: [
-        license({
-            banner: {
-                content: notice,
-                commentStyle: 'none'
-            }
-        })
-    ]
+    }
 }];
