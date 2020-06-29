d95307073438347b2ee6d9c7ee95c25a24b7c195
Donovan Hutchence <slimbuck7@gmail.com>
2020-06-29 12:58:00 +0100

    fix test

--- tests/bundles/test_bundle_loader.js
+++ tests/bundles/test_bundle_loader.js
@@ -69,8 +69,12 @@ describe('Test Bundle Loader', function () {
             })
         ];
 
-        // flag the cubemap faces for loading
-        this.assets[4].loadFaces = true;
+        // setup cubemap correctly so faces load
+        var id = this.assets[7].id;
+        this.assets[5].loadFaces = true;
+        this.assets[5].data = {
+            textures: [id, id, id, id, id, id]
+        };
 
         // expected types of asset types
         this.expectedTypes = {
