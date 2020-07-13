24b917c9601f603c54cf2470c01477fb2926f518
Elliott Thompson <elliott@moo.com>
2020-07-13 18:03:10 +0100

    update the anim state graph to support the new data schema

--- src/framework/components/anim/state-graph.js
+++ src/framework/components/anim/state-graph.js
@@ -1,22 +1,45 @@
 function AnimStateGraph(data) {
     this._layers = [];
     this._parameters = {};
-    if (data) {
-        if (data.states) {
-            this._layers = [];
-            this._layers.push({
-                name: 'Base',
-                states: data.states,
-                transitions: data.transitions
-            });
-        } else {
-            this._layers = data.layers;
+    var i;
+    if (!Array.isArray(data.layers)) {
+        for (var layerId in data.layers) {
+            var dataLayer = data.layers[layerId];
+            var layer = {
+                name: dataLayer.name,
+                states: [],
+                transitions: []
+            };
+            for (i = 0; i < dataLayer.states.length; i++) {
+                layer.states.push(data.states[dataLayer.states[i]]);
+            }
+            for (i = 0; i < dataLayer.transitions.length; i++) {
+                var dataLayerTransition = data.transitions[dataLayer.transitions[i]];
+                if (dataLayerTransition.conditions && !Array.isArray(dataLayerTransition.conditions)) {
+                    var conditionKeys = Object.keys(dataLayerTransition.conditions);
+                    var conditions = [];
+                    for (var j = 0; j < conditionKeys.length; j++) {
+                        var condition = dataLayerTransition.conditions[conditionKeys[j]];
+                        if (condition.parameterName) {
+                            conditions.push(condition);
+                        }
+                    }
+                    dataLayerTransition.conditions = conditions;
+                }
+                if (Number.isInteger(dataLayerTransition.from)) {
+                    dataLayerTransition.from = data.states[dataLayerTransition.from].name;
+                }
+                if (Number.isInteger(dataLayerTransition.to)) {
+                    dataLayerTransition.to = data.states[dataLayerTransition.to].name;
+                }
+                layer.transitions.push(dataLayerTransition);
+            }
+            this._layers.push(layer);
         }
-        this._parameters = {};
-        for (var paramId in data.parameters) {
-            var param = data.parameters[paramId];
-            this._parameters[param.name] = { type: param.type, value: param.value };
-        }
+    }
+    for (var paramId in data.parameters) {
+        var param = data.parameters[paramId];
+        this._parameters[param.name] = { type: param.type, value: param.value };
     }
 }
 
