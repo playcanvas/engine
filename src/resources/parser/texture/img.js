7b58c661843931771f119d0ce9eef843930750f3
Donovan Hutchence <slimbuck7@gmail.com>
2020-07-15 11:24:03 +0100

    disable ImageBitmap (#2286)

--- src/resources/parser/texture/img.js
+++ src/resources/parser/texture/img.js
@@ -16,7 +16,8 @@ function ImgParser(registry, retryRequests) {
     // by default don't try cross-origin, because some browsers send different cookies (e.g. safari) if this is set.
     this.crossOrigin = registry.prefix ? 'anonymous' : null;
     this.retryRequests = !!retryRequests;
-    this.useImageBitmap = typeof ImageBitmap !== 'undefined' && /Firefox/.test( navigator.userAgent ) === false;
+    // disable ImageBitmap
+    this.useImageBitmap = false && typeof ImageBitmap !== 'undefined' && /Firefox/.test( navigator.userAgent ) === false;
 }
 
 Object.assign(ImgParser.prototype, {
