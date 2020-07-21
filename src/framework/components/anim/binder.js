9b0fd5fd96c7c6951de4a311d5efa3659c1098a3
Elliott Thompson <ellth92@gmail.com>
2020-07-21 09:48:55 +0100

    AnimBinder no model fix (#2293)
    * fix anim binder crashes when a model animation is loaded before a model component is added to the entity
    
    Co-authored-by: Will Eastcott <will@playcanvas.com>

--- src/framework/components/anim/binder.js
+++ src/framework/components/anim/binder.js
@@ -41,7 +41,7 @@ Object.assign(AnimComponentBinder.prototype, {
                 propertyComponent = entity;
                 break;
             case 'graph':
-                if (!this.nodes[entityHierarchy[0]]) {
+                if (!this.nodes || !this.nodes[entityHierarchy[0]]) {
                     return null;
                 }
                 propertyComponent = this.nodes[entityHierarchy[0]].node;
@@ -59,7 +59,7 @@ Object.assign(AnimComponentBinder.prototype, {
         // flag active nodes as dirty
         var activeNodes = this.activeNodes;
         if (activeNodes) {
-            for (var i = 0; i < activeNodes.length; ++i) {
+            for (var i = 0; i < activeNodes.length; i++) {
                 activeNodes[i]._dirtifyLocal();
             }
         }
