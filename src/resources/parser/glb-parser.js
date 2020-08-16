547e684d73d9d2273a509ca385fb9780c6fed419
aidinabedi <aidinabedi@gmail.com>
2020-08-16 19:49:59 +0200

    add temp workaround for replicating glTF clear-coat visuals

--- src/resources/parser/glb-parser.js
+++ src/resources/parser/glb-parser.js
@@ -1043,7 +1043,7 @@ var createMaterial = function (gltfMaterial, textures) {
         var ccData = gltfMaterial.extensions.KHR_materials_clearcoat;
 
         if (ccData.hasOwnProperty('clearcoatFactor')) {
-            material.clearCoat = ccData.clearcoatFactor;
+            material.clearCoat = ccData.clearcoatFactor * 0.25; // TODO: remove temporary workaround for replicating glTF clear-coat visuals
         } else {
             material.clearCoat = 0;
         }
