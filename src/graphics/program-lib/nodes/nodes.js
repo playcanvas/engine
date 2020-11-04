35cd936ee9eed351ebe0712f54fa1e31e8997697
Ray Tran <raytranuk@users.noreply.github.com>
2020-11-04 11:50:51 +0000

    add pc.shaderNodes js file

---
+++ src/graphics/program-lib/nodes/nodes.js
@@ -0,0 +1,12 @@
+/**
+ * @static
+ * @readonly
+ * @type {object}
+ * @name pc.shaderNodes
+ * @description Object in which shader graph nodes are registered
+ */
+var shaderNodes = {
+
+};
+
+export { shaderNodes };
