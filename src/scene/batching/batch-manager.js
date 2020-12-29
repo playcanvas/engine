fbd1865a1c093cf3e07f9e47b403a7acfa6f5652
Martin Valigursky <mvaligursky@snap.com>
2020-12-29 17:22:40 +0000

    removed auto added params

--- src/scene/batching/batch-manager.js
+++ src/scene/batching/batch-manager.js
@@ -82,10 +82,6 @@ function getScaleSign(mi) {
  * @class
  * @name pc.BatchManager
  * @classdesc Glues many mesh instances into a single one for better performance.
- * @param node
- * @param arr
- * @param group
- * @param groupMeshInstances
  * @param {pc.GraphicsDevice} device - The graphics device used by the batch manager.
  * @param {pc.Entity} root - The entity under which batched models are added.
  * @param {pc.Scene} scene - The scene that the batch manager affects.
