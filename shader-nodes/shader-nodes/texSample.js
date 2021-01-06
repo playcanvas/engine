b46f260facb59d41a06734fc9354d37587fdf7e1
Martin Valigursky <mvaligursky@snap.com>
2021-01-06 09:54:38 +0000

    using ES6 template to build shared program

--- shader-nodes/shader-nodes/texSample.js
+++ shader-nodes/shader-nodes/texSample.js
@@ -7,21 +7,16 @@ var texSample = {
     gen: function ( argTypes, options ) {
         // header
         var precision = (options && options.precision) ? `_${options.precision}` : ''; // alter name to create variant
-        var code = `void texSample${precision}(in sampler2D tex, in vec2 uv, out vec4 rgba, out vec4 srgba, out vec3 rgbm)\n`;
 
-        // body
-        code += '{\n';
-
-        code += 'rgba = texture2D(tex, uv);\n';
-
-        code += 'srgba.rgb = gammaCorrectInput(rgba.rgb);\n';
-        code += 'srgba.a = rgba.a;\n';
-
-        code += 'rgbm.rgb = decodeRGBM(rgba);\n';
-
-        code += '}\n';
-
-        return code;
+        return `
+        void texSample${precision}(in sampler2D tex, in vec2 uv, out vec4 rgba, out vec4 srgba, out vec3 rgbm)
+        {
+            rgba = texture2D(tex, uv);
+            srgba.rgb = gammaCorrectInput(rgba.rgb);
+            srgba.a = rgba.a;
+            rgbm.rgb = decodeRGBM(rgba);
+        }
+        `;
     }
 };
 
