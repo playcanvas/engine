ecfa48e0e2a81828fe7fed57b7c7125b82315839
Elliott Thompson <ellth92@gmail.com>
2021-02-19 16:50:21 +0000

    add synchronised durations for 1D blend trees

--- src/anim/controller/anim-blend-tree-1d.js
+++ src/anim/controller/anim-blend-tree-1d.js
@@ -18,18 +18,29 @@ class AnimBlendTree1D extends AnimBlendTree {
     calculateWeights() {
         if (this.updateParameterValues()) return;
         var i;
+        var meanDuration = 0.0;
         this._children[0].weight = 0.0;
-        for (i = 0; i < this._children.length - 1; i++) {
-            var c1 = this._children[i];
-            var c2 = this._children[i + 1];
-            if (math.between(parameterValue, c1.point, c2.point, true)) {
-                var child2Distance = Math.abs(c1.point - c2.point);
-                var parameterDistance = Math.abs(c1.point - parameterValue);
-                var weight = (child2Distance - parameterDistance) / child2Distance;
-                c1.weight = weight;
-                c2.weight = (1.0 - weight);
-            } else {
-                c2.weight = 0.0;
+        for (i = 0; i < this._children.length; i++) {
+            if (i !== this._children.length - 1) {
+                var c1 = this._children[i];
+                var c2 = this._children[i + 1];
+                if (math.between(this._parameterValues[0], c1.point, c2.point, true)) {
+                    var child2Distance = Math.abs(c1.point - c2.point);
+                    var parameterDistance = Math.abs(c1.point - this._parameterValues[0]);
+                    var weight = (child2Distance - parameterDistance) / child2Distance;
+                    c1.weight = weight;
+                    c2.weight = (1.0 - weight);
+                } else {
+                    c2.weight = 0.0;
+                }
+            }
+            if (this._syncDurations) {
+                meanDuration += this._children[i].animTrack.duration * this._children[i].weight;
+            }
+        }
+        if (this._syncDurations) {
+            for (i = 0; i < this._children.length; i++) {
+                this._children[i]._weightedSpeed = this._children[i].animTrack.duration / meanDuration;
             }
         }
     }
