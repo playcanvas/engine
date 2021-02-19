7180ccffcb618b322218214c32beba7b6d06d03e
Elliott Thompson <ellth92@gmail.com>
2021-02-19 16:39:08 +0000

    BlendTree1D fix

--- src/anim/controller/anim-blend-tree-1d.js
+++ src/anim/controller/anim-blend-tree-1d.js
@@ -25,9 +25,9 @@ class AnimBlendTree1D extends AnimBlendTree {
             if (c1.point === c2.point) {
                 c1.weight = 0.5;
                 c2.weight = 0.5;
-            } else if (math.between(parameterValue, c1.point, c2.point, true)) {
+            } else if (math.between(this._parameterValues[0], c1.point, c2.point, true)) {
                 var child2Distance = Math.abs(c1.point - c2.point);
-                var parameterDistance = Math.abs(c1.point - parameterValue);
+                var parameterDistance = Math.abs(c1.point - this._parameterValues[0]);
                 var weight = (child2Distance - parameterDistance) / child2Distance;
                 c1.weight = weight;
                 c2.weight = (1.0 - weight);
