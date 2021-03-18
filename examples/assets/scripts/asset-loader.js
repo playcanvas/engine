22f4e4a4aeae69582a023c5386cf0dd165eddac0
Elliott Thompson <ellth92@gmail.com>
2021-03-18 15:55:56 +0000

    more updates

--- examples/assets/scripts/asset-loader.js
+++ examples/assets/scripts/asset-loader.js
@@ -12,8 +12,9 @@ function loadManifestAssets(app, manifest, onLoaded) {
     Object.keys(manifest).forEach(function (key) {
         if (manifest.hasOwnProperty(key)) {
             var entry = manifest[key];
-            app.assets.loadFromUrl(entry.url, entry.type, function (err, asset) {
-                if (!err && asset) {
+            if (entry.data) {
+                var asset = new pc.Asset(key, entry.type, entry.url, entry.data);
+                asset.on('load', function (asset) {
                     count--;
                     entry.asset = asset;
                     if (count === 0) {
@@ -21,8 +22,22 @@ function loadManifestAssets(app, manifest, onLoaded) {
                             onLoaded();
                         }
                     }
-                }
-            });
+                });
+                app.assets.add(asset);
+                app.assets.load(asset);
+            } else {
+                app.assets.loadFromUrl(entry.url, entry.type, function (err, asset) {
+                    if (!err && asset) {
+                        count--;
+                        entry.asset = asset;
+                        if (count === 0) {
+                            if (onLoaded) {
+                                onLoaded();
+                            }
+                        }
+                    }
+                });
+            }
         }
     });
 }
