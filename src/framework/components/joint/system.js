355aded0d339b59188a14510e9d7b116a59d1450
Will Eastcott <will@playcanvas.com>
2021-03-22 07:56:48 +0000

    Refactor initializeComponentData

--- src/framework/components/joint/system.js
+++ src/framework/components/joint/system.js
@@ -1,3 +1,5 @@
+import { Vec2 } from '../../../math/vec2.js';
+
 import { Component } from '../component.js';
 import { ComponentSystem } from '../system.js';
 
@@ -29,91 +31,26 @@ class JointComponentSystem extends ComponentSystem {
     }
 
     initializeComponentData(component, data, properties) {
-        component.enabled = data.hasOwnProperty('enabled') ? !!data.enabled : true;
+        const props = [
+            'angularEquilibriumX', 'angularEquilibriumY', 'angularEquilibriumZ',
+            'angularLimitsX', 'angularLimitsY', 'angularLimitsZ',
+            'angularSpringX', 'angularSpringY', 'angularSpringZ',
+            'angularStiffnessX', 'angularStiffnessY', 'angularStiffnessZ',
+            'breakForce', 'enableCollision', 'enabled', 'entityA', 'entityB',
+            'linearEquilibriumX', 'linearEquilibriumY', 'linearEquilibriumZ',
+            'linearLimitsX', 'linearLimitsY', 'linearLimitsZ',
+            'linearSpringX', 'linearSpringY', 'linearSpringZ',
+            'linearStiffnessX', 'linearStiffnessY', 'linearStiffnessZ'
+        ];
 
-        if (data.angularLimitsX !== undefined) {
-            component._angularLimitsX.copy(data.angularLimitsX);
-        }
-        if (data.angularSpringX !== undefined) {
-            component._angularSpringX = data.angularSpringX;
-        }
-        if (data.angularStiffnessX !== undefined) {
-            component._angularStiffnessX = data.angularStiffnessX;
-        }
-        if (data.angularEquilibriumX !== undefined) {
-            component._angularEquilibriumX = data.angularEquilibriumX;
-        }
-        if (data.angularLimitsY !== undefined) {
-            component._angularLimitsY.copy(data.angularLimitsY);
-        }
-        if (data.angularSpringY !== undefined) {
-            component._angularSpringY = data.angularSpringY;
-        }
-        if (data.angularStiffnessY !== undefined) {
-            component._angularStiffnessY = data.angularStiffnessY;
-        }
-        if (data.angularEquilibriumY !== undefined) {
-            component._angularEquilibriumY = data.angularEquilibriumY;
-        }
-        if (data.angularLimitsZ !== undefined) {
-            component._angularLimitsZ.copy(data.angularLimitsZ);
-        }
-        if (data.angularSpringZ !== undefined) {
-            component._angularSpringZ = data.angularSpringZ;
-        }
-        if (data.angularStiffnessZ !== undefined) {
-            component._angularStiffnessZ = data.angularStiffnessZ;
-        }
-        if (data.angularEquilibriumZ !== undefined) {
-            component._angularEquilibriumZ = data.angularEquilibriumZ;
-        }
-        if (data.breakForce !== undefined) {
-            component._breakForce = data.breakForce;
-        }
-        if (data.enableCollision !== undefined) {
-            component._enableCollision = data.enableCollision;
-        }
-        if (data.entityA !== undefined) {
-            component._entityA = data.entityA;
-        }
-        if (data.entityB !== undefined) {
-            component._entityB = data.entityB;
-        }
-        if (data.linearLimitsX !== undefined) {
-            component._linearLimitsX.copy(data.linearLimitsX);
-        }
-        if (data.linearSpringX !== undefined) {
-            component._linearSpringX = data.linearSpringX;
-        }
-        if (data.linearStiffnessX !== undefined) {
-            component._linearStiffnessX = data.linearStiffnessX;
-        }
-        if (data.linearEquilibriumX !== undefined) {
-            component._linearEquilibriumX = data.linearEquilibriumX;
-        }
-        if (data.linearLimitsY !== undefined) {
-            component._linearLimitsY.copy(data.linearLimitsY);
-        }
-        if (data.linearSpringY !== undefined) {
-            component._linearSpringY = data.linearSpringY;
-        }
-        if (data.linearStiffnessY !== undefined) {
-            component._linearStiffnessY = data.linearStiffnessY;
-        }
-        if (data.linearEquilibriumY !== undefined) {
-            component._linearEquilibriumY = data.linearEquilibriumY;
-        }
-        if (data.linearLimitsZ !== undefined) {
-            component._linearLimitsZ.copy(data.linearLimitsZ);
-        }
-        if (data.linearSpringZ !== undefined) {
-            component._linearSpringZ = data.linearSpringZ;
-        }
-        if (data.linearStiffnessZ !== undefined) {
-            component._linearStiffnessZ = data.linearStiffnessZ;
-        }
-        if (data.linearEquilibriumZ !== undefined) {
-            component._linearEquilibriumZ = data.linearEquilibriumZ;
+        for (const prop of props) {
+            if (data.hasOwnProperty(prop)) {
+                if (data[prop] instanceof Vec2) {
+                    component['_' + prop].copy(data[prop]);
+                } else {
+                    component['_' + prop] = data[prop];
+                }
+            }
         }
 
         component._createConstraint();
