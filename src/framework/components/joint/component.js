8e05a31cfed8ef0df8cb32654c64f52b64d40704
Will Eastcott <will@playcanvas.com>
2021-03-22 11:37:46 +0000

    Refactor joint getters and setters

--- src/framework/components/joint/component.js
+++ src/framework/components/joint/component.js
@@ -30,35 +30,41 @@ class JointComponent extends Component {
         this._breakForce = 3.4e+38;
         this._enableCollision = true;
 
+        this._linearDampingX = 1;
+        this._linearEquilibriumX = 0;
         this._linearLimitsX = new Vec2(0, 0);
         this._linearSpringX = false;
         this._linearStiffnessX = 0;
-        this._linearEquilibriumX = 0;
 
+        this._linearDampingY = 1;
+        this._linearEquilibriumY = 0;
         this._linearLimitsY = new Vec2(0, 0);
         this._linearSpringY = false;
         this._linearStiffnessY = 0;
-        this._linearEquilibriumY = 0;
 
+        this._linearDampingZ = 1;
+        this._linearEquilibriumZ = 0;
         this._linearLimitsZ = new Vec2(0, 0);
         this._linearSpringZ = false;
         this._linearStiffnessZ = 0;
-        this._linearEquilibriumZ = 0;
 
+        this._angularDampingX = 1;
+        this._angularEquilibriumX = 0;
         this._angularLimitsX = new Vec2(0, 0);
         this._angularSpringX = false;
         this._angularStiffnessX = 0;
-        this._angularEquilibriumX = 0;
 
+        this._angularDampingY = 1;
+        this._angularEquilibriumY = 0;
         this._angularLimitsY = new Vec2(0, 0);
         this._angularSpringY = false;
         this._angularStiffnessY = 0;
-        this._angularEquilibriumY = 0;
 
+        this._angularDampingZ = 1;
+        this._angularEquilibriumZ = 0;
         this._angularLimitsZ = new Vec2(0, 0);
         this._angularSpringZ = false;
         this._angularStiffnessZ = 0;
-        this._angularEquilibriumZ = 0;
 
         this.on('set_enabled', this._onSetEnabled, this);
     }
@@ -115,39 +121,6 @@ class JointComponent extends Component {
         return this._angularLimitsX;
     }
 
-    set angularSpringX(value) {
-        if (this._angularSpringX !== value) {
-            this._angularSpringX = value;
-            this._constraint.enableSpring(3, value);
-        }
-    }
-
-    get angularSpringX() {
-        return this._angularSpringX;
-    }
-
-    set angularStiffnessX(value) {
-        if (this._angularStiffnessX !== value) {
-            this._angularStiffnessX = value;
-            this._constraint.setStiffness(3, value);
-        }
-    }
-
-    get angularStiffnessX() {
-        return this._angularStiffnessX;
-    }
-
-    set angularEquilibriumX(value) {
-        if (this._angularEquilibriumX !== value) {
-            this._angularEquilibriumX = value;
-            this._constraint.setEquilibriumPoint(3, value);
-        }
-    }
-
-    get angularEquilibriumX() {
-        return this._angularEquilibriumX;
-    }
-
     set angularLimitsY(limits) {
         if (!this._angularLimitsY.equals(limits)) {
             this._angularLimitsY.copy(limits);
@@ -157,39 +130,6 @@ class JointComponent extends Component {
 
     get angularLimitsY() {
         return this._angularLimitsY;
-    }
-
-    set angularSpringY(value) {
-        if (this._angularSpringY !== value) {
-            this._angularSpringY = value;
-            this._constraint.enableSpring(4, value);
-        }
-    }
-
-    get angularSpringY() {
-        return this._angularSpringY;
-    }
-
-    set angularStiffnessY(value) {
-        if (this._angularStiffnessY !== value) {
-            this._angularStiffnessY = value;
-            this._constraint.setStiffness(4, value);
-        }
-    }
-
-    get angularStiffnessY() {
-        return this._angularStiffnessY;
-    }
-
-    set angularEquilibriumY(value) {
-        if (this._angularEquilibriumY !== value) {
-            this._angularEquilibriumY = value;
-            this._constraint.setEquilibriumPoint(4, value);
-        }
-    }
-
-    get angularEquilibriumY() {
-        return this._angularEquilibriumY;
     }
 
     set angularLimitsZ(limits) {
@@ -203,39 +143,6 @@ class JointComponent extends Component {
         return this._angularLimitsZ;
     }
 
-    set angularSpringZ(value) {
-        if (this._angularSpringZ !== value) {
-            this._angularSpringZ = value;
-            this._constraint.enableSpring(5, value);
-        }
-    }
-
-    get angularSpringZ() {
-        return this._angularSpringZ;
-    }
-
-    set angularStiffnessZ(value) {
-        if (this._angularStiffnessZ !== value) {
-            this._angularStiffnessZ = value;
-            this._constraint.setStiffness(5, value);
-        }
-    }
-
-    get angularStiffnessZ() {
-        return this._angularStiffnessZ;
-    }
-
-    set angularEquilibriumZ(value) {
-        if (this._angularEquilibriumZ !== value) {
-            this._angularEquilibriumZ = value;
-            this._constraint.setEquilibriumPoint(5, value);
-        }
-    }
-
-    get angularEquilibriumZ() {
-        return this._angularEquilibriumZ;
-    }
-
     set linearLimitsX(limits) {
         if (!this._linearLimitsX.equals(limits)) {
             this._linearLimitsX.copy(limits);
@@ -245,39 +152,6 @@ class JointComponent extends Component {
 
     get linearLimitsX() {
         return this._linearLimitsX;
-    }
-
-    set linearSpringX(value) {
-        if (this._linearSpringX !== value) {
-            this._linearSpringX = value;
-            this._constraint.enableSpring(0, value);
-        }
-    }
-
-    get linearSpringX() {
-        return this._linearSpringX;
-    }
-
-    set linearStiffnessX(value) {
-        if (this._linearStiffnessX !== value) {
-            this._linearStiffnessX = value;
-            this._constraint.setStiffness(0, value);
-        }
-    }
-
-    get linearStiffnessX() {
-        return this._linearStiffnessX;
-    }
-
-    set linearEquilibriumX(value) {
-        if (this._linearEquilibriumX !== value) {
-            this._linearEquilibriumX = value;
-            this._constraint.setEquilibriumPoint(0, value);
-        }
-    }
-
-    get linearEquilibriumX() {
-        return this._linearEquilibriumX;
     }
 
     set linearLimitsY(limits) {
@@ -291,39 +165,6 @@ class JointComponent extends Component {
         return this._linearLimitsY;
     }
 
-    set linearSpringY(value) {
-        if (this._linearSpringY !== value) {
-            this._linearSpringY = value;
-            this._constraint.enableSpring(1, value);
-        }
-    }
-
-    get linearSpringY() {
-        return this._linearSpringY;
-    }
-
-    set linearStiffnessY(value) {
-        if (this._linearStiffnessY !== value) {
-            this._linearStiffnessY = value;
-            this._constraint.setStiffness(1, value);
-        }
-    }
-
-    get linearStiffnessY() {
-        return this._linearStiffnessY;
-    }
-
-    set linearEquilibriumY(value) {
-        if (this._linearEquilibriumY !== value) {
-            this._linearEquilibriumY = value;
-            this._constraint.setEquilibriumPoint(1, value);
-        }
-    }
-
-    get linearEquilibriumY() {
-        return this._linearEquilibriumY;
-    }
-
     set linearLimitsZ(limits) {
         if (!this._linearLimitsZ.equals(limits)) {
             this._linearLimitsZ.copy(limits);
@@ -335,46 +176,13 @@ class JointComponent extends Component {
         return this._linearLimitsZ;
     }
 
-    set linearSpringZ(value) {
-        if (this._linearSpringZ !== value) {
-            this._linearSpringZ = value;
-            this._constraint.enableSpring(2, value);
-        }
-    }
-
-    get linearSpringZ() {
-        return this._linearSpringZ;
-    }
-
-    set linearStiffnessZ(value) {
-        if (this._linearStiffnessZ !== value) {
-            this._linearStiffnessZ = value;
-            this._constraint.setStiffness(2, value);
-        }
-    }
-
-    get linearStiffnessZ() {
-        return this._linearStiffnessZ;
-    }
-
-    set linearEquilibriumZ(value) {
-        if (this._linearEquilibriumZ !== value) {
-            this._linearEquilibriumZ = value;
-            this._constraint.setEquilibriumPoint(2, value);
-        }
-    }
-
-    get linearEquilibriumZ() {
-        return this._linearEquilibriumZ;
-    }
-
     _convertTransform(pcTransform, ammoTransform) {
-        var pos = pcTransform.getTranslation();
-        var rot = new Quat();
+        const pos = pcTransform.getTranslation();
+        const rot = new Quat();
         rot.setFromMat4(pcTransform);
 
-        var ammoVec = new Ammo.btVector3(pos.x, pos.y, pos.z);
-        var ammoQuat = new Ammo.btQuaternion(rot.x, rot.y, rot.z, rot.w);
+        const ammoVec = new Ammo.btVector3(pos.x, pos.y, pos.z);
+        const ammoQuat = new Ammo.btQuaternion(rot.x, rot.y, rot.z, rot.w);
 
         ammoTransform.setOrigin(ammoVec);
         ammoTransform.setRotation(ammoQuat);
@@ -384,13 +192,13 @@ class JointComponent extends Component {
     }
 
     _updateAngularLimits() {
-        var constraint = this._constraint;
+        const constraint = this._constraint;
         if (constraint) {
-            var alx = this._angularLimitsX;
-            var aly = this._angularLimitsY;
-            var alz = this._angularLimitsZ;
+            const alx = this._angularLimitsX;
+            const aly = this._angularLimitsY;
+            const alz = this._angularLimitsZ;
 
-            var limits = new Ammo.btVector3(alx.x, aly.x, alz.x);
+            const limits = new Ammo.btVector3(alx.x, aly.x, alz.x);
             constraint.setAngularLowerLimit(limits);
             limits.setValue(alx.y, aly.y, alz.y);
             constraint.setAngularUpperLimit(limits);
@@ -399,13 +207,13 @@ class JointComponent extends Component {
     }
 
     _updateLinearLimits() {
-        var constraint = this._constraint;
+        const constraint = this._constraint;
         if (constraint) {
-            var llx = this._linearLimitsX;
-            var lly = this._linearLimitsY;
-            var llz = this._linearLimitsZ;
+            const llx = this._linearLimitsX;
+            const lly = this._linearLimitsY;
+            const llz = this._linearLimitsZ;
 
-            var limits = new Ammo.btVector3(llx.x, lly.x, llz.x);
+            const limits = new Ammo.btVector3(llx.x, lly.x, llz.x);
             constraint.setLinearLowerLimit(limits);
             limits.setValue(llx.y, lly.y, llz.y);
             constraint.setLinearUpperLimit(limits);
@@ -417,31 +225,33 @@ class JointComponent extends Component {
         if (this._entityA && this._entityA.rigidbody) {
             this._destroyConstraint();
 
-            var mat = new Mat4();
+            const mat = new Mat4();
 
-            var bodyA = this._entityA.rigidbody.body;
-            var bodyB;
-            var frameA = new Ammo.btTransform();
+            const bodyA = this._entityA.rigidbody.body;
+            bodyA.activate();
 
-            var jointWtm = this.entity.getWorldTransform();
+            const jointWtm = this.entity.getWorldTransform();
 
-            var entityWtm = this._entityA.getWorldTransform();
-            var invEntityWtm = entityWtm.clone().invert();
-            mat.mul2(invEntityWtm, jointWtm);
+            const entityAWtm = this._entityA.getWorldTransform();
+            const invEntityAWtm = entityAWtm.clone().invert();
+            mat.mul2(invEntityAWtm, jointWtm);
 
+            const frameA = new Ammo.btTransform();
             this._convertTransform(mat, frameA);
 
             if (this._entityB && this._entityB.rigidbody) {
-                bodyB = this._entityB.rigidbody.body;
-                var frameB = new Ammo.btTransform();
+                const bodyB = this._entityB.rigidbody.body;
+                bodyB.activate();
 
-                entityWtm = this._entityB.getWorldTransform();
-                invEntityWtm = entityWtm.clone().invert();
-                mat.mul2(invEntityWtm, jointWtm);
+                const entityBWtm = this._entityB.getWorldTransform();
+                const invEntityBWtm = entityBWtm.clone().invert();
+                mat.mul2(invEntityBWtm, jointWtm);
 
+                const frameB = new Ammo.btTransform();
                 this._convertTransform(mat, frameB);
 
                 this._constraint = new Ammo.btGeneric6DofSpringConstraint(bodyA, bodyB, frameA, frameB, !this._enableCollision);
+
                 Ammo.destroy(frameB);
             } else {
                 this._constraint = new Ammo.btGeneric6DofSpringConstraint(bodyA, frameA, !this._enableCollision);
@@ -449,44 +259,31 @@ class JointComponent extends Component {
 
             Ammo.destroy(frameA);
 
+            const axis = ['X', 'Y', 'Z', 'X', 'Y', 'Z'];
+
+            for (let i = 0; i < 6; i++) {
+                const type = i < 3 ? 'linear' : 'angular';
+                this._constraint.enableSpring(i, this['_' + type + 'Spring' + axis[i]]);
+                this._constraint.setDamping(i, this['_' + type + 'Damping' + axis[i]]);
+                this._constraint.setEquilibriumPoint(i, this['_' + type + 'Equilibrium' + axis[i]]);
+                this._constraint.setStiffness(i, this['_' + type + 'Stiffness' + axis[i]]);
+            }
+
             this._constraint.setBreakingImpulseThreshold(this._breakForce);
-            this._constraint.enableSpring(0, this._linearSpringX);
-            this._constraint.setStiffness(0, this._linearStiffnessX);
-            this._constraint.setEquilibriumPoint(0, this._linearEquilibriumX);
-            this._constraint.enableSpring(1, this._linearSpringY);
-            this._constraint.setStiffness(1, this._linearStiffnessY);
-            this._constraint.setEquilibriumPoint(1, this._linearEquilibriumY);
-            this._constraint.enableSpring(2, this._linearSpringZ);
-            this._constraint.setStiffness(2, this._linearStiffnessZ);
-            this._constraint.setEquilibriumPoint(2, this._linearEquilibriumZ);
-            this._constraint.enableSpring(3, this._angularSpringX);
-            this._constraint.setStiffness(3, this._angularStiffnessX);
-            this._constraint.setEquilibriumPoint(3, this._angularEquilibriumX);
-            this._constraint.enableSpring(4, this._angularSpringY);
-            this._constraint.setStiffness(4, this._angularStiffnessY);
-            this._constraint.setEquilibriumPoint(4, this._angularEquilibriumY);
-            this._constraint.enableSpring(5, this._angularSpringZ);
-            this._constraint.setStiffness(5, this._angularStiffnessZ);
-            this._constraint.setEquilibriumPoint(5, this._angularEquilibriumZ);
 
             this._updateLinearLimits();
             this._updateAngularLimits();
 
-            var app = this.system.app;
-            var dynamicsWorld = app.systems.rigidbody.dynamicsWorld;
+            const app = this.system.app;
+            const dynamicsWorld = app.systems.rigidbody.dynamicsWorld;
             dynamicsWorld.addConstraint(this._constraint, !this._enableCollision);
-
-            bodyA.activate();
-            if (bodyB) {
-                bodyB.activate();
-            }
         }
     }
 
     _destroyConstraint() {
         if (this._constraint) {
-            var app = this.system.app;
-            var dynamicsWorld = app.systems.rigidbody.dynamicsWorld;
+            const app = this.system.app;
+            const dynamicsWorld = app.systems.rigidbody.dynamicsWorld;
             dynamicsWorld.removeConstraint(this._constraint);
 
             Ammo.destroy(this._constraint);
@@ -496,11 +293,13 @@ class JointComponent extends Component {
 
     initFromData(data) {
         const props = [
+            'angularDampingX', 'angularDampingY', 'angularDampingZ',
             'angularEquilibriumX', 'angularEquilibriumY', 'angularEquilibriumZ',
             'angularLimitsX', 'angularLimitsY', 'angularLimitsZ',
             'angularSpringX', 'angularSpringY', 'angularSpringZ',
             'angularStiffnessX', 'angularStiffnessY', 'angularStiffnessZ',
             'breakForce', 'enableCollision', 'enabled', 'entityA', 'entityB',
+            'linearDampingX', 'linearDampingY', 'linearDampingZ',
             'linearEquilibriumX', 'linearEquilibriumY', 'linearEquilibriumZ',
             'linearLimitsX', 'linearLimitsY', 'linearLimitsZ',
             'linearSpringX', 'linearSpringY', 'linearSpringZ',
@@ -535,5 +334,38 @@ class JointComponent extends Component {
         this.fire('remove');
     }
 }
+
+const functionMap = {
+    Damping: 'setDamping',
+    Equilibrium: 'setEquilibriumPoint',
+    Spring: 'enableSpring',
+    Stiffness: 'setStiffness'
+};
+
+['linear', 'angular'].forEach(type => {
+    ['Damping', 'Equilibrium', 'Spring', 'Stiffness'].forEach(name => {
+        ['X', 'Y', 'Z'].forEach(axis => {
+            const prop = type + name + axis;
+            const propInternal = '_' + prop;
+
+            let index = (type === 'linear') ? 0 : 3;
+            if (axis === 'Y') index += 1;
+            if (axis === 'Z') index += 2;
+
+            Object.defineProperty(JointComponent.prototype, prop, {
+                get: function () {
+                    return this[propInternal];
+                },
+
+                set: function (value) {
+                    if (this[propInternal] !== value) {
+                        this[propInternal] = value;
+                        this._constraint[functionMap[name]](index, value);
+                    }
+                }
+            });
+        });
+    });
+});
 
 export { JointComponent };
