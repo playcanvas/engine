866778d01f3ca88284a6321437ba7bf902cafc17
Martin Valigursky <mvaligursky@snap.com>
2021-03-24 13:41:50 +0000

    reproject compatible with IE11

--- src/graphics/reproject-texture.js
+++ src/graphics/reproject-texture.js
@@ -76,6 +76,7 @@ function reprojectTexture(device, source, target, specularPower = 1, numSamples 
         "#define SOURCE_FUNC " + sourceFunc + "\n" +
         "#define TARGET_FUNC " + targetFunc + "\n" +
         "#define NUM_SAMPLES " + numSamples + "\n\n" +
+        "#define NUM_SAMPLES_SQRT " + Math.sqrt(numSamples).toFixed(1) + "\n\n" +
         shaderChunks.reprojectPS,
         processFunc + decodeFunc + encodeFunc + sourceFunc + targetFunc,
         null,
