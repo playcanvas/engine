10111c4443ccee5570bbc9633c0a3b5331f5b7cd
Martin Valigursky <mvaligursky@snap.com>
2021-04-16 17:35:48 +0100

    code formatting

--- src/polyfill/set.js
+++ src/polyfill/set.js
@@ -1,8 +1,14 @@
 // implementation of isEqual functionality for the Set
 if (!Set.prototype.isEqual) {
     Set.prototype.isEqual = function(otherSet) {
-        if(this.size !== otherSet.size) return false;
-        for(let item of this) if(!otherSet.has(item)) return false;
+        if (this.size !== otherSet.size) {
+            return false;
+        }
+        for (let item of this) {
+            if (!otherSet.has(item)) {
+                return false;
+            }
+        }
         return true;
     };
 }
