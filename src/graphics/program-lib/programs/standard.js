b99cf8990f7da8fe1c5fbdc770d1b6655fd31171
Martin Valigursky <mvaligursky@snap.com>
2021-04-20 13:52:02 +0100

    one more shader chunk

--- src/graphics/program-lib/programs/standard.js
+++ src/graphics/program-lib/programs/standard.js
@@ -454,59 +454,6 @@ var standard = {
         return code;
     },
 
-    _addClusteredLighting: function () {
-        return `
-
-        // world space position to 3d integer cell cordinates in the cluster structure
-        vec3 cellCoords = floor((vPositionW - clusterBoundsMin) * clusterCellsCountByBoundsSize);
-
-        // no lighting when cell coordinate is out of range
-        if (!(any(lessThan(cellCoords, vec3(0.0))) || any(greaterThanEqual(cellCoords, clusterCellsMax)))) {
-
-            // cell index (mapping from 3d cell coordinates to linear memory)
-            float cellIndex = dot(clusterCellsDot, cellCoords);
-
-            // convert cell index to uv coordinates
-            float clusterV = floor(cellIndex * clusterTextureSize.y);
-            float clusterU = cellIndex - (clusterV * clusterTextureSize.x);
-            clusterV = (clusterV + 0.5) * clusterTextureSize.z;
-
-            // loop over maximum possible number of supported light cells
-            const float maxLightCells = 256.0 / 4.0;  // 8 bit index, each stores 4 lights
-            for (float lightCellIndex = 0.5; lightCellIndex < maxLightCells; lightCellIndex++) {
-
-                vec4 lightIndices = texture2D(clusterWorldTexture, vec2(clusterTextureSize.y * (clusterU + lightCellIndex), clusterV));
-                vec4 indices = lightIndices * 255.0;
-
-                if (indices.x <= 0.0)
-                    break;
-
-                EvaluateClusterLight(indices.x);
-
-                if (indices.y <= 0.0)
-                    break;
-
-                EvaluateClusterLight(indices.y);
-
-                if (indices.z <= 0.0)
-                    break;
-
-                EvaluateClusterLight(indices.z);
-
-                if (indices.w <= 0.0)
-                    break;
-
-                EvaluateClusterLight(indices.w);
-
-                // end of the cell array
-                if (lightCellIndex > clusterPixelsPerCell) {
-                    break;
-                }
-            }
-        }
-        `;
-    },
-
     createShaderDefinition: function (device, options) {
         var i, p;
         var lighting = options.lights.length > 0;
@@ -1460,7 +1407,7 @@ var standard = {
             usesSpot = true;
 
             const clusterTextureFormat = WorldClusters.lightTextureFormat === WorldClusters.FORMAT_FLOAT ? "FLOAT" : "8BIT";
-            code += `#define CLUSTER_TEXTURE_${clusterTextureFormat}`;
+            code += `#define CLUSTER_TEXTURE_${clusterTextureFormat}\n`;
             code += chunks.clusteredLightPS;
         }
 
@@ -1601,7 +1548,7 @@ var standard = {
 
                 usesLinearFalloff = true;
                 hasPointLights = true;
-                code += this._addClusteredLighting();
+                code += chunks.clusteredLightLoopPS;
             }
 
             for (i = 0; i < options.lights.length; i++) {
