14e649ad9ee6a435314000912fb6870026bce44e
Will Eastcott <will@playcanvas.com>
2021-04-22 09:14:57 +0100

    Improve WebGL context creation (#3111)
    * Improve WebGL context creation
    
    * Lint fix

--- src/graphics/graphics-device.js
+++ src/graphics/graphics-device.js
@@ -246,7 +246,6 @@ class GraphicsDevice extends EventHandler {
     constructor(canvas, options) {
         super();
 
-        var i;
         this.canvas = canvas;
         this._enableAutoInstancing = false;
         this.autoInstancingMaxObjects = 16384;
@@ -290,18 +289,15 @@ class GraphicsDevice extends EventHandler {
         // Retrieve the WebGL context
         var preferWebGl2 = (options && options.preferWebGl2 !== undefined) ? options.preferWebGl2 : true;
 
-        var names = preferWebGl2 ? ["webgl2", "experimental-webgl2", "webgl", "experimental-webgl"] :
-            ["webgl", "experimental-webgl"];
+        var names = preferWebGl2 ? ["webgl2", "webgl", "experimental-webgl"] : ["webgl", "experimental-webgl"];
         var gl = null;
         options = options || {};
         options.stencil = true;
-        for (i = 0; i < names.length; i++) {
-            try {
-                gl = canvas.getContext(names[i], options);
-            } catch (e) { }
+        for (let i = 0; i < names.length; i++) {
+            gl = canvas.getContext(names[i], options);
 
             if (gl) {
-                this.webgl2 = preferWebGl2 && i < 2;
+                this.webgl2 = (names[i] === 'webgl2');
                 break;
             }
         }
@@ -613,7 +609,7 @@ class GraphicsDevice extends EventHandler {
         this._drawCallsPerFrame = 0;
         this._shaderSwitchesPerFrame = 0;
         this._primsPerFrame = [];
-        for (i = PRIMITIVE_POINTS; i <= PRIMITIVE_TRIFAN; i++) {
+        for (let i = PRIMITIVE_POINTS; i <= PRIMITIVE_TRIFAN; i++) {
             this._primsPerFrame[i] = 0;
         }
         this._renderTargetCreationTime = 0;
