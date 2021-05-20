5c65621c6d71d69fbc2362e586eb22858111c889
unknown <Leonidas>
2021-05-20 12:25:37 +0200

    Lint fixes

--- scripts/posteffects/posteffect-pixelate.js
+++ scripts/posteffects/posteffect-pixelate.js
@@ -7,7 +7,7 @@
  * @augments PostEffect
  * @param {GraphicsDevice} graphicsDevice - The graphics device of the application.
  */
- function PixelateEffect(graphicsDevice) {
+function PixelateEffect(graphicsDevice) {
     pc.PostEffect.call(this, graphicsDevice);
 
     this.shader = new pc.Shader(graphicsDevice, {
