1bce3ec5c50b0cc87535fdd59b8ba7bae5870370
Elliott Thompson <ellth92@gmail.com>
2021-06-03 11:37:31 +0100

    add an example to the anim events docs

--- src/anim/evaluator/anim-events.js
+++ src/anim/evaluator/anim-events.js
@@ -4,6 +4,14 @@
  * @classdesc AnimEvents stores a sorted array of animation events which should fire sequentially during the playback of an pc.AnimTrack.
  * @description Create a new collection of animation events.
  * - @param {object[]}  events- An array of animation events.
+ * @example
+ * const events = new AnimEvents([
+ *     {
+ *         name: 'my_event',
+ *         time: 1.3 // given in seconds
+ *     }
+ * ]);
+ * animTrack.events = events;
  */
 class AnimEvents {
     constructor(events) {
