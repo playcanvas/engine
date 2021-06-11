bfb663ccf351651b2941da3330dac6e89ea414f0
Steven <yaustar@users.noreply.github.com>
2021-06-11 17:19:54 +0100

    Move app destroy to frame end (#3230)
    * Moved app destroy to frame end
    
    * Revert "Update input devices as a first thing in the frame (#3216)"
    
    This reverts commit d7256676ac0b3c30ac04c90d17c5642a3c070b9b.
    
    * Revert "Revert "Update input devices as a first thing in the frame (#3216)""
    
    This reverts commit 7c1c60a7ad3a6d441d22937dee89a839ea360f60.
    
    * Added flag to know if the engine is in the middle of a tick
    
    * Got rid of the double function destroy declaration
    
    * Renamed property, fix scope issue in makeTick
    
    Co-authored-by: Steven Yau <social@stevenyau.co.uk>
    Co-authored-by: Steven Yau <syau@snapchat.com>

--- src/framework/application.js
+++ src/framework/application.js
@@ -408,6 +408,9 @@ class Application extends EventHandler {
 
         app = this;
 
+        this._destroyRequested = false;
+        this._inFrameUpdate = false;
+
         this._time = 0;
         this.timeScale = 1;
         this.maxDeltaTime = 0.1; // Maximum delta is 0.1s or 10 fps.
@@ -1954,11 +1957,17 @@ class Application extends EventHandler {
     /**
      * @function
      * @name Application#destroy
-     * @description Destroys application and removes all event listeners.
+     * @description Destroys application and removes all event listeners at the end of the current engine frame update.
+     * However, if called outside of the engine frame update, calling destroy() will destroy the application immediately.
      * @example
      * this.app.destroy();
      */
     destroy() {
+        if (this._inFrameUpdate) {
+            this._destroyRequested = true;
+            return;
+        }
+
         var i, l;
         var canvasId = this.graphicsDevice.canvas.id;
 
@@ -2162,6 +2171,7 @@ var makeTick = function (_app) {
         application._fillFrameStats();
         // #endif
 
+        application._inFrameUpdate = true;
         application.fire("frameupdate", ms);
 
         if (frame) {
@@ -2190,6 +2200,12 @@ var makeTick = function (_app) {
 
         if (application.vr && application.vr.display && application.vr.display.presenting) {
             application.vr.display.submitFrame();
+        }
+
+        application._inFrameUpdate = false;
+
+        if (application._destroyRequested) {
+            application.destroy();
         }
     };
 };
