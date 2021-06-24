7a25c8774bcc430f89cd37e05b1938aef12e258f
Martin Valigursky <mvaligursky@snapchat.com>
2021-06-24 11:52:16 +0100

    Updated post effects engine example to more showcase the effects

--- examples/src/examples/graphics/post-effects.tsx
+++ examples/src/examples/graphics/post-effects.tsx
@@ -13,8 +13,8 @@ class PostEffectsExample extends Example {
             <AssetLoader name='bokeh' type='script' url='static/scripts/posteffects/posteffect-bokeh.js' />
             <AssetLoader name='sepia' type='script' url='static/scripts/posteffects/posteffect-sepia.js' />
             <AssetLoader name='vignette' type='script' url='static/scripts/posteffects/posteffect-vignette.js' />
-            <AssetLoader name='statue' type='container' url='static/assets/models/statue.glb' />
             <AssetLoader name='font' type='font' url='static/assets/fonts/arial.json' />
+            <AssetLoader name='helipad.dds' type='cubemap' url='static/assets/cubemaps/helipad.dds' data={{ type: pc.TEXTURETYPE_RGBM }}/>
         </>;
     }
 
@@ -29,150 +29,110 @@ class PostEffectsExample extends Example {
         app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
         app.setCanvasResolution(pc.RESOLUTION_AUTO);
 
-        function createMaterial(colors: pc.Color[]) {
+        // setup skydome
+        app.scene.setSkybox(assets['helipad.dds'].resources);
+        app.scene.skyboxMip = 3;
+        app.scene.exposure = 1.6;
+
+        // helper function to create a 3d primitive including its material
+        function createPrimitive(primitiveType: string, position: pc.Vec3, scale: pc.Vec3, brightness: number, allowEmissive = true) {
+
+            // create a material
             const material = new pc.StandardMaterial();
-            for (const param in colors) {
-                // @ts-ignore engine-tsd
-                material[param] = colors[param];
+            material.shininess = 40;
+            material.metalness = 0.6;
+            material.useMetalness = true;
+
+            // random diffuse and emissive color
+            material.diffuse = new pc.Color(brightness, brightness, brightness);
+            if (allowEmissive && Math.random() < 0.15) {
+                material.emissive = new pc.Color(Math.random(), Math.random(), Math.random());
             }
             material.update();
-            return material;
+
+            // create the primitive using the material
+            const primitive = new pc.Entity();
+            primitive.addComponent('render', {
+                type: primitiveType,
+                material: material
+            });
+
+            // set position and scale and add it to scene
+            primitive.setLocalPosition(position);
+            primitive.setLocalScale(scale);
+            app.root.addChild(primitive);
+
+            return primitive;
         }
 
-        app.scene.ambientLight = new pc.Color(0.4, 0.4, 0.4);
+        // create the ground plane from the boxes
+        for (let x = -2; x <= 2; x += 0.5) {
+            for (let z = 0; z <= 10; z += 0.5) {
+                createPrimitive("box", new pc.Vec3(x * 40, -5, z * 40), new pc.Vec3(18, 2, 18), Math.random());
+            }
+        }
 
-        // Generate some materials to assign to scene objects
-        const gray = createMaterial({
-            // @ts-ignore engine-tsd
-            ambient: new pc.Color(0.1, 0.1, 0.1),
-            diffuse: new pc.Color(0.5, 0.5, 0.5)
-        });
-        const white = createMaterial({
-            // @ts-ignore engine-tsd
-            emissive: new pc.Color(1, 1, 1)
-        });
-        const blue = createMaterial({
-            // @ts-ignore engine-tsd
-            diffuse: new pc.Color(0, 0, 0),
-            emissive: new pc.Color(0, 0, 1)
-        });
+        // create the towers from the boxes
+        let scale = 16;
+        for (let y = 0; y <= 6; y++) {
+            for (let x = -1; x <= 1; x += 2) {
+                for (let z = 0; z <= 10; z += 2) {
+                    createPrimitive("box", new pc.Vec3(x * 40, 2 + y * 10, z * 40), new pc.Vec3(scale, scale, scale), Math.random());
+                }
+            }
+            scale -= 2;
+        }
 
-        const entity = assets.statue.resource.instantiateRenderEntity({
-            castShadows: true
-        });
-        app.root.addChild(entity);
+        // create a sphere which represents the point of focus for the bokeh filter
+        const focusPrimitive = createPrimitive("sphere", pc.Vec3.ZERO, new pc.Vec3(10, 10, 10), 2.8, false);
 
-        // Create an Entity with a camera component
+        // add an omni light as a child of this sphere
+        const light = new pc.Entity();
+        light.addComponent("light", {
+            type: "omni",
+            color: pc.Color.WHITE,
+            intensity: 4,
+            range: 100,
+            castShadows: false
+        });
+        focusPrimitive.addChild(light);
+
+        // Create an Entity with a camera component, and attach postprocessing effects scripts on it
         const camera: any = new pc.Entity();
         camera.addComponent("camera", {
             clearColor: new pc.Color(0.4, 0.45, 0.5),
-            farClip: 50
+            farClip: 500
         });
         camera.addComponent("script");
         camera.script.create("bloom", {
             attributes: {
-                bloomIntensity: 1,
-                bloomThreshold: 0.1,
-                blurAmount: 4
+                bloomIntensity: 0.8,
+                bloomThreshold: 0.8,
+                blurAmount: 15
             }
         });
         camera.script.create("sepia", {
             attributes: {
-                amount: 0.7
+                amount: 0.4
             }
         });
         camera.script.create("vignette", {
             attributes: {
-                darkness: 2,
-                offset: 1
+                darkness: 1,
+                offset: 1.2
             }
         });
         camera.script.create("bokeh", {
             attributes: {
-                aperture: 1,
-                maxBlur: 0.02
+                aperture: 0.1,
+                maxBlur: 0.01
             }
         });
 
-        camera.addComponent("script");
-        camera.script.create("bloom", {
-            attributes: {
-                bloomIntensity: 1,
-                bloomThreshold: 0.1,
-                blurAmount: 4
-            }
-        });
-        camera.script.create("sepia", {
-            attributes: {
-                amount: 0.7
-            }
-        });
-        camera.script.create("vignette", {
-            attributes: {
-                darkness: 2,
-                offset: 1
-            }
-        });
-        camera.script.create("bokeh", {
-            attributes: {
-                aperture: 1,
-                maxBlur: 0.02
-            }
-        });
-
-        camera.translate(0, 7, 24);
-        camera.rotate(0, 0, 0);
-
-        // Create an Entity for the ground
-        const ground = new pc.Entity();
-        ground.addComponent("model", {
-            type: "box"
-        });
-        ground.setLocalScale(50, 1, 50);
-        ground.setLocalPosition(0, -0.5, 0);
-        ground.model.material = gray;
-
-
-        // Create an spot light
-        const light = new pc.Entity();
-        light.addComponent("light", {
-            type: "spot",
-            color: new pc.Color(1, 1, 1),
-            outerConeAngle: 60,
-            innerConeAngle: 40,
-            range: 100,
-            intensity: 1,
-            castShadows: true,
-            shadowBias: 0.005,
-            normalOffsetBias: 0.01,
-            shadowResolution: 2048
-        });
-
-        const cone = new pc.Entity();
-        cone.addComponent("model", {
-            type: "cone"
-        });
-        cone.model.material = white;
-        light.addChild(cone);
-
-        // Create a point light
-        const pointlight = new pc.Entity();
-        pointlight.addComponent("light", {
-            type: "point",
-            color: new pc.Color(0, 0, 1),
-            range: 100,
-            intensity: 1
-        });
-        pointlight.addComponent("model", {
-            type: "sphere"
-        });
-        pointlight.model.material = blue;
-
-        // Add Entities into the scene hierarchy
+        // position the camera in the world
+        camera.setLocalPosition(0, 30, -60);
+        camera.lookAt(0, 0, 100);
         app.root.addChild(camera);
-        app.root.addChild(light);
-        app.root.addChild(pointlight);
-        app.root.addChild(ground);
 
         // Allow user to toggle individual post effects
         app.keyboard.on("keydown", function (e) {
@@ -199,15 +159,7 @@ class PostEffectsExample extends Example {
             }
         }, this);
 
-        // Simple update loop to rotate the light
-        const radius = 20;
-        const height = 5;
-        let angle = 0;
-
-        const pointRadius = 5;
-        const pointHeight = 10;
-
-        // Create a 2D screen
+        // Create a 2D screen to place UI on
         const screen = new pc.Entity();
         screen.addComponent("screen", {
             referenceResolution: new pc.Vec2(1280, 720),
@@ -215,11 +167,9 @@ class PostEffectsExample extends Example {
             scaleMode: pc.SCALEMODE_BLEND,
             screenSpace: true
         });
-
         app.root.addChild(screen);
 
-        // create text to show which effects are enabled
-        // Create a basic text element
+        // create a text element to show which effects are enabled
         const text = new pc.Entity();
         text.addComponent("element", {
             anchor: new pc.Vec4(0.1, 0.1, 0.5, 0.5),
@@ -231,38 +181,34 @@ class PostEffectsExample extends Example {
         });
         screen.addChild(text);
 
+        // update things every frame
+        let angle = 0;
         app.on("update", function (dt) {
-            angle += 20 * dt;
-            if (angle > 360) {
-                angle -= 360;
-            }
-            if (entity) {
-                light.lookAt(entity.getPosition());
-                light.rotateLocal(90, 0, 0);
-                light.setLocalPosition(radius * Math.sin(angle * pc.math.DEG_TO_RAD), height, radius * Math.cos(angle * pc.math.DEG_TO_RAD));
+            angle += dt;
 
-                pointlight.setLocalPosition(pointRadius * Math.sin(-2 * angle * pc.math.DEG_TO_RAD), pointHeight, pointRadius * Math.cos(-2 * angle * pc.math.DEG_TO_RAD));
+            // rotate the skydome
+            app.scene.skyboxRotation = new pc.Quat().setFromEulerAngles(0, angle * 20, 0);
 
-                // @ts-ignore engine-tsd
-                if (camera.script.bokeh) {
-            // @ts-ignore engine-tsd
-                    camera.script.bokeh.focus = light.getLocalPosition().z - camera.getLocalPosition().z;
-                }
-            }
+            // move the focus sphere in the world closer and further away
+            const focusPosition = new pc.Vec3(0, 10, Math.abs(Math.sin(angle * 0.1)) * 400);
+            focusPrimitive.setPosition(focusPosition);
+
+            // set the focus distance to the bohek effect
+            // - it's a negative distance between the camera and the focus sphere
+            camera.script.bokeh.focus = -focusPosition.sub(camera.getPosition()).length();
 
             // update text showing which post effects are enabled
-            if (text) {
-                text.element.text = `[Key 1] Bloom: ${camera.script.bloom.enabled}
-    [Key 2] Sepia: ${camera.script.sepia.enabled}
-    [Key 3] Vignette: ${camera.script.vignette.enabled}
-    [Key 4] Bokeh: ${camera.script.bokeh.enabled}
-    [Key 5] Post-process UI: ${camera.camera.disablePostEffectsLayer !== pc.LAYERID_UI}`;
-            }
+            text.element.text =
+                `[Key 1] Bloom: ${camera.script.bloom.enabled}\n` +
+                `[Key 2] Sepia: ${camera.script.sepia.enabled}\n` +
+                `[Key 3] Vignette: ${camera.script.vignette.enabled}\n` +
+                `[Key 4] Bokeh: ${camera.script.bokeh.enabled}\n` +
+                `[Key 5] Post-process UI: ${camera.camera.disablePostEffectsLayer !== pc.LAYERID_UI}\n`;
 
-            // display the depth texture
+            // display the depth textur if bokeh is enabled
             if (camera.script.bokeh.enabled) {
                 // @ts-ignore engine-tsd
-                app.renderDepthTexture(0.6, 0.7, 0.6, 0.3);
+                app.renderDepthTexture(0.7, -0.7, 0.5, 0.5);
             }
         });
     }
