975188763b125cb9f4c299d200590c9ad15f330e
Donovan Hutchence <slimbuck7@gmail.com>
2021-06-28 19:32:58 +0100

    debug

--- src/resources/parser/texture/basis.js
+++ src/resources/parser/texture/basis.js
@@ -1,6 +1,21 @@
 import { http } from '../../../net/http.js';
 
-import { ADDRESS_CLAMP_TO_EDGE, ADDRESS_REPEAT, TEXHINT_ASSET } from '../../../graphics/constants.js';
+import {
+    ADDRESS_CLAMP_TO_EDGE,
+    ADDRESS_REPEAT, TEXHINT_ASSET,
+    PIXELFORMAT_ETC1,
+    PIXELFORMAT_ETC2_RGBA,
+    PIXELFORMAT_DXT1,
+    PIXELFORMAT_DXT5,
+    PIXELFORMAT_PVRTC_4BPP_RGB_1,
+    PIXELFORMAT_PVRTC_4BPP_RGBA_1,
+    PIXELFORMAT_ASTC_4x4,
+    PIXELFORMAT_ATC_RGB,
+    PIXELFORMAT_ATC_RGBA,
+    PIXELFORMAT_R8_G8_B8_A8,
+    PIXELFORMAT_R5_G6_B5,
+    PIXELFORMAT_R4_G4_B4_A4
+} from '../../../graphics/constants.js';
 import { Texture } from '../../../graphics/texture.js';
 
 import { basisTranscode } from '../../basis.js';
@@ -63,6 +78,24 @@ class BasisParser {
 
     // our async transcode call provides the neat structure we need to create the texture instance
     open(url, data, device) {
+
+        const mapping = {
+            [PIXELFORMAT_ETC1]: 'etc1',
+            [PIXELFORMAT_ETC2_RGBA]: 'etc2',
+            [PIXELFORMAT_DXT1]: 'dxt1',
+            [PIXELFORMAT_DXT5]: 'dxt5',
+            [PIXELFORMAT_PVRTC_4BPP_RGB_1]: 'pvrtc',
+            [PIXELFORMAT_PVRTC_4BPP_RGBA_1]: 'pvrtc_a',
+            [PIXELFORMAT_ASTC_4x4]: 'astc',
+            [PIXELFORMAT_ATC_RGB]: 'atc',
+            [PIXELFORMAT_ATC_RGBA]: 'atc_a',
+            [PIXELFORMAT_R8_G8_B8_A8]: 'rgba8',
+            [PIXELFORMAT_R5_G6_B5]: 'rgb565',
+            [PIXELFORMAT_R4_G4_B4_A4]: 'rgba4'
+        };
+
+        console.log(`${data.width} x ${data.height} x ${data.levels.length} x ${mapping[data.format]}`);
+
         const texture = new Texture(device, {
             name: url,
             // #if _PROFILER
