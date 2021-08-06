1786fdb3f30b839bef41ff5d2e57cca056714952
Donovan Hutchence <slimbuck7@gmail.com>
2021-08-06 10:15:20 +0100

    Support ktx2 supercompressed textures (#3380)

--- src/resources/parser/texture/basis.js
+++ src/resources/parser/texture/basis.js
@@ -29,7 +29,7 @@ class BasisParser {
         http.get(
             url.load,
             options,
-            function (err, result) {
+            (err, result) => {
                 if (err) {
                     callback(err, result);
                 } else {
