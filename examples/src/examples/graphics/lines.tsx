58db218cede39e6e2d0f4b0a2fb2b254297b4289
Martin Valigursky <mvaligursky@snapchat.com>
2021-08-17 15:48:58 +0100

    moved example to new format

--- examples/src/examples/graphics/lines.tsx
+++ examples/src/examples/graphics/lines.tsx
@@ -1,55 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>PlayCanvas Lines</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-    <link rel="icon" type="image/png" href="../playcanvas-favicon.png" />
-    <script src="../../build/playcanvas.dbg.js"></script>
-    <script src="../../build/playcanvas-extras.js"></script>
-    <style>
-        body { 
-            margin: 0;
-            overflow-y: hidden;
-        }
-    </style>
-</head>
+import React from 'react';
+import * as pc from 'playcanvas/build/playcanvas.js';
+import { AssetLoader } from '../../app/helpers/loader';
+import Example from '../../app/example';
 
-<body>
-    <!-- The canvas element -->
-    <canvas id="application-canvas"></canvas>
+class LinesExample extends Example {
+    static CATEGORY = 'Graphics';
+    static NAME = 'Lines';
 
-    <script>
-        function createMaterial(colors) {
-            var material = new pc.StandardMaterial();
-            for (var param in colors) {
-                material[param] = colors[param];
-            }
-            material.update();
-            return material;
-        }
-    </script>
+    load() {
+        return <>
+            <AssetLoader name='helipad.dds' type='cubemap' url='static/assets/cubemaps/helipad.dds' data={{ type: pc.TEXTURETYPE_RGBM }}/>
+        </>;
+    }
 
-    <!-- The script -->
-    <script>
-        var canvas = document.getElementById("application-canvas");
+    // @ts-ignore: override class function
+    example(canvas: HTMLCanvasElement, assets: {'helipad.dds': pc.Asset}): void {
 
         // Create the application and start the update loop
-        var app = new pc.Application(canvas);
+        const app = new pc.Application(canvas, {});
         app.start();
+
+        // setup skydome
+        app.scene.skyboxMip = 2;
+        app.scene.exposure = 0.7;
+        app.scene.setSkybox(assets['helipad.dds'].resources);
 
         // Set the canvas to fill the window and automatically change resolution to be the same as the canvas size
         app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
         app.setCanvasResolution(pc.RESOLUTION_AUTO);
 
-        window.addEventListener("resize", function () {
-            app.resizeCanvas(canvas.width, canvas.height);
-        });
-
-        var miniStats = new pcx.MiniStats(app);
-
         // Create an Entity with a camera component
-        var camera = new pc.Entity();
+        const camera = new pc.Entity();
         camera.addComponent("camera", {
             clearColor: new pc.Color(0.1, 0.1, 0.1)
         });
@@ -58,7 +39,7 @@
         app.root.addChild(camera);
 
         // Create a directional light
-        var directionallight = new pc.Entity();
+        const directionallight = new pc.Entity();
         directionallight.addComponent("light", {
             type: "directional",
             color: pc.Color.WHITE,
@@ -67,22 +48,20 @@
         });
         app.root.addChild(directionallight);
 
-
         // create a circle of sphere meshes
-        var spheres = [];
-        var numInstances = 10;
-        var i;
-        for (i = 0; i < numInstances; i++) {
-            var entity = new pc.Entity();
+        const spheres: Array<pc.Entity> = [];
+        const numInstances = 10;
+        for (let i = 0; i < numInstances; i++) {
+            const entity = new pc.Entity();
             entity.setLocalScale(4, 4, 4);
 
             // use material with random color
-            var material = new pc.StandardMaterial();
+            const material = new pc.StandardMaterial();
             material.diffuse = new pc.Color(Math.random(), Math.random(), Math.random());
             material.update();
 
             // create render component
-            var component = entity.addComponent("render", {
+            entity.addComponent("render", {
                 type: (i % 2 ? "sphere" : "cylinder"),
                 material: material
             });
@@ -97,39 +76,38 @@
         }
 
         // helper function to generate elevation of a point with [x, y] coordinates
-        function groundElevation(time, x, z) {
+        function groundElevation(time: number, x: number, z: number) {
             return Math.sin(time + 0.2 * x) * 2 + Math.cos(time * 0.2 + 0.5 * z + 0.2 * x);
         }
 
         // helper function to generate a color for 3d point by lerping between green and red color
         // based on its y coordinate
-        function groundColor(point) {
+        function groundColor(point: pc.Vec3) {
             return new pc.Color().lerp(pc.Color.GREEN, pc.Color.RED, pc.math.clamp((point.y + 3) * 0.25, 0, 1));
         }
 
         // access two layers, used to render lines to them
-        var worldLayer = app.scene.layers.getLayerByName("World");
-        var immediateLayer = app.scene.layers.getLayerById(pc.LAYERID_IMMEDIATE);
+        const worldLayer = app.scene.layers.getLayerByName("World");
+        const immediateLayer = app.scene.layers.getLayerById(pc.LAYERID_IMMEDIATE);
 
         // bounding box of the meshes
-        var bounds = new pc.BoundingBox();
+        const bounds = new pc.BoundingBox();
 
-        // update loop executes each frame
-        var time = 0;
+        // Set an update function on the app's update event
+        let time = 0;
         app.on("update", function (dt) {
             time += dt;
 
             // generate grid of lines
-            var x, y;
-            var positions = [];
-            var colors = [];
-            for (x = 1; x < 60; x++) {
-                for (z = 1; z < 60; z++) {
+            const positions = [];
+            const colors = [];
+            for (let x = 1; x < 60; x++) {
+                for (let z = 1; z < 60; z++) {
 
                     // generate 3 points: one start point, one along x and one along z axis
-                    var pt1 = new pc.Vec3(x, groundElevation(time, x, z), z);
-                    var pt2 = new pc.Vec3(x - 1, groundElevation(time, x - 1, z), z);
-                    var pt3 = new pc.Vec3(x, groundElevation(time, x, z - 1), z - 1);
+                    const pt1 = new pc.Vec3(x, groundElevation(time, x, z), z);
+                    const pt2 = new pc.Vec3(x - 1, groundElevation(time, x - 1, z), z);
+                    const pt3 = new pc.Vec3(x, groundElevation(time, x, z - 1), z - 1);
 
                     // add line connecting points along z axis
                     if (x > 1) {
@@ -149,11 +127,11 @@
             app.renderLines(positions, colors);
 
             // handle the array of sphere meshes
-            for (i = 0; i < numInstances; i++) {
+            for (let i = 0; i < numInstances; i++) {
 
                 // move them equally spaced out around in the circle
-                var offset = i * Math.PI * 2 / numInstances;
-                entity = spheres[i];
+                const offset = i * Math.PI * 2 / numInstances;
+                const entity = spheres[i];
                 entity.setLocalPosition(
                     30 + 20 * Math.sin(time * 0.2 + offset),
                     5 + 2 * Math.sin(time + 3 * i / numInstances),
@@ -161,36 +139,41 @@
                 );
 
                 // update bounding box for all meshes
-                var thisBounds = entity.render.meshInstances[0].aabb;
+                const thisBounds = entity.render.meshInstances[0].aabb;
                 if (i === 0) {
                     bounds.copy(thisBounds);
                 } else {
                     bounds.add(thisBounds);
                 }
 
-                var options = {
+                const options = {
 
                     // half of them are rendered in immediate layer, the other half in world layer
                     layer: i < 0.5 * numInstances ? immediateLayer : worldLayer,
 
                     // half of them uses depth testing, the other does not, and so lines show through the sphere
                     depthTest: i < 0.5 * numInstances
-                }
+                };
 
                 // render either wiframe sphere or cube around the sphere objects
                 if (i % 2) {
+                    // @ts-ignore
                     app.renderWireSphere(entity.getLocalPosition(), 2.2, pc.Color.YELLOW, 30, options);
                 } else {
 
                     // rotate the cylinders
                     entity.rotate((i + 1) * dt, 4 * (i + 1) * dt, 6 * (i + 1) * dt);
+                    // @ts-ignore
                     app.renderWireCube(entity.getWorldTransform(), pc.Color.CYAN, options);
                 }
             }
 
             // wireframe box for the bounds around all meshes
+            // @ts-ignore
             app.renderWireAlignedBox(bounds.getMin(), bounds.getMax(), pc.Color.WHITE);
+
         });
-    </script>
-</body>
-</html>
+    }
+}
+
+export default LinesExample;
