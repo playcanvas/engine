7e0e6da0b594689d52a7e3ef2adca1fddb34e2f6
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2021-08-29 02:58:30 -0700

    Update src/scene/scene.js
    Co-authored-by: Will Eastcott <will@playcanvas.com>

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -85,7 +85,7 @@ import { StandardMaterial } from './materials/standard-material.js';
  *
  * Defaults to {@link BAKE_COLORDIR}.
  * @property {boolean} lightmapFilterEnabled Enables bilateral filter on runtime baked color lightmaps, which removes the noise and banding while preserving the edges. Defaults to false;
- * Note that the filtering takes place in the image space of the lightmap, and it does not filter across lightmap UV space seams, often making the seams more visible. It's important to balance the strenght of the filter with number of samples used
+* Note that the filtering takes place in the image space of the lightmap, and it does not filter across lightmap UV space seams, often making the seams more visible. It's important to balance the strength of the filter with number of samples used
  * for lightmap baking to limit the visible artifacts.
  * @property {number} lightmapFilterRange A range parameter of the bilateral filter. It's used when {@link Scene#lightmapFilterEnabled} is enabled. Larger value applies more widespread blur. This needs to be positive non-zero value. Defaults to 10.
  * @property {number} lightmapFilterSmoothness A spatial parameter of the bilateral filter. It's used when {@link Scene#lightmapFilterEnabled} is enabled. Larger value blurs less similar colors. This needs to be positive non-zero value. Defaults to 0.2.
