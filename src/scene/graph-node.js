3db919afdfb3b591df24f88b2d8886200a9be42d
Martin Valigursky <mvaligursky@snapchat.com>
2021-08-31 15:52:01 +0100

    remove test if fire exists

--- src/scene/graph-node.js
+++ src/scene/graph-node.js
@@ -1101,10 +1101,7 @@ class GraphNode extends EventHandler {
 
     // fires an event on all children of the node
     _fireOnHierarchy(name) {
-        if (this.fire) {
-            this.fire(name);
-        }
-
+        this.fire(name);
         for (let i = 0; i < this._children.length; i++) {
             this._children[i]._fireOnHierarchy(name);
         }
