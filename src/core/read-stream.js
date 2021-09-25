a669a654c49485bca1466d1d50993fe05bba8740
Donovan Hutchence <slimbuck7@gmail.com>
2021-09-25 10:38:42 +0100

    revert unwanted change

--- src/core/read-stream.js
+++ src/core/read-stream.js
@@ -33,18 +33,6 @@ class ReadStream {
         return this.offset - amount;
     }
 
-    readChar() {
-        return String.fromCharCode(this.dataView.getUint8(this.offset++));
-    }
-
-    readChars(numChars) {
-        let result = '';
-        for (let i = 0; i < numChars; ++i) {
-            result += this.readChar();
-        }
-        return result;
-    }
-
     readU8() {
         return this.dataView.getUint8(this.offset++);
     }
@@ -90,6 +78,4 @@ class ReadStream {
     }
 }
 
-export {
-    ReadStream
-};
+export { ReadStream };
