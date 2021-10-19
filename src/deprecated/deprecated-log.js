05b3cf746a9c0acfbff236164643b7193c3ab2f3
Martin Valigursky <mvaligursky@snapchat.com>
2021-10-19 10:17:36 +0100

    no longer need _DEBUG

--- src/deprecated/deprecated-log.js
+++ src/deprecated/deprecated-log.js
@@ -1,16 +1,12 @@
 // class responsible for logging each unique deprecated message one time
 class DeprecatedLog {
-    // #if _DEBUG
     static _loggedMessages = new Set();
-    // #endif
 
     static log(message) {
-        // #if _DEBUG
         if (!DeprecatedLog._loggedMessages.has(message)) {
             DeprecatedLog._loggedMessages.add(message);
             console.warn(message);
         }
-        // #endif
     }
 }
 
