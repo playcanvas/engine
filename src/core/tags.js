8a41febdeb16b9a897fed7325ffdb7f295afc689
Will Eastcott <will@playcanvas.com>
2021-11-21 13:19:21 +0000

    Fix Tags declarations

--- src/core/tags.js
+++ src/core/tags.js
@@ -1,15 +1,17 @@
 import { EventHandler } from './event-handler.js';
 
 /**
- * @class
- * @name Tags
+ * Set of tag names. Tags are automatically available on {@link Entity} and {@link Asset} as `tags`
+ * field.
+ *
  * @augments EventHandler
- * @classdesc Set of tag names.
- * @description Create an instance of a Tags.
- * @param {object} [parent] - Parent object who tags belong to.
- * Note: Tags are automatically available on {@link Entity} and {@link Asset} as `tags` field.
  */
 class Tags extends EventHandler {
+    /**
+     * Create an instance of a Tags.
+     *
+     * @param {object} [parent] - Parent object who tags belong to.
+     */
     constructor(parent) {
         super();
 
