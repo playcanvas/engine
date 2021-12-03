c34d4da319d5b02368a9a77ec718b34a293284fd
mrmaxm <core@moka.co>
2021-12-03 14:47:09 +0200

    Add SHADOWPASS #define to shader (#3746)
    * add shadowpass define
    
    * add #define to shaders for different passes
    
    * rename PASS define string and move to separate method

--- src/graphics/program-lib/programs/standard.js
+++ src/graphics/program-lib/programs/standard.js
@@ -277,6 +277,17 @@ const standard = {
         }
     },
 
+    _getPassDefineString: function (pass) {
+        if (pass === SHADER_PICK) {
+            return '#define PICK_PASS\n';
+        } else if (pass === SHADER_DEPTH) {
+            return '#define DEPTH_PASS\n';
+        } else if (pass >= SHADER_SHADOW && pass <= 17) {
+            return '#define SHADOW_PASS\n';
+        }
+        return '';
+    },
+
     _vsAddTransformCode: function (code, device, chunks, options) {
         code += chunks.transformVS;
 
@@ -511,6 +522,7 @@ const standard = {
             chunks = customChunks;
         }
 
+        code += this._getPassDefineString(options.pass);
 
         // code += chunks.baseVS;
         code = this._vsAddBaseCode(code, device, chunks, options);
@@ -785,6 +797,8 @@ const standard = {
         }
 
         code += options.forceFragmentPrecision ? "precision " + options.forceFragmentPrecision + " float;\n\n" : precisionCode(device);
+
+        code += this._getPassDefineString(options.pass);
 
         if (options.pass === SHADER_PICK) {
             // ##### PICK PASS #####
