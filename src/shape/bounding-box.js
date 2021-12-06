63e942c131bdd520b9e03e4b148988833e92188f
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2021-12-06 01:13:34 -0800

    Update src/shape/bounding-box.js
    Co-authored-by: Will Eastcott <will@playcanvas.com>

--- src/shape/bounding-box.js
+++ src/shape/bounding-box.js
@@ -21,7 +21,7 @@ class BoundingBox {
     constructor(center = new Vec3(), halfExtents = new Vec3(0.5, 0.5, 0.5)) {
 
         Debug.assert(!Object.isFrozen(center), `The constructor of 'BoundingBox' does not accept a constant (frozen) object as a 'center' parameter`);
-        Debug.assert(!Object.isFrozen(center), `The constructor of 'BoundingBox' does not accept a constant (frozen) object as a 'halfExtents' parameter`);
+        Debug.assert(!Object.isFrozen(halfExtents), `The constructor of 'BoundingBox' does not accept a constant (frozen) object as a 'halfExtents' parameter`);
 
         this.center = center;
         this.halfExtents = halfExtents;
