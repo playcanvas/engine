1b389b1e22452c33a0c06811ca1560cf8cf37565
Martin Valigursky <mvaligursky@snapchat.com>
2021-12-14 13:22:14 +0000

    updated example

--- examples/src/examples/graphics/box-reflection.tsx
+++ examples/src/examples/graphics/box-reflection.tsx
@@ -14,8 +14,6 @@ import BindingTwoWay from '@playcanvas/pcui/BindingTwoWay';
 import SelectInput from '@playcanvas/pcui/SelectInput/component';
 // @ts-ignore: library file import
 import { Observer } from '@playcanvas/observer';
-// @ts-ignore: library file import
-import BooleanInput from '@playcanvas/pcui/BooleanInput/component';
 
 class ClusteredShadowsOmniExample extends Example {
     static CATEGORY = 'Graphics';
@@ -26,33 +24,27 @@ class ClusteredShadowsOmniExample extends Example {
             <AssetLoader name='script' type='script' url='static/scripts/camera/orbit-camera.js' />
             <AssetLoader name='script' type='script' url='static/scripts/utils/cubemap-renderer.js' />
             <AssetLoader name='normal' type='texture' url='static/assets/textures/normal-map.png' />
-            <AssetLoader name="xmas_negx" type="texture" url="static/assets/cubemaps/xmas_faces/xmas_negx.png" />
-            <AssetLoader name="xmas_negy" type="texture" url="static/assets/cubemaps/xmas_faces/xmas_negy.png" />
-            <AssetLoader name="xmas_negz" type="texture" url="static/assets/cubemaps/xmas_faces/xmas_negz.png" />
-            <AssetLoader name="xmas_posx" type="texture" url="static/assets/cubemaps/xmas_faces/xmas_posx.png" />
-            <AssetLoader name="xmas_posy" type="texture" url="static/assets/cubemaps/xmas_faces/xmas_posy.png" />
-            <AssetLoader name="xmas_posz" type="texture" url="static/assets/cubemaps/xmas_faces/xmas_posz.png" />
         </>;
     }
 
     controls(data: Observer) {
         return <>
             <Panel headerText='Settings'>
-                {<LabelGroup text='Filter'>
-                    <SelectInput binding={new BindingTwoWay()} link={{ observer: data, path: 'settings.shadowType' }} type="number" options={[
-                        { v: pc.SHADOW_PCF1, t: 'PCF1' },
-                        { v: pc.SHADOW_PCF3, t: 'PCF3' },
-                        { v: pc.SHADOW_PCF5, t: 'PCF5' }
+                {<LabelGroup text='Update'>
+                    <SelectInput binding={new BindingTwoWay()} link={{ observer: data, path: 'settings.updateFrequency' }} type="number" options={[
+                        { v: 1, t: 'Every frame' },
+                        { v: 5, t: 'Every 5 frames' },
+                        { v: 30, t: 'Every 30 frames' }
                     ]} />
                 </LabelGroup>}
-                <LabelGroup text='Shadow Res'>
-                    <SliderInput binding={new BindingTwoWay()} link={{ observer: data, path: 'settings.shadowAtlasResolution' }} min={512} max={4096} precision={0}/>
+                <LabelGroup text='Shininess'>
+                    <SliderInput binding={new BindingTwoWay()} link={{ observer: data, path: 'settings.shininess' }} min={0} max={100} precision={0}/>
                 </LabelGroup>
-                <LabelGroup text='Shadows On'>
-                    <BooleanInput type='toggle' binding={new BindingTwoWay()} link={{ observer: data, path: 'settings.shadowsEnabled' }} value={data.get('settings.shadowsEnabled')}/>
+                <LabelGroup text='Metalness'>
+                    <SliderInput binding={new BindingTwoWay()} link={{ observer: data, path: 'settings.metalness' }} min={0} max={1} precision={2}/>
                 </LabelGroup>
-                <LabelGroup text='Cookies On'>
-                    <BooleanInput type='toggle' binding={new BindingTwoWay()} link={{ observer: data, path: 'settings.cookiesEnabled' }} value={data.get('settings.cookiesEnabled')}/>
+                <LabelGroup text='Bumpiness'>
+                    <SliderInput binding={new BindingTwoWay()} link={{ observer: data, path: 'settings.bumpiness' }} min={0} max={1} precision={2}/>
                 </LabelGroup>
             </Panel>
         </>;
@@ -65,59 +57,123 @@ class ClusteredShadowsOmniExample extends Example {
         app.start();
 
         data.set('settings', {
-            shadowAtlasResolution: 1300,     // shadow map resolution storing all shadows
-            shadowType: pc.SHADOW_PCF3,      // shadow filter type
-            shadowsEnabled: true,
-            cookiesEnabled: true
+            updateFrequency: 1,
+            shininess: 90,
+            metalness: 0.5,
+            bumpiness: 0.2
         });
 
         // Set the canvas to fill the window and automatically change resolution to be the same as the canvas size
         app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
         app.setCanvasResolution(pc.RESOLUTION_AUTO);
 
-        const roomMaterial = new pc.StandardMaterial();
-        roomMaterial.diffuse = new pc.Color(0.7, 0.7, 0.7);
+        // create a layer for object that do not render into reflection cubemap
+        const excludedLayer = new pc.Layer({ name: "Excluded" });
+        app.scene.layers.push(excludedLayer);
 
-        // normal map
+        // get world layer
+        const worldLayer = app.scene.layers.getLayerByName("World");
+
+        // create an envAtlas texture, which will hold a prefiltering lighting generated from the cubemap.
+        // This represents a reflection prefiltered for different levels of roughness
+        const envAtlas = new pc.Texture(app.graphicsDevice, {
+            width: 512,
+            height: 512,
+            format: pc.PIXELFORMAT_R8_G8_B8_A8,
+            type: pc.TEXTURETYPE_RGBM,
+            projection: pc.TEXTUREPROJECTION_EQUIRECT,
+            addressU: pc.ADDRESS_CLAMP_TO_EDGE,
+            addressV: pc.ADDRESS_CLAMP_TO_EDGE,
+            mipmaps: false
+        });
+
+        // material for the walls
+        const roomMaterial = new pc.StandardMaterial();
+        roomMaterial.useMetalness = true;
+        roomMaterial.diffuse = pc.Color.WHITE;
         roomMaterial.normalMap = assets.normal.resource;
         roomMaterial.normalMapTiling.set(5, 5);
-        roomMaterial.bumpiness = 0.7;
-        roomMaterial.shininess = 40;
-        roomMaterial.metalness = 0.3;
-        roomMaterial.useMetalness = true;
+        roomMaterial.bumpiness = 0.1;
+        roomMaterial.shininess = 90;
+        roomMaterial.envAtlas = envAtlas; // use reflection from env atlas
+        roomMaterial.metalness = 0.5;
+
+        // the material uses box projected cubemap for reflections. Set its bounding box the the size of the room
+        // so that the reflections line up
+        roomMaterial.cubeMapProjection = pc.CUBEPROJ_BOX;
+        roomMaterial.cubeMapProjectionBox = new pc.BoundingBox(new pc.Vec3(0, 200, 0), new pc.Vec3(400, 200, 400));
         roomMaterial.update();
 
+        // material for the magenta emissive beams
+        const emissiveMaterial = new pc.StandardMaterial();
+        emissiveMaterial.emissive = pc.Color.MAGENTA;
+        emissiveMaterial.diffuse = pc.Color.BLACK;
+        emissiveMaterial.update();
+
+        // material for the white sphere representing an omni light
+        const lightMaterial = new pc.StandardMaterial();
+        lightMaterial.emissive = pc.Color.WHITE;
+        lightMaterial.diffuse = pc.Color.BLACK;
+        lightMaterial.update();
+
+        // material for the reflective sphere in the center
+        const sphereMaterial = new pc.StandardMaterial();
+        sphereMaterial.useMetalness = true;
+        sphereMaterial.diffuse = pc.Color.WHITE;
+        sphereMaterial.normalMap = assets.normal.resource;
+        sphereMaterial.normalMapTiling.set(5, 5);
+        sphereMaterial.bumpiness = 0.7;
+        sphereMaterial.shininess = 90;
+        sphereMaterial.metalness = 0.6;
+        sphereMaterial.envAtlas = envAtlas; // use reflection from env atlas
+        sphereMaterial.update();
+
+        // set up video playback into a texture
+        const videoTexture = new pc.Texture(app.graphicsDevice, {
+            format: pc.PIXELFORMAT_R5_G6_B5,
+            mipmaps: false,
+            minFilter: pc.FILTER_LINEAR,
+            magFilter: pc.FILTER_LINEAR,
+            addressU: pc.ADDRESS_CLAMP_TO_EDGE,
+            addressV: pc.ADDRESS_CLAMP_TO_EDGE
+        });
+
+        // create a HTML element with the video
+        const video: HTMLVideoElement = document.createElement('video');
+        video.id = 'vid';
+        video.loop = true;
+        video.muted = true;
+        video.autoplay = true;
+        video.playsInline = true;
+        video.crossOrigin = "anonymous";
+        video.setAttribute('style', 'display: block; width: 1px; height: 1px; position: absolute; opacity: 0; z-index: -1000; top: 0px; pointer-events: none');
+        video.src = 'static/assets/video/SampleVideo_1280x720_1mb.mp4';
+        document.body.append(video);
+        video.addEventListener('canplaythrough', function () {
+            videoTexture.setSource(video);
+        });
+
+        // materials used on the TV screen to display the video texture
+        const screenMaterial = new pc.StandardMaterial();
+        screenMaterial.useLighting = false;
+        screenMaterial.emissiveMap = videoTexture;
+        screenMaterial.update();
+
         // helper function to create a 3d primitive including its material
-        function createPrimitive(primitiveType: string, position: pc.Vec3, scale: pc.Vec3, material: pc.Material = null) {
-
-            // create a material
-            if (!material) {
-                material = new pc.StandardMaterial();
-                material.diffuse = new pc.Color(0.7, 0.7, 0.7);
-
-                // normal map
-                material.normalMap = assets.normal.resource;
-                material.normalMapTiling.set(5, 5);
-                material.bumpiness = 0.7;
-
-                // emissive
-                material.emissive = new pc.Color(Math.random() * 3, Math.random() * 3, Math.random() * 3);
-                material.update();
-            }
+        function createPrimitive(primitiveType: string, position: pc.Vec3, scale: pc.Vec3, material: pc.Material) {
 
             // create the primitive using the material
             const primitive = new pc.Entity();
             primitive.addComponent('render', {
                 type: primitiveType,
-                material: material
+                material: material,
+                layers: [worldLayer.id, excludedLayer.id]
             });
 
             // set position and scale and add it to scene
             primitive.setLocalPosition(position);
             primitive.setLocalScale(scale);
             app.root.addChild(primitive);
-
-            return primitive;
         }
 
         // create the ground plane from the boxes
@@ -127,48 +183,66 @@ class ClusteredShadowsOmniExample extends Example {
         // walls
         createPrimitive("box", new pc.Vec3(400, 200, 0), new pc.Vec3(2, 400, 800), roomMaterial);
         createPrimitive("box", new pc.Vec3(-400, 200, 0), new pc.Vec3(2, 400, 800), roomMaterial);
-        createPrimitive("box", new pc.Vec3(0, 200, 400), new pc.Vec3(800, 400, 0), roomMaterial);
         createPrimitive("box", new pc.Vec3(0, 200, -400), new pc.Vec3(800, 400, 0), roomMaterial);
+        createPrimitive("box", new pc.Vec3(0, 200, 400), new pc.Vec3(800, 400, 0), roomMaterial);
 
-        const numTowers = 7;
-        for (let i = 0; i < numTowers; i++) {
-            let scale = 25;
-            const fraction = i / numTowers * Math.PI * 2;
-            const radius = (i % 2) ? 340 : 210;
-            for (let y = 0; y <= 7; y++) {
-                const prim = createPrimitive("box", new pc.Vec3(radius * Math.sin(fraction), 2 + y * 25, radius * Math.cos(fraction)), new pc.Vec3(scale, scale, scale));
-                prim.setLocalEulerAngles(Math.random() * 360, Math.random() * 360, Math.random() * 360);
-            }
-            scale -= 1.5;
-        }
+        // emissive pillars
+        createPrimitive("box", new pc.Vec3(400, 200, -50), new pc.Vec3(20, 400, 20), emissiveMaterial);
+        createPrimitive("box", new pc.Vec3(400, 200, 50), new pc.Vec3(20, 400, 20), emissiveMaterial);
+        createPrimitive("box", new pc.Vec3(-400, 200, 50), new pc.Vec3(20, 400, 20), emissiveMaterial);
+        createPrimitive("box", new pc.Vec3(-400, 200, -50), new pc.Vec3(20, 400, 20), emissiveMaterial);
+        createPrimitive("box", new pc.Vec3(0, 400, 50), new pc.Vec3(800, 20, 20), emissiveMaterial);
+        createPrimitive("box", new pc.Vec3(0, 400, -50), new pc.Vec3(800, 20, 20), emissiveMaterial);
+
+        // screen
+        createPrimitive("box", new pc.Vec3(0, 200, 400), new pc.Vec3(500, 250, 5), screenMaterial);
+
+        // sphere
+        createPrimitive("sphere", new pc.Vec3(0, 150, 0), new pc.Vec3(150, 150, 150), sphereMaterial);
+
+        // create an omni light white orbits the room to avoid it being completely dark
+        const lightOmni = new pc.Entity();
+        lightOmni.addComponent("light", {
+            type: "omni",
+            layers: [excludedLayer.id], // add it to excluded layer, we don't want the light captured in the reflection
+            castShadows: false,
+            color: pc.Color.WHITE,
+            intensity: 4,
+            range: 1000
+        });
+
+        // add a white sphere to light so that we can see where it is. This sphere is excluded from the reflections.
+        lightOmni.addComponent("render", {
+            type: "sphere",
+            layers: [excludedLayer.id],
+            material: lightMaterial
+        });
+        lightOmni.setLocalScale(20, 20, 20);
+        app.root.addChild(lightOmni);
 
         // create an Entity with a camera component
         const camera = new pc.Entity();
         camera.addComponent("camera", {
-            fov: 80,
-            clearColor: new pc.Color(0.9, 0.9, 0.9),
+            fov: 100,
+            layers: [worldLayer.id, excludedLayer.id],
             farClip: 1500
         });
-
-        // and position it in the world
-        camera.setLocalPosition(300, 120, 25);
+        camera.setLocalPosition(270, 90, -260);
 
         // add orbit camera script with a mouse and a touch support
         camera.addComponent("script");
         camera.script.create("orbitCamera", {
             attributes: {
                 inertiaFactor: 0.2,
-                focusEntity: app.root,
-                distanceMax: 400
+                distanceMax: 390,
+                frameOnStart: false
             }
         });
         camera.script.create("orbitCameraInputMouse");
         camera.script.create("orbitCameraInputTouch");
         app.root.addChild(camera);
 
-
-
-        // create a probe object with cubemapRenderer script component which takes care of rendering dynamic cubemap
+        // create a probe object with cubemapRenderer script which takes care of rendering dynamic cubemap
         const probe = new pc.Entity();
         probe.addComponent('script');
 
@@ -181,46 +255,75 @@ class ClusteredShadowsOmniExample extends Example {
             // priority - render before world camera
             priority: -1,
 
+            // only render meshes on the worldLayer (and not excluded layer)
+            layers: [worldLayer.id],
+
             // disable as this is not a camera that renders cube map but only a container for properties for cube map rendering
             enabled: false
         });
 
+        // Add a cubemap renderer script, which renders to a cubemap of size 128 with mipmaps, which is directly useable
+        // as a lighting source for envAtlas generation
+        // Position it in the center of the room.
         probe.script.create('cubemapRenderer', {
             attributes: {
-                resolution: 256,
+                resolution: 128,
                 mipmaps: true,
                 depth: true
             }
         });
-
+        probe.setPosition(0, 200, 0);
         app.root.addChild(probe);
 
+        // handle onCubemapPostRender event fired by the cubemapRenderer when all faces of the cubemap are done rendering
+        probe.on('onCubemapPostRender', () => {
 
-        // handle HUD changes - update properties on the scene
-        data.on('*:set', (path: string, value: any) => {
-            const pathArray = path.split('.');
+            // prefilter just rendered cubemap into envAtlas, so that it can be used for reflection during the rest of the frame
             // @ts-ignore
-            lighting[pathArray[1]] = value;
+            pc.EnvLighting.generateAtlas(probe.script.cubemapRenderer.cubeMap, {
+                target: envAtlas
+            });
         });
 
         // Set an update function on the app's update event
         let time = 0;
+        let updateProbeCount = 1;
+        let updateVideo = true;
         app.on("update", function (dt: number) {
             time += dt * 0.3;
 
-            const srcCube = probe.script.cubemapRenderer.cubeMap;
+            // Update the video data to the texture every other frame
+            if (updateVideo) {
+                videoTexture.upload();
+            }
+            updateVideo = !updateVideo;
 
-            const lightingSource = pc.EnvLighting.generateLightingSource(srcCube);
-            const atlas = pc.EnvLighting.generateAtlas(lightingSource);
+            // move the light around
+            lightOmni.setLocalPosition(300 * Math.sin(time), 300, 300 * Math.cos(time));
 
-            // roomMaterial.envAtlas = atlas;
-            // roomMaterial.update();
+            // update the reflection probe as needed
+            updateProbeCount--;
+            if (updateProbeCount <= 0) {
+                probe.enabled = true;
+                updateProbeCount = data.get('settings.updateFrequency');
+            } else {
+                probe.enabled = false;
+            }
 
-            app.scene.envAtlas = atlas;
+            // update material properties based on settings
+            const shininess = data.get('settings.shininess');
+            const metalness = data.get('settings.metalness');
+            const bumpiness = data.get('settings.bumpiness');
 
-            // @ts-ignore engine-tsd
-            app.drawTexture(-0.6, 0.7, 0.8, 0.4, atlas);
+            roomMaterial.shininess = shininess;
+            roomMaterial.metalness = metalness;
+            roomMaterial.bumpiness = bumpiness;
+            roomMaterial.update();
 
+            sphereMaterial.shininess = shininess;
+            sphereMaterial.metalness = metalness;
+            sphereMaterial.bumpiness = bumpiness;
+            sphereMaterial.update();
         });
     }
 }
