a85f81a32723783e2cc8a1cd98975e9d0549adf0
Martin Valigursky <mvaligursky@snapchat.com>
2022-01-12 10:49:04 +0000

    lint - unused import

--- src/scene/renderer/forward-renderer.js
+++ src/scene/renderer/forward-renderer.js
@@ -37,7 +37,6 @@ import { StaticMeshes } from './static-meshes.js';
 import { CookieRenderer } from './cookie-renderer.js';
 import { LightCamera } from './light-camera.js';
 import { WorldClustersDebug } from '../lighting/world-clusters-debug.js';
-import { Color } from '../../index.js';
 
 /** @typedef {import('../../graphics/graphics-device.js').GraphicsDevice} GraphicsDevice */
 
