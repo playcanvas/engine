1998f56f15a0c58abbb5fb0adfc4caa99994689f
Will Eastcott <will@playcanvas.com>
2022-02-07 14:04:33 +0000

    Revert the object shorthand change since it's unrelated to this PR

--- rollup.config.js
+++ rollup.config.js
@@ -83,7 +83,7 @@ function shaderChunks(removeComments) {
             });
 
             return {
-                code,
+                code: code,
                 map: null
             };
         }
