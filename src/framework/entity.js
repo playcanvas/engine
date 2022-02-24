5db8e0cf3a1eceba8484349cd9988c38f57d0277
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2022-02-24 10:48:55 -0800

    Small refactor to move temp array to where its used from the Application (#4049)
    * Small refactor to move temp array to where its used from the Application
    
    * added ignore tag
    
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/framework/entity.js
+++ src/framework/entity.js
@@ -28,6 +28,12 @@ import { Application } from './application.js';
 /** @typedef {import('./components/sprite/component.js').SpriteComponent} SpriteComponent */
 
 /**
+ * @type {GraphNode[]}
+ * @ignore
+ */
+const _enableList = [];
+
+/**
  * The Entity is the core primitive of a PlayCanvas game. Generally speaking an object in your game
  * will consist of an {@link Entity}, and a set of {@link Component}s which are managed by their
  * respective {@link ComponentSystem}s. One of those components maybe a {@link ScriptComponent}
@@ -445,7 +451,7 @@ class Entity extends GraphNode {
      */
     _notifyHierarchyStateChanged(node, enabled) {
         let enableFirst = false;
-        if (node === this && this._app._enableList.length === 0)
+        if (node === this && _enableList.length === 0)
             enableFirst = true;
 
         node._beingEnabled = true;
@@ -453,7 +459,7 @@ class Entity extends GraphNode {
         node._onHierarchyStateChanged(enabled);
 
         if (node._onHierarchyStatePostChanged)
-            this._app._enableList.push(node);
+            _enableList.push(node);
 
         const c = node._children;
         for (let i = 0, len = c.length; i < len; i++) {
@@ -465,11 +471,11 @@ class Entity extends GraphNode {
 
         if (enableFirst) {
             // do not cache the length here, as enableList may be added to during loop
-            for (let i = 0; i < this._app._enableList.length; i++) {
-                this._app._enableList[i]._onHierarchyStatePostChanged();
+            for (let i = 0; i < _enableList.length; i++) {
+                _enableList[i]._onHierarchyStatePostChanged();
             }
 
-            this._app._enableList.length = 0;
+            _enableList.length = 0;
         }
     }
 
