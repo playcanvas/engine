a591376cfb8bc0580cf2274ab55146075c84bc09
kungfooman <lama12345@gmail.com>
2022-02-26 13:36:11 +0100

    Fix lint (adding descriptions to everything)

--- src/core/sort.js
+++ src/core/sort.js
@@ -1,10 +1,12 @@
 /**
- * @param {{priority: number}} a
- * @param {{priority: number}} b
+ * @param {{priority: number}} a - First object with priority property.
+ * @param {{priority: number}} b - Second object with priority property.
+ * @returns {number} A number indicating the relative position.
  */
-export const cmpPriority  = (a, b) => a.priority - b.priority;
+export const cmpPriority = (a, b) => a.priority - b.priority;
 
 /**
- * @param {Array<{priority: number}>} arr
+ * @param {Array<{priority: number}>} arr - Array to be sorted in place where each element contains an object with at least a priority property.
+ * @returns {Array<{priority: number}>} In place sorted array
  */
 export const sortPriority = arr => arr.sort(cmpPriority);
