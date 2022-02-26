37652566f35566a73b8c3b2d26dd69a21948652d
kungfooman <lama12345@gmail.com>
2022-02-26 13:18:18 +0100

    Remove two blank lines under JSDoc comment

--- src/core/sort.js
+++ src/core/sort.js
@@ -2,11 +2,9 @@
  * @param {{priority: number}} a
  * @param {{priority: number}} b
  */
-
 export const cmpPriority  = (a, b) => a.priority - b.priority;
 
 /**
  * @param {Array<{priority: number}>} arr
  */
-
 export const sortPriority = arr => arr.sort(cmpPriority);
