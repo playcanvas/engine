d3043577f44898326df31c00f11297314ca4a13c
Will Eastcott <will@playcanvas.com>
2022-02-28 09:29:14 +0000

    <100 col width

--- src/core/sort.js
+++ src/core/sort.js
@@ -7,7 +7,8 @@
 export const cmpPriority = (a, b) => a.priority - b.priority;
 
 /**
- * @param {Array<{priority: number}>} arr - Array to be sorted in place where each element contains an object with at least a priority property.
+ * @param {Array<{priority: number}>} arr - Array to be sorted in place where each element contains
+ * an object with at least a priority property.
  * @returns {Array<{priority: number}>} In place sorted array
  * @ignore
  */
