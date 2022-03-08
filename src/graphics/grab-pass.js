fec57736c2d0b1341aa4c08fcf6a4f0c6c503e19
Donovan Hutchence <slimbuck7@gmail.com>
2022-03-08 10:21:33 +0000

    revert changes to grabpass

--- src/graphics/grab-pass.js
+++ src/graphics/grab-pass.js
@@ -8,6 +8,7 @@ import { Texture } from "./texture.js";
  * A class used by the graphics device to handle the capture of the current framebuffer into a
  * texture to be used by following draw calls to implement refraction.
  *
+ * @ignore
  */
 class GrabPass {
     /**
@@ -15,26 +16,18 @@ class GrabPass {
      *
      * @param {GraphicsDevice} graphicsDevice - The graphics device used to manage this grab pass.
      * @param {boolean} useAlpha - Whether the grab pass should have an alpha channel.
-     * @param {boolean} useMipmaps - Whether to generate mipmaps. Only compatible under webgl2.
      */
-    constructor(device, useAlpha, useMipmaps = true, name = "texture_grabPass") {
+    constructor(device, useAlpha) {
         this.device = device;
         this.useAlpha = useAlpha;
+
         // for now we only support mipmaps on webgl2 devices due to the support for mipmaps for textures of any dimension
         // (captured framebuffer is usually not power of 2). See TODO in generateMipmaps function.
-        this.useMipmaps = useMipmaps && device.webgl2;
-        this.name = name;
+        this.useMipmaps = device.webgl2;
 
         this.texture = null;
         this.renderTarget = null;
         this.textureId = null;
-
-        const handler = () => {
-            this.destroy();
-        };
-
-        device.once('destroy', handler);
-        device.on('devicelost', handler);
     }
 
     /**
@@ -58,25 +51,28 @@ class GrabPass {
      * Create the texture and render target used by the grab pass.
      */
     create() {
-        const texture = new Texture(this.device, {
-            name: this.name,
-            format: this.useAlpha ? PIXELFORMAT_R8_G8_B8_A8 : PIXELFORMAT_R8_G8_B8,
-            minFilter: this.useMipmaps ? FILTER_LINEAR_MIPMAP_LINEAR : FILTER_LINEAR,
-            magFilter: FILTER_LINEAR,
-            addressU: ADDRESS_CLAMP_TO_EDGE,
-            addressV: ADDRESS_CLAMP_TO_EDGE,
-            mipmaps: this.useMipmaps
-        });
+        if (!this.texture) {
 
-        this.texture = texture;
+            const texture = new Texture(this.device, {
+                name: 'texture_grabPass',
+                format: this.useAlpha ? PIXELFORMAT_R8_G8_B8_A8 : PIXELFORMAT_R8_G8_B8,
+                minFilter: this.useMipmaps ? FILTER_LINEAR_MIPMAP_LINEAR : FILTER_LINEAR,
+                magFilter: FILTER_LINEAR,
+                addressU: ADDRESS_CLAMP_TO_EDGE,
+                addressV: ADDRESS_CLAMP_TO_EDGE,
+                mipmaps: this.useMipmaps
+            });
 
-        this.renderTarget = new RenderTarget({
-            colorBuffer: texture,
-            depth: false
-        });
+            this.texture = texture;
 
-        this.textureId = this.device.scope.resolve(texture.name);
-        this.textureId.setValue(texture);
+            this.renderTarget = new RenderTarget({
+                colorBuffer: texture,
+                depth: false
+            });
+
+            this.textureId = this.device.scope.resolve(texture.name);
+            this.textureId.setValue(texture);
+        }
     }
 
     /**
@@ -84,17 +80,14 @@ class GrabPass {
      *
      * @returns {boolean} - Whether the grab pass was successfully resolved or not.
      */
-     prepareTexture() {
+    update() {
         const device = this.device;
+        const gl = device.gl;
 
         // print error if we cannot grab framebuffer at this point
         if (!device.grabPassAvailable) {
             Debug.error("texture_grabPass cannot be used when rendering shadows and similar passes, exclude your object from rendering to them");
             return false;
-        }
-
-        if (!this.texture) {
-            this.create();
         }
 
         // render target currently being rendered to (these are null if default framebuffer is active)
@@ -104,7 +97,6 @@ class GrabPass {
         const texture = this.texture;
         const width = device.width;
         const height = device.height;
-        const gl = device.gl;
 
         DebugGraphics.pushGpuMarker(device, "grabPass");
 
@@ -149,16 +141,36 @@ class GrabPass {
             }
         }
 
+        DebugGraphics.popGpuMarker(device);
+
+        return true;
+    }
+
+    /**
+     * Generate mipmaps for the grab pass texture.
+     */
+    generateMipmaps() {
         // TODO: implement support for WebGL 1, which requires the texture to be a power of two, by
         // first downscaling the captured framebuffer texture to smaller power of 2 texture, and
         // then generate mipmaps and use it for rendering
         if (this.useMipmaps) {
             this.device.gl.generateMipmap(this.texture.impl._glTarget);
         }
+    }
 
-        DebugGraphics.popGpuMarker(device);
+    /**
+     * Grab a copy of the frame buffer to a texture and generate mipmaps for it.
+     *
+     * @returns {boolean} - Whether the grab pass was successfully updated or not.
+     */
+    prepareTexture() {
+        // capture the framebuffer
+        const updated = this.update();
+        if (updated) {
+            this.generateMipmaps();
+        }
 
-        return true;
+        return updated;
     }
 }
 
