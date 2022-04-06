3d90a9887f5880f3b9467468a07eb13b882071f4
Elliott Thompson <ellth92@gmail.com>
2022-04-06 16:51:11 +0100

    Array.from polyfill

--- examples/scripts/iframe/index.mjs
+++ examples/scripts/iframe/index.mjs
@@ -19,6 +19,7 @@ fs.copyFileSync(`${MAIN_DIR}/./node_modules/url-search-params-polyfill/index.js`
 fs.copyFileSync(`${MAIN_DIR}/./node_modules/promise-polyfill/dist/polyfill.min.js`, `${MAIN_DIR}/dist/build/promisePolyfill.js`);
 fs.copyFileSync(`${MAIN_DIR}/./node_modules/whatwg-fetch/dist/fetch.umd.js`, `${MAIN_DIR}/dist/build/fetchPolyfill.js`);
 fs.copyFileSync(`${MAIN_DIR}/./node_modules/regenerator-runtime/runtime.js`, `${MAIN_DIR}/dist/build/regeneratorRuntimePolyfill.js`);
+fs.copyFileSync(`${MAIN_DIR}/lib/arrayFromPolyfill.js`, `${MAIN_DIR}/dist/build/arrayFromPolyfill.js`);
 
 function loadHtmlTemplate(data) {
     const html = fs.readFileSync(`${MAIN_DIR}/scripts/iframe/index.mustache`, "utf8");
