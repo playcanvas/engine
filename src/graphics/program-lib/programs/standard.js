f306d7cad5a86358e49f6e2889be27b2746524e6
Donovan Hutchence <slimbuck7@gmail.com>
2022-04-27 12:36:52 +0100

    Fix for invalid shaders with Ao (#4215)

--- src/graphics/program-lib/programs/standard.js
+++ src/graphics/program-lib/programs/standard.js
@@ -1423,10 +1423,10 @@ const standard = {
             }
 
             if (options.fresnelModel > 0) code += "   getFresnel();\n";
+        }
 
-            if (useAo) {
-                code += "  getAO();\n";
-            }
+        if (useAo) {
+            code += "  getAO();\n";
         }
 
         if (addAmbient) {
