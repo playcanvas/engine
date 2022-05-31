599355ac20293e11d021776850b732f4f6db88e0
querielo <104348270+querielo@users.noreply.github.com>
2022-05-31 04:49:13 +0400

    Fix linear fog color mix: colors have to be mixed in linear space, not factor (#4283)
    Co-authored-by: kirill.osipov@faraway.gg <Kirill Osipov>

--- src/graphics/program-lib/chunks/lit/frag/fogLinear.js
+++ src/graphics/program-lib/chunks/lit/frag/fogLinear.js
@@ -8,7 +8,6 @@ vec3 addFog(vec3 color) {
     float depth = gl_FragCoord.z / gl_FragCoord.w;
     float fogFactor = (fog_end - depth) / (fog_end - fog_start);
     fogFactor = clamp(fogFactor, 0.0, 1.0);
-    fogFactor = gammaCorrectInput(fogFactor);
     return mix(fog_color * dBlendModeFogFactor, color, fogFactor);
 }
 `;
