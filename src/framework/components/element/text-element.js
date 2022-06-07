5bb74571ac354a1e3a1365c467c22df4cf0963bd
kirill.osipov@faraway.gg <Kirill Osipov>
2022-06-07 15:54:53 +0400

    Review comment: https://github.com/playcanvas/engine/pull/4302#discussion_r891109729

--- src/framework/components/element/text-element.js
+++ src/framework/components/element/text-element.js
@@ -58,17 +58,11 @@ function createTextMesh(device, meshInfo) {
     const mesh = new Mesh(device);
 
     mesh.setPositions(meshInfo.positions);
-
     mesh.setNormals(meshInfo.normals);
-
     mesh.setColors32(meshInfo.colors);
-
     mesh.setUvs(0, meshInfo.uvs);
-
     mesh.setIndices(meshInfo.indices);
-
     mesh.setVertexStream(SEMANTIC_ATTR8, meshInfo.outlines, 3, undefined, TYPE_FLOAT32, false);
-
     mesh.setVertexStream(SEMANTIC_ATTR9, meshInfo.shadows, 3, undefined, TYPE_FLOAT32, false);
 
     mesh.update();
