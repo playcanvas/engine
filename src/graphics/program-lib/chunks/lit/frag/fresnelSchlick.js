4003d6c1cf445a611676896fc4ba303b15ea2ebd
Gustav Sterbrant <gsterbrant@snapchat.com>
2022-07-01 14:40:09 +0100

    Removed unnecessary parentheses.

--- src/graphics/program-lib/chunks/lit/frag/fresnelSchlick.js
+++ src/graphics/program-lib/chunks/lit/frag/fresnelSchlick.js
@@ -2,13 +2,13 @@ export default /* glsl */`
 // Schlick's approximation
 vec3 getFresnel(float cosTheta, vec3 f0) {
     float fresnel = pow(1.0 - max(cosTheta, 0.0f), 5.0);
-    float glossSq = (dGlossiness * dGlossiness);
+    float glossSq = dGlossiness * dGlossiness;
     return f0 + (max(vec3(glossSq), f0) - f0) * fresnel;
 }
 
 vec3 getFresnel(float cosTheta, vec3 f0, float specularityFactor) {
     float fresnel = pow(1.0 - max(cosTheta, 0.0f), 5.0);
-    float glossSq = (dGlossiness * dGlossiness);
+    float glossSq = dGlossiness * dGlossiness;
     return f0 + (max(vec3(glossSq), f0) - f0) * fresnel * specularityFactor;
 }
 `;
