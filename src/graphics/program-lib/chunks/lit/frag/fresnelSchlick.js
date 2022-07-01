5ffcec1c96274e9e2afed83fd1f66bc3a321f0a9
Gustav Sterbrant <gsterbrant@snapchat.com>
2022-07-01 12:47:05 +0100

    Replaced fresnel balancing function with more correct version.

--- src/graphics/program-lib/chunks/lit/frag/fresnelSchlick.js
+++ src/graphics/program-lib/chunks/lit/frag/fresnelSchlick.js
@@ -1,14 +1,14 @@
 export default /* glsl */`
 // Schlick's approximation
 vec3 getFresnel(float cosTheta, vec3 f0) {
-    float fresnel = pow(1.0 - abs(cosTheta), 4.0);
-    float glossSq = dGlossiness * dGlossiness;
-    return f0 + (1.0 - f0) * fresnel * glossSq;
+    float fresnel = pow(1.0 - max(cosTheta, 0.0f), 5.0);
+    float glossSq = (dGlossiness * dGlossiness);
+    return f0 + (max(vec3(glossSq), f0) - f0) * fresnel;
 }
 
 vec3 getFresnel(float cosTheta, vec3 f0, float specularityFactor) {
-    float fresnel = pow(1.0 - abs(cosTheta), 5.0);
-    float glossSq = dGlossiness * dGlossiness;
-    return f0 + (1.0 - f0) * fresnel * glossSq * specularityFactor;
+    float fresnel = pow(1.0 - max(cosTheta, 0.0f), 5.0);
+    float glossSq = (dGlossiness * dGlossiness);
+    return f0 + (max(vec3(glossSq), f0) - f0) * fresnel * specularityFactor;
 }
 `;
