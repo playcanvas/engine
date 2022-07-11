53d00c2f3750f0cc88a698c98d97b498f16599fb
Kirill Osipov <104348270+querielo@users.noreply.github.com>
2022-07-11 13:02:25 +0300

    Issue: https://github.com/playcanvas/engine/issues/4308 . Added tests (#4311)
    Co-authored-by: kirill.osipov@faraway.gg <Kirill Osipov>

--- src/graphics/program-lib/chunks/common/vert/msdf.js
+++ src/graphics/program-lib/chunks/common/vert/msdf.js
@@ -10,13 +10,18 @@ varying vec2 shadow_offset;
 void unpackMsdfParams() {
     outline_color.rb = mod(vertex_outlineParameters.xy, 256.) / 256.;
     outline_color.ga = vertex_outlineParameters.xy / 256. / 256.;
-    outline_thickness = vertex_outlineParameters.z / 255.;
+
+    // _outlineThicknessScale === 0.2
+    outline_thickness = vertex_outlineParameters.z / 255. * 0.2;
 
     vec3 little = mod(vertex_shadowParameters, 256.) / 256.;
     vec3 big = vertex_shadowParameters / 256. / 256.;
 
     shadow_color.rb = little.xy;
     shadow_color.ga = big.xy;
-    shadow_offset = vec2(little.z, big.z);
+
+    // vec2(little.z, big.z) * 2. srink from (0.5, 0.5) to (1, 1)
+    // _shadowOffsetScale === 0.005
+    shadow_offset = (vec2(little.z, big.z) * 2. - 1.) * 0.005;
 }
 `;
