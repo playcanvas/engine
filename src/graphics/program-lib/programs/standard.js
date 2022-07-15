7cfc3fb4de83fd8285b55b759aeafc039f0ac3aa
Gustav Sterbrant <107400752+GSterbrant@users.noreply.github.com>
2022-07-15 12:01:17 +0100

    Only degamma specular if we use a metalness specular color map (#4436)
    * Only degamma specular if we use a metalness specular color map.
    
    * Changed gamma calculation to used the encoding in the texture.
    
    * Removed the specular encoding option in favour of handling the switch in the frontend directly.

--- src/graphics/program-lib/programs/standard.js
+++ src/graphics/program-lib/programs/standard.js
@@ -345,7 +345,7 @@ const standard = {
                     func.append("getSpecularityFactor();");
                 }
                 if (options.useSpecularColor) {
-                    code.append(this._addMap("specular", "specularPS", options, litShader.chunks));
+                    code.append(this._addMap("specular", "specularPS", options, litShader.chunks, options.useMetalness ? 'srgb' : 'linear'));
                 } else {
                     code.append("void getSpecularity() { dSpecularity = vec3(1); }");
                 }
