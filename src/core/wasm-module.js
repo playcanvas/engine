84947f93c55d3870e882d6ede4c1f8a362f45069
Will Eastcott <will@playcanvas.com>
2022-07-18 14:00:57 +0100

    [FIX] Remove guard that always evaluates to true in wasmSupported

--- src/core/wasm-module.js
+++ src/core/wasm-module.js
@@ -3,20 +3,16 @@ class Impl {
 
     // returns true if the running host supports wasm modules (all browsers except IE)
     static wasmSupported() {
-        let supported = null;
-
-        if (supported === null) {
-            supported = (() => {
-                try {
-                    if (typeof WebAssembly === "object" && typeof WebAssembly.instantiate === "function") {
-                        const module = new WebAssembly.Module(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
-                        if (module instanceof WebAssembly.Module)
-                            return new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
-                    }
-                } catch (e) { }
-                return false;
-            })();
-        }
+        const supported = (() => {
+            try {
+                if (typeof WebAssembly === "object" && typeof WebAssembly.instantiate === "function") {
+                    const module = new WebAssembly.Module(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
+                    if (module instanceof WebAssembly.Module)
+                        return new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
+                }
+            } catch (e) { }
+            return false;
+        })();
 
         return supported;
     }
