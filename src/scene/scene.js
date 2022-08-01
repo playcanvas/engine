27dbe188403660319f2b1e211c7947e67cd00900
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2022-08-01 13:56:21 +0100

    Revert "Enable clustered lighting by default (#4364)" (#4494)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -216,7 +216,7 @@ class Scene extends EventHandler {
         this._lightmapFilterSmoothness = 0.2;
 
         // clustered lighting
-        this._clusteredLightingEnabled = true;
+        this._clusteredLightingEnabled = false;
         this._lightingParams = new LightingParams(this.device.supportsAreaLights, this.device.maxTextureSize, () => {
             this._layers._dirtyLights = true;
         });
@@ -326,8 +326,8 @@ class Scene extends EventHandler {
 
     set clusteredLightingEnabled(value) {
 
-        if (!this._clusteredLightingEnabled && value) {
-            console.error('Turning on disabled clustered lighting is not currently supported');
+        if (this._clusteredLightingEnabled && !value) {
+            console.error('Turning off enabled clustered lighting is not currently supported');
             return;
         }
 
