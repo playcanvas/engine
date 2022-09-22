6af5ed19379ca6f7cee261d5fa41238ee9c16417
Gustav Sterbrant <107400752+GSterbrant@users.noreply.github.com>
2022-09-22 16:56:59 +0100

    Clear coat fresnel fix (#4657)
    * Working on clearcoat balancing
    
    * Fix clearcoat energy balance.
    
    * Make sure specularity factor is only factored in if we have specular colour with metalness enabled
    
    * Simplify the ccReflection to a vec3.

--- src/graphics/program-lib/chunks/lit/frag/reflectionCC.js
+++ src/graphics/program-lib/chunks/lit/frag/reflectionCC.js
@@ -1,9 +1,7 @@
 export default /* glsl */`
 #ifdef LIT_CLEARCOAT
-uniform float material_clearCoatReflectivity;
-
 void addReflectionCC() {
-    ccReflection += vec4(calcReflection(ccReflDirW, ccGlossiness), material_clearCoatReflectivity);
+    ccReflection += calcReflection(ccReflDirW, ccGlossiness);
 }
 #endif
 `;
