6c7d3dc2dae80364dfedebb2bd371a48aba976d2
Donovan Hutchence <slimbuck7@gmail.com>
2022-09-26 17:18:46 +0100

    Fix chunk decode calls (#4670)

--- src/graphics/program-lib/chunks/standard/frag/diffuseDetailMap.js
+++ src/graphics/program-lib/chunks/standard/frag/diffuseDetailMap.js
@@ -1,7 +1,7 @@
 export default /* glsl */`
 vec3 addAlbedoDetail(vec3 albedo) {
 #ifdef MAPTEXTURE
-    vec3 albedoDetail = $DECODE(texture2DBias($SAMPLER, $UV, textureBias).$CH);
+    vec3 albedoDetail = $DECODE(texture2DBias($SAMPLER, $UV, textureBias)).$CH;
     return detailMode_$DETAILMODE(albedo, albedoDetail);
 #else
     return albedo;
