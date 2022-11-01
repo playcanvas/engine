322246b9abf5d8c1c1494c9a284d95738b6b8858
kungfooman <lama12345@gmail.com>
2022-11-01 14:58:26 +0100

    Changes for Rollup 3

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -20,12 +20,18 @@ import { visualizer } from 'rollup-plugin-visualizer';
 /** @typedef {import('@rollup/plugin-babel').RollupBabelInputPluginOptions} RollupBabelInputPluginOptions */
 /** @typedef {import('@rollup/plugin-strip').RollupStripOptions           } RollupStripOptions            */
 
+/**
+ * @returns {string} Version string like `1.58.0-dev`
+ */
 function getVersion() {
     const text = fs.readFileSync('./package.json', 'utf8');
     const json = JSON.parse(text);
     return json.version;
 }
 
+/**
+ * @returns {string} Revision string like `644d08d39` (9 digits/chars).
+ */
 function getRevision() {
     let revision;
     try {
