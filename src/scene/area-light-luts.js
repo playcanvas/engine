6e3be93d7d5af64987925d34fd5ee08a2077a656
Martin Valigursky <mvaligursky@snapchat.com>
2022-11-04 15:28:58 +0000

    lint import

--- src/scene/area-light-luts.js
+++ src/scene/area-light-luts.js
@@ -5,7 +5,7 @@ import { DeviceCache } from '../platform/graphics/device-cache.js';
 import {
     ADDRESS_CLAMP_TO_EDGE,
     FILTER_LINEAR, FILTER_NEAREST,
-    PIXELFORMAT_R8_G8_B8_A8, PIXELFORMAT_RGBA16F, PIXELFORMAT_RGBA32F,
+    PIXELFORMAT_RGBA16F, PIXELFORMAT_RGBA32F,
     TEXTURETYPE_DEFAULT
 } from '../platform/graphics/constants.js';
 
