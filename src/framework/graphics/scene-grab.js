6c4935fb1d23590690363b751e8e7aa3f0af604d
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2022-11-14 17:19:41 +0000

    [Fix] Fixing grab depth texture on WebGL1 (#4845)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/framework/graphics/scene-grab.js
+++ src/framework/graphics/scene-grab.js
@@ -250,7 +250,7 @@ class SceneGrab {
                 if (camera.renderSceneDepthMap) {
 
                     // reallocate RT if needed
-                    if (self.resizeCondition(this.depthRenderTarget, camera.renderTarget?.depthBuffer, device)) {
+                    if (!this.depthRenderTarget.depthBuffer || self.resizeCondition(this.depthRenderTarget, camera.renderTarget?.depthBuffer, device)) {
                         this.depthRenderTarget.destroyTextureBuffers();
                         this.depthRenderTarget = self.allocateRenderTarget(this.depthRenderTarget, camera.renderTarget, device, PIXELFORMAT_RGBA8, false, false, true);
                     }
