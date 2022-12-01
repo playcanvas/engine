94c7f7e3239582e2f254af9787213db993922f85
Martin Valigursky <mvaligursky@snapchat.com>
2022-12-01 12:27:38 +0000

    removed comment

--- src/scene/renderer/forward-renderer.js
+++ src/scene/renderer/forward-renderer.js
@@ -969,7 +969,6 @@ class ForwardRenderer extends Renderer {
         const camera = startLayer.cameras[startRenderAction.cameraIndex];
 
         // depth grab pass on webgl1 is normal render pass (scene gets re-rendered)
-        // const isWebgl1DepthGrabPass = isGrabPass && !this.device.webgl2 && camera.renderSceneDepthMap;
         const grabPassRequired = isGrabPass && SceneGrab.requiresRenderPass(this.device, camera);
         const isRealPass = !isGrabPass || grabPassRequired;
 
