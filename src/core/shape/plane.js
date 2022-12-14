e9dcb082e5a735092914a74de59bd38e3579ba3a
kungfooman <lama12345@gmail.com>
2022-12-14 17:43:09 +0100

    Plane/Ray add copy/clone methods + prefer ownership over references

--- src/core/shape/plane.js
+++ src/core/shape/plane.js
@@ -9,16 +9,40 @@ const tmpVecA = new Vec3();
  */
 class Plane {
     /**
+     * The starting point of the plane.
+     *
+     * @type {Vec3}
+     */
+    point = new Vec3();
+
+    /**
+     * The normal of the plane.
+     *
+     * @type {Vec3}
+     */
+    normal = new Vec3();
+
+    /**
      * Create a new Plane instance.
      *
-     * @param {Vec3} [point] - Point position on the plane. The constructor takes a reference of
-     * this parameter.
-     * @param {Vec3} [normal] - Normal of the plane. The constructor takes a reference of this
-     * parameter.
+     * @param {Vec3} [point] - Point position on the plane. The constructor copies this parameter.
+     * @param {Vec3} [normal] - Normal of the plane. The constructor copies this parameter.
      */
-    constructor(point = new Vec3(), normal = new Vec3(0, 0, 1)) {
-        this.point = point;
-        this.normal = normal;
+    constructor(point = Vec3.ZERO, normal = Vec3.BACK) {
+        this.set(point, normal);
+    }
+
+    /**
+     * Sets point and normal to the supplied vector values.
+     *
+     * @param {Vec3} point - The starting point of the plane.
+     * @param {Vec3} normal - The normal of the plane.
+     * @returns {Plane} Self for chaining.
+     */
+    set(point, normal) {
+        this.point.copy(point);
+        this.normal.copy(normal);
+        return this;
     }
 
     /**
@@ -60,6 +84,25 @@ class Plane {
             point.copy(ray.direction).mulScalar(t).add(ray.origin);
 
         return intersects;
+    }
+
+    /**
+     * Copies the contents of a source Plane.
+     *
+     * @param {Plane} src - The Plane to copy from.
+     * @returns {Plane} Self for chaining.
+     */
+    copy(src) {
+        return this.set(src.point, src.normal);
+    }
+
+    /**
+     * Returns a clone of the Plane.
+     *
+     * @returns {this} A duplicate Plane.
+     */
+    clone() {
+        return new this.constructor(this.point, this.normal);
     }
 }
 
