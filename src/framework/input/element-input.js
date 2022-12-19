c4200ec66cd8e9c61536b51fe0eafcb1b98d2499
Steven Yau <syau@snapchat.com>
2022-12-19 18:29:28 +0000

    Added a selectmove event for elements

--- src/framework/input/element-input.js
+++ src/framework/input/element-input.js
@@ -837,8 +837,9 @@ class ElementInput {
             if (hoveredNow) this._fireEvent('selectenter', new ElementSelectEvent(event, hoveredNow, camera, inputSource));
         }
 
-        if (eventType === 'selectmove' && hoveredBefore) {
-            this._fireEvent('selectmove', new ElementSelectEvent(event, hoveredBefore, camera, inputSource));
+        const pressed = this._selectedPressedElements[inputSource.id];
+        if (eventType === 'selectmove' && pressed) {
+            this._fireEvent('selectmove', new ElementSelectEvent(event, pressed, camera, inputSource));
         }
 
         if (eventType === 'selectstart') {
@@ -846,16 +847,19 @@ class ElementInput {
             if (hoveredNow) this._fireEvent('selectstart', new ElementSelectEvent(event, hoveredNow, camera, inputSource));
         }
 
-        const pressed = this._selectedPressedElements[inputSource.id];
         if (!inputSource.elementInput && pressed) {
             delete this._selectedPressedElements[inputSource.id];
-            if (hoveredBefore) this._fireEvent('selectend', new ElementSelectEvent(event, hoveredBefore, camera, inputSource));
+            if (hoveredBefore) {
+                this._fireEvent('selectend', new ElementSelectEvent(event, pressed, camera, inputSource));
+            }
         }
 
         if (eventType === 'selectend' && inputSource.elementInput) {
             delete this._selectedPressedElements[inputSource.id];
 
-            if (hoveredBefore) this._fireEvent('selectend', new ElementSelectEvent(event, hoveredBefore, camera, inputSource));
+            if (pressed) {
+                this._fireEvent('selectend', new ElementSelectEvent(event, pressed, camera, inputSource));
+            }
 
             if (pressed && pressed === hoveredBefore) {
                 this._fireEvent('click', new ElementSelectEvent(event, pressed, camera, inputSource));
