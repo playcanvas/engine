c68c5f79e4c658fdf1f5d9513d93e5134f1e52b5
Donovan Hutchence <slimbuck7@gmail.com>
2023-01-10 14:53:42 +0000

    keep 1.58 chunk

--- src/platform/graphics/constants.js
+++ src/platform/graphics/constants.js
@@ -1257,4 +1257,5 @@ export const CHUNKAPI_1_51 = '1.51';
 export const CHUNKAPI_1_55 = '1.55';
 export const CHUNKAPI_1_56 = '1.56';
 export const CHUNKAPI_1_57 = '1.57';
+export const CHUNKAPI_1_58 = '1.58';
 export const CHUNKAPI_1_60 = '1.60';
