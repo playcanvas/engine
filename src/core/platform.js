d2a3e2722223607412facc4c5851377c5b7ac435
kungfooman <lama12345@gmail.com>
2023-01-31 11:34:51 +0100

    Fix lint

--- src/core/platform.js
+++ src/core/platform.js
@@ -17,7 +17,7 @@ const getDummy = () => dummy;
 /**
  * Always returns an array of HTML5 Gamepads, even if the environment has no
  * Gamepad support (like node), to prevent special casing everywhere.
- * 
+ *
  * @type {typeof navigator.getGamepads}
  */
 let getGamepads = getDummy;
