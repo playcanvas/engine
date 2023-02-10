8d1a2c408572d8c69979ab2e942646ee1d88f979
Steven <yaustar@users.noreply.github.com>
2023-02-10 09:32:54 +0000

    WebXR - Fixed bug where the wrong number of views is being added (#5063)
    * Fixed bug where the number of XR views to add was changing
    
    * Replaced with while loop
    
    * Removed if statements from review feedback
    
    ---------
    
    Co-authored-by: Steven Yau <syau@snapchat.com>

--- src/framework/xr/xr-manager.js
+++ src/framework/xr/xr-manager.js
@@ -694,32 +694,28 @@ class XrManager extends EventHandler {
         const lengthOld = this.views.length;
         const lengthNew = pose.views.length;
 
-        if (lengthNew > this.views.length) {
-            // add new views into list
-            for (let i = 0; i <= (lengthNew - this.views.length); i++) {
-                let view = this.viewsPool.pop();
-                if (!view) {
-                    view = {
-                        viewport: new Vec4(),
-                        projMat: new Mat4(),
-                        viewMat: new Mat4(),
-                        viewOffMat: new Mat4(),
-                        viewInvMat: new Mat4(),
-                        viewInvOffMat: new Mat4(),
-                        projViewOffMat: new Mat4(),
-                        viewMat3: new Mat3(),
-                        position: new Float32Array(3),
-                        rotation: new Quat()
-                    };
-                }
+        while (lengthNew > this.views.length) {
+            let view = this.viewsPool.pop();
+            if (!view) {
+                view = {
+                    viewport: new Vec4(),
+                    projMat: new Mat4(),
+                    viewMat: new Mat4(),
+                    viewOffMat: new Mat4(),
+                    viewInvMat: new Mat4(),
+                    viewInvOffMat: new Mat4(),
+                    projViewOffMat: new Mat4(),
+                    viewMat3: new Mat3(),
+                    position: new Float32Array(3),
+                    rotation: new Quat()
+                };
+            }
 
-                this.views.push(view);
-            }
-        } else if (lengthNew <= this.views.length) {
-            // remove views from list into pool
-            for (let i = 0; i < (this.views.length - lengthNew); i++) {
-                this.viewsPool.push(this.views.pop());
-            }
+            this.views.push(view);
+        }
+        // remove views from list into pool
+        while (lengthNew < this.views.length) {
+            this.viewsPool.push(this.views.pop());
         }
 
         // reset position
