239aa343faddc6060ef1a8d901f0d2a1823e721b
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2023-02-22 12:15:02 +0000

    Fixed docs

--- src/platform/input/game-pads.js
+++ src/platform/input/game-pads.js
@@ -413,7 +413,7 @@ class GamePad {
         /**
          * The buttons present on the GamePad. Some buttons may be null. Order is provided by API, use GamePad#buttons instead.
          *
-         * @type {(GamePadButton|null)[]}
+         * @type {GamePadButton[]}
          * @ignore
          */
         this._buttons = gamepad.buttons.map((b) => {
