089bd4738c448c7c3f2c2a0863144356c838b2e7
Gustav Sterbrant <gsterbrant@snapchat.com>
2023-02-22 14:21:04 +0000

    Just some spacing

--- src/scene/shader-lib/chunks/lit/frag/TBNObjectSpace.js
+++ src/scene/shader-lib/chunks/lit/frag/TBNObjectSpace.js
@@ -6,19 +6,19 @@ void getTBN() {
 
     if (dot(B,B)==0.0) // deal with case when vObjectSpaceUpW dVertexNormalW are parallel
     {
-        float major=max(max(dVertexNormalW.x, dVertexNormalW.y),dVertexNormalW.z);
+        float major=max(max(dVertexNormalW.x, dVertexNormalW.y), dVertexNormalW.z);
 
-        if (dVertexNormalW.x==major)
+        if (dVertexNormalW.x == major)
         {
             B=cross(dVertexNormalW, vec3(0,1,0));
             T=cross(dVertexNormalW, B);
         }
-        else if (dVertexNormalW.y==major)
+        else if (dVertexNormalW.y == major)
         {
             B=cross(dVertexNormalW, vec3(0,0,1));
             T=cross(dVertexNormalW, B);
         }
-        else if (dVertexNormalW.z==major)
+        else if (dVertexNormalW.z == major)
         {
             B=cross(dVertexNormalW, vec3(1,0,0));
             T=cross(dVertexNormalW, B);
