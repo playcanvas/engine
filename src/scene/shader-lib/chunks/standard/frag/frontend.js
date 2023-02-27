defd2668bb12f43be89f321013d20cc1011fd44e
Gustav Sterbrant <gsterbrant@snapchat.com>
2023-02-27 12:22:35 +0000

    Separation of lit shader parameters based on effect.

--- src/scene/shader-lib/chunks/standard/frag/frontend.js
+++ src/scene/shader-lib/chunks/standard/frag/frontend.js
@@ -1,18 +1,25 @@
 export default /* glsl */`
 struct Frontend {
     vec2 dUvOffset;
+
     float dAlpha;
+
     vec3 dNormalW;
+    
     vec3 dAlbedo;
+
     float dTransmission;
     float dThickness;
+
     vec3 dIridescenceFresnel;
     float dIridescence;
     float dIridescenceThickness;
+
     vec3 dSpecularity;
     float dGlossiness;
     float dMetalness;
     float dSpecularityFactor;
+
     float dAo;
 
     vec3 dEmission;
