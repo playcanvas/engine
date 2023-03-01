bf185437f87dc0e7d280bde8394809206e137549
Gustav Sterbrant <gsterbrant@snapchat.com>
2023-03-01 17:58:05 +0000

    Merge with main sheen specular occlusion.

--- src/scene/shader-lib/chunks/lit/frag/aoSpecOccConstSimple.js
+++ src/scene/shader-lib/chunks/lit/frag/aoSpecOccConstSimple.js
@@ -2,5 +2,10 @@ export default /* glsl */`
 void occludeSpecular(float ao) {
     dSpecularLight *= ao;
     dReflection *= ao;
+
+#ifdef LIT_SHEEN
+    sSpecularLight *= ao;
+    sReflection *= ao;
+#endif
 }
 `;
