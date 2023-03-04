8dbaf611e7afe8ee14280450251a35ec8a44cf3c
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2023-03-04 16:27:56 +0000

    Updated `ZoneComponentData` content

--- src/framework/components/zone/data.js
+++ src/framework/components/zone/data.js
@@ -1,6 +1,12 @@
+import { Vec3 } from '../../../core/math/vec3';
+
 class ZoneComponentData {
     constructor() {
         this.enabled = true;
+        this.shape = 'box';
+        this.halfExtents = new Vec3(0.5, 0.5, 0.5);
+        this.radius = 0.5;
+        this.useColliders = true;
     }
 }
 
