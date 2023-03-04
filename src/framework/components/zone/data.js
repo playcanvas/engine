dfe14e846846d6026a711515309e75c3575a308b
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2023-03-04 17:29:56 +0000

    Removed data

--- src/framework/components/zone/data.js
+++ src/framework/components/zone/data.js
@@ -1,12 +1,6 @@
-import { Vec3 } from '../../../core/math/vec3.js';
-
 class ZoneComponentData {
     constructor() {
         this.enabled = true;
-        this.shape = 'box';
-        this.halfExtents = new Vec3(0.5, 0.5, 0.5);
-        this.radius = 0.5;
-        this.useColliders = true;
     }
 }
 
