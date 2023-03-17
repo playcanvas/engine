a8c0e6774761da0ff8bc1fc179d272854271cb47
Gustav Sterbrant <gsterbrant@snapchat.com>
2023-03-17 14:17:51 +0000

    Fix typo

--- src/scene/shader-lib/chunks/lit/frag/aoSpecOcc.js
+++ src/scene/shader-lib/chunks/lit/frag/aoSpecOcc.js
@@ -3,7 +3,7 @@ uniform float material_occludeSpecularIntensity;
 
 void occludeSpecular(float gloss, float ao, vec3 worldNormal) {
     // approximated specular occlusion from AO
-    float specPow = exp2(lgloss * 11.0);
+    float specPow = exp2(gloss * 11.0);
     // http://research.tri-ace.com/Data/cedec2011_RealtimePBR_Implementation_e.pptx
     float specOcc = saturate(pow(dot(worldNormal, dViewDirW) + ao, 0.01*specPow) - 1.0 + ao);
     specOcc = mix(1.0, specOcc, material_occludeSpecularIntensity);
