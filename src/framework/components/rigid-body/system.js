471a02a406abee31324f33066fed74cff979dcd4
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2023-03-22 15:24:11 +0000

    Removed extra examples

--- src/framework/components/rigid-body/system.js
+++ src/framework/components/rigid-body/system.js
@@ -571,21 +571,6 @@ class RigidBodyComponentSystem extends ComponentSystem {
      * });
      * @example
      * // Return all results of a raycast between 0, 2, 2 and 0, -2, -2
-     * // where hit entity is tagged by `carnivore` AND `mammal`
-     * const hits = this.app.systems.rigidbody.raycastAll(new Vec3(0, 2, 2), new Vec3(0, -2, -2), {
-     *     filterTags: [ [ "carnivore", "mammal" ] ]
-     * });
-     * @example
-     * // Return all results of a raycast between 0, 2, 2 and 0, -2, -2
-     * // where hit entity is tagged by (`carnivore` AND `mammal`) OR (`carnivore` AND `reptile`)
-     * const hits = this.app.systems.rigidbody.raycastAll(new Vec3(0, 2, 2), new Vec3(0, -2, -2), {
-     *     filterTags: [
-     *         [ "carnivore", "mammal" ],
-     *         [ "carnivore", "reptile" ]
-     *     ]
-     * });
-     * @example
-     * // Return all results of a raycast between 0, 2, 2 and 0, -2, -2
      * // where hit entity is having a `camera` component
      * const hits = this.app.systems.rigidbody.raycastAll(new Vec3(0, 2, 2), new Vec3(0, -2, -2), {
      *     filterCallback: (entity) => entity && entity.camera
