01a00a39f9b36e70d7b690102a25485266f0d45a
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2023-04-24 15:58:38 +0100

    Generate define for each varying variable in standard fragment shader (#5267)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/scene/shader-lib/chunks/lit/frag/debug-process-frontend.js
+++ src/scene/shader-lib/chunks/lit/frag/debug-process-frontend.js
@@ -4,6 +4,10 @@ litShaderArgs.albedo = vec3(0.5);
 #endif
 
 #ifdef DEBUG_UV0_PASS
+#ifdef VARYING_VUV0
 litShaderArgs.albedo = vec3(vUv0, 0);
+#else
+litShaderArgs.albedo = vec3(0);
+#endif
 #endif
 `;
