68ccb810cca137d1ab605aab5f731f564ad215d6
Martin Valigursky <mvaligursky@snapchat.com>
2023-05-04 09:00:50 +0100

    Fix to debug shader rendering to gamma correct values

--- src/scene/shader-lib/chunks/lit/frag/debug-output.js
+++ src/scene/shader-lib/chunks/lit/frag/debug-output.js
@@ -1,6 +1,6 @@
 export default /* glsl */`
 #ifdef DEBUG_ALBEDO_PASS
-gl_FragColor = vec4(litShaderArgs.albedo , 1.0);
+gl_FragColor = vec4(gammaCorrectOutput(litShaderArgs.albedo), 1.0);
 #endif
 
 #ifdef DEBUG_UV0_PASS
@@ -28,10 +28,10 @@ gl_FragColor = vec4(vec3(litShaderArgs.metalness) , 1.0);
 #endif
 
 #ifdef DEBUG_AO_PASS
-gl_FragColor = vec4(vec3(litShaderArgs.ao) , 1.0);
+gl_FragColor = vec4(gammaCorrectOutput(vec3(litShaderArgs.ao)) , 1.0);
 #endif
 
 #ifdef DEBUG_EMISSION_PASS
-gl_FragColor = vec4(litShaderArgs.emission, 1.0);
+gl_FragColor = vec4(gammaCorrectOutput(litShaderArgs.emission), 1.0);
 #endif
 `;
