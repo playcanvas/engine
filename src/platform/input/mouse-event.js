cf28baf6f1ccd5568597946c62c1696a4ad9919e
Will Eastcott <will@playcanvas.com>
2023-05-24 22:16:15 +0100

    Various @link and JSDoc type fixes (#5351)

--- src/platform/input/mouse-event.js
+++ src/platform/input/mouse-event.js
@@ -59,7 +59,7 @@ class MouseEvent {
 
         /**
          * A value representing the amount the mouse wheel has moved, only valid for
-         * {@link mousewheel} events.
+         * {@link EVENT_MOUSEWHEEL} events.
          *
          * @type {number}
          */
