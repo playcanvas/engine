d93baeb9d8c56d98efdf32f6b52b97236ec212fc
Elliott Thompson <ellth92@gmail.com>
2023-06-01 10:08:15 +0100

    updates

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -2,15 +2,79 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from '@rollup/plugin-replace';
 import typescript from 'rollup-plugin-typescript2';
-import copy from 'rollup-plugin-copy-watch';
 import { terser } from 'rollup-plugin-terser';
 import alias from '@rollup/plugin-alias';
+import date from 'date-and-time';
 import path from 'path';
+import fs from 'fs';
+import fse from 'fs-extra';
 
 const ENGINE_PATH = process.env.ENGINE_PATH ? path.resolve(process.env.ENGINE_PATH) : path.resolve('../build/playcanvas.js');
 const PCUI_PATH = process.env.PCUI_PATH || 'node_modules/@playcanvas/pcui';
 const PCUI_REACT_PATH = path.resolve(PCUI_PATH, 'react');
 const PCUI_STYLES_PATH = path.resolve(PCUI_PATH, 'styles');
+
+const staticFiles = [
+    { src: 'src/static', dest: 'dist/' },
+    { src: './assets', dest: 'dist/static/assets/' },
+    { src: './src/lib', dest: 'dist/static/lib/' },
+    { src: '../scripts', dest: 'dist/static/scripts/' },
+    { src: '../build/playcanvas.d.ts', dest: 'dist/playcanvas.d.ts' },
+    { src: 'src/iframe/index.html', dest: 'dist/iframe/index.html' }
+];
+
+function timestamp() {
+    return {
+        name: 'timestamp',
+        writeBundle() {
+            console.log("\x1b[32m", "Finished at: " + date.format(new Date(), 'HH:mm:ss'));
+        }
+    };
+}
+
+function copyStaticFiles(targets) {
+    function watch(src) {
+        const srcStats = fs.statSync(src);
+        if (srcStats.isFile()) {
+            this.addWatchFile(path.resolve(__dirname, src));
+            return;
+        }
+
+        const filesToWatch = fs.readdirSync(src);
+
+        for (const file of filesToWatch) {
+            const fullPath = path.join(src, file);
+            const stats = fs.statSync(fullPath);
+
+            if (stats.isFile()) {
+                this.addWatchFile(path.resolve(__dirname, fullPath));
+
+            } else if (stats.isDirectory()) {
+                watch.bind(this)(fullPath);
+            }
+        }
+    }
+    return {
+        name: 'copy-and-watch',
+        async load() {
+            return 'console.log("This temp file is created when copying static files, it should be removed during the build process.");';
+        },
+        async buildStart() {
+            if (process.env.NODE_ENV !== 'development') return;
+            targets.forEach((target) => {
+                watch.bind(this)(target.src, target.dest);
+            });
+        },
+        async generateBundle() {
+            targets.forEach((target) => {
+                fse.copySync(target.src, target.dest, { overwrite: true });
+            });
+        },
+        async writeBundle() {
+            fs.unlinkSync('dist/copy.tmp');
+        }
+    };
+}
 
 // define supported module overrides
 const aliasEntries = {
@@ -34,16 +98,6 @@ const builds = [
             format: 'es'
         },
         plugins: [
-            copy({
-                watch: process.env.NODE_ENV === 'development' ? '../scripts' : null,
-                targets: [
-                    { src: 'src/static/*', dest: 'dist/' },
-                    { src: './assets/*', dest: 'dist/static/assets/' },
-                    { src: './src/lib/*', dest: 'dist/static/lib/' },
-                    { src: '../scripts/*', dest: 'dist/static/scripts/' },
-                    { src: '../build/playcanvas.d.ts', dest: 'dist/' }
-                ]
-            }),
             alias({ entries: aliasEntries }),
             commonjs(),
             resolve(),
@@ -58,7 +112,8 @@ const builds = [
                 },
                 preventAssignment: true
             }),
-            (process.env.NODE_ENV === 'production' && terser())
+            (process.env.NODE_ENV === 'production' && terser()),
+            timestamp()
         ]
     },
     {
@@ -70,11 +125,6 @@ const builds = [
             dir: 'dist/iframe'
         },
         plugins: [
-            copy({
-                targets: [
-                    { src: 'src/iframe/index.html', dest: 'dist/iframe' }
-                ]
-            }),
             alias({
                 entries: {
                     'playcanvas': ENGINE_PATH,
@@ -91,7 +141,8 @@ const builds = [
             }),
             typescript({
                 tsconfig: 'tsconfig.json'
-            })
+            }),
+            timestamp()
         ]
     },
     {
@@ -113,7 +164,18 @@ const builds = [
             resolve(),
             typescript({
                 tsconfig: 'tsconfig.json'
-            })
+            }),
+            timestamp()
+        ]
+    },
+    {
+        input: 'src/static/index.html',
+        output: {
+            file: `dist/copy.tmp`
+        },
+        plugins: [
+            copyStaticFiles(staticFiles),
+            timestamp()
         ]
     }
 ];
