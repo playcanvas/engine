4ed67a1e560c9d286a317258cf8e5f2e663d6241
Elliott Thompson <ellth92@gmail.com>
2023-06-15 14:54:55 +0100

    AnimBlendTree fix (#5401)
    * make the fineParameter and comsumeTrigger functions anonymous

--- src/framework/components/anim/component.js
+++ src/framework/components/anim/component.js
@@ -292,8 +292,8 @@ class AnimComponent extends Component {
             transitions,
             this._activate,
             this,
-            this.findParameter.bind(this),
-            this.consumeTrigger.bind(this)
+            this.findParameter,
+            this.consumeTrigger
         );
         this._layers.push(new AnimComponentLayer(name, controller, this, weight, blendType));
         this._layerIndices[name] = layerIndex;
@@ -631,13 +631,28 @@ class AnimComponent extends Component {
         Debug.log(`Cannot set parameter value. No parameter found in anim controller named "${name}" of type "${type}"`);
     }
 
-    findParameter(name) {
+    /**
+     * Returns the parameter object for the specified parameter name. This function is anonymous so that it can be passed to the AnimController
+     * while still being called in the scope of the AnimComponent.
+     *
+     * @param {string} name - The name of the parameter to return the value of.
+     * @returns {object} The parameter object.
+     * @private
+     */
+    findParameter = (name) => {
         return this._parameters[name];
-    }
+    };
 
-    consumeTrigger(name) {
+    /**
+     * Sets a trigger parameter as having been used by a transition. This function is anonymous so that it can be passed to the AnimController
+     * while still being called in the scope of the AnimComponent.
+     *
+     * @param {string} name - The name of the trigger to set as consumed.
+     * @private
+     */
+    consumeTrigger = (name) => {
         this._consumedTriggers.add(name);
-    }
+    };
 
     /**
      * Returns a float parameter value by name.
