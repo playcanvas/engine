37166e55a51a56baa8de4281e8743c41b26eb522
Gustav Sterbrant <107400752+GSterbrant@users.noreply.github.com>
2023-06-21 15:41:00 +0100

    Change behaviour of lightSize (#5421)
    * Change behaviour of lightsize
    
    Have light size be a factor of how many pixels per meter we want to fetch.
    Premultiply division with shadow resolution in shader.
    Premultiply the fov aspect correction on CPU.
    
    * Remove the arbitrary * 2 in the PCSS for point lights.
    
    * Add spacing
    
    * Revert bad merge
    
    * Renamed lightSize to shadowSearchArea
    
    * Changed lightSize to penumbraSize.
    
    * Update example
    
    * Fix build

--- src/framework/components/light/component.js
+++ src/framework/components/light/component.js
@@ -89,9 +89,9 @@ const _lightPropsDefault = [];
  * angle is specified in degrees. Affects spot lights only. Defaults to 40.
  * @property {number} outerConeAngle The angle at which the spotlight cone has faded to nothing.
  * The angle is specified in degrees. Affects spot lights only. Defaults to 45.
- * @property {number} lightSize Size of the light used for contact hardening shadows. For area lights
- * acts as a size multiplier with the area light dimensions. For punctual and directional lights
- * acts as the actual size of the light. Defaults to 1.0.
+ * @property {number} penumbraSize Size of penumbra for contact hardening shadows. For area lights
+ * acts as a multiplier with the dimensions of the area light. For punctual and directional lights
+ * it's the area size of the light. Defaults to 1.0.
  * @property {number} falloffMode Controls the rate at which a light attenuates from its position.
  * Can be:
  *
@@ -337,12 +337,12 @@ class LightComponent extends Component {
         return this.light.shadowUpdateOverrides;
     }
 
-    set lightSize(value) {
-        this.light.lightSize = value;
+    set penumbraSize(value) {
+        this.light.penumbraSize = value;
     }
 
-    get lightSize() {
-        return this.light.lightSize;
+    get penumbraSize() {
+        return this.light.penumbraSize;
     }
 }
 
@@ -567,7 +567,7 @@ function _defineProps() {
         }
     });
 
-    _lightProps.push("lightSize");
+    _lightProps.push("penumbraSize");
     _lightPropsDefault.push(1);
 }
 
