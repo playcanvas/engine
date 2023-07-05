c2402f727b0976b21dd04561bdc19380f6f6d181
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2023-07-05 10:24:14 +0100

    [Fix] Workaround for shader compile issue on iOS 15 related to MRT (#5459)
    * [Fix] Workaround for shader compile issue on iOS 15 related to MRT
    
    * cleanup
    
    ---------
    
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/platform/graphics/shader-chunks/frag/gles3.js
+++ src/platform/graphics/shader-chunks/frag/gles3.js
@@ -1,32 +1,46 @@
 export default /* glsl */`
 layout(location = 0) out highp vec4 pc_fragColor;
 
+#ifndef REMOVE_COLOR_ATTACHMENT_1
 #if COLOR_ATTACHMENT_1
 layout(location = 1) out highp vec4 pc_fragColor1;
 #endif
+#endif
 
+#ifndef REMOVE_COLOR_ATTACHMENT_2
 #if COLOR_ATTACHMENT_2
 layout(location = 2) out highp vec4 pc_fragColor2;
 #endif
+#endif
 
+#ifndef REMOVE_COLOR_ATTACHMENT_3
 #if COLOR_ATTACHMENT_3
 layout(location = 3) out highp vec4 pc_fragColor3;
 #endif
+#endif
 
+#ifndef REMOVE_COLOR_ATTACHMENT_4
 #if COLOR_ATTACHMENT_4
 layout(location = 4) out highp vec4 pc_fragColor4;
 #endif
+#endif
 
+#ifndef REMOVE_COLOR_ATTACHMENT_5
 #if COLOR_ATTACHMENT_5
 layout(location = 5) out highp vec4 pc_fragColor5;
 #endif
+#endif
 
+#ifndef REMOVE_COLOR_ATTACHMENT_6
 #if COLOR_ATTACHMENT_6
 layout(location = 6) out highp vec4 pc_fragColor6;
 #endif
+#endif
 
+#ifndef REMOVE_COLOR_ATTACHMENT_7
 #if COLOR_ATTACHMENT_7
 layout(location = 7) out highp vec4 pc_fragColor7;
+#endif
 #endif
 
 #define gl_FragColor pc_fragColor
