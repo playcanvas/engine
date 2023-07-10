5a95e0054fedd4b47ecc0811c8b7f2beae4c6bec
Gustav Sterbrant <gsterbrant@snapchat.com>
2023-07-10 17:46:47 +0100

    Update example to use textures and better highlight some of the custom material features.

--- examples/src/examples/graphics/custom-material.tsx
+++ examples/src/examples/graphics/custom-material.tsx
@@ -16,7 +16,9 @@ class CustomMaterialExample {
             orbitCamera: new pc.Asset('script', 'script', { url: '/static/scripts/camera/orbit-camera.js' }),
             helipad: new pc.Asset('helipad-env-atlas', 'texture', { url: '/static/assets/cubemaps/helipad-env-atlas.png' }, { type: pc.TEXTURETYPE_RGBP }),
             font: new pc.Asset('font', 'font', { url: '/static/assets/fonts/arial.json' }),
-            checkerboard: new pc.Asset('checkerboard', 'texture', { url: '/static/assets/textures/checkboard.png' })
+            color: new pc.Asset('color', 'texture', { url: '/static/assets/textures/seaside-rocks01-color.jpg' }),
+            normal: new pc.Asset('normal', 'texture', { url: '/static/assets/textures/seaside-rocks01-normal.jpg' }),
+            gloss: new pc.Asset('gloss', 'texture', { url: '/static/assets/textures/seaside-rocks01-gloss.jpg' })
         };
 
         pc.createGraphicsDevice(canvas, gfxOptions).then((device: pc.GraphicsDevice) => {
@@ -74,8 +76,8 @@ class CustomMaterialExample {
                 camera.script.create("orbitCamera", {
                     attributes: {
                         inertiaFactor: 0.2,
-                        distanceMin: 8,
-                        distanceMax: 50
+                        distanceMin: 2,
+                        distanceMax: 15
                     }
                 });
                 camera.script.create("orbitCameraInputMouse");
@@ -90,14 +92,18 @@ class CustomMaterialExample {
                     type: "omni",
                     color: pc.Color.WHITE,
                     intensity: 1,
-                    range: 100
+                    range: 10
                 });
-                light.translate(0, 3, 0);
+                light.translate(0, 1, 0);
                 app.root.addChild(light);
 
                 const material = new pc.CustomMaterial();
                 material.setParameter("texture_envAtlas", assets.helipad.resource);
                 material.setParameter("material_reflectivity", 1.0);
+                material.setParameter("material_normalMapIntensity", 1.0);
+                material.setParameter("texture_diffuseMap", assets.color.resource);
+                material.setParameter("texture_glossMap", assets.gloss.resource);
+                material.setParameter("texture_normalMap", assets.normal.resource);
                 const options = new pc.LitOptions();
                 options.shadingModel = pc.SPECULAR_BLINN;
                 options.useSpecular = true;
@@ -108,17 +114,25 @@ class CustomMaterialExample {
                 options.ambientSource = 'envAtlas';
                 options.ambientEncoding = assets.helipad.resource.encoding;
                 options.clusteredLightingEnabled = app.scene.clusteredLightingEnabled;
+                options.normalMapEnabled = true;
                 material.litOptions = options;
 
                 const argumentsChunk = `
+                uniform sampler2D texture_diffuseMap;
+                uniform sampler2D texture_glossMap;
+                uniform sampler2D texture_normalMap;
+                uniform float material_normalMapIntensity;
                 void evaluateFrontend() {
                     litArgs_emission = vec3(0, 0, 0);
-                    litArgs_albedo = dVertexNormalW;
                     litArgs_metalness = 0.5;
                     litArgs_specularity = vec3(1,1,1);
                     litArgs_specularityFactor = 1.0;
-                    litArgs_gloss = 0.5;
-                    litArgs_worldNormal = dVertexNormalW;
+                    litArgs_gloss = texture2D(texture_glossMap, vUv0).r;
+
+                    vec3 normalMap = texture2D(texture_normalMap, vUv0).xyz * 2.0 - 1.0;
+                    litArgs_worldNormal = normalize(dTBN * mix(vec3(0,0,1), normalMap, material_normalMapIntensity));
+                    litArgs_albedo = vec3(0.2) + texture2D(texture_diffuseMap, vUv0).xyz;
+
                     litArgs_ao = 0.0;
                     litArgs_opacity = 1.0;
                 }`;
@@ -134,6 +148,12 @@ class CustomMaterialExample {
 
                 // set position and scale and add it to scene
                 app.root.addChild(primitive);
+
+                let time = 0;
+                app.on("update", function (dt: number) {
+                    time += dt;
+                    material.setParameter("material_normalMapIntensity", (Math.sin(time) + 1.0) * 0.5);
+                });
             });
         });
     }
