e7e17dd2c637f31fe2f085b050331d5351575e9e
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2023-07-14 14:10:55 +0100

    Example ModelOutline works on WebGPU now (#5479)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- examples/src/examples/graphics/model-outline.tsx
+++ examples/src/examples/graphics/model-outline.tsx
@@ -3,6 +3,7 @@ import * as pc from '../../../../';
 class ModelOutlineExample {
     static CATEGORY = 'Graphics';
     static NAME = 'Model Outline';
+    static WEBGPU_ENABLED = true;
 
     example(canvas: HTMLCanvasElement, deviceType: string): void {
 
