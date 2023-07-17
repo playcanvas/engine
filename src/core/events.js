caa45cbfe6d4252ad14e816b950cdc13e77a054b
Will Eastcott <will@playcanvas.com>
2023-07-17 16:25:45 +0100

    Fix event.js

--- src/core/events.js
+++ src/core/events.js
@@ -19,7 +19,7 @@ const events = {
         target.fire = ev.fire;
         target.once = ev.once;
         target.hasEvent = ev.hasEvent;
-        target._callbacks = { };
+        target._callbacks = new Map();
         return target;
     },
 
