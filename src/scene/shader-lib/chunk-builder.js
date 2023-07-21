e205e169b82e7da4e80535c9080afe437ca298bd
kungfooman <lama12345@gmail.com>
2023-07-21 17:46:42 +0200

    Backwards compatibility for litArgs

--- src/scene/shader-lib/chunk-builder.js
+++ src/scene/shader-lib/chunk-builder.js
@@ -1,14 +1,34 @@
+/**
+ * Helper function to ensure a bit of backwards compatibility
+ * @example
+ * toLitArgs('litShaderArgs.sheen.specularity'); // Result: 'litArgs_sheen_specularity'
+ * @param {string} src 
+ * @returns {string}
+ */
+const toLitArgs = src => src.replace(/litShaderArgs([\.a-zA-Z]+)+/g, (a, b) => {
+    return 'litArgs' + b.replaceAll(/\./g, '_');
+});
 // helper class for combining shader chunks together
 // ensures every chunk ends with a new line otherwise shaders can be ill-formed
 class ChunkBuilder {
-    code = '';
+    _code = '';
+
+    set code(newCode) {
+        this._code = newCode;
+    }
+    get code() {
+        if (this._code.includes('litShaderArgs')) {
+            this._code = toLitArgs(this._code);
+        }
+        return this._code;
+    }
 
     append(...chunks) {
         chunks.forEach((chunk) => {
             if (chunk.endsWith('\n')) {
-                this.code += chunk;
+                this._code += chunk;
             } else {
-                this.code += chunk + '\n';
+                this._code += chunk + '\n';
             }
         });
     }
@@ -16,9 +36,9 @@ class ChunkBuilder {
     prepend(...chunks) {
         chunks.forEach((chunk) => {
             if (chunk.endsWith('\n')) {
-                this.code = chunk + this.code;
+                this._code = chunk + this._code;
             } else {
-                this.code = chunk + '\n' + this.code;
+                this._code = chunk + '\n' + this._code;
             }
         });
     }
