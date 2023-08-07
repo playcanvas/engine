56ab04b64b7b27f4f896bcab745318eb2b8409c3
kungfooman <lama12345@gmail.com>
2023-08-07 12:26:23 +0200

    examples/src/iframe: SoC + clean up / refactoring

--- examples/src/iframe/index.mjs
+++ examples/src/iframe/index.mjs
@@ -3,8 +3,229 @@ import * as pcuiReal from '@playcanvas/pcui';
 import * as pcui from '@playcanvas/pcui/react';
 import React from 'react';
 import * as pcx from 'playcanvas-extras';
+import * as moduleExamples from "../examples/index.mjs";
+import * as pc from "playcanvas";
 window.pcuiReal = pcuiReal;
 window.observer = observer;
 window.pcui = window.top.pcui || pcui;
 window.React = window.top.React || React;
 window.pcx = pcx;
+window.pc = pc;
+// make pc available outside of the iframe
+window.top.pc = window.pc;
+Object.assign(window, {moduleExamples});
+window.examples = moduleExamples;
+function setupApplication(app) {
+    const canvas = app.graphicsDevice.canvas;
+    // handle resizing
+    var canvasContainerElement = canvas.parentElement;
+    canvas.setAttribute('width', window.innerWidth + 'px');
+    canvas.setAttribute('height', window.innerHeight + 'px');
+    var resizeTimeout = null;
+    app.setCanvasResolution(pc.RESOLUTION_AUTO);
+    if (window.ResizeObserver) {
+        new ResizeObserver(function() {
+            canvas.width = canvasContainerElement.clientWidth;
+            canvas.height = canvasContainerElement.clientHeight;
+        }).observe(canvasContainerElement);
+    }
+    if (app.graphicsDevice.deviceType !== 'webgpu' && !Example.MINISTATS) {
+        // set up miniStats
+        var miniStats = new pcx.MiniStats(app);
+        if (urlParams.get('miniStats') === 'false') {
+            miniStats.enabled = false;
+        }
+        app.on('update', function () {
+            if (window.top._showMiniStats !== undefined) miniStats.enabled = window.top._showMiniStats;
+        });
+    }
+}
+function loadResource(app, resource, callback) {
+    if (!resource.type) {
+        fetch(resource.url)
+            .then(function(response) { return response.text() })
+            .then(function(data) {
+                var module = {
+                    exports: {}
+                };
+                window[resource.name] = (Function('module', 'exports', data).call(module, module, module.exports), module).exports;
+                callback({});
+            });
+        return;
+    }
+}
+function loadResources(app, callback) {
+    var assets = [];
+    window.assets = assets;
+    var assetManifest = {};
+debugger;
+    // stub out the react create element function
+    window.stubbedReact =  {
+        default: {
+            createElement:  function(type, props) {
+                console.log("omg remove this")
+                if (props && props.name && props.url) window.assets.push(props);
+            }
+        }
+    };
+    window.stubbedScriptLoader = {};
+    let loadFunctionString = window.loadFunction.toString();
+    loadFunctionString = loadFunctionString.replace('function () {', '');
+    loadFunctionString = loadFunctionString.replaceAll('React__default', 'stubbedReact');
+    loadFunctionString = loadFunctionString.replaceAll('ScriptLoader', 'stubbedScriptLoader');
+    loadFunctionString = loadFunctionString.slice(0, loadFunctionString.length - 1);
+    // call the stubbed load function to add all assets to the assets list
+    new Function(loadFunctionString)();
+    delete window.stubbedReact;
+    delete window.stubbedScriptLoader;
+    var count = assets.length;
+    function onLoadedResource(key, asset) {
+        count--;
+        if (key) {
+            assetManifest[key] = asset;
+        }
+        if (count <= 0) {
+            callback(assetManifest);
+        }
+    }
+    assets.forEach(function (resource) {
+        loadResource(app, resource, function(asset) {
+            onLoadedResource(resource.name, asset);
+        });
+    });
+}
+/**
+ * @example
+ * const argNames = getFunctionArguments(function(
+ *   canvas,
+ *   deviceType,data
+ * ) {});
+ * console.log(argNames); // Outputs: ['canvas', 'deviceType', 'data']
+ * @param {Function} fn 
+ */
+function getFunctionArguments(fn) {
+    const argsString = fn.toString().match(/.*?\(([^)]*)\)/)[1];
+    return argsString
+        .replace(/ /g, '')
+        .replace(/\n/g, '')
+        .split(',');
+}
+/**
+ * @param {HTMLCanvasElement} canvas - The canvas.
+ * @param {object} assetManifest 
+ * @param {any[]|undefined} files 
+ * @param {observer.Observer} data 
+ */
+function callExample(canvas, assetManifest, files, data) {
+    console.log("callExample", {canvas, assetManifest, files, data})
+    const argNames = getFunctionArguments(window.exampleFunction);
+    const args = argNames.map(function(arg) {
+        if (arg === 'canvas') {
+            return canvas;
+        } else if (arg === 'assetManifest') {
+            return assetManifest;
+        } else if (arg === 'files') {
+            return files;
+        } else if (arg === 'data') {
+            return data;
+        } else if (arg === 'pcx') {
+            return pcx;
+        } else if (arg === 'deviceType') {
+            //debugger;
+            if (Example.WEBGPU_ENABLED) {
+                //return window.top.preferredGraphicsDevice || 'webgpu';
+                return window.top.preferredGraphicsDevice || 'webgl2';
+            } else if (['webgl1', 'webgl2'].includes(window.top.preferredGraphicsDevice)) {
+                return window.top.preferredGraphicsDevice;
+            } else {
+                return 'webgl2';
+            }
+        }
+    });
+    window.exampleFunction.apply(this, args);
+    const pollHandler = setInterval(appCreationPoll, 50);
+    function appCreationPoll() {
+        //debugger;
+        if (pc.app && pc.app.graphicsDevice.canvas) {
+            clearInterval(pollHandler);
+            setupApplication(pc.app);
+            class ExampleLoadEvent extends CustomEvent {
+                /** @type {string} */
+                deviceType;
+                /**
+                 * @param {string} deviceType
+                 */
+                constructor(deviceType) {
+                    super("exampleLoad");
+                    this.deviceType = deviceType;
+                }
+            }
+            var event = new ExampleLoadEvent(pc.app.graphicsDevice.deviceType);
+            window.top.dispatchEvent(event);
+        }
+    }
+}
+//console.log({moduleExamples});
+// polyfill slice on UInt8Array
+if (!Uint8Array.prototype.slice) {
+    Object.defineProperty(Uint8Array.prototype, 'slice', {
+        value: function (begin, end) {
+            return new Uint8Array(Array.prototype.slice.call(this, begin, end));
+        }
+    });
+}
+// get url parameters
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+// notify the parent window that the example is loading
+const event = new CustomEvent("exampleLoading");
+window.top.dispatchEvent(event);
+let found = false;
+if (urlParams.get('category') && urlParams.get('example')) {
+    window.Example = examples[urlParams.get('category')][`${urlParams.get('example')}Example`];
+    if (window.Example) {
+        found = true;
+    }
+}
+if (!found) {
+    console.warn('No example specified, displaying a list of all examples instead.')
+    const examplesContainer = document.createElement('div');
+    Object.keys(examples).forEach(function(category) {
+        const categoryHeader = document.createElement('h2');
+        categoryHeader.innerText = category;
+        examplesContainer.append(categoryHeader);
+        Object.keys(examples[category]).forEach(function(example) {
+            const exampleLink = document.createElement('a');
+            exampleLink.href = `?category=${category}&example=${example.replace('Example', '')}`;
+            exampleLink.innerText = example.replace('Example', '');
+            examplesContainer.append(exampleLink);
+            examplesContainer.append(document.createElement('br'));
+        });
+    });
+    document.body.prepend(examplesContainer);
+    document.body.removeChild(document.getElementById('app'))
+    document.body.style = 'background-color: white; padding: 15px; overflow: auto;';
+} else {
+    const example = new Example();
+    window.exampleFunction = window.top.localStorage.getItem(window.top.location.hash.replace('#', ''));
+    if (!window.exampleFunction) {
+        window.exampleFunction = example.example;
+    } else {
+        window.exampleFunction = new Function('canvas', 'deviceType', 'data', exampleFunction);
+    }
+    window.loadFunction = example.load;
+    window.files = window.top.editedFiles || example.constructor.FILES;
+    // create the example observer 
+    const data = new observer.Observer({});
+    window.top.observerData = data;
+    // load the engine, create the application, load the resources if necessary, then call the example
+    const canvas = document.getElementById('application-canvas');
+    console.log("window.loadFunction", window.loadFunction);
+    if (!window.loadFunction) {
+        callExample(canvas, {}, window.files, data);
+    } else {
+        loadResources(undefined, function(assetManifest) {
+            callExample(canvas, assetManifest, window.files, data);
+        });
+    }
+}
