fa30f7b64847a02bba504cd478d7b6e9094239da
kungfooman <lama12345@gmail.com>
2023-08-15 17:51:05 +0200

    fix read-dir.mjs

--- examples/src/app/helpers/read-dir.mjs
+++ examples/src/app/helpers/read-dir.mjs
@@ -1,15 +1,16 @@
 import fs from 'fs';
 
 const ignoreList = [
-    'lit-material.tsx'
+    'lit-material.mjs',
 ];
 
 const getExamplesList = (mainDir, category) => {
     let examples = fs.readdirSync(`${mainDir}/src/examples/${category}`);
     examples = examples.filter((e) => {
         const exclude = ignoreList.includes(e);
-        if (exclude)
+        if (exclude) {
             console.log(`Excluding ignored file: ${e}`);
+        }
         return !exclude;
     });
     return examples;
