ab11856ffe68c7ac5f166b4de5d2fd98a8af7f5b
Martin Valigursky <mvaligursky@snapchat.com>
2023-08-17 15:13:20 +0200

    lint import

--- src/scene/composition/render-action.js
+++ src/scene/composition/render-action.js
@@ -1,7 +1,3 @@
-import {
-    LIGHTTYPE_DIRECTIONAL
-} from '../constants.js';
-
 /**
  * Class representing an entry in the final order of rendering of cameras and layers in the engine
  * this is populated at runtime based on LayerComposition
