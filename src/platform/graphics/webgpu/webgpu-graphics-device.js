15cd2e04cfd513f5d74564cf6994c60651348ab7
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2023-10-05 13:38:29 +0100

    WebGPU support for alpha backbuffer to allow composition (#5719)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/platform/graphics/webgpu/webgpu-graphics-device.js
+++ src/platform/graphics/webgpu/webgpu-graphics-device.js
@@ -86,6 +86,9 @@ class WebgpuGraphicsDevice extends GraphicsDevice {
         super(canvas, options);
         options = this.initOptions;
 
+        // alpha defaults to true
+        options.alpha = options.alpha ?? true;
+
         this.backBufferAntialias = options.antialias ?? false;
         this.isWebGPU = true;
         this._deviceType = DEVICETYPE_WEBGPU;
@@ -242,7 +245,7 @@ class WebgpuGraphicsDevice extends GraphicsDevice {
         this.canvasConfig = {
             device: this.wgpu,
             colorSpace: 'srgb',
-            alphaMode: 'opaque',  // could also be 'premultiplied'
+            alphaMode: this.initOptions.alpha ? 'premultiplied' : 'opaque',
 
             // use preferred format for optimal performance on mobile
             format: preferredCanvasFormat,
