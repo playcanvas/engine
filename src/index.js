4be3946de4dbfbec3258a40f07eece354a53bba7
Martin Valigursky <mvaligursky@snapchat.com>
2023-10-31 15:59:21 +0000

    Support for FLOAT16 (half) vertex attribute format on WebGL2 and WebGPU

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ export { math } from './core/math/math.js';
 export { Color } from './core/math/color.js';
 export { Curve } from './core/math/curve.js';
 export { CurveSet } from './core/math/curve-set.js';
+export { FloatPacking } from './core/math/float-packing.js';
 export { Mat3 } from './core/math/mat3.js';
 export { Mat4 } from './core/math/mat4.js';
 export { Quat } from './core/math/quat.js';
