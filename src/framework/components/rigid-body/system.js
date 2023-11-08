4d7eb54675e164ac34e37b7eb4b571bb67e5147c
Mark Lundin <mlundin@snapchat.com>
2023-11-08 12:27:30 +0000

    Removing RigidBodyComponent now correctly destroys RigidBody. Fixes #4145

--- src/framework/components/rigid-body/system.js
+++ src/framework/components/rigid-body/system.js
@@ -332,7 +332,6 @@ class RigidBodyComponentSystem extends ComponentSystem {
         this.frameCollisions = {};
 
         this.on('beforeremove', this.onBeforeRemove, this);
-        this.on('remove', this.onRemove, this);
     }
 
     /**
@@ -433,16 +432,9 @@ class RigidBodyComponentSystem extends ComponentSystem {
         if (component.enabled) {
             component.enabled = false;
         }
-    }
 
-    onRemove(entity, component) {
-        const body = component.body;
-        if (body) {
-            this.removeBody(body);
-            this.destroyBody(body);
-
-            component.body = null;
-        }
+        this.destroyBody(component.body);
+        component.body = null;
     }
 
     addBody(body, group, mask) {
