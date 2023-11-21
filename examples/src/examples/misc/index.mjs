0b76ad7b41ee4253e1134d402428e8b99adc03fa
Mark Lundin <mlundin@snapchat.com>
2023-11-21 12:48:44 +0000

    Removed Earthatile ESM example

--- examples/src/examples/misc/index.mjs
+++ examples/src/examples/misc/index.mjs
@@ -1,5 +1,4 @@
 export * from "./hello-world.mjs";
 export * from "./hello-script-modules.mjs";
-export * from "./earthatile-esm.mjs";
 export * from "./mini-stats.mjs";
 export * from "./spineboy.mjs";
