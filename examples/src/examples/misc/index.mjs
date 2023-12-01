ef4f2be088e551ee2ddb08071b9b7e2b4a770c5b
Mark Lundin <mlundin@snapchat.com>
2023-12-01 12:37:08 +0000

    Removed redundant NAME field from examples. Sorted imports.

--- examples/src/examples/misc/index.mjs
+++ examples/src/examples/misc/index.mjs
@@ -1,4 +1,4 @@
-export * from "./hello-world.mjs";
 export * from "./hello-script-modules.mjs";
+export * from "./hello-world.mjs";
 export * from "./mini-stats.mjs";
 export * from "./spineboy.mjs";
