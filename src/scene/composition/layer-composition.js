c1aa445aa7cd57d53568a9fe56812e647bca55bf
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2023-12-01 16:14:42 +0000

    [Fix] Framebuffer clear flags on the Layer were not correctly used (#5875)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/scene/composition/layer-composition.js
+++ src/scene/composition/layer-composition.js
@@ -314,9 +314,10 @@ class LayerComposition extends EventHandler {
 
         // clear flags - use camera clear flags in the first render action for each camera,
         // or when render target (from layer) was not yet cleared by this camera
-        const needsClear = cameraFirstRenderAction || !used;
-        if (needsClear) {
-            renderAction.setupClears(needsClear ? camera : undefined, layer);
+        const needsCameraClear = cameraFirstRenderAction || !used;
+        const needsLayerClear = layer.clearColorBuffer || layer.clearDepthBuffer || layer.clearStencilBuffer;
+        if (needsCameraClear || needsLayerClear) {
+            renderAction.setupClears(needsCameraClear ? camera : undefined, layer);
         }
 
         return renderAction;
