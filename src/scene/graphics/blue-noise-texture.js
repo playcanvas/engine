d33fd54eb39d52be83328c760e61d52252b38be4
Liam Don <liamdon@gmail.com>
2024-01-15 16:49:34 -0800

    Add comment explaining data, use volume on WebGL2, textureArray on WebGPU

--- src/scene/graphics/blue-noise-texture.js
+++ src/scene/graphics/blue-noise-texture.js
@@ -13,25 +13,50 @@ function getBlueNoiseTexture(device) {
         const data = blueNoiseData();
         const size = 32;
 
-        const texture = new Texture(device, {
-            name: `BlueNoise${size}`,
-            width: size,
-            height: size,
-            arrayLength: size,
-            volume: false,
-            format: PIXELFORMAT_L8,
-            addressU: ADDRESS_REPEAT,
-            addressV: ADDRESS_REPEAT,
-            type: TEXTURETYPE_DEFAULT,
-            magFilter: FILTER_NEAREST,
-            minFilter: FILTER_NEAREST,
-            anisotropy: 1,
-            mipmaps: false,
-            levels: [data]
-        });
-
-        // texture.lock().set(data);
-        // texture.unlock();
+        // On WebGPU we use a 2D texture array, while on WebGL2 we use a 3D texture
+        // This is because:
+        // * WebGPU still has poor for support 3D textures, engine hasn't implemented them yet
+        // * WebGL2 did not like using L8 format in an array texture - possible that engine doesn't handle this correctly
+        let texture;
+        if (device.isWebGPU) {
+            texture = new Texture(device, {
+                name: `BlueNoise${size}`,
+                width: size,
+                height: size,
+                volume: false,
+                arrayLength: size,
+                format: PIXELFORMAT_L8,
+                addressU: ADDRESS_REPEAT,
+                addressV: ADDRESS_REPEAT,
+                type: TEXTURETYPE_DEFAULT,
+                magFilter: FILTER_NEAREST,
+                minFilter: FILTER_NEAREST,
+                anisotropy: 1,
+                mipmaps: false,
+                levels: [data]
+            });
+        } else {
+            texture = new Texture(device, {
+                name: `BlueNoise${size}`,
+                width: size,
+                height: size,
+                depth: size,
+                volume: true,
+                format: PIXELFORMAT_L8,
+                addressU: ADDRESS_REPEAT,
+                addressV: ADDRESS_REPEAT,
+                type: TEXTURETYPE_DEFAULT,
+                magFilter: FILTER_NEAREST,
+                minFilter: FILTER_NEAREST,
+                anisotropy: 1,
+                mipmaps: false
+            });
+            const textureData = texture.lock();
+            for (let i = 0; i < size; i++) {
+                textureData.set(data[i], i * 32 * 32);
+            }
+            texture.unlock();
+        }
 
         return texture;
     });
--- src/scene/shader-lib/chunks/standard/frag/opacity-dither.js
+++ src/scene/shader-lib/chunks/standard/frag/opacity-dither.js
@@ -1,12 +1,21 @@
 export default /* glsl */`
 
 #ifndef DITHER_BAYER8
+#ifdef WEBGPU
     precision highp sampler2DArray;
     uniform sampler2DArray blueNoiseTex32;
-    uniform float blueNoiseFrame;
+    const float depthDivisor = 1.0;
+#else
+    precision highp sampler3D;
+    uniform sampler3D blueNoiseTex32;
+    const float depthDivisor = 32.0;
+#endif
+uniform float blueNoiseFrame;
 #endif
 
 uniform vec4 blueNoiseJitter;
+
+
 
 void opacityDither(float alpha, float id) {
     #ifdef DITHER_BAYER8
@@ -16,7 +25,7 @@ void opacityDither(float alpha, float id) {
     #else   // blue noise
 
         vec2 uv2D = fract(gl_FragCoord.xy / 32.0 + blueNoiseJitter.xy + id);
-        vec3 uv3D = vec3(uv2D, blueNoiseFrame);
+        vec3 uv3D = vec3(uv2D, blueNoiseFrame / depthDivisor);
         float noise = texture(blueNoiseTex32, uv3D).r;
 
     #endif
