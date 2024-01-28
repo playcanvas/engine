8e071aeb38c14bce20e2865b7740ac45f2171647
kpal81xd <kpal81xd@gmail.com>
2024-01-28 17:51:29 +0000

    used pool of puppeteer instances for faster thumbnail generation; fixed process killing on windows

--- examples/scripts/thumbnails.mjs
+++ examples/scripts/thumbnails.mjs
@@ -6,14 +6,55 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { kebabCaseToPascalCase, toKebabCase } from '../src/app/helpers/strings.mjs';
 import * as categories from "../src/examples/index.mjs";
-import { spawn } from 'node:child_process';
+import { spawn, execSync } from 'node:child_process';
 
-const port = process.env.PORT || '12321';
 // @ts-ignore
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const PORT = process.env.PORT || '12321';
 const MAIN_DIR = `${__dirname}/../`;
-const debug = false;
+const TIMEOUT = 1e8;
+const DEBUG = false;
+
+// Minimal chrome arguments
+// https://www.bannerbear.com/blog/ways-to-speed-up-puppeteer-screenshots/
+const MINIMAL_ARGS = [
+    '--autoplay-policy=user-gesture-required',
+    '--disable-background-networking',
+    '--disable-background-timer-throttling',
+    '--disable-backgrounding-occluded-windows',
+    '--disable-breakpad',
+    '--disable-client-side-phishing-detection',
+    '--disable-component-update',
+    '--disable-default-apps',
+    '--disable-dev-shm-usage',
+    '--disable-domain-reliability',
+    '--disable-extensions',
+    '--disable-features=AudioServiceOutOfProcess',
+    '--disable-hang-monitor',
+    '--disable-ipc-flooding-protection',
+    '--disable-notifications',
+    '--disable-offer-store-unmasked-wallet-cards',
+    '--disable-popup-blocking',
+    '--disable-print-preview',
+    '--disable-prompt-on-repost',
+    '--disable-renderer-backgrounding',
+    '--disable-setuid-sandbox',
+    '--disable-speech-api',
+    '--disable-sync',
+    '--hide-scrollbars',
+    '--ignore-gpu-blacklist',
+    '--metrics-recording-only',
+    '--mute-audio',
+    '--no-default-browser-check',
+    '--no-first-run',
+    '--no-pings',
+    '--no-sandbox',
+    '--no-zygote',
+    '--password-store=basic',
+    '--use-gl=swiftshader',
+    '--use-mock-keychain'
+];
 
 /** @type {{category: string, example: string}[]} */
 const exampleList = [];
@@ -33,44 +74,180 @@ if (!fs.existsSync(`${MAIN_DIR}/dist/thumbnails`)) {
     fs.mkdirSync(`${MAIN_DIR}/dist/thumbnails`);
 }
 
+class PuppeteerPool {
+    /**
+     * Index of browser with the fewest open pages
+     *
+     * @type {number}
+     */
+    _minPageIdx = 0;
+
+    /**
+     * Internal size of pool size. Defaults to 4.
+     *
+     * @type {number}
+     */
+    _size = 4;
+
+    /**
+     * @typedef {object} Item
+     * @property {import("puppeteer").Browser} browser - Browser instance.
+     * @property {number} pages - Number of open pages.
+     */
+    /**
+     * @type {Item[]}
+     */
+    _pool = [];
+
+    /**
+     * @param {number} size - Pool size.
+     */
+    constructor(size) {
+        if (size < 1) {
+            throw new Error('size must be >1');
+        }
+        this._size = size;
+    }
+
+    /**
+     * @param {import("puppeteer").PuppeteerLaunchOptions} options - Launch options.
+     */
+    async launch(options = {}) {
+        for (let i = 0; i < this._size; i++) {
+            this._pool.push({
+                browser: await puppeteer.launch(options),
+                pages: 0
+            });
+        }
+    }
+
+    /**
+     * Allocates the pool items whos browser has the fewest pages open.
+     *
+     * @returns {Item} - The pool item
+     */
+    allocPoolItem() {
+        for (let i = 0; i < this._pool.length; i++) {
+            if (this._pool[i].pages < this._pool[this._minPageIdx].pages) {
+                this._minPageIdx = i;
+            }
+        }
+        const item = this._pool[this._minPageIdx];
+        return item;
+    }
+
+    /**
+     * @param {Item} item - The pool item.
+     * @returns {Promise<import("puppeteer").Page>} - The created page
+     */
+    newPage(item) {
+        const promise = item.browser.newPage();
+        item.pages++;
+        return promise;
+    }
+
+    /**
+     * @param {Item} item - The pool item.
+     * @param {import("puppeteer").Page} page - The page to close.
+     * @returns {Promise<void>} - The close promise
+     */
+    closePage(item, page) {
+        const promise = page.close();
+        item.pages--;
+        return promise;
+    }
+
+    close() {
+        return Promise.all(this._pool.map((item) => {
+            item.pages = 0;
+            return item.browser.close();
+        }));
+    }
+}
+
+/**
+ * @param {PuppeteerPool} pool - The pool instance.
+ * @param {string} exampleSlug - Example slug.
+ * @param {string} categorySlug - Category slug.
+ * @param {string} example - Example kebab name.
+ * @param {string} category - Category kebab name.
+ */
+async function takeThumbnails(pool, exampleSlug, categorySlug, example, category) {
+    const poolItem = pool.allocPoolItem();
+    const page = await pool.newPage(poolItem);
+    if (DEBUG) {
+        page.on('console', message => console.log(`${message.type().substring(0, 3).toUpperCase()} ${message.text()}`));
+        page.on('pageerror', ({ message }) => console.log(message));
+        page.on('requestfailed', request => console.log(`${request.failure()?.errorText} ${request.url()}`));
+    }
+
+    // navivate to example
+    const link = `http://localhost:${PORT}/iframe/${category}_${example}.html?miniStats=false`;
+    if (DEBUG) {
+        console.log("goto", link);
+    }
+    await page.goto(link, { timeout: TIMEOUT });
+
+    // wait to load
+    if (DEBUG) {
+        console.log("wait for", link);
+    }
+    await page.waitForFunction("window?.pc?.app?._time > 1000", { timeout: TIMEOUT });
+
+    // screenshot page
+    await page.screenshot({ path: `${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}.png` });
+
+    // copy and crop image for large thumbnail
+    await sharp(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}.png`)
+        .resize(320, 240)
+        .toFile(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}_large.png`);
+
+    // copy and crop image for small thumbnail
+    await sharp(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}.png`)
+        .resize(64, 48)
+        .toFile(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}_small.png`);
+
+    // remove screenshot
+    fs.rmSync(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}.png`);
+
+    // close page
+    await pool.closePage(poolItem, page);
+
+    console.log(`screenshot taken for: ${category}/${example}`);
+}
+
 async function takeScreenshots() {
+    if (exampleList.length === 0) {
+        return;
+    }
+
+    // create browser instance with new page
+    const pool = new PuppeteerPool(4);
+    await pool.launch({ headless: 'new', args: MINIMAL_ARGS });
+
+    const screenshotPromises = [];
     for (let i = 0; i < exampleList.length; i++) {
         const exampleListItem = exampleList[i];
         const exampleSlug = exampleListItem.example;
         const categorySlug = exampleListItem.category;
         const example = kebabCaseToPascalCase(exampleListItem.example);
         const category = kebabCaseToPascalCase(exampleListItem.category);
+
+        // check if thumbnail exists
         if (fs.existsSync(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}_large.png`)) {
             console.log(`skipped: ${category}/${example}`);
             continue;
         }
-        const browser = await puppeteer.launch({ headless: 'new' });
-        const page = await browser.newPage();
-        if (debug) {
-            page.on('console', message => console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`));
-            page.on('pageerror', ({ message }) => console.log(message));
-            page.on('requestfailed', request => console.log(`${request.failure()?.errorText} ${request.url()}`));
-        }
-        const link = `http://localhost:${port}/iframe/${category}_${example}.html?miniStats=false`;
-        if (debug) {
-            console.log("goto", link);
-        }
-        await page.goto(link);
-        if (debug) {
-            console.log("wait for", link);
-        }
-        await page.waitForFunction("window?.pc?.app?._time > 1000");
-        await page.screenshot({ path: `${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}.png` });
-        await sharp(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}.png`)
-            .resize(320, 240)
-            .toFile(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}_large.png`);
-        await sharp(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}.png`)
-            .resize(64, 48)
-            .toFile(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}_small.png`);
-        fs.rmSync(`${MAIN_DIR}/dist/thumbnails/${categorySlug}_${exampleSlug}.png`);
-        console.log(`screenshot taken for: ${category}/${example}`);
-        await browser.close();
+
+        screenshotPromises.push(takeThumbnails(pool, exampleSlug, categorySlug, example, category));
+
     }
+
+    // ensure all screenshots have finished.
+    await Promise.all(screenshotPromises);
+
+    // close pool
+    await pool.close();
 }
 
 /**
@@ -84,22 +261,29 @@ function sleep(ms = 0) {
 }
 
 async function main() {
-    console.log('Spawn server on', port);
+    console.log('Spawn server on', PORT);
     // We need this kind of command:
     // npx serve dist --config ../serve.json
     // Reason: https://github.com/vercel/serve/issues/732
     // (who *ever* thought that stripping .html was a good idea in the first place...)
-    const cmd = process.platform === 'win32' ? 'npx.cmd' : 'npx';
-    const server = spawn(cmd, ['serve', 'dist', '-l', port, '--no-request-logging', '--config', '../serve.json']);
+    const isWin = process.platform === 'win32';
+    const cmd = isWin ? 'npx.cmd' : 'npx';
+    const server = spawn(cmd, ['serve', 'dist', '-l', PORT, '--no-request-logging', '--config', '../serve.json']);
     await sleep(1000); // give a second to spawn server
     console.log("Starting puppeteer screenshot process");
     try {
+        console.time('Time');
         await takeScreenshots();
+        console.timeEnd('Time');
     } catch (e) {
         console.error(e);
     }
-    console.log('Kill server on', port);
-    server.kill();
+    if (isWin) {
+        execSync(`taskkill /f /pid ${server.pid}`);
+    } else {
+        server.kill();
+    }
+    console.log('Killed server on', PORT);
     return 0;
 }
 main().then(process.exit);
