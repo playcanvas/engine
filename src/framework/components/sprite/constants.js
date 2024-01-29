9907f94b0fb852f4ce64f31ab85f2310606e6aad
Will Eastcott <will@playcanvas.com>
2024-01-29 10:56:02 +0000

    Assign categories to constants

--- src/framework/components/sprite/constants.js
+++ src/framework/components/sprite/constants.js
@@ -2,6 +2,7 @@
  * A {@link SpriteComponent} that displays a single frame from a sprite asset.
  *
  * @type {string}
+ * @category Graphics
  */
 export const SPRITETYPE_SIMPLE = 'simple';
 
@@ -9,5 +10,6 @@ export const SPRITETYPE_SIMPLE = 'simple';
  * A {@link SpriteComponent} that renders sprite animations.
  *
  * @type {string}
+ * @category Graphics
  */
 export const SPRITETYPE_ANIMATED = 'animated';
