a793cd2f9e6983de05ea351ab3ce8009790a41e0
Erik Dubbelboer <erik@dubbelboer.com>
2024-02-11 07:19:01 +0100

    Fix suggestion

--- src/scene/shader-lib/chunks/lit/frag/TBN.js
+++ src/scene/shader-lib/chunks/lit/frag/TBN.js
@@ -1,13 +1,9 @@
 export default /* glsl */`
 void getTBN(vec3 tangent, vec3 binormal, vec3 normal) {
+    dTBN = mat3(normalize(tangent), normalize(binormal), normalize(normal));
+
     #ifdef TWO_SIDED_LIGHTING
-        dTBN = mat3(
-            normalize((gl_FrontFacing ? tangent  : -tangent ) * twoSidedLightingNegScaleFactor),
-            normalize((gl_FrontFacing ? binormal : -binormal) * twoSidedLightingNegScaleFactor),
-            normalize((gl_FrontFacing ? normal   : -normal  ) * twoSidedLightingNegScaleFactor)
-        );
-    #else
-        dTBN = mat3(normalize(tangent), normalize(binormal), normalize(normal));
+    dTBN *= ((gl_FrontFacing ? 1.0 : -1.0) * twoSidedLightingNegScaleFactor);
     #endif
 }
 `;
