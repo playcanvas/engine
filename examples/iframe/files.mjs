0d46a5393c3d128bf381fa5a3348091b41e542f2
kpal <kpal81xd@gmail.com>
2024-02-16 16:11:31 +0000

    moved loader to separate file and cleaned up exposing attributes and functions

--- examples/iframe/files.mjs
+++ examples/iframe/files.mjs
@@ -1,5 +1,7 @@
-/**
- * This is the store for the files object. It is updated by passing its reference between components.
- */
-const files = {};
+import config from '@examples/config';
+const files = {
+    'example.mjs': '',
+    'controls.mjs': ''
+};
+Object.assign(files, config.FILES ?? {});
 export default files;
