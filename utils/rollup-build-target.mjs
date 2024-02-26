2b89fc6ff49274f0a269c636b5bdbffae2af25fd
Mark Lundin <mlundin@snapchat.com>
2024-02-26 13:10:48 +0000

    Add rollup transform

--- utils/rollup-build-target.mjs
+++ utils/rollup-build-target.mjs
@@ -11,6 +11,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 import { shaderChunks } from './rollup-shader-chunks.mjs';
 import { engineLayerImportValidation } from './rollup-import-validation.mjs';
 import { spacesToTabs } from './rollup-spaces-to-tabs.mjs';
+import { dynamicImportTransform } from './rollup-dynamic-import-transform.mjs';
 
 import { version, revision } from './rollup-version-revision.mjs';
 import { getBanner } from './rollup-get-banner.mjs';
@@ -177,6 +178,7 @@ function buildTarget(buildType, moduleFormat, input = 'src/index.js', buildDir =
         output: outputOptions,
         plugins: [
             jscc(jsccParam),
+            moduleFormat === 'es5' ? dynamicImportTransform() : undefined,
             shaderChunks({ enabled: buildType !== 'debug' }),
             engineLayerImportValidation(input, buildType === 'debug'),
             buildType !== 'debug' ? strip(stripOptions) : undefined,
