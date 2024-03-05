48c8fb112c3ddb37d33bf72ddc1e1a420e26557a
Will Eastcott <will@playcanvas.com>
2024-03-05 15:40:06 +0000

    Update banner to mention MIT license

--- utils/rollup-get-banner.mjs
+++ utils/rollup-get-banner.mjs
@@ -7,13 +7,14 @@ import { version, revision } from './rollup-version-revision.mjs';
  * @returns {string} - The banner.
  */
 function getBanner(config) {
-    return [
-        '/**',
-        ' * @license',
-        ' * PlayCanvas Engine v' + version + ' revision ' + revision + config,
-        ' * Copyright 2011-' + new Date().getFullYear() + ' PlayCanvas Ltd. All rights reserved.',
-        ' */'
-    ].join('\n');
+    return `/**
+ * @license
+ * PlayCanvas Engine v${version} revision ${revision}${config}
+ * Copyright 2011-${new Date().getFullYear()} PlayCanvas Ltd. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */`;
 }
 
 export { getBanner };
