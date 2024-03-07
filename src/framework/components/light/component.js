bc2e3f9f62f0cd10282ceebda9f5dd2dd1a5d5a9
kpal <kpal81xd@gmail.com>
2024-03-07 15:51:47 +0000

    lint fix

--- src/framework/components/light/component.js
+++ src/framework/components/light/component.js
@@ -54,7 +54,7 @@ class LightComponent extends Component {
         this._cookieMatrix = null;
     }
 
-    //TODO: Remove this override in upgrading component
+    // TODO: Remove this override in upgrading component
     /**
      * @type {import('./data.js').LightComponentData}
      */
