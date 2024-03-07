c86854abaf269b299f00c2fb13ca5b3cf2805ff5
KPal <48248865+kpal81xd@users.noreply.github.com>
2024-03-07 17:21:11 +0000

    Revert "embedded empty script handler to remove types fixup (#6123)"
    This reverts commit 67a55f5f9602f191f68e21ffbfbd33d1b6f2c6e2.

--- utils/types-fixup.mjs
+++ utils/types-fixup.mjs
@@ -525,3 +525,34 @@ import { BoundingBox } from '../../core/shape/bounding-box.js';
 import { Texture } from '../../platform/graphics/texture.js';
 `;
 fs.writeFileSync(path, dts);
+
+path = './types/framework/script/script-type.d.ts';
+dts = fs.readFileSync(path, 'utf8');
+dts = dts.replace('get enabled(): boolean;', `get enabled(): boolean;
+    /**
+     * Called when script is about to run for the first time.
+     */
+    initialize?(): void;
+    /**
+     * Called after all initialize methods are executed in the same tick or enabling chain of actions.
+     */
+    postInitialize?(): void;
+    /**
+     * Called for enabled (running state) scripts on each tick.
+     * @param dt - The delta time in seconds since the last frame.
+     */
+    update?(dt: number): void;
+    /**
+     * Called for enabled (running state) scripts on each tick, after update.
+     * @param dt - The delta time in seconds since the last frame.
+     */
+    postUpdate?(dt: number): void;
+    /**
+     * Called when a ScriptType that already exists in the registry gets redefined. If the new
+     * ScriptType has a \`swap\` method in its prototype, then it will be executed to perform
+     * hot-reload at runtime.
+     * @param old - Old instance of the scriptType to copy data to the new instance.
+     */
+    swap?(old: ScriptType): void;
+`);
+fs.writeFileSync(path, dts);
