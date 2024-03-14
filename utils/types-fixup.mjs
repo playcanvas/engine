ad3e95546aeb064d6562b2dd2743ca0cb0bb3721
kpal <kpal81xd@gmail.com>
2024-03-14 16:56:58 +0000

    ported collision component to get/set

--- utils/types-fixup.mjs
+++ utils/types-fixup.mjs
@@ -52,25 +52,6 @@ import { Asset } from '../../../framework/asset/asset.js';
 `;
 fs.writeFileSync(path, dts);
 
-const collisionComponentProps = [
-    ['axis', 'number'],
-    ['halfExtents', 'Vec3'],
-    ['height', 'number'],
-    ['model', 'Model|null'],
-    ['radius', 'number'],
-    ['type', 'string']
-];
-
-path = './types/framework/components/collision/component.d.ts';
-dts = fs.readFileSync(path, 'utf8');
-dts = dts.replace(regexConstructor, '$&\n' + getDeclarations(collisionComponentProps));
-// We need to import types that are newly introduced in the property list above
-dts += `
-import { Vec3 } from '../../../core/math/vec3.js';
-import { Model } from '../../../scene/model.js';
-`;
-fs.writeFileSync(path, dts);
-
 const elementComponentProps = [
     ['alignment', 'Vec2'],
     ['autoFitHeight', 'boolean'],
