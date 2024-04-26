e2ec5ebb0c84882e58cbd694e7c3b3b8438a12aa
Martin Valigursky <mvaligursky@snapchat.com>
2024-04-26 13:01:17 +0100

    Internal class to expose engine features

---
+++ src/framework/features.js
@@ -0,0 +1,11 @@
+/**
+ * Pure static class exposing a set of flags of engine features, allowing the external tools like
+ * the Editor to work with multiple engine versions easily.
+ *
+ * @ignore
+ */
+class Features {
+    static removedWebGL1 = true;
+}
+
+export { Features };
