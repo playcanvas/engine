c3c7e6cf0143b3aa87f44a314c627a5a9e6c3f80
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2024-05-10 14:44:29 +0100

    Updated one missed VSM shader chunk for WebGPU compatibility (#6348)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/scene/shader-lib/chunks/lit/frag/shadowEVSM.js
+++ src/scene/shader-lib/chunks/lit/frag/shadowEVSM.js
@@ -1,14 +1,14 @@
 export default /* glsl */`
-float VSM$(sampler2D tex, vec2 texCoords, float resolution, float Z, float vsmBias, float exponent) {
+float VSM$(TEXTURE_ACCEPT(tex), vec2 texCoords, float resolution, float Z, float vsmBias, float exponent) {
     vec3 moments = texture2D(tex, texCoords).xyz;
     return calculateEVSM(moments, Z, vsmBias, exponent);
 }
 
-float getShadowVSM$(sampler2D shadowMap, vec3 shadowCoord, vec4 shadowParams, float exponent, vec3 lightDir) {
-    return VSM$(shadowMap, shadowCoord.xy, shadowParams.x, shadowCoord.z, shadowParams.y, exponent);
+float getShadowVSM$(TEXTURE_ACCEPT(shadowMap), vec3 shadowCoord, vec4 shadowParams, float exponent, vec3 lightDir) {
+    return VSM$(TEXTURE_PASS(shadowMap), shadowCoord.xy, shadowParams.x, shadowCoord.z, shadowParams.y, exponent);
 }
 
-float getShadowSpotVSM$(sampler2D shadowMap, vec3 shadowCoord, vec4 shadowParams, float exponent, vec3 lightDir) {
-    return VSM$(shadowMap, shadowCoord.xy, shadowParams.x, length(lightDir) * shadowParams.w + shadowParams.z, shadowParams.y, exponent);
+float getShadowSpotVSM$(TEXTURE_ACCEPT(shadowMap), vec3 shadowCoord, vec4 shadowParams, float exponent, vec3 lightDir) {
+    return VSM$(TEXTURE_PASS(shadowMap), shadowCoord.xy, shadowParams.x, length(lightDir) * shadowParams.w + shadowParams.z, shadowParams.y, exponent);
 }
 `;
