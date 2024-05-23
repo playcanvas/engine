bf9ee4fdb36dd884b221f9071eab21b1127cd8f7
KPal <48248865+kpal81xd@users.noreply.github.com>
2024-05-23 11:52:03 +0100

    Examples app bundle v1 (#6594)
    * treeshake smallest enabled
    
    * ignore playcanvas PCUI from being treeshaken
    
    * treeshake ignore only pcui
    
    * added device type constants directly (no playcanvas dependency)
    
    * format rollup
    
    * Updated PCUI
    
    * removed PCUI aliasing

--- examples/src/app/constants.mjs
+++ examples/src/app/constants.mjs
@@ -1,1 +1,9 @@
 export const MIN_DESKTOP_WIDTH = 601;
+
+export const DEVICETYPE_WEBGL2 = 'webgl2';
+
+export const DEVICETYPE_WEBGL1 = 'webgl1';
+
+export const DEVICETYPE_WEBGPU = 'webgpu';
+
+export const DEVICETYPE_NULL = 'null';
