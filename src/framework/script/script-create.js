3c7d7db7f2f9cc289a6ce8920d75a5ac1059ece9
Mark Lundin <mlundin@snapchat.com>
2024-06-06 14:17:02 +0100

    Remove legacy script system

--- src/framework/script/script-create.js
+++ src/framework/script/script-create.js
@@ -1,9 +1,5 @@
-import { Debug } from '../../core/debug.js';
 import { EventHandler } from '../../core/event-handler.js';
-
-import { script } from '../script.js';
 import { AppBase } from '../app-base.js';
-
 import { ScriptAttributes } from './script-attributes.js';
 import { ScriptType } from './script-type.js';
 import { ScriptTypes } from './script-types.js';
@@ -58,11 +54,6 @@ function getReservedScriptNames() {
  * @category Script
  */
 function createScript(name, app) {
-    if (script.legacy) {
-        Debug.error('This project is using the legacy script system. You cannot call pc.createScript().');
-        return null;
-    }
-
     if (reservedScriptNames.has(name))
         throw new Error(`Script name '${name}' is reserved, please rename the script`);
 
@@ -128,11 +119,6 @@ createScript.reservedAttributes = reservedAttributes;
  * @category Script
  */
 function registerScript(script, name, app) {
-    if (script.legacy) {
-        Debug.error('This project is using the legacy script system. You cannot call pc.registerScript().');
-        return;
-    }
-
     if (typeof script !== 'function')
         throw new Error(`script class: '${script}' must be a constructor function (i.e. class).`);
 
@@ -150,7 +136,7 @@ function registerScript(script, name, app) {
     const registry = app ? app.scripts : AppBase.getApplication().scripts;
     registry.add(script);
 
-    ScriptTypes.push(script, script.legacy);
+    ScriptTypes.push(script);
 }
 /* eslint-enable jsdoc/check-examples */
 
