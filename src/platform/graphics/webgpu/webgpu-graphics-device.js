d9f301d8120d7d3633f427b0f225349cead8b6e3
Catalin Moldovan <moldovan.catalin@gmail.com>
2024-06-15 14:18:35 +0300

    WebGL _uploadDirtyTextures

--- src/platform/graphics/webgpu/webgpu-graphics-device.js
+++ src/platform/graphics/webgpu/webgpu-graphics-device.js
@@ -658,7 +658,7 @@ class WebgpuGraphicsDevice extends GraphicsDevice {
     _uploadDirtyTextures() {
 
         this.textures.forEach((texture) => {
-            if (texture._needsUpload || texture._needsMipmaps) {
+            if (texture._needsUpload || texture._needsMipmapsUpload) {
                 texture.upload();
             }
         });
