a22e50ed9216377909d2dd889542e693e8a913e2
Catalin Moldovan <moldovan.catalin@gmail.com>
2024-06-16 13:03:44 +0300

    Fix back creation for empty cubemaps

--- src/platform/graphics/webgl/webgl-texture.js
+++ src/platform/graphics/webgl/webgl-texture.js
@@ -544,7 +544,7 @@ class WebglTexture {
                     resMult = 1 / Math.pow(2, mipLevel);
                     for (face = 0; face < texture.slices; face++) {
                         const texData = mipObject[face];
-                        if (!texture._levelsUpdated[0][face] || !texData)
+                        if (!texture._levelsUpdated[0][face])
                             continue;
                         }
 
