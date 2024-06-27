3d4d0cd0f99fc89e2edf81e4655e5c8a6d2be238
kpal <kpal81xd@gmail.com>
2024-06-27 17:33:52 +0100

    use distance instead of length for scale

--- src/extras/gizmo/gizmo.js
+++ src/extras/gizmo/gizmo.js
@@ -373,7 +373,7 @@ class Gizmo extends EventHandler {
         if (this._camera.projection === PROJECTION_PERSPECTIVE) {
             const gizmoPos = this.root.getPosition();
             const cameraPos = this._camera.entity.getPosition();
-            const dist = gizmoPos.sub(cameraPos).length();
+            const dist = gizmoPos.distance(cameraPos);
             this._scale = Math.tan(0.5 * this._camera.fov * math.DEG_TO_RAD) * dist * PERS_SCALE_RATIO;
         } else {
             this._scale = this._camera.orthoHeight * ORTHO_SCALE_RATIO;
