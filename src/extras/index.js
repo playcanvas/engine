80473b4c44b1a3e63ca0570ea785c3377194a664
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2024-09-06 09:23:38 +0100

    Refactored the API for RenderPassCameraFrame (#6916)
    * Refactored the API for RenderPassCameraFrame
    
    * lint
    
    * lint
    
    ---------
    
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/extras/index.js
+++ src/extras/index.js
@@ -15,7 +15,7 @@ export { GltfExporter } from './exporters/gltf-exporter.js';
 
 // RENDER PASSES
 export { SSAOTYPE_NONE, SSAOTYPE_LIGHTING, SSAOTYPE_COMBINE } from './render-passes/render-pass-camera-frame.js';
-export { RenderPassCameraFrame } from './render-passes/render-pass-camera-frame.js';
+export { RenderPassCameraFrame, CameraFrameOptions } from './render-passes/render-pass-camera-frame.js';
 export { RenderPassCompose } from './render-passes/render-pass-compose.js';
 export { RenderPassDepthAwareBlur } from './render-passes/render-pass-depth-aware-blur.js';
 export { RenderPassDownsample } from './render-passes/render-pass-downsample.js';
