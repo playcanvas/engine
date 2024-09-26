acc134aabcd1928ebf48f495ae61289c216cc5ac
kpal <kpal81xd@gmail.com>
2024-09-26 14:41:20 +0100

    WIP: added planes for render

--- src/extras/gizmo/bounding-box-gizmo.js
+++ src/extras/gizmo/bounding-box-gizmo.js
@@ -1,16 +1,28 @@
+import { Color } from '../../core/math/color.js';
+import { Vec3 } from '../../core/math/vec3.js';
 import { BoundingBox } from '../../core/shape/bounding-box.js';
+import { GIZMOAXIS_X, GIZMOAXIS_Y, GIZMOAXIS_Z } from './constants.js';
 import { Gizmo } from './gizmo.js';
+import { PlaneShape } from './shape/plane-shape.js';
 
 /**
+ * @import { Shape } from './shape/shape.js'
  * @import { CameraComponent } from '../../framework/components/camera/component.js'
  * @import { Layer } from '../../scene/layer.js'
  */
+
+const tmpV1 = new Vec3();
 
 class BoundingBoxGizmo extends Gizmo {
     /**
      * @type {BoundingBox}
      */
     _bb = new BoundingBox();
+
+    /**
+     * @type {Color}
+     */
+    _color = new Color(1, 1, 1, 1);
 
     /**
      * Creates a new TransformGizmo object.
@@ -24,7 +36,40 @@ class BoundingBoxGizmo extends Gizmo {
         super(camera, layer);
 
         for (let i = 0; i < this.nodes.length; i++) {
-            this._bb.add(this.nodes[i].getBoundingBox());
+            const node = this.nodes[i];
+            const bb = new BoundingBox(
+                node.getPosition(),
+                tmpV1.copy(node.getScale()).mulScalar(0.5)
+            );
+            this._bb.add(bb);
+        }
+
+        const shapes = {
+            x: new PlaneShape(this._device, {
+                axis: GIZMOAXIS_X,
+                layers: [this._layer.id],
+                defaultColor: this._color,
+                hoverColor: this._color
+            }),
+            y: new PlaneShape(this._device, {
+                axis: GIZMOAXIS_Y,
+                layers: [this._layer.id],
+                defaultColor: this._color,
+                hoverColor: this._color
+            }),
+            z: new PlaneShape(this._device, {
+                axis: GIZMOAXIS_Z,
+                layers: [this._layer.id],
+                defaultColor: this._color,
+                hoverColor: this._color
+            })
+        };
+
+        for (const key in shapes) {
+            const shape = shapes[key];
+            shape.gap = 0;
+            shape.size = 2;
+            this.root.addChild(shape.entity);
         }
     }
 }
