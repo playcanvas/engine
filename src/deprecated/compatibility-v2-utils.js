7445a2a3cf84a5f9d991dc3484738891cfb786ea
Martin Valigursky <mvaligursky@snapchat.com>
2024-10-25 14:30:41 +0100

    added aoIntensity

--- src/deprecated/compatibility-v2-utils.js
+++ src/deprecated/compatibility-v2-utils.js
@@ -1,4 +1,5 @@
 // add StandardMaterial parameter types the engine v2 supports to avoid validation warnings
 export function __adjustStandardMaterialParameterTypes(types) {
     types.useGamma = 'boolean';
+    types.aoIntensity = 'number';
 }
--- src/deprecated/compatibility-v2.js
+++ src/deprecated/compatibility-v2.js
@@ -10,6 +10,15 @@ Object.defineProperty(StandardMaterial.prototype, 'useGamma', {
     }
 });
 
+// ignore aoIntensity as it's not supported
+Object.defineProperty(StandardMaterial.prototype, 'aoIntensity', {
+    get: function () {
+        return 1;
+    },
+    set: function (value) {
+    }
+});
+
 // dummy export to avoid the module being removed by rollup
 // TODO: remove this export when the module has other exports
 export function __dummyFunction() {
