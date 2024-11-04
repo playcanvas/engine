243f243251d9441d7d0a145231adcb9a8587bd65
Martin Valigursky <mvaligursky@snapchat.com>
2024-11-04 14:33:03 +0000

    lint

--- src/deprecated/compatibility-v2-utils.js
+++ src/deprecated/compatibility-v2-utils.js
@@ -9,12 +9,12 @@ const _tintProperties = [
     'emissiveTint',
     'diffuseTint',
     'sheenTint'
-]
+];
 
 // in the engine v2 material json data, the tints were removed and are assumed to be always true
 // for the engine v1, force them to be true
 export function __adjustStandardMaterialData(data) {
-    _tintProperties.forEach(prop => {
+    _tintProperties.forEach((prop) => {
         if (data[prop] === undefined) {
             data[prop] = true;
         }
