c570f6b1fe09a1f9c77c8e07662bf6c5a16800e5
kpal <kpal81xd@gmail.com>
2024-11-13 14:02:43 +0000

    Added grid at correct layer index

--- scripts/grid.mjs
+++ scripts/grid.mjs
@@ -387,7 +387,7 @@ class Grid extends Script {
         }
         this._camera = camera;
 
-        layer ??= Grid.createLayer(this.app);
+        layer ??= Grid.createLayer(this.app, undefined, 1);
         camera.layers = camera.layers.concat(layer.id);
 
         camera.onPreRenderLayer = (_layer, transparent) => {
