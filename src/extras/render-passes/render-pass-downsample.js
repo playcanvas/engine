193966637f2271f87878283f50bfbf65b8f64f4e
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2024-11-13 09:38:55 +0000

    Small bloom render pass optimization (#7104)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/extras/render-passes/render-pass-downsample.js
+++ src/extras/render-passes/render-pass-downsample.js
@@ -7,10 +7,12 @@ import { RenderPassShaderQuad } from '../../scene/graphics/render-pass-shader-qu
  * @ignore
  */
 class RenderPassDownsample extends RenderPassShaderQuad {
-    constructor(device, sourceTexture) {
+    constructor(device, sourceTexture, fast = false) {
         super(device);
         this.sourceTexture = sourceTexture;
-        this.shader = this.createQuadShader('DownSampleShader', /* glsl */`
+        this.shader = this.createQuadShader(`DownSampleShader${fast ? 'Fast' : ''}`, /* glsl */`
+
+            ${fast ? '#define FAST' : ''}
 
             uniform sampler2D sourceTexture;
             uniform vec2 sourceInvResolution;
@@ -18,30 +20,37 @@ class RenderPassDownsample extends RenderPassShaderQuad {
 
             void main()
             {
-                float x = sourceInvResolution.x;
-                float y = sourceInvResolution.y;
+                vec3 e = texture2D (sourceTexture, vec2 (uv0.x, uv0.y)).rgb;
 
-                vec3 a = texture2D (sourceTexture, vec2 (uv0.x - 2.0 * x, uv0.y + 2.0 * y)).rgb;
-                vec3 b = texture2D (sourceTexture, vec2 (uv0.x,           uv0.y + 2.0 * y)).rgb;
-                vec3 c = texture2D (sourceTexture, vec2 (uv0.x + 2.0 * x, uv0.y + 2.0 * y)).rgb;
+                #ifdef FAST
+                    vec3 value = e;
+                #else
 
-                vec3 d = texture2D (sourceTexture, vec2 (uv0.x - 2.0 * x, uv0.y)).rgb;
-                vec3 e = texture2D (sourceTexture, vec2 (uv0.x,           uv0.y)).rgb;
-                vec3 f = texture2D (sourceTexture, vec2 (uv0.x + 2.0 * x, uv0.y)).rgb;
+                    float x = sourceInvResolution.x;
+                    float y = sourceInvResolution.y;
 
-                vec3 g = texture2D (sourceTexture, vec2 (uv0.x - 2.0 * x, uv0.y - 2.0 * y)).rgb;
-                vec3 h = texture2D (sourceTexture, vec2 (uv0.x,           uv0.y - 2.0 * y)).rgb;
-                vec3 i = texture2D (sourceTexture, vec2 (uv0.x + 2.0 * x, uv0.y - 2.0 * y)).rgb;
+                    vec3 a = texture2D (sourceTexture, vec2 (uv0.x - 2.0 * x, uv0.y + 2.0 * y)).rgb;
+                    vec3 b = texture2D (sourceTexture, vec2 (uv0.x,           uv0.y + 2.0 * y)).rgb;
+                    vec3 c = texture2D (sourceTexture, vec2 (uv0.x + 2.0 * x, uv0.y + 2.0 * y)).rgb;
 
-                vec3 j = texture2D (sourceTexture, vec2 (uv0.x - x, uv0.y + y)).rgb;
-                vec3 k = texture2D (sourceTexture, vec2 (uv0.x + x, uv0.y + y)).rgb;
-                vec3 l = texture2D (sourceTexture, vec2 (uv0.x - x, uv0.y - y)).rgb;
-                vec3 m = texture2D (sourceTexture, vec2 (uv0.x + x, uv0.y - y)).rgb;
+                    vec3 d = texture2D (sourceTexture, vec2 (uv0.x - 2.0 * x, uv0.y)).rgb;
+                    vec3 f = texture2D (sourceTexture, vec2 (uv0.x + 2.0 * x, uv0.y)).rgb;
 
-                vec3 value = e * 0.125;
-                value += (a + c + g + i) * 0.03125;
-                value += (b + d + f + h) * 0.0625;
-                value += (j + k + l + m) * 0.125;
+                    vec3 g = texture2D (sourceTexture, vec2 (uv0.x - 2.0 * x, uv0.y - 2.0 * y)).rgb;
+                    vec3 h = texture2D (sourceTexture, vec2 (uv0.x,           uv0.y - 2.0 * y)).rgb;
+                    vec3 i = texture2D (sourceTexture, vec2 (uv0.x + 2.0 * x, uv0.y - 2.0 * y)).rgb;
+
+                    vec3 j = texture2D (sourceTexture, vec2 (uv0.x - x, uv0.y + y)).rgb;
+                    vec3 k = texture2D (sourceTexture, vec2 (uv0.x + x, uv0.y + y)).rgb;
+                    vec3 l = texture2D (sourceTexture, vec2 (uv0.x - x, uv0.y - y)).rgb;
+                    vec3 m = texture2D (sourceTexture, vec2 (uv0.x + x, uv0.y - y)).rgb;
+
+
+                    vec3 value = e * 0.125;
+                    value += (a + c + g + i) * 0.03125;
+                    value += (b + d + f + h) * 0.0625;
+                    value += (j + k + l + m) * 0.125;
+                #endif
 
                 gl_FragColor = vec4(value, 1.0);
             }`
