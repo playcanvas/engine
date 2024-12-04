8d10e1891d3690146440426354c26c5c4289b842
Donovan Hutchence <slimbuck7@gmail.com>
2024-12-04 17:48:59 +0000

    move HandleEventCallback to fix types

--- src/core/event-handler.js
+++ src/core/event-handler.js
@@ -1,17 +1,7 @@
 import { EventHandle } from './event-handle.js';
 
 /**
- * Callback used by {@link EventHandler} functions. Note the callback is limited to 8 arguments.
- *
- * @callback HandleEventCallback
- * @param {*} [arg1] - First argument that is passed from caller.
- * @param {*} [arg2] - Second argument that is passed from caller.
- * @param {*} [arg3] - Third argument that is passed from caller.
- * @param {*} [arg4] - Fourth argument that is passed from caller.
- * @param {*} [arg5] - Fifth argument that is passed from caller.
- * @param {*} [arg6] - Sixth argument that is passed from caller.
- * @param {*} [arg7] - Seventh argument that is passed from caller.
- * @param {*} [arg8] - Eighth argument that is passed from caller.
+ * @import { HandleEventCallback } from './event-handle.js'
  */
 
 /**
