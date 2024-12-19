e81a610897be9c22aad5ee8ff33ef5359515d934
mrmaxm <core@moka.co>
2024-12-19 15:54:51 +0200

    pr comment

--- src/framework/components/render/component.js
+++ src/framework/components/render/component.js
@@ -719,7 +719,9 @@ class RenderComponent extends Component {
                 this._rootBone = value;
             } else if (isString) {
                 this._rootBone = this.system.app.getEntityFromIndex(value) || null;
-                Debug.assert(this._rootBone, 'Failed to find rootBone Entity by GUID');
+                if (!this._rootBone) {
+                    Debug.warn('Failed to find rootBone Entity by GUID');
+                }
             } else {
                 this._rootBone = null;
             }
