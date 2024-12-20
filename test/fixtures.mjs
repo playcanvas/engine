8fc8829b7313802419ffd6fab5d53873fa086e89
Will Eastcott <will@playcanvas.com>
2024-12-20 13:39:57 +0000

    Drop xhr2

--- test/fixtures.mjs
+++ test/fixtures.mjs
@@ -1,16 +1,12 @@
 import globalJsdom from 'global-jsdom';
 import handler from 'serve-handler';
 import http from 'http';
-import XMLHttpRequest from 'xhr2';
 let server;
 
 export const mochaGlobalSetup = () => {
     globalJsdom(undefined, {
         resources: 'usable'
     });
-
-    // Provide a polyfill for XMLHttpRequest required by the engine
-    global.XMLHttpRequest = XMLHttpRequest;
 
     server = http.createServer((request, response) => {
         return handler(request, response);
