eee7633a935b677c114eafae563ee2ee420e6f98
Will Eastcott <will@playcanvas.com>
2024-12-23 13:20:02 +0000

    Migrate SpriteComponent tests from Karma to Node (#7232)
    * Migrate SpriteComponent tests from Karma to Node
    
    * Lint fixes

--- test/framework/components/sprite/component.test.mjs
+++ test/framework/components/sprite/component.test.mjs
@@ -1,43 +1,37 @@
-describe("pc.SpriteComponent", function () {
-    var app = null;
-    var atlasAsset = null;
-    var spriteAsset = null;
-    var spriteAsset2 = null;
+import { expect } from 'chai';
 
-    beforeEach(function (done) {
-        app = new pc.Application(document.createElement("canvas"));
+import { Asset } from '../../../../src/framework/asset/asset.js';
+import { Entity } from '../../../../src/framework/entity.js';
+import { createApp } from '../../../app.mjs';
+import { jsdomSetup, jsdomTeardown } from '../../../jsdom.mjs';
 
-        loadAssets(function () {
-            done();
-        });
-    });
+describe('SpriteComponent', function () {
+    let app;
+    let atlasAsset = null;
+    let spriteAsset = null;
+    let spriteAsset2 = null;
 
-    afterEach(function () {
-        app.destroy();
-    });
-
-
-    var loadAssets = function (cb) {
-        var i = 0;
-        var check = function () {
+    const loadAssets = function (cb) {
+        let i = 0;
+        const check = function () {
             i++;
             if (i === 3) {
                 return true;
-            } else {
-                return false;
             }
+            return false;
+
         };
 
-        atlasAsset = new pc.Asset('red-atlas', 'textureatlas', {
-            url: 'base/tests/test-assets/sprite/red-atlas.json'
+        atlasAsset = new Asset('red-atlas', 'textureatlas', {
+            url: 'http://localhost:3000/test/assets/sprites/red-atlas.json'
         });
 
-        spriteAsset = new pc.Asset('red-sprite', 'sprite', {
-            url: 'base/tests/test-assets/sprite/red-sprite.json'
+        spriteAsset = new Asset('red-sprite', 'sprite', {
+            url: 'http://localhost:3000/test/assets/sprites/red-sprite.json'
         });
 
-        spriteAsset2 = new pc.Asset('red-sprite-2', 'sprite', {
-            url: 'base/tests/test-assets/sprite/red-sprite.json'
+        spriteAsset2 = new Asset('red-sprite-2', 'sprite', {
+            url: 'http://localhost:3000/test/assets/sprites/red-sprite.json'
         });
 
         app.assets.add(atlasAsset);
@@ -59,33 +53,34 @@ describe("pc.SpriteComponent", function () {
         spriteAsset2.ready(function () {
             if (check()) cb();
         });
-    }
+    };
+
+    beforeEach(function (done) {
+        jsdomSetup();
+        app = createApp();
+
+        loadAssets(done);
+    });
+
+    afterEach(function () {
+        app?.destroy();
+        app = null;
+        jsdomTeardown();
+        atlasAsset = null;
+        spriteAsset = null;
+        spriteAsset2 = null;
+    });
 
     it('Add new Component', function () {
-        var e = new pc.Entity();
+        const e = new Entity();
 
-        e.addComponent('sprite', {});
+        e.addComponent('sprite');
 
         expect(e.sprite).to.exist;
     });
 
-    // TODO This and other tests in this file are skipped because of the following error:
-    //
-    //      Uncaught TypeError: Cannot read property 'length' of undefined
-    //        at SpriteAnimationClip._setFrame (sprite-animation-clip.js:183)
-    //        atSpriteAnimationClip.set (sprite-animation-clip.js:381)
-    //        atSpriteAnimationClip.set (sprite-animation-clip.js:368)
-    //        atSpriteAnimationClip._onSpriteAssetLoad (sprite-animation-clip.js:84)
-    //        atSpriteAnimationClip._bindSpriteAsset (sprite-animation-clip.js:56)
-    //        atSpriteAnimationClip.set (sprite-animation-clip.js:298)
-    //        atSpriteComponent.set (component.js:586)
-    //        atSpriteComponentSystem.initializeComponentData (system.js:149)
-    //        atSpriteComponentSystem.addComponent (system.js:84)
-    //        atEntity.addComponent (entity.js:116)
-    //
-    // Once this has been addressed they can be re-enabled.
     it('Add / Remove Component', function () {
-        var e = new pc.Entity();
+        const e = new Entity();
 
         e.addComponent('sprite', {});
 
@@ -97,7 +92,7 @@ describe("pc.SpriteComponent", function () {
     });
 
     it('Remove after destroy', function () {
-        var e = new pc.Entity();
+        const e = new Entity();
         e.addComponent('sprite', {
             spriteAsset: spriteAsset
         });
@@ -112,7 +107,7 @@ describe("pc.SpriteComponent", function () {
         expect(!spriteAsset.hasEvent('load')).to.exist;
         expect(!spriteAsset.hasEvent('remove')).to.exist;
 
-        var e = new pc.Entity();
+        const e = new Entity();
         e.addComponent('sprite', {
             spriteAsset: spriteAsset
         });
@@ -129,7 +124,7 @@ describe("pc.SpriteComponent", function () {
         expect(!spriteAsset.hasEvent('load')).to.exist;
         expect(!spriteAsset.hasEvent('remove')).to.exist;
 
-        var e = new pc.Entity();
+        const e = new Entity();
         e.addComponent('sprite', {
             spriteAsset: spriteAsset
         });
@@ -140,6 +135,5 @@ describe("pc.SpriteComponent", function () {
         expect(!spriteAsset.hasEvent('load')).to.exist;
         expect(!spriteAsset.hasEvent('remove')).to.exist;
     });
+
 });
-
-
