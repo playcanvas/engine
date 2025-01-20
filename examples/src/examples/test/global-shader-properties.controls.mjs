42431c0ea8b545d06e2834ce0a87d5d9242a1c2b
Martin Valigursky <mvaligursky@snapchat.com>
2025-01-20 13:00:05 +0000

    examples lint

--- examples/src/examples/test/global-shader-properties.controls.mjs
+++ examples/src/examples/test/global-shader-properties.controls.mjs
@@ -1,9 +1,11 @@
+import * as pc from 'playcanvas';
+
 /**
  * @param {import('../../app/components/Example.mjs').ControlOptions} options - The options.
  * @returns {JSX.Element} The returned JSX Element.
  */
 export const controls = ({ observer, ReactPCUI, React, jsx, fragment }) => {
-    const { BindingTwoWay, BooleanInput, LabelGroup, Panel, SliderInput, SelectInput } = ReactPCUI;
+    const { BindingTwoWay, BooleanInput, LabelGroup, Panel, SelectInput } = ReactPCUI;
     return fragment(
         jsx(
             Panel,
