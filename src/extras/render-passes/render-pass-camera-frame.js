2c446316e38b11cc8223191187286ba159125c0d
Martin Valigursky <mvaligursky@snapchat.com>
2025-02-06 15:28:25 +0000

    lint

--- src/extras/render-passes/render-pass-camera-frame.js
+++ src/extras/render-passes/render-pass-camera-frame.js
@@ -1,4 +1,4 @@
-import { LAYERID_SKYBOX, LAYERID_IMMEDIATE, TONEMAP_NONE, GAMMA_NONE, GAMMA_SRGB } from '../../scene/constants.js';
+import { LAYERID_SKYBOX, LAYERID_IMMEDIATE, TONEMAP_NONE, GAMMA_NONE } from '../../scene/constants.js';
 import { ADDRESS_CLAMP_TO_EDGE, FILTER_LINEAR, PIXELFORMAT_RGBA8 } from '../../platform/graphics/constants.js';
 import { Texture } from '../../platform/graphics/texture.js';
 import { RenderPass } from '../../platform/graphics/render-pass.js';
