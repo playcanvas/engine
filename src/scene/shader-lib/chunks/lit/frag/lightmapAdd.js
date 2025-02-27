1be786af7570d44c512042ecd0a731dffd896518
Martin Valigursky <mvaligursky@snapchat.com>
2025-02-27 14:28:00 +0000

    whitespce

--- src/scene/shader-lib/chunks/lit/frag/lightmapAdd.js
+++ src/scene/shader-lib/chunks/lit/frag/lightmapAdd.js
@@ -15,41 +15,41 @@ void addLightMap(
 #endif
 ) {
 
-// directional lightmap
-#if defined(LIT_SPECULAR) && defined(LIT_DIR_LIGHTMAP)
+    // directional lightmap
+    #if defined(LIT_SPECULAR) && defined(LIT_DIR_LIGHTMAP)
 
-    if (dot(dir, dir) < 0.0001) {
-            dDiffuseLight += lightmap;
-    } else {
-        float vlight = saturate(dot(dir, -vertexNormal));
-        float flight = saturate(dot(dir, -worldNormal));
-        float nlight = (flight / max(vlight, 0.01)) * 0.5;
+        if (dot(dir, dir) < 0.0001) {
+                dDiffuseLight += lightmap;
+        } else {
+            float vlight = saturate(dot(dir, -vertexNormal));
+            float flight = saturate(dot(dir, -worldNormal));
+            float nlight = (flight / max(vlight, 0.01)) * 0.5;
 
-        dDiffuseLight += lightmap * nlight * 2.0;
+            dDiffuseLight += lightmap * nlight * 2.0;
 
-        vec3 halfDir = normalize(-dir + viewDir);
-        vec3 specularLight = lightmap * getLightSpecular(halfDir, reflectionDir, worldNormal, viewDir, dir, gloss, tbn);
+            vec3 halfDir = normalize(-dir + viewDir);
+            vec3 specularLight = lightmap * getLightSpecular(halfDir, reflectionDir, worldNormal, viewDir, dir, gloss, tbn);
 
-        #ifdef LIT_SPECULAR_FRESNEL
+            #ifdef LIT_SPECULAR_FRESNEL
 
-            specularLight *= 
-                getFresnel(dot(viewDir, halfDir), 
-                gloss, 
-                specularity
-            #if defined(LIT_IRIDESCENCE)
-                , iridescenceFresnel,
-                iridescenceIntensity
+                specularLight *= 
+                    getFresnel(dot(viewDir, halfDir), 
+                    gloss, 
+                    specularity
+                #if defined(LIT_IRIDESCENCE)
+                    , iridescenceFresnel,
+                    iridescenceIntensity
+                #endif
+                    );
             #endif
-                );
-        #endif
 
-        dSpecularLight += specularLight;
-    }
+            dSpecularLight += specularLight;
+        }
 
-#else // non-directional lightmap
+    #else // non-directional lightmap
 
-    dDiffuseLight += lightmap;
+        dDiffuseLight += lightmap;
 
-#endif
+    #endif
 }
 `;
