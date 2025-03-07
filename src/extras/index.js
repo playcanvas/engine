a59bcefe018dd27d36c222e81942684d7bf3fb91
kpal <kpal81xd@gmail.com>
2025-03-07 13:23:01 +0000

    Removed exporting base input and model classes

--- src/extras/index.js
+++ src/extras/index.js
@@ -28,14 +28,12 @@ export { RenderPassTAA } from './render-passes/render-pass-taa.js';
 export { CameraFrame } from './render-passes/camera-frame.js';
 
 // INPUTS
-export { Input } from './inputs/input.js';
 export { JoystickDoubleInput } from './inputs/joystickdouble-input.js';
 export { JoystickTouchInput } from './inputs/joysticktouch-input.js';
 export { KeyboardMouseInput } from './inputs/keyboardmouse-input.js';
 export { MultiTouchInput } from './inputs/multitouch-input.js';
 
 // MODELS
-export { Model } from './models/model.js';
 export { FlyModel } from './models/fly-model.js';
 export { OrbitModel } from './models/orbit-model.js';
 
