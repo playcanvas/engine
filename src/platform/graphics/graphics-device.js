f23bcc932eccd7c5543845040f702a7dfcdc9fc8
Martin Valigursky <mvaligursky@snapchat.com>
2025-03-13 11:03:44 +0000

    only in debug build

--- src/platform/graphics/graphics-device.js
+++ src/platform/graphics/graphics-device.js
@@ -1026,23 +1026,25 @@ class GraphicsDevice extends EventHandler {
      */
     validateAttributes(shader, vb0Format, vb1Format) {
 
-        // add all attribute names from vertex formats to the set
-        _tempSet.clear();
-        vb0Format?.elements.forEach(element => _tempSet.add(element.name));
-        vb1Format?.elements.forEach(element => _tempSet.add(element.name));
+        Debug.call(() => {
+            // add all attribute names from vertex formats to the set
+            _tempSet.clear();
+            vb0Format?.elements.forEach(element => _tempSet.add(element.name));
+            vb1Format?.elements.forEach(element => _tempSet.add(element.name));
 
-        // this is an object, we need to iterator over keys and make sure the value is in the temp set
-        const attributes = shader.definition.attributes;
-        for (const name in attributes) {
-            const value = attributes[name];
-            if (!_tempSet.has(value)) {
-                Debug.errorOnce(`Attribute [${name}: ${value}] required by the shader is not present in the currently assigned vertex buffers, while rendering [${DebugGraphics.toString()}]`, {
-                    shader,
-                    vb0Format,
-                    vb1Format
-                });
+            // this is an object, we need to iterator over keys and make sure the value is in the temp set
+            const attributes = shader.definition.attributes;
+            for (const name in attributes) {
+                const value = attributes[name];
+                if (!_tempSet.has(value)) {
+                    Debug.errorOnce(`Attribute [${name}: ${value}] required by the shader is not present in the currently assigned vertex buffers, while rendering [${DebugGraphics.toString()}]`, {
+                        shader,
+                        vb0Format,
+                        vb1Format
+                    });
+                }
             }
-        }
+        });
     }
 }
 
