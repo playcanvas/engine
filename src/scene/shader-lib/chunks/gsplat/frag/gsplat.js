1afd1d13d0eca77e0bda23372eb1bc420999dfe5
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2025-03-26 11:51:34 +0000

    GSplat shaders can output depth during Depth Prepass (#7483)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- src/scene/shader-lib/chunks/gsplat/frag/gsplat.js
+++ src/scene/shader-lib/chunks/gsplat/frag/gsplat.js
@@ -10,8 +10,13 @@ export default /* glsl */`
     #include "pickPS"
 #endif
 
-#if defined(SHADOW_PASS) || defined(PICK_PASS)
+#if defined(SHADOW_PASS) || defined(PICK_PASS) || defined(PREPASS_PASS)
     uniform float alphaClip;
+#endif
+
+#ifdef PREPASS_PASS
+    varying float vLinearDepth;
+    #include "floatAsUintPS"
 #endif
 
 varying mediump vec2 gaussianUV;
@@ -26,17 +31,23 @@ void main(void) {
     // evaluate alpha
     mediump float alpha = exp(-A * 4.0) * gaussianColor.a;
 
-    #ifdef PICK_PASS
+    #if defined(SHADOW_PASS) || defined(PICK_PASS) || defined(PREPASS_PASS)
         if (alpha < alphaClip) {
             discard;
         }
+    #endif
+
+    #ifdef PICK_PASS
+
         gl_FragColor = getPickOutput();
+
     #elif SHADOW_PASS
 
-        if (alpha < alphaClip) {
-            discard;
-        }
         gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+
+    #elif PREPASS_PASS
+
+        gl_FragColor = float2vec4(vLinearDepth);
 
     #else
         if (alpha < 1.0 / 255.0) {
