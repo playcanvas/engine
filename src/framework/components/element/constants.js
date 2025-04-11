7cb5335f0045d4febcd24f9acbdc172302d4976c
Will Eastcott <will@playcanvas.com>
2025-04-11 17:34:01 +0100

    Remove redundant @type tags from constants

--- src/framework/components/element/constants.js
+++ src/framework/components/element/constants.js
@@ -1,7 +1,6 @@
 /**
  * A {@link ElementComponent} that contains child {@link ElementComponent}s.
  *
- * @type {string}
  * @category User Interface
  */
 export const ELEMENTTYPE_GROUP = 'group';
@@ -9,7 +8,6 @@ export const ELEMENTTYPE_GROUP = 'group';
 /**
  * A {@link ElementComponent} that displays an image.
  *
- * @type {string}
  * @category User Interface
  */
 export const ELEMENTTYPE_IMAGE = 'image';
@@ -17,7 +15,6 @@ export const ELEMENTTYPE_IMAGE = 'image';
 /**
  * A {@link ElementComponent} that displays text.
  *
- * @type {string}
  * @category User Interface
  */
 export const ELEMENTTYPE_TEXT = 'text';
@@ -25,7 +22,6 @@ export const ELEMENTTYPE_TEXT = 'text';
 /**
  * Fit the content exactly to Element's bounding box.
  *
- * @type {string}
  * @category User Interface
  */
 export const FITMODE_STRETCH = 'stretch';
@@ -33,7 +29,6 @@ export const FITMODE_STRETCH = 'stretch';
 /**
  * Fit the content within the Element's bounding box while preserving its Aspect Ratio.
  *
- * @type {string}
  * @category User Interface
  */
 export const FITMODE_CONTAIN = 'contain';
@@ -41,7 +36,6 @@ export const FITMODE_CONTAIN = 'contain';
 /**
  * Fit the content to cover the entire Element's bounding box while preserving its Aspect Ratio.
  *
- * @type {string}
  * @category User Interface
  */
 export const FITMODE_COVER = 'cover';
--- src/framework/constants.js
+++ src/framework/constants.js
@@ -1,14 +1,12 @@
 /**
  * When resizing the window the size of the canvas will not change.
  *
- * @type {string}
  */
 export const FILLMODE_NONE = 'NONE';
 
 /**
  * When resizing the window the size of the canvas will change to fill the window exactly.
  *
- * @type {string}
  */
 export const FILLMODE_FILL_WINDOW = 'FILL_WINDOW';
 
@@ -16,7 +14,6 @@ export const FILLMODE_FILL_WINDOW = 'FILL_WINDOW';
  * When resizing the window the size of the canvas will change to fill the window as best it can,
  * while maintaining the same aspect ratio.
  *
- * @type {string}
  */
 export const FILLMODE_KEEP_ASPECT = 'KEEP_ASPECT';
 
@@ -24,7 +21,6 @@ export const FILLMODE_KEEP_ASPECT = 'KEEP_ASPECT';
  * When the canvas is resized the resolution of the canvas will change to match the size of the
  * canvas.
  *
- * @type {string}
  */
 export const RESOLUTION_AUTO = 'AUTO';
 
@@ -32,6 +28,5 @@ export const RESOLUTION_AUTO = 'AUTO';
  * When the canvas is resized the resolution of the canvas will remain at the same value and the
  * output will just be scaled to fit the canvas.
  *
- * @type {string}
  */
 export const RESOLUTION_FIXED = 'FIXED';
