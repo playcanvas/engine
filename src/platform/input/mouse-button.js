81974423ef3b4e755097f1d1818a46c9969fdb43
Will Eastcott <will@playcanvas.com>
2025-04-29 15:49:12 +0100

    Add MouseButton declaration

---
+++ src/platform/input/mouse-button.js
@@ -0,0 +1,20 @@
+/**
+ * Mouse buttons.
+ *
+ * @readonly
+ * @enum {ValueOf<typeof MouseButton>}
+ * @category Input
+ */
+export const MouseButton = Object.freeze({
+    /** No specific projection format is used. */
+    None: -1,
+
+    /** The left mouse button. */
+    Left: 0,
+
+    /** The middle mouse button. */
+    Middle: 1,
+
+    /** The right mouse button. */
+    Right: 2
+});
