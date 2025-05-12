65b9d2641d84d9b9f51f1b0c3d4bcebb178753b4
kpal <kpal81xd@gmail.com>
2025-05-12 16:29:01 +0100

    Fix release script to use current branch name and improve branch handling logic

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-MAIN_BRANCH="version"
 RELEASE_PREFIX="release-"
+RELEASE_REGEX="^$RELEASE_PREFIX[0-9]+.[0-9]+$"
 
 BRANCH=$(git branch --show-current)
 VERSION=$(npm pkg get version | sed 's/"//g')
@@ -19,27 +19,56 @@ fi
 
 echo "Version [MAJOR=$MAJOR, MINOR=$MINOR, PATCH=$PATCH, BUILD=$BUILD]"
 
-if [ "$BRANCH" == "$MAIN_BRANCH" ]; then
+if [ "$BRANCH" == "version" ]; then
+    echo "Creating release branch from $BRANCH"
+
     RELEASE_BRANCH="$RELEASE_PREFIX$MAJOR.$MINOR"
     RELEASE_MESSAGE="Branch $MAJOR.$MINOR"
 
-    read -p "About to create minor release branch '$RELEASE_BRANCH' taken from '$MAIN_BRANCH' branch. Continue? (Y/n) " -r
+    read -p "About to create minor release branch '$RELEASE_BRANCH' taken from '$BRANCH' branch. Continue? (Y/n) " -r
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         echo "Aborted."
         exit 1
     fi
 
-    git branch $RELEASE_BRANCH $MAIN_BRANCH
+    # Create release branch from main branch
+    git branch $RELEASE_BRANCH $BRANCH
 
+    # Bump minor prelease version on main 
     npm version preminor --preid=dev --no-git-tag-version
     git commit -m "$RELEASE_MESSAGE" -- package.json package-lock.json
+
+    # Switch to release branch
     git checkout $RELEASE_BRANCH
 
+    # Change prerelease version to preview
     npm version prerelease --preid=preview --no-git-tag-version
     git commit -m "$RELEASE_MESSAGE" -- package.json package-lock.json
     exit 0
 fi
 
-if [[ $BRANCH == $RELEASE_PREFIX* ]]; then
+if [[ $BRANCH =~ $RELEASE_REGEX ]]; then
+    echo "Finalizing release branch $BRANCH"
 
+    FINALIZED_VERSION="$MAJOR.$MINOR.$PATCH"
+
+    # Check if tag already exists
+    git fetch --tags
+    if git tag | grep -q "$FINALIZED_VERSION"; then
+        echo "$FINALIZED_VERSION tag already exists. Aborting."
+        exit 1
+    fi
+
+    read -p "About to finalize and tag branch '$BRANCH' with version '$FINALIZED_VERSION'. Continue? (Y/n) " -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        echo "Aborted."
+        exit 1
+    fi
+
+    # Bump patch version (with tag)
+    npm version patch
+    exit 0
 fi
+
+echo "Unrecognized branch '$BRANCH'."
+exit 1
