c1e865f52896ffd0acae13e3e1d87d47cf7c665c
Donovan Hutchence <slimbuck7@gmail.com>
2025-05-12 16:30:31 +0100

    Sogs updates (#7662)

--- src/scene/shader-lib/chunks-glsl/gsplat/vert/gsplatCommon.js
+++ src/scene/shader-lib/chunks-glsl/gsplat/vert/gsplatCommon.js
@@ -13,8 +13,28 @@ struct SplatCenter {
     vec3 view;          // center in view space
     vec4 proj;          // center in clip space
     mat4 modelView;     // model-view matrix
-    float projMat00;    // elememt [0][0] of the projection matrix
+    float projMat00;    // element [0][0] of the projection matrix
 };
+
+mat3 quatToMat3(vec4 R) {
+    vec4 R2 = R + R;
+    float X = R2.x * R.w;
+    vec4 Y  = R2.y * R;
+    vec4 Z  = R2.z * R;
+    float W = R2.w * R.w;
+
+    return mat3(
+        1.0 - Z.z - W,
+              Y.z + X,
+              Y.w - Z.x,
+              Y.z - X,
+        1.0 - Y.y - W,
+              Z.w + Y.x,
+              Y.w + Z.x,
+              Z.w - Y.x,
+        1.0 - Y.y - Z.z
+    );
+}
 
 // stores the offset from center for the current gaussian
 struct SplatCorner {
