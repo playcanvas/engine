b849d2b33017d30174124618895c1fafea5c98a3
Yuki Shimada <emadurandal@gmail.com>
2025-05-16 15:02:00 +0900

    fix: dAnisotropyRotation calculation

--- src/scene/shader-lib/chunks-glsl/standard/frag/anisotropy.js
+++ src/scene/shader-lib/chunks-glsl/standard/frag/anisotropy.js
@@ -12,8 +12,11 @@ void getAnisotropy() {
     #ifdef STD_ANISOTROPY_TEXTURE
     dAnisotropy *= texture2DBias({STD_ANISOTROPY_TEXTURE_NAME}, {STD_ANISOTROPY_TEXTURE_UV}, textureBias).b;
 
-    dAnisotropyRotation = texture2D({STD_ANISOTROPY_TEXTURE_NAME}, {STD_ANISOTROPY_TEXTURE_UV}).rg * 2.0 - vec2(1.0);
-    dAnisotropyRotation = mat2(dAnisotropyRotation.x, dAnisotropyRotation.y, -dAnisotropyRotation.y, dAnisotropyRotation.x) * normalize(dAnisotropyRotation);
+    vec2 anisotropyRotationFromTex = texture2D({STD_ANISOTROPY_TEXTURE_NAME}, {STD_ANISOTROPY_TEXTURE_UV}).rg * 2.0 - vec2(1.0);
+    mat2 rotationMatrix = mat2(dAnisotropyRotation.x, dAnisotropyRotation.y, -dAnisotropyRotation.y, dAnisotropyRotation.x);
+    dAnisotropyRotation = rotationMatrix * anisotropyRotationFromTex;
     #endif
+    
+    dAnisotropy = clamp(dAnisotropy, 0.0, 1.0);
 }
 `;
