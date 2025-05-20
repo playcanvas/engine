e65875cb9cf5e6643cfa1a1ff79a5056c0835d3e
Yuki Shimada <emadurandal@gmail.com>
2025-05-20 21:51:34 +0900

    texture2D -> texture2DBias

--- src/scene/shader-lib/chunks-glsl/standard/frag/anisotropy.js
+++ src/scene/shader-lib/chunks-glsl/standard/frag/anisotropy.js
@@ -17,7 +17,7 @@ void getAnisotropy() {
     #ifdef STD_ANISOTROPY_TEXTURE
     dAnisotropy *= texture2DBias({STD_ANISOTROPY_TEXTURE_NAME}, {STD_ANISOTROPY_TEXTURE_UV}, textureBias).b;
 
-    vec2 anisotropyRotationFromTex = texture2D({STD_ANISOTROPY_TEXTURE_NAME}, {STD_ANISOTROPY_TEXTURE_UV}).rg * 2.0 - vec2(1.0);
+    vec2 anisotropyRotationFromTex = texture2DBias({STD_ANISOTROPY_TEXTURE_NAME}, {STD_ANISOTROPY_TEXTURE_UV}, textureBias).rg * 2.0 - vec2(1.0);
     mat2 rotationMatrix = mat2(dAnisotropyRotation.x, dAnisotropyRotation.y, -dAnisotropyRotation.y, dAnisotropyRotation.x);
     dAnisotropyRotation = rotationMatrix * anisotropyRotationFromTex;
     #endif
