d9abfe8f4ce558d995e9b6021efc9f36fd5d89a7
Vincent Woo <me@vincentwoo.com>
2025-05-22 14:24:37 -0700

    Fix lint

--- src/scene/shader-lib/glsl/chunks/gsplat/vert/gsplatSogsSH.js
+++ src/scene/shader-lib/glsl/chunks/gsplat/vert/gsplatSogsSH.js
@@ -23,26 +23,28 @@ void readSHData(in SplatSource source, out vec3 sh[15], out float scale) {
 }
 
 void readSHData(in SplatSource source, out vec3 sh[8], out float scale) {
-	ivec2 t = ivec2(texelFetch(sh_labels, source.uv, 0).xy * 255.0);
-	int n = t.x + t.y * 256;
-	int u = (n % 64) * 8;
-	int v = n / 64;
- 
-	for (int i = 0; i < 8; i++) {
-		sh[i] = mix(vec3(shN_mins), vec3(shN_maxs), texelFetch(sh_centroids, ivec2(u + i, v), 0).xyz);
-	}
-	scale = 1.0;
+    ivec2 t = ivec2(texelFetch(sh_labels, source.uv, 0).xy * 255.0);
+    int n = t.x + t.y * 256;
+    int u = (n % 64) * 8;
+    int v = n / 64;
+
+    for (int i = 0; i < 8; i++) {
+        sh[i] = mix(vec3(shN_mins), vec3(shN_maxs), texelFetch(sh_centroids, ivec2(u + i, v), 0).xyz);
+    }
+
+    scale = 1.0;
 }
 
 void readSHData(in SplatSource source, out vec3 sh[3], out float scale) {
-	ivec2 t = ivec2(texelFetch(sh_labels, source.uv, 0).xy * 255.0);
-	int n = t.x + t.y * 256;
-	int u = (n % 64) * 3;
-	int v = n / 64;
- 
-	for (int i = 0; i < 3; i++) {
-		sh[i] = mix(vec3(shN_mins), vec3(shN_maxs), texelFetch(sh_centroids, ivec2(u + i, v), 0).xyz);
-	}
-	scale = 1.0;
+    ivec2 t = ivec2(texelFetch(sh_labels, source.uv, 0).xy * 255.0);
+    int n = t.x + t.y * 256;
+    int u = (n % 64) * 3;
+    int v = n / 64;
+
+    for (int i = 0; i < 3; i++) {
+        sh[i] = mix(vec3(shN_mins), vec3(shN_maxs), texelFetch(sh_centroids, ivec2(u + i, v), 0).xyz);
+    }
+
+    scale = 1.0;
 }
 `;
