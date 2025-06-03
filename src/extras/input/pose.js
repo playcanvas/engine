37086bd3173a01edf5bf5a43b9e8b247abd9628d
kpal <kpal81xd@gmail.com>
2025-06-03 15:04:43 +0100

    Refactor: Enhance Pose class with pitch and yaw range management, and implement move and rotate methods

--- src/extras/input/pose.js
+++ src/extras/input/pose.js
@@ -1,3 +1,5 @@
+import { math } from '../../core/math/math.js';
+import { Vec2 } from '../../core/math/vec2.js';
 import { Vec3 } from '../../core/math/vec3.js';
 
 class Pose {
@@ -14,6 +16,16 @@ class Pose {
      * @type {Vec3}
      */
     angles = new Vec3();
+
+    /**
+     * @type {Vec2}
+     */
+    pitchRange = new Vec2(-Infinity, Infinity);
+
+    /**
+     * @type {Vec2}
+     */
+    yawRange = new Vec2(-Infinity, Infinity);
 
     /**
      * Creates a new Pose instance.
@@ -46,6 +58,53 @@ class Pose {
      */
     copy(other) {
         return this.set(other.position, other.angles);
+    }
+
+    /**
+     * Lerps between two poses based on the given alpha values.
+     *
+     * @param {Pose} lhs - The left-hand side pose.
+     * @param {Pose} rhs - The right-hand side pose.
+     * @param {number} alpha1 - The alpha value for position interpolation.
+     * @param {number} alpha2 - The alpha value for angles interpolation.
+     * @returns {Pose} The updated Pose instance.
+     */
+    lerp(lhs, rhs, alpha1, alpha2) {
+        this.position.lerp(lhs.position, rhs.position, alpha1);
+        this.angles.x = math.lerpAngle(lhs.angles.x, rhs.angles.x, alpha2) % 360;
+        this.angles.y = math.lerpAngle(rhs.angles.y, rhs.angles.y, alpha2) % 360;
+        return this;
+    }
+
+    /**
+     * Moves the pose by the given vector.
+     *
+     * @param {Vec3} offset - The vector to move by.
+     * @returns {Pose} The updated Pose instance.
+     */
+    move(offset) {
+        this.position.add(offset);
+        return this;
+    }
+
+    /**
+     * Rotates the pose by the given angles in degrees.
+     *
+     * @param {Vec3} euler - The angles to rotate by.
+     * @returns {Pose} The updated Pose instance.
+     */
+    rotate(euler) {
+        this.angles.add(euler);
+
+        // wrap angles to [0, 360)
+        this.angles.x %= 360;
+        this.angles.y %= 360;
+
+        // clamp pitch and yaw
+        this.angles.x = math.clamp(this.angles.x, this.pitchRange.x, this.pitchRange.y);
+        this.angles.y = math.clamp(this.angles.y, this.yawRange.x, this.yawRange.y);
+
+        return this;
     }
 }
 
