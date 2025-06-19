39d2dc1802898fac0f83b7d6851850b1c349353e
kpal <kpal81xd@gmail.com>
2025-06-19 15:55:51 +0100

    Refactor: Update VirtualJoystick to use _base and _stick for position tracking

--- src/extras/input/sources/virtual-joystick.js
+++ src/extras/input/sources/virtual-joystick.js
@@ -15,7 +15,13 @@ class VirtualJoystick extends EventHandler {
      * @type {Vec2}
      * @private
      */
-    _position = new Vec2();
+    _base = new Vec2();
+
+    /**
+     * @type {Vec2}
+     * @private
+     */
+    _stick = new Vec2();
 
     /**
      * @type {Vec2}
@@ -33,11 +39,38 @@ class VirtualJoystick extends EventHandler {
     }
 
     /**
+     * The base position of the joystick, where the stick is anchored.
+     *
+     * @type {Vec2}
+     */
+    get base() {
+        return this._base;
+    }
+
+    /**
+     * The position of the stick, which moves based on user input.
+     *
+     * @type {Vec2}
+     */
+    get stick() {
+        return this._stick;
+    }
+
+    /**
+     * The vector value of the joystick, normalized to the range of -1 to 1.
+     *
+     * @type {Vec2}
+     */
+    get value() {
+        return this._value;
+    }
+
+    /**
      * @param {number} x - The x position.
      * @param {number} y - The y position.
      */
     down(x, y) {
-        this._position.set(x, y);
+        this._base.set(x, y);
         this._value.set(0, 0);
         this.fire('position', x, y, x, y);
     }
@@ -47,7 +80,7 @@ class VirtualJoystick extends EventHandler {
      * @param {number} y - The y position of the stick
      */
     move(x, y) {
-        v.set(x - this._position.x, y - this._position.y);
+        v.set(x - this._base.x, y - this._base.y);
         if (v.length() > this._range) {
             v.normalize().mulScalar(this._range);
         }
@@ -55,18 +88,16 @@ class VirtualJoystick extends EventHandler {
             math.clamp(v.x / this._range, -1, 1),
             math.clamp(v.y / this._range, -1, 1)
         );
-        const { x: px, y: py } = this._position;
-        this.fire('position', px, py, px + v.x, py + v.y);
+        this._stick.copy(v).add(this._base);
+        const { x: bx, y: by } = this._base;
+        const { x: sx, y: sy } = this._stick;
+        this.fire('position', bx, by, sx, sy);
     }
 
     up() {
-        this._position.set(0, 0);
+        this._base.set(0, 0);
         this._value.set(0, 0);
         this.fire('position', -1, -1, -1, -1);
-    }
-
-    get value() {
-        return this._value;
     }
 }
 
