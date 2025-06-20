1227c0d7cb3709e43caf64b313b78dae5d9c5ac5
kpal <kpal81xd@gmail.com>
2025-06-20 15:43:52 +0100

    Enhance GamepadSource: Add button state tracking and improve documentation

--- src/extras/input/sources/gamepad-source.js
+++ src/extras/input/sources/gamepad-source.js
@@ -7,13 +7,41 @@ import { InputSource } from '../input.js';
  * @alpha
  *
  * @typedef {object} GamepadSourceDeltas
+ * @property {number[]} buttons - The button deltas, represented as an array of button states (0 or 1).
  * @property {number[]} leftStick - The left stick deltas, represented as an array of [x, y] coordinates.
  * @property {number[]} rightStick - The right stick deltas, represented as an array of [x, y] coordinates.
  * @augments {InputSource<GamepadSourceDeltas>}
  */
 class GamepadSource extends InputSource {
+    /**
+     * The button codes.
+     *
+     * @readonly
+     */
+    static buttoncode = {
+        A: 0,
+        B: 1,
+        X: 2,
+        Y: 3,
+        LB: 4,
+        RB: 5,
+        LT: 6,
+        RT: 7,
+        SELECT: 8,
+        START: 9,
+        LEFT_STICK: 10,
+        RIGHT_STICK: 11
+    };
+
+    /**
+     * @type {number[]}
+     * @private
+     */
+    _buttonPrev = Array(11).fill(0);
+
     constructor() {
         super({
+            buttons: Array(11).fill(0),
             leftStick: [0, 0],
             rightStick: [0, 0]
         });
@@ -57,7 +85,21 @@ class GamepadSource extends InputSource {
                 continue;
             }
 
-            const { axes } = gp;
+            // check if gamepad has enough buttons
+            if (gp.buttons.length < 12) {
+                continue;
+            }
+
+            const { buttons, axes } = gp;
+
+            // buttons
+            for (let j = 0; j < this._buttonPrev.length; j++) {
+                const state = +buttons[j].pressed;
+                this.deltas.buttons[j] = state - this._buttonPrev[j];
+                this._buttonPrev[j] = state;
+            }
+
+            // sticks
             this.deltas.leftStick.append([axes[0], axes[1]]);
             this.deltas.rightStick.append([axes[2], axes[3]]);
         }
