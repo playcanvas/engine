35fcd33f36c41adb938ad66b2f66bf9f78fc1693
Donovan Hutchence <slimbuck7@gmail.com>
2025-06-23 15:10:48 +0100

    latest

--- src/scene/gsplat/gsplat-resolve-sh.js
+++ src/scene/gsplat/gsplat-resolve-sh.js
@@ -12,14 +12,14 @@ import { Vec2 } from '../../core/math/vec2.js';
 import { RenderPass } from '../../platform/graphics/render-pass.js';
 import { QuadRender } from '../graphics/quad-render.js';
 
-const genVS = /* glsl */`
+const vertexGLSL = /* glsl */`
     attribute vec2 vertex_position;
     void main(void) {
         gl_Position = vec4(vertex_position, 0.0, 1.0);
     }
 `;
 
-const genFS = /* glsl */`
+const fragmentGLSL = /* glsl */`
     #if SH_BANDS == 1
         #define SH_COEFFS 3
     #elif SH_BANDS == 2
@@ -109,6 +109,112 @@ const genFS = /* glsl */`
     }
 `;
 
+const vertexWGSL = /* wgsl */`
+    attribute vertex_position: vec2f;
+    @vertex
+    fn vertexMain(input: VertexInput) -> VertexOutput {
+        var output: VertexOutput;
+        output.position = vec4f(vertex_position, 0.0, 1.0);
+        return output;
+    }
+`;
+
+const fragmentWGSL = /* wgsl */`
+    #if SH_BANDS == 1
+        const SH_COEFFS: i32 = 3;
+    #elif SH_BANDS == 2
+        const SH_COEFFS: i32 = 8;
+    #elif SH_BANDS == 3
+        const SH_COEFFS: i32 = 15;
+    #endif
+
+    const SH_C1: f32 = 0.4886025119029199f;
+
+    #if SH_BANDS > 1
+        const SH_C2_0: f32 = 1.0925484305920792f;
+        const SH_C2_1: f32 = -1.0925484305920792f;
+        const SH_C2_2: f32 = 0.31539156525252005f;
+        const SH_C2_3: f32 = -1.0925484305920792f;
+        const SH_C2_4: f32 = 0.5462742152960396f;
+    #endif
+
+    #if SH_BANDS > 2
+        const SH_C3_0: f32 = -0.5900435899266435f;
+        const SH_C3_1: f32 = 2.890611442640554f;
+        const SH_C3_2: f32 = -0.4570457994644658f;
+        const SH_C3_3: f32 = 0.3731763325901154f;
+        const SH_C3_4: f32 = -0.4570457994644658f;
+        const SH_C3_5: f32 = 1.445305721320277f;
+        const SH_C3_6: f32 = -0.5900435899266435f;
+    #endif
+
+    // see https://github.com/graphdeco-inria/gaussian-splatting/blob/main/utils/sh_utils.py
+    fn evalSH(sh: array<vec3f, SH_COEFFS>, dir: vec3f) -> vec3f {
+        let x: f32 = dir.x;
+        let y: f32 = dir.y;
+        let z: f32 = dir.z;
+
+        // 1st degree
+        var result: vec3f = SH_C1 * (-sh[0] * y + sh[1] * z - sh[2] * x);
+
+        #if SH_BANDS > 1
+            // 2nd degree
+            let xx: f32 = x * x;
+            let yy: f32 = y * y;
+            let zz: f32 = z * z;
+            let xy: f32 = x * y;
+            let yz: f32 = y * z;
+            let xz: f32 = x * z;
+
+            result +=
+                sh[3] * (SH_C2_0 * xy) +
+                sh[4] * (SH_C2_1 * yz) +
+                sh[5] * (SH_C2_2 * (2.0 * zz - xx - yy)) +
+                sh[6] * (SH_C2_3 * xz) +
+                sh[7] * (SH_C2_4 * (xx - yy));
+        #endif
+
+        #if SH_BANDS > 2
+            // 3rd degree
+            result +=
+                sh[8]  * (SH_C3_0 * y * (3.0 * xx - yy)) +
+                sh[9]  * (SH_C3_1 * xy * z) +
+                sh[10] * (SH_C3_2 * y * (4.0 * zz - xx - yy)) +
+                sh[11] * (SH_C3_3 * z * (2.0 * zz - 3.0 * xx - 3.0 * yy)) +
+                sh[12] * (SH_C3_4 * x * (4.0 * zz - xx - yy)) +
+                sh[13] * (SH_C3_5 * z * (xx - yy)) +
+                sh[14] * (SH_C3_6 * x * (xx - 3.0 * yy));
+        #endif
+
+        return result;
+    }
+
+    uniform dir: vec3f;
+    uniform shN_mins: f32;
+    uniform shN_maxs: f32;
+
+    var centroids: texture_2d<f32>;
+
+    @fragment
+    fn fragmentMain(input: FragmentInput) -> FragmentOutput {
+        var output: FragmentOutput;
+
+        var uv = vec2i(input.position.xy) * vec2i(SH_COEFFS, 1);
+        var coefficients: array<vec3f, SH_COEFFS>;
+
+        // read coefficients
+        for (var i: i32 = 0; i < SH_COEFFS; i++) {
+            let s: vec3f = textureLoad(centroids, vec2i(uv.x + i, uv.y), 0).xyz;
+            coefficients[i] = mix(vec3f(uniform.shN_mins), vec3f(uniform.shN_maxs), s);
+        }
+
+        // evaluate
+        output.color = vec4f(evalSH(coefficients, uniform.dir) * 0.25 + 0.5, 1.0);
+
+        return output;
+    }
+`;
+
 const resolve = (scope, values) => {
     for (const key in values) {
         scope.resolve(key).setValue(values[key]);
@@ -137,8 +243,10 @@ class GSplatResolveSH {
 
         this.shader = ShaderUtils.createShader(device, {
             uniqueName: 'gsplatResolveSH',
-            vertexGLSL: genVS,
-            fragmentGLSL: genFS,
+            vertexGLSL,
+            fragmentGLSL,
+            vertexWGSL,
+            fragmentWGSL,
             vertexIncludes: includes,
             fragmentIncludes: includes,
             fragmentDefines: new Map([
--- src/scene/shader-lib/wgsl/chunks/gsplat/vert/gsplatSogsColor.js
+++ src/scene/shader-lib/wgsl/chunks/gsplat/vert/gsplatSogsColor.js
@@ -1,5 +1,7 @@
 export default /* wgsl */`
 var sh0: texture_2d<f32>;
+var sh_labels: texture_2d<f32>;
+var sh_result: texture_2d<f32>;
 
 uniform sh0_mins: vec4f;
 uniform sh0_maxs: vec4f;
@@ -7,7 +9,17 @@ uniform sh0_maxs: vec4f;
 const SH_C0: f32 = 0.28209479177387814;
 
 fn readColor(source: ptr<function, SplatSource>) -> vec4f {
-    let clr: vec4f = mix(uniform.sh0_mins, uniform.sh0_maxs, textureLoad(sh0, source.uv, 0));
-    return vec4f(vec3f(0.5) + clr.xyz * SH_C0, 1.0 / (1.0 + exp(-clr.w)));
+    // sample base color
+    let baseSample: vec4f = mix(uniform.sh0_mins, uniform.sh0_maxs, textureLoad(sh0, source.uv, 0));
+    let base = vec4f(vec3f(0.5) + baseSample.xyz * SH_C0, 1.0 / (1.0 + exp(-baseSample.w)));
+
+    // extract spherical harmonics palette index
+    let labelSample: vec2i = vec2i(textureLoad(sh_labels, source.uv, 0).xy * 255.0);
+    let n = labelSample.x + labelSample.y * 256;
+
+    let shSample: vec3f = textureLoad(sh_result, vec2i(n % 64, n / 64), 0).xyz;
+    let sh: vec3f = (shSample - vec3f(0.5)) * 4.0;
+
+    return vec4f(base.xyz + sh, base.w);
 }
 `;
