7e22e1b3173461f976af42e50f8900b39c425292
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2025-07-07 12:48:11 +0100

    Updated few gsplat examples to use compressed version of the splat asset (#7823)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- examples/src/examples/gaussian-splatting/simple.example.mjs
+++ examples/src/examples/gaussian-splatting/simple.example.mjs
@@ -45,7 +45,7 @@ app.on('destroy', () => {
 });
 
 const assets = {
-    biker: new pc.Asset('gsplat', 'gsplat', { url: `${rootPath}/static/assets/splats/biker.ply` }),
+    biker: new pc.Asset('gsplat', 'gsplat', { url: `${rootPath}/static/assets/splats/biker.compressed.ply` }),
     orbit: new pc.Asset('script', 'script', { url: `${rootPath}/static/scripts/camera/orbit-camera.js` })
 };
 
