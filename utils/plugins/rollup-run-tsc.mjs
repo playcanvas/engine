9fcba314d268ecc3a308279db7aa60c2d1ab6e40
KPal <48248865+kpal81xd@users.noreply.github.com>
2025-07-11 09:48:37 +0100

    Engine & Examples build fixes (#7830)
    * Moved all engine deps to one location
    
    * Refactor engineRollupOptions to handle unpacked and packed sources more clearly
    
    * Moved types build to rollup-build-target file and renamed buildTarget to buildJSOptions
    
    * Fixed jscc import typing
    
    * Update import statements for RollupOptions and OutputOptions in configuration files
    
    * Refactor buildTypesOption to ensure consistent input path for TypeScript definitions
    
    * Allow types to be build directly from examples rollup instead of running via NPM
    
    * Removed unused execSync
    
    * Cleaned up rollup for examples
    
    * Add regex support for external URLs in example targets
    
    * Renamed rollup to have mjs ext
    
    * Update module settings in jsconfig.json to use esnext for module and target
    
    * Refactor import statement for fs module in rollup.config.mjs
    
    * Refactor type annotations for build types, module format, and bundle states in rollup.config.mjs
    
    * Fixed tsc watching for types

--- utils/plugins/rollup-run-tsc.mjs
+++ utils/plugins/rollup-run-tsc.mjs
@@ -1,8 +1,39 @@
-import * as fs from 'node:fs';
+import fs from 'fs';
+import path from 'path';
 import { execSync } from 'node:child_process';
+
+/** @import { Plugin, PluginContext } from 'rollup' */
 
 const GREEN_OUT = '\x1b[32m';
 
+/**
+ * @param {PluginContext} context - The Rollup plugin context.
+ * @param {string} src - File or path to watch.
+ */
+const addWatch = (context, src) => {
+    const srcStats = fs.statSync(src);
+    if (srcStats.isFile()) {
+        context.addWatchFile(path.resolve('.', src));
+        return;
+    }
+    const filesToWatch = fs.readdirSync(src);
+    for (const file of filesToWatch) {
+        const fullPath = path.join(src, file);
+        const stats = fs.statSync(fullPath);
+        if (stats.isFile()) {
+            context.addWatchFile(path.resolve('.', fullPath));
+        } else if (stats.isDirectory()) {
+            addWatch(context, fullPath);
+        }
+    }
+};
+
+/**
+ * Run TypeScript compiler (tsc) with the specified configuration file.
+ *
+ * @param {string} [config] - The path to the TypeScript configuration file.
+ * @returns {Plugin} - The rollup plugin.
+ */
 export function runTsc(config = 'tsconfig.json') {
     if (!fs.existsSync(config)) {
         throw new Error(`tsconfig file does not exist: ${config}`);
@@ -10,6 +41,8 @@ export function runTsc(config = 'tsconfig.json') {
     return {
         name: 'run-tsc',
         buildStart() {
+            addWatch(this, 'src');
+
             const cmd = `tsc --project ${config}`;
             console.log(`${GREEN_OUT}${cmd}`);
             execSync(cmd);
