dc9ab718c78da5554275c9dac01b396abff4ac41
Donovan Hutchence <slimbuck7@gmail.com>
2025-08-22 13:55:44 +0100

    latest

--- src/scene/shader-lib/wgsl/chunks/gsplat/vert/gsplatSogsColor.js
+++ src/scene/shader-lib/wgsl/chunks/gsplat/vert/gsplatSogsColor.js
@@ -1,5 +1,5 @@
 export default /* wgsl */`
-var sh0: texture_2d<f32>;
+var packedSh0: texture_2d<f32>;
 
 uniform sh0_mins: f32;
 uniform sh0_maxs: f32;
@@ -7,7 +7,7 @@ uniform sh0_maxs: f32;
 const SH_C0: f32 = 0.28209479177387814;
 
 fn readColor(source: ptr<function, SplatSource>) -> vec4f {
-    let clr = mix(vec3f(uniform.sh0_mins), vec3f(uniform.sh0_maxs), unpack111110(pack8888(textureLoad(sh0, source.uv, 0))));
+    let clr = mix(vec3f(uniform.sh0_mins), vec3f(uniform.sh0_maxs), unpack111110(pack8888(textureLoad(packedSh0, source.uv, 0))));
     let alpha = f32(((packedSample.z & 0x3fu) << 2) + (packedSample.w & 0x3u)) / 255.0;
     return vec4f(vec3f(0.5) + clr.xyz * SH_C0, alpha);
 }
