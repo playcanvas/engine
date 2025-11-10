373eed8dc487ec19502d9d39d4ad28d67db7b3db
Martin Valigursky <mvaligursky@snapchat.com>
2025-11-10 10:46:29 +0000

    Refactor gsplat shader chunks for tree-shaking

---
+++ src/scene/shader-lib/wgsl/collections/gsplat-chunks-wgsl.js
@@ -0,0 +1,50 @@
+import gsplatCenterVS from '../chunks/gsplat/vert/gsplatCenter.js';
+import gsplatColorVS from '../chunks/gsplat/vert/gsplatColor.js';
+import gsplatCommonVS from '../chunks/gsplat/vert/gsplatCommon.js';
+import gsplatCompressedDataVS from '../chunks/gsplat/vert/gsplatCompressedData.js';
+import gsplatCompressedSHVS from '../chunks/gsplat/vert/gsplatCompressedSH.js';
+import gsplatCustomizeVS from '../chunks/gsplat/vert/gsplatCustomize.js';
+import gsplatEvalSHVS from '../chunks/gsplat/vert/gsplatEvalSH.js';
+import gsplatHelpersVS from '../chunks/gsplat/vert/gsplatHelpers.js';
+import gsplatQuatToMat3VS from '../chunks/gsplat/vert/gsplatQuatToMat3.js';
+import gsplatSogsColorVS from '../chunks/gsplat/vert/gsplatSogsColor.js';
+import gsplatSogsDataVS from '../chunks/gsplat/vert/gsplatSogsData.js';
+import gsplatSogsSHVS from '../chunks/gsplat/vert/gsplatSogsSH.js';
+import gsplatSourceFormatVS from '../chunks/gsplat/vert/gsplatSourceFormat.js';
+import gsplatStructsVS from '../chunks/gsplat/vert/gsplatStructs.js';
+import gsplatCornerVS from '../chunks/gsplat/vert/gsplatCorner.js';
+import gsplatDataVS from '../chunks/gsplat/vert/gsplatData.js';
+import gsplatOutputVS from '../chunks/gsplat/vert/gsplatOutput.js';
+import gsplatPS from '../chunks/gsplat/frag/gsplat.js';
+import gsplatSHVS from '../chunks/gsplat/vert/gsplatSH.js';
+import gsplatSourceVS from '../chunks/gsplat/vert/gsplatSource.js';
+import gsplatVS from '../chunks/gsplat/vert/gsplat.js';
+import gsplatWorkBufferVS from '../chunks/gsplat/vert/gsplatWorkBuffer.js';
+import gsplatPackingPS from '../chunks/gsplat/frag/gsplatPacking.js';
+
+export const gsplatChunksWGSL = {
+    gsplatCenterVS,
+    gsplatCornerVS,
+    gsplatColorVS,
+    gsplatCommonVS,
+    gsplatCompressedDataVS,
+    gsplatCompressedSHVS,
+    gsplatCustomizeVS,
+    gsplatEvalSHVS,
+    gsplatHelpersVS,
+    gsplatSourceFormatVS,
+    gsplatStructsVS,
+    gsplatQuatToMat3VS,
+    gsplatSogsColorVS,
+    gsplatSogsDataVS,
+    gsplatSogsSHVS,
+    gsplatDataVS,
+    gsplatOutputVS,
+    gsplatPS,
+    gsplatSHVS,
+    gsplatSourceVS,
+    gsplatVS,
+    gsplatWorkBufferVS,
+    gsplatPackingPS
+};
+
