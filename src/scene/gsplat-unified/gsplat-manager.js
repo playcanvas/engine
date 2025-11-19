9a3490a806e915f89838f1227cc12684ac5aff3f
Martin Valigursky <mvaligursky@snapchat.com>
2025-11-19 13:53:49 +0000

    Fix unified gsplat rendering when viewport is resized

--- src/scene/gsplat-unified/gsplat-manager.js
+++ src/scene/gsplat-unified/gsplat-manager.js
@@ -522,6 +522,9 @@ class GSplatManager {
         // apply any pending sorted results
         this.sorter.applyPendingSorted();
 
+        // update viewport for renderer
+        this.renderer.updateViewport(this.cameraNode);
+
         let fullUpdate = false;
         this.framesTillFullUpdate--;
         if (this.framesTillFullUpdate <= 0) {
@@ -763,7 +766,6 @@ class GSplatManager {
         });
 
         this.sorter.setSortParams(sorterRequest, this.scene.gsplat.radialSorting);
-        this.renderer.updateViewport(cameraNode);
     }
 
     /**
