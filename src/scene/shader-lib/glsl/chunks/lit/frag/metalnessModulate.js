cf16240d26cd60368b8f9eaa0716c9d48fc342af
Will Eastcott <will@playcanvas.com>
2025-12-21 08:49:37 +0000

    [FIX] Fix KHR_materials_specular incorrectly affecting metals and diffuse (#8293)

--- src/scene/shader-lib/glsl/chunks/lit/frag/metalnessModulate.js
+++ src/scene/shader-lib/glsl/chunks/lit/frag/metalnessModulate.js
@@ -1,7 +1,9 @@
 export default /* glsl */`
 
-vec3 getSpecularModulate(in vec3 specularity, in vec3 albedo, in float metalness, in float f0) {
-    vec3 dielectricF0 = f0 * specularity;
+vec3 getSpecularModulate(in vec3 specularity, in vec3 albedo, in float metalness, in float f0, in float specularityFactor) {
+    // Apply specularityFactor to dielectric F0 only. For metals (metalness=1), F0 is the albedo
+    // and should not be affected by specularityFactor per the KHR_materials_specular glTF spec.
+    vec3 dielectricF0 = f0 * specularity * specularityFactor;
     return mix(dielectricF0, albedo, metalness);
 }
 
