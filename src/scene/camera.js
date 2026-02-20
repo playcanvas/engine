d3af96dfdd43c6fb4701c76e87c01674e8e123cb
Donovan Hutchence <slimbuck7@gmail.com>
2026-02-20 09:59:24 +0000

    latest

--- src/scene/camera.js
+++ src/scene/camera.js
@@ -268,10 +268,6 @@ class Camera {
             this._farClip = newValue;
             this._projMatDirty = true;
         }
-        if (this._xr?.active) {
-            this._xrProperties.farClip = newValue;
-            this._xr._setClipPlanes(this._xrProperties.nearClip, newValue);
-        }
     }
 
     get farClip() {
@@ -333,10 +329,6 @@ class Camera {
         if (this._nearClip !== newValue) {
             this._nearClip = newValue;
             this._projMatDirty = true;
-        }
-        if (this._xr?.active) {
-            this._xrProperties.nearClip = newValue;
-            this._xr._setClipPlanes(newValue, this._xrProperties.farClip);
         }
     }
 
@@ -772,10 +764,10 @@ class Camera {
      * @ignore
      */
     fillShaderParams(output) {
-        const f = this.farClip;
+        const f = this._farClip;
         output[0] = 1 / f;
         output[1] = f;
-        output[2] = this.nearClip;
+        output[2] = this._nearClip;
         output[3] = this._projection === PROJECTION_ORTHOGRAPHIC ? 1 : 0;
         return output;
     }
