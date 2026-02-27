198a060f25a04c9831ef215d16aea3fc82b02949
Donovan Hutchence <slimbuck7@gmail.com>
2026-02-27 11:19:29 +0000

    latest

--- src/scene/gsplat/gsplat-sorter.js
+++ src/scene/gsplat/gsplat-sorter.js
@@ -1,42 +1,59 @@
 import { EventHandler } from '../../core/event-handler.js';
-import { TEXTURELOCK_READ } from '../../platform/graphics/constants.js';
 import { platform } from '../../core/platform.js';
+import { UploadStream } from '../../platform/graphics/upload-stream.js';
 import { SortWorker } from './gsplat-sort-worker.js';
+
+/**
+ * @import { GraphicsDevice } from '../../platform/graphics/graphics-device.js'
+ * @import { StorageBuffer } from '../../platform/graphics/storage-buffer.js'
+ * @import { Texture } from '../../platform/graphics/texture.js'
+ */
 
 class GSplatSorter extends EventHandler {
     worker;
 
-    orderTexture;
+    /** @type {Texture|StorageBuffer} */
+    target;
+
+    /** @type {ArrayBuffer} */
+    orderData;
 
     centers;
 
     scene;
 
-    constructor(scene) {
+    /** @type {UploadStream} */
+    uploadStream;
+
+    /**
+     * @param {GraphicsDevice} device - The graphics device.
+     * @param {import('../scene.js').Scene} [scene] - The scene to fire sort timing events on.
+     */
+    constructor(device, scene) {
         super();
         this.scene = scene ?? null;
+        this.uploadStream = new UploadStream(device);
 
         const messageHandler = (message) => {
             const msgData = message.data ?? message;
 
-            // Fire sortTime event on scene
             if (this.scene && msgData.sortTime !== undefined) {
                 this.scene.fire('gsplat:sorted', msgData.sortTime);
             }
 
             const newOrder = msgData.order;
-            const oldOrder = this.orderTexture._levels[0].buffer;
+            const oldOrder = this.orderData;
 
-            // send vertex storage to worker to start the next frame
+            // send previous buffer to worker for reuse
             this.worker.postMessage({
                 order: oldOrder
             }, [oldOrder]);
 
-            // write the new order data to gpu texture memory
-            this.orderTexture._levels[0] = new Uint32Array(newOrder);
-            this.orderTexture.upload();
+            // upload new order data to GPU
+            const data = new Uint32Array(newOrder);
+            this.orderData = newOrder;
+            this.uploadStream.upload(data, this.target);
 
-            // set new data directly on texture
             this.fire('updated', msgData.count);
         };
 
@@ -58,22 +75,27 @@ class GSplatSorter extends EventHandler {
     destroy() {
         this.worker.terminate();
         this.worker = null;
+        this.uploadStream.destroy();
+        this.uploadStream = null;
     }
 
-    init(orderTexture, centers, chunks) {
-        this.orderTexture = orderTexture;
+    /**
+     * @param {Texture|StorageBuffer} target - The GPU target for order data uploads.
+     * @param {number} numSplats - The number of splats.
+     * @param {Float32Array} centers - The splat center positions.
+     * @param {Uint32Array} [chunks] - Optional chunk data.
+     */
+    init(target, numSplats, centers, chunks) {
+        this.target = target;
         this.centers = centers.slice();
 
-        // get the texture's storage buffer and make a copy
-        const orderBuffer = this.orderTexture.lock({
-            mode: TEXTURELOCK_READ
-        }).slice();
-        this.orderTexture.unlock();
-
-        // initialize order data
-        for (let i = 0; i < orderBuffer.length; ++i) {
+        const orderBuffer = new Uint32Array(numSplats);
+        for (let i = 0; i < numSplats; ++i) {
             orderBuffer[i] = i;
         }
+
+        // second buffer for double-buffering with the worker
+        this.orderData = new ArrayBuffer(numSplats * 4);
 
         const obj = {
             order: orderBuffer.buffer,
@@ -83,13 +105,11 @@ class GSplatSorter extends EventHandler {
 
         const transfer = [orderBuffer.buffer, centers.buffer].concat(chunks ? [chunks.buffer] : []);
 
-        // send the initial buffer to worker
         this.worker.postMessage(obj, transfer);
     }
 
     setMapping(mapping) {
         if (mapping) {
-            // create new centers array
             const centers = new Float32Array(mapping.length * 3);
             for (let i = 0; i < mapping.length; ++i) {
                 const src = mapping[i] * 3;
@@ -99,13 +119,11 @@ class GSplatSorter extends EventHandler {
                 centers[dst + 2] = this.centers[src + 2];
             }
 
-            // update worker with new centers and mapping for the subset of splats
             this.worker.postMessage({
                 centers: centers.buffer,
                 mapping: mapping.buffer
             }, [centers.buffer, mapping.buffer]);
         } else {
-            // restore original centers
             const centers = this.centers.slice();
             this.worker.postMessage({
                 centers: centers.buffer,
