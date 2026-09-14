dc0c80ccbb82c31432e5b9d6406b8bc08964c5ce
Martin Valigursky <mvaligursky@snapchat.com>
2026-09-14 13:34:23 +0100

    feat: Add rendering without entities example

---
+++ examples/src/examples/graphics-advanced/rendering-without-entities.controls.jsx
@@ -0,0 +1,47 @@
+import {
+    BindingTwoWay,
+    BooleanInput,
+    LabelGroup,
+    Panel,
+    SliderInput
+} from '@playcanvas/pcui/react';
+
+/**
+ * @import { Observer } from '@playcanvas/observer'
+ * @import { ReactElement } from 'react'
+ */
+
+/**
+ * @param {{ observer: Observer }} props - The control panel props.
+ * @returns {ReactElement} The control panel.
+ */
+export function Controls({ observer }) {
+    return (
+        <Panel headerText='Forest'>
+            <LabelGroup text='Show Trees'>
+                <BooleanInput
+                    type='toggle'
+                    binding={new BindingTwoWay()}
+                    link={{ observer, path: 'settings.showTrees' }}
+                />
+            </LabelGroup>
+            <LabelGroup text='Tree Count'>
+                <SliderInput
+                    binding={new BindingTwoWay()}
+                    link={{ observer, path: 'settings.treeCount' }}
+                    min={0}
+                    max={192}
+                    precision={0}
+                    step={1}
+                />
+            </LabelGroup>
+            <LabelGroup text='Orbit Camera'>
+                <BooleanInput
+                    type='toggle'
+                    binding={new BindingTwoWay()}
+                    link={{ observer, path: 'settings.orbit' }}
+                />
+            </LabelGroup>
+        </Panel>
+    );
+}
