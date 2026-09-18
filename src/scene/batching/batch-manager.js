802b28fff251277b8c78b79e773c04f434e51e96
Alexapp <alexsandrshamarin@yandex.ru>
2026-09-18 15:13:32 +0300

    Replace batch.origMeshInstances[0] with sourceMeshInstance variable

--- src/scene/batching/batch-manager.js
+++ src/scene/batching/batch-manager.js
@@ -858,22 +858,26 @@ class BatchManager {
             }
 
             // Create meshInstance
-            const meshInstance = new MeshInstance(mesh, material, this.rootNode);
-            meshInstance.castShadow = batch.origMeshInstances[0].castShadow;
+            const batchedMeshInstance = new MeshInstance(mesh, material, this.rootNode);
+            const sourceMeshInstance = batch.origMeshInstances[0];
+
+            batchedMeshInstance.castShadow = sourceMeshInstance.castShadow;
+            batchedMeshInstance.shadowCascadeMask = sourceMeshInstance.shadowCascadeMask;
+
             // copy the parameters through setParameter, which splits them between the scope and the
             // material uniform buffer for the material of the batch
-            for (const [name, parameter] of batch.origMeshInstances[0].parameters) {
-                meshInstance.setParameter(name, parameter.data);
+            for (const [name, parameter] of sourceMeshInstance.parameters) {
+                batchedMeshInstance.setParameter(name, parameter.data);
             }
-            meshInstance.layer = batch.origMeshInstances[0].layer;
-            meshInstance._shaderDefs = batch.origMeshInstances[0]._shaderDefs;
-            meshInstance.batching = true;
+            batchedMeshInstance.layer = sourceMeshInstance.layer;
+            batchedMeshInstance._shaderDefs = sourceMeshInstance._shaderDefs;
+            batchedMeshInstance.batching = true;
 
             // meshInstance culling - don't cull UI elements, as they use custom culling Component.isVisibleForCamera
-            meshInstance.cull = batch.origMeshInstances[0].cull;
+            batchedMeshInstance.cull = sourceMeshInstance.cull;
             const batchGroup = this._batchGroups[batchGroupId];
             if (batchGroup && batchGroup._ui) {
-                meshInstance.cull = false;
+                batchedMeshInstance.cull = false;
             }
 
             if (dynamic) {
@@ -882,23 +886,22 @@ class BatchManager {
                 for (let i = 0; i < batch.origMeshInstances.length; i++) {
                     nodes.push(batch.origMeshInstances[i].node);
                 }
-                meshInstance.skinInstance = new SkinBatchInstance(this.device, nodes, this.rootNode);
+                batchedMeshInstance.skinInstance = new SkinBatchInstance(this.device, nodes, this.rootNode);
             }
 
             // disable aabb update, gets updated manually by batcher
-            meshInstance._updateAabb = false;
+            batchedMeshInstance._updateAabb = false;
 
-            meshInstance.drawOrder = batch.origMeshInstances[0].drawOrder;
-            meshInstance.stencilFront = batch.origMeshInstances[0].stencilFront;
-            meshInstance.stencilBack = batch.origMeshInstances[0].stencilBack;
-            meshInstance.flipFacesFactor = getScaleSign(batch.origMeshInstances[0]);
-            meshInstance.shadowCascadeMask = batch.origMeshInstances[0].shadowCascadeMask;
+            batchedMeshInstance.drawOrder = sourceMeshInstance.drawOrder;
+            batchedMeshInstance.stencilFront = sourceMeshInstance.stencilFront;
+            batchedMeshInstance.stencilBack = sourceMeshInstance.stencilBack;
+            batchedMeshInstance.flipFacesFactor = getScaleSign(sourceMeshInstance);
 
             // assigning this prepares the batched mesh for the render style, generating its
             // wireframe indices or points primitive as needed
-            meshInstance.renderStyle = batch.origMeshInstances[0].renderStyle;
+            batchedMeshInstance.renderStyle = sourceMeshInstance.renderStyle;
 
-            batch.meshInstance = meshInstance;
+            batch.meshInstance = batchedMeshInstance;
             batch.updateBoundingBox();
         }
 
